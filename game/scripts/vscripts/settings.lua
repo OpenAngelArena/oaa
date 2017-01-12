761da48e8293d265892bdf8f7ed7c611f4d13032
SquawkyArctangent <arctangentpuckett@gmail.com>
2017-01-11 23:24:00 -0600

    Increased MAX_LEVEL to 5000
    Also changed experience table to match DOTA's, with a placeholder formula for levels past 25.

--- game/scripts/vscripts/settings.lua
+++ game/scripts/vscripts/settings.lua
@@ -43,13 +43,39 @@ END_GAME_ON_KILLS = true                -- Should the game end after a certain n
 KILLS_TO_END_GAME_FOR_TEAM = 50         -- How many kills for a team should signify an end of game?
 
 USE_CUSTOM_HERO_LEVELS = true           -- Should we allow heroes to have custom levels?
-MAX_LEVEL = 50                          -- What level should we let heroes get to?
+MAX_LEVEL = 5000                        -- What level should we let heroes get to?
 USE_CUSTOM_XP_VALUES = true             -- Should we use custom XP values to level up heroes, or the default Dota numbers?
 
 -- Fill this table up with the required XP per level if you want to change it
-XP_PER_LEVEL_TABLE = {}
-for i=1,MAX_LEVEL do
-  XP_PER_LEVEL_TABLE[i] = (i-1) * 100
+XP_PER_LEVEL_TABLE = {
+	0,
+	200,
+	500,
+	900,
+	1400,
+	2000,
+	2640,
+	3300,
+	3980,
+	4680,
+	5400,
+	6140,
+	7340,
+	8565,
+	9815,
+	11090,
+	12390,
+	13715,
+	15115,
+	16605,
+	18205,
+	20105,
+	22305,
+	24805,
+	27500,
+}
+for i = #XP_PER_LEVEL_TABLE + 1, MAX_LEVEL do
+	XP_PER_LEVEL_TABLE[i] = XP_PER_LEVEL_TABLE[i - 1] + ( i * 108 )
 end
 
 ENABLE_FIRST_BLOOD = true               -- Should we enable first blood for the first kill in this game?
