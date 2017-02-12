861e05607833bdd58963e58c0663d9729514fe8a
Chronophylos <admiral.chronophylos@web.de>
2017-02-12 16:06:30 +0100

    Added win condition checking

--- game/scripts/vscripts/components/points/points.lua
+++ game/scripts/vscripts/components/points/points.lua
@@ -1,12 +1,14 @@
 
 -- Taken from bb template
 if PointsManager == nil then
-    DebugPrint ( '[points/points] creating new PointsManager object' )
-    PointsManager = class({})
+  DebugPrint ( '[points/PointsManager] creating new PointsManager object' )
+  PointsManager = class({})
 end
 
 function PointsManager:Init ()
-  DebugPrint ( '[points/points] Initialize' )
+  DebugPrint ( '[points/PointsManager] Initialize' )
+  PointsManager.hasWon = false
+
   if GameRules.GameLength == "long" then
     CustomNetTables:SetTableValue( "team_scores", "limit", { value = 200 } )
   elseif GameRules.GameLength == "short" then
@@ -18,4 +20,42 @@ function PointsManager:Init ()
 
   -- set initial values for current scores
   CustomNetTables:SetTableValue( "team_scores", "score", { radiant = 0, dire = 0 } )
+
+  Timers:CreateTimer(0, Dynamic_Wrap(PointsManager, "Think"))
+end
+
+
+function PointsManager:Debug ()
+  local scores = CustomNetTables:GetTableValue("team_scores", "score")
+  CustomNetTables:SetTableValue("team_scores", "score", { radiant = scores.radiant + 1,
+                                                          dire = scores.dire })
+end
+
+function PointsManager:Think ()
+  DebugPrint("[points/PointsManager] Thinking..")
+  PointsManager:Debug()
+  local interval = 5 -- when do we want to check for win conditions
+  if hasWon then return end
+  local limit = CustomNetTables:GetTableValue("team_scores", "limit").value
+  local scores = CustomNetTables:GetTableValue("team_scores", "score")
+  local radiant = scores.radiant
+  local dire = scores.dire
+
+  if radiant >= limit then
+    PointsManager:OnRadiantWin()
+  elseif dire >= limit then
+    PointsManager:OnDireWin()
+  end
+
+  return interval
+end
+
+function PointsManager:OnRadiantWin ()
+  GameRules.SetMode(DOTA_GAMERULES_STATE_POST_GAME)
+  DebugPrint("[points/PointsManager] Radiant win!")
+end
+
+function PointsManager:OnDireWin ()
+  GameRules.SetMode(DOTA_GAMERULES_STATE_POST_GAME)
+  DebugPrint("[points/PointsManager] Dire win!")
 end
