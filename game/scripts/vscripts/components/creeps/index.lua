dc6b1d04fa0bd632b927e0f2c6594f2c01f314fa
Chris Vickery <chrisinajar@gmail.com>
2017-02-13 20:48:10 -0800

    break out creep data files, normalize spawns, set interval, add base xp

--- game/scripts/vscripts/components/creeps/index.lua
+++ game/scripts/vscripts/components/creeps/index.lua
@@ -1,2 +1,7 @@
+-- creep data first
+require('components/creeps/creep_types')
+require('components/creeps/creep_power')
+-- then files
 require('components/creeps/spawner')
+
 -- todo: ALL OF THE OTHER THINGS
