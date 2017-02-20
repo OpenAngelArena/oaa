3234ddf3889f30b8f50e87d315a4d5c917d8b6ac
Chronophylos <admiral.chronophylos@web.de>
2017-02-20 19:07:29 +0100

    Made Points be displayed
    Using Vanilla Display
    Note: I can't figure out how to fit points and limit into the Display

--- content/panorama/scripts/custom_game/pointsmanager.js
+++ content/panorama/scripts/custom_game/pointsmanager.js
@@ -5,21 +5,40 @@ var console = {
 };
 
 (function () {
-  PlayerTables.SubscribeNetTableListener('team_scores', onScoreChange);
+  CustomNetTables.SubscribeNetTableListener('team_scores', onScoreChange);
   GameEvents.Subscribe('points_won', onTeamWin);
 }());
 
-function onScoreChange(table, data) {
-  var limit = data.limit.value;
-  var goodguys = data.score.goodguys;
-  var badguys = data.score.badguys;
+function onScoreChange(table, key, data) {
+  console.log('[PointsManager] onScoreChange');
+
+  if (key === 'score') {
+    var limit = CustomNetTables.GetTableValue('team_scores', 'limit')['value'];
+    var goodguys = data['goodguys'];
+    var badguys = data['badguys'];
+  } else if (key === 'limit') {
+    var limit = data['value'];
+    var score = CustomNetTables.GetTableValue('team_scores', 'score');
+    var goodguys = score['goodguys'];
+    var badguys = score['badguys'];
+  }
+
+  console.log('limit: ' + limit);
+  console.log('goodguys: ' + goodguys);
+  console.log('badguys: ' + badguys);
 
   UpdatePointsHud(limit, goodguys, badguys);
 }
 
 function UpdatePointsHud(limit, goodguys, badguys) {
-  console.log(GetDotaHud());
-  var goodguysLabel = GetDotaHudElement('GoodGuysLabel')
-  var badguysLabel = GetDotaHudElement('BadGuysLabel')
+  var goodguysLabel = FindDotaHudElement('TopBarRadiantScore');
+  var badguysLabel = FindDotaHudElement('TopBarDireScore');
+
+  // BUG: limit doesnt get displayed correctly
+  goodguysLabel.text = goodguys + '\n' + limit;
+  badguysLabel.text = badguys + '\n' + limit;
+}
+
+function onTeamWin(data) {
 
 }
