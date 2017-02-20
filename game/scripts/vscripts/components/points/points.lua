0daabcc384d2841df276d54c914cea388824d6aa
Chronophylos <admiral.chronophylos@web.de>
2017-02-20 16:23:08 +0100

    Add more interactions for PointsManager
    Added: SetScore(side)
    Added: IncrementScore(side)

--- game/scripts/vscripts/components/points/points.lua
+++ game/scripts/vscripts/components/points/points.lua
@@ -1,14 +1,17 @@
 
 -- Taken from bb template
 if PointsManager == nil then
-  DebugPrint ( '[points/PointsManager] creating new PointsManager object' )
+  DebugPrint ( '[points/PointsManager] Creating new PointsManager object.' )
   PointsManager = class({})
 end
 
 function PointsManager:Init ()
-  DebugPrint ( '[points/PointsManager] Initialize' )
-  PointsManager.hasDireWon = false
-  PointsManager.hasRadiantWon = false
+  DebugPrint ( '[points/PointsManager] Initialize.' )
+
+  PointsManager.haveBadguysWon = false
+  PointsManager.haveGoodguysWon = false
+  PointsManager.goodguysName = "Radiant"
+  PointsManager.badguysName = "Dire"
 
   if GameRules.GameLength == "long" then
     CustomNetTables:SetTableValue( "team_scores", "limit", { value = 200 } )
@@ -20,39 +23,32 @@ function PointsManager:Init ()
   end
 
   -- set initial values for current scores
-  CustomNetTables:SetTableValue( "team_scores", "score", { radiant = 0, dire = 0 } )
+  CustomNetTables:SetTableValue( "team_scores", "score", { goodguys = 0, badguys = 0 } )
 
+  -- Start Thinking
   Timers:CreateTimer(0, Dynamic_Wrap(PointsManager, "Think"))
-end
-
-
-function PointsManager:Debug ()
-  local scores = CustomNetTables:GetTableValue("team_scores", "score")
-  CustomNetTables:SetTableValue("team_scores", "score", { radiant = scores.radiant + 1,
-                                                          dire = scores.dire })
 end
 
 function PointsManager:Think ()
   DebugPrint("[points/PointsManager] Thinking..")
-  PointsManager:Debug()
 
   local interval = 5 -- when do we want to check for win conditions
   local limit = CustomNetTables:GetTableValue("team_scores", "limit").value
   local scores = CustomNetTables:GetTableValue("team_scores", "score")
-  local radiant = scores.radiant
-  local dire = scores.dire
+  local goodguys = scores.goodguys
+  local badguys = scores.badguys
 
   DebugPrintTable(limit)
   DebugPrintTable(scores)
-  DebugPrint("hasRadiantWon" .. hasRadiantWon)
-  DebugPrint("hasDireWon" .. hasDireWon)
+  DebugPrint("haveGoodguysWon" .. haveGoodguysWon)
+  DebugPrint("haveBadguysWon" .. haveBadguysWon)
 
-  if hasRadiantWon or hasDireWon then return end
+  if haveGoodguysWon or haveBadguysWon then return end
 
-  if radiant >= limit then
-    PointsManager:OnWin("Radiant")
-  elseif dire >= limit then
-    PointsManager:OnWin("Radiant")
+  if goodguys >= limit then
+    PointsManager:OnWin(goodguysName)
+  elseif badguys >= limit then
+    PointsManager:OnWin(badguysName)
   end
 
   return interval
@@ -60,13 +56,38 @@ end
 
 function PointsManager:OnWin (side)
   --GameRules.SetMode(DOTA_GAMERULES_STATE_POST_GAME)
-  DebugPrint("[points/PointsManager] " .. side .. " win!")
-  hasDireWon = true
+  DebugPrint("[points/PointsManager] " .. side .. " wins!")
+
+  haveBadguysWon = true
   CustomGameEventManager:Send_ServerToAllClients("points_won", {
     who=side
   })
 end
 
 function PointsManager:SetScore (side, newScore)
+  DebugPrint("[points/PointsManager] Set Score of " .. side .. " to " .. newScore .. ".")
 
+  local score = CustomNetTables:GetTableValue("team_scores", "score")
+
+  if side == goodguysName then
+    score.goodguys = newScore
+  elseif side == badguysName then
+    score.badguys = newScore
+  end
+
+  CustomNetTables:SetTableValue("team_scores", "score", score)
+end
+
+function PointsManager:IncreaseScore (side)
+  DebugPrint("[points/PointsManager] Increase Score of " .. side .. " by one.")
+
+  local score = CustomNetTables:GetTableValue("team_scores", "score")
+
+  if side == goodguysName then
+    score.goodguys = score.goodguys + 1
+  elseif side == badguysName then
+    score.badguys = score.badguys + 1
+  end
+
+  CustomNetTables:SetTableValue("team_scores", "score", score)
 end
