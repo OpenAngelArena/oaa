b0a3818841d2b4b2437467f5d185cab9d2ecf929
Chronophylos <admiral.chronophylos@web.de>
2017-02-21 13:24:34 +0100

    Following @chrisinajar suggestions
    Renamed: IncrementPoints -> `AddPoints`
    Added: Optional Parameter amount for `AddPoints(side, amount)`
    Renamed: Debug Indentifier "points/PointsManager" -> "points/points"
    Renamed: `onTeamWin` -> `handleVictory()`
    Muted Debug Output

--- game/scripts/vscripts/components/points/points.lua
+++ game/scripts/vscripts/components/points/points.lua
@@ -1,84 +1,82 @@
 -- Taken from bb template
 if PointsManager == nil then
-  DebugPrint ( '[points/PointsManager] Creating new PointsManager object.' )
+  DebugPrint ( '[points/points] Creating new PointsManager object.' )
   PointsManager = class({})
 end
 
 function PointsManager:Init ()
-  DebugPrint ( '[points/PointsManager] Initialize.' )
+  DebugPrint ( '[points/points] Initializing.' )
 
   PointsManager.haveBadguysWon = false
   PointsManager.haveGoodguysWon = false
-  PointsManager.goodguysName = "Radiant"
-  PointsManager.badguysName = "Dire"
+  PointsManager.goodguysName = 'Radiant'
+  PointsManager.badguysName = 'Dire'
   PointsManager.goodguysID = 2
   PointsManager.badguysID = 3
 
-  if GameRules.GameLength == "long" then
-    CustomNetTables:SetTableValue( "team_scores", "limit", { value = 200 } )
-  elseif GameRules.GameLength == "short" then
-    CustomNetTables:SetTableValue( "team_scores", "limit", { value = 50 } )
+  if GameRules.GameLength == 'long' then
+    CustomNetTables:SetTableValue( 'team_scores', 'limit', { value = 200 } )
+  elseif GameRules.GameLength == 'short' then
+    CustomNetTables:SetTableValue( 'team_scores', 'limit', { value = 50 } )
   else
     -- default to 100 in case of no selection / invalid selection
-    CustomNetTables:SetTableValue( "team_scores", "limit", { value = 100 } )
+    CustomNetTables:SetTableValue( 'team_scores', 'limit', { value = 100 } )
   end
 
   -- set initial values for current scores
-  CustomNetTables:SetTableValue( "team_scores", "score", { goodguys = 0,
+  CustomNetTables:SetTableValue( 'team_scores', 'score', { goodguys = 0,
                                                            badguys = 0
                                                          })
 
   -- Start Thinking
-  Timers:CreateTimer(0, Dynamic_Wrap(PointsManager, "Think"))
+  Timers:CreateTimer( 0, Dynamic_Wrap( PointsManager, 'Think' ) )
 end
 
-function PointsManager:Think ()
-  DebugPrint("[points/PointsManager] Thinking..")
+function PointsManager:Think ( )
+  --DebugPrint('[points/points] Thinking..')
 
   local interval = 5 -- when do we want to check for win conditions
-  local limit = CustomNetTables:GetTableValue("team_scores", "limit").value
-  local scores = CustomNetTables:GetTableValue("team_scores", "score")
+  local limit = CustomNetTables:GetTableValue('team_scores', 'limit').value
+  local scores = CustomNetTables:GetTableValue('team_scores', 'score')
   local goodguys = scores.goodguys
   local badguys = scores.badguys
 
-  DebugPrintTable(limit)
-  DebugPrintTable(scores)
-  DebugPrint("haveGoodguysWon: " .. tostring(PointsManager.haveGoodguysWon))
-  DebugPrint("haveBadguysWon: " .. tostring(PointsManager.haveBadguysWon))
+  DebugPrintTable( limit )
+  DebugPrintTable( scores )
+  DebugPrint( 'haveGoodguysWon: ' .. tostring( PointsManager.haveGoodguysWon ) )
+  DebugPrint( 'haveBadguysWon: ' .. tostring( PointsManager.haveBadguysWon ) )
 
-  PointsManager:SetPoints(PointsManager.goodguysName, 200)
+  PointsManager:SetPoints( PointsManager.goodguysName, 200 )
 
   if PointsManager.haveGoodguysWon or PointsManager.haveBadguysWon then
     return
   end
 
   if goodguys >= limit then
-    PointsManager:onTeamWin(PointsManager.goodguysName)
+    PointsManager:handleVictory( PointsManager.goodguysName )
   elseif badguys >= limit then
-    PointsManager:onTeamWin(PointsManager.badguysName)
+    PointsManager:handleVictory( PointsManager.badguysName )
   end
 
   return interval
 end
 
-function PointsManager:onTeamWin (side)
-  -- NOTE Maybe set new GameState
-  --GameRules.SetMode(DOTA_GAMERULES_STATE_POST_GAME)
-  DebugPrint("[points/PointsManager] " .. side .. " wins!")
+function PointsManager:handleVictory ( side )
+  DebugPrint( '[points/points] ' .. side .. ' wins!' )
 
   if side == PointsManager.goodguysName then
     PointsManager.haveGoodguysWon = true
-    GameRules:SetGameWinner(PointsManager.goodguysID)
+    GameRules:SetGameWinner( PointsManager.goodguysID )
   elseif side == PointsManager.badguysName then
     PointsManager.haveBadguysWon = true
-    GameRules:SetGameWinner(PointsManager.badguysID)
+    GameRules:SetGameWinner( PointsManager.badguysID )
   end
 end
 
-function PointsManager:SetPoints (side, newPoints)
-  --DebugPrint("[points/PointsManager] Set Score of " .. side .. " to " .. newScore .. ".")
+function PointsManager:SetPoints ( side, newPoints )
+  --DebugPrint('[points/points] Set Score of ' .. side .. ' to ' .. newScore .. '.')
 
-  local score = CustomNetTables:GetTableValue("team_scores", "score")
+  local score = CustomNetTables:GetTableValue( 'team_scores', 'score' )
 
   if side == PointsManager.goodguysName then
     score.goodguys = newPoints
@@ -86,19 +84,23 @@ function PointsManager:SetPoints (side, newPoints)
     score.badguys = newPoints
   end
 
-  CustomNetTables:SetTableValue("team_scores", "score", score)
+  CustomNetTables:SetTableValue( 'team_scores', 'score', score )
 end
 
-function PointsManager:IncrementPoints (side)
-  --DebugPrint("[points/PointsManager] Increase Score of " .. side .. " by one.")
-
-  local score = CustomNetTables:GetTableValue("team_scores", "score")
-
-  if side == PointsManager.goodguysName then
-    score.goodguys = score.goodguys + 1
-  elseif side == PointsManager.badguysName then
-    score.badguys = score.badguys + 1
+function PointsManager:AddPoints ( side, amount )
+  if amount == nil then
+    amount = 1
   end
 
-  CustomNetTables:SetTableValue("team_scores", "score", score)
+  --DebugPrint( '[points/points] Increase Score of ' .. side .. ' by ' .. amount .. '.' )
+
+  local score = CustomNetTables:GetTableValue( 'team_scores', 'score' )
+
+  if side == PointsManager.goodguysName then
+    score.goodguys = score.goodguys + amount
+  elseif side == PointsManager.badguysName then
+    score.badguys = score.badguys + amount
+  end
+
+  CustomNetTables:SetTableValue( 'team_scores', 'score', score )
 end
