ab9dbc7b5314ace8990e108e1a0479e7d9019058
Nikolai <Chronophylos@users.noreply.github.com>
2017-02-22 18:22:48 +0100

    Feature: Kill Limit #1 (#78)
    * Added win condition checking
    
    * Added Event when someone one and prepared to display Points
    
    * Added Debugging Information
    
    * Reduced code
    
    Removed: UpdatePoints
    Removed: OnRadiantWin and OnDireWin
    Added: OnWin(side): like OnRadiantWin or OnDireWin but shorter
    
    * Add more interactions for PointsManager
    
    Added: SetScore(side)
    Added: IncrementScore(side)
    
    * Fixed Bugs and added first iter of PointsManager.js
    
    * Fixed another concatenating bug
    
    nil values are no strings
    
    * Fixed Bug
    
    Forgot `PointsManager.` on goodguysName and badguysName.
    I don't like Lua classes...
    
    * Added Include for pointsmanager.js
    
    * More Bug Fixes
    
    * Made Points be displayed
    
    Using Vanilla Display
    Note: I can't figure out how to fit points and limit into the Display
    
    * Fixed Limit and Point Display
    
    * Fixed onTeamWin calling multiple times
    
    * Cleanup
    
    * Renamed Score -> Points
    
    * Renamed: Score -> Points
    
    * Addded: Add Points on Player Kill
    
    * Removed event
    
    * Removed comment
    
    * Fixed to use SetGameWinner() instead of whatever I did
    
    Fixed Typo
    Removed unnecessary code
    Also thanks to Keril Artemov for telling me about this function
    
    * Add Mini Doc for PointsManager
    
    * Following @chrisinajar suggestions
    
    Renamed: IncrementPoints -> `AddPoints`
    Added: Optional Parameter amount for `AddPoints(side, amount)`
    Renamed: Debug Indentifier "points/PointsManager" -> "points/points"
    Renamed: `onTeamWin` -> `handleVictory()`
    Muted Debug Output
    
    * Commented Debug Functions
    
    * Fixed Bug from when I did't know how teamnumbers work

--- game/scripts/vscripts/components/points/points.lua
+++ game/scripts/vscripts/components/points/points.lua
@@ -1,21 +1,106 @@
-
 -- Taken from bb template
 if PointsManager == nil then
-    DebugPrint ( '[points/points] creating new PointsManager object' )
-    PointsManager = class({})
+  DebugPrint ( '[points/points] Creating new PointsManager object.' )
+  PointsManager = class({})
 end
 
 function PointsManager:Init ()
-  DebugPrint ( '[points/points] Initialize' )
-  if GameRules.GameLength == "long" then
-    CustomNetTables:SetTableValue( "team_scores", "limit", { value = 200 } )
-  elseif GameRules.GameLength == "short" then
-    CustomNetTables:SetTableValue( "team_scores", "limit", { value = 50 } )
+  DebugPrint ( '[points/points] Initializing.' )
+
+  PointsManager.haveBadguysWon = false
+  PointsManager.haveGoodguysWon = false
+  PointsManager.goodguysName = 'Radiant'
+  PointsManager.badguysName = 'Dire'
+  PointsManager.goodguysID = 2
+  PointsManager.badguysID = 3
+
+  if GameRules.GameLength == 'long' then
+    CustomNetTables:SetTableValue( 'team_scores', 'limit', { value = 200 } )
+  elseif GameRules.GameLength == 'short' then
+    CustomNetTables:SetTableValue( 'team_scores', 'limit', { value = 50 } )
   else
     -- default to 100 in case of no selection / invalid selection
-    CustomNetTables:SetTableValue( "team_scores", "limit", { value = 100 } )
+    CustomNetTables:SetTableValue( 'team_scores', 'limit', { value = 100 } )
   end
 
   -- set initial values for current scores
-  CustomNetTables:SetTableValue( "team_scores", "score", { radiant = 0, dire = 0 } )
+  CustomNetTables:SetTableValue( 'team_scores', 'score', { goodguys = 0,
+                                                           badguys = 0
+                                                         })
+
+  -- Start Thinking
+  Timers:CreateTimer( 0, Dynamic_Wrap( PointsManager, 'Think' ) )
+end
+
+function PointsManager:Think ( )
+  --DebugPrint('[points/points] Thinking..')
+
+  local interval = 5 -- when do we want to check for win conditions
+  local limit = CustomNetTables:GetTableValue('team_scores', 'limit').value
+  local scores = CustomNetTables:GetTableValue('team_scores', 'score')
+  local goodguys = scores.goodguys
+  local badguys = scores.badguys
+
+  --DebugPrintTable( limit )
+  --DebugPrintTable( scores )
+  --DebugPrint( 'haveGoodguysWon: ' .. tostring( PointsManager.haveGoodguysWon ) )
+  --DebugPrint( 'haveBadguysWon: ' .. tostring( PointsManager.haveBadguysWon ) )
+
+  PointsManager:SetPoints( PointsManager.goodguysName, 200 )
+
+  if PointsManager.haveGoodguysWon or PointsManager.haveBadguysWon then
+    return
+  end
+
+  if goodguys >= limit then
+    PointsManager:handleVictory( PointsManager.goodguysName )
+  elseif badguys >= limit then
+    PointsManager:handleVictory( PointsManager.badguysName )
+  end
+
+  return interval
+end
+
+function PointsManager:handleVictory ( side )
+  DebugPrint( '[points/points] ' .. side .. ' wins!' )
+
+  if side == PointsManager.goodguysName then
+    PointsManager.haveGoodguysWon = true
+    GameRules:SetGameWinner( PointsManager.goodguysID )
+  elseif side == PointsManager.badguysName then
+    PointsManager.haveBadguysWon = true
+    GameRules:SetGameWinner( PointsManager.badguysID )
+  end
+end
+
+function PointsManager:SetPoints ( side, newPoints )
+  --DebugPrint('[points/points] Set Score of ' .. side .. ' to ' .. newScore .. '.')
+
+  local score = CustomNetTables:GetTableValue( 'team_scores', 'score' )
+
+  if side == PointsManager.goodguysName then
+    score.goodguys = newPoints
+  elseif side == PointsManager.badguysName then
+    score.badguys = newPoints
+  end
+
+  CustomNetTables:SetTableValue( 'team_scores', 'score', score )
+end
+
+function PointsManager:AddPoints ( side, amount )
+  if amount == nil then
+    amount = 1
+  end
+
+  --DebugPrint( '[points/points] Increase Score of ' .. side .. ' by ' .. amount .. '.' )
+
+  local score = CustomNetTables:GetTableValue( 'team_scores', 'score' )
+
+  if side == PointsManager.goodguysName then
+    score.goodguys = score.goodguys + amount
+  elseif side == PointsManager.badguysName then
+    score.badguys = score.badguys + amount
+  end
+
+  CustomNetTables:SetTableValue( 'team_scores', 'score', score )
 end
