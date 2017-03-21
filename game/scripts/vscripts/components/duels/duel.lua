6572e8f180f523f5b60ef7b7f63c27d41c9174ce
SphereKatzen <decdexter@googlemail.com>
2017-03-21 07:55:42 +0000

    Refactored duel mechanics substantially
    I've refactored duels quite a lot, duels.lua now manages team selection and countdowns and then instantiates new duel objects from duel.lua when duels are ready. These duel objects then handle player states and movement of players into and out of the arena zones. As there have been a number of bugs with duels so far hopefully this would make it easier to maintain in future

---
+++ game/scripts/vscripts/components/duels/duel.lua
@@ -0,0 +1,156 @@
+Duel = class({})
+
+function Duel:Init (teams, spawns, zone)
+  self.zone = zone
+  self.states = {}
+  DebugPrint('Starting a duel')
+  self.teams = teams
+  self.spawns = spawns
+  self.scores = {}
+
+  for index, team in pairs(teams) do
+    self.scores[index] = {#team}
+	for _,player in pairs(team) do
+	  player.team = index
+	end
+  end
+
+  GameEvents:OnHeroKilled(function (keys)
+    Duel:CheckDuelStatus(keys)
+  end)
+  Duel:Start()
+
+  self.over = false
+end
+
+function Duel:Start()
+  for index, team in pairs(self.teams) do
+    local teamSpawn = self.spawns[index]
+	for _, player in pairs(team) do
+	  Duel:SpawnPlayer(player, teamSpawn)
+	end
+  end
+end
+
+function Duel.Map(func)
+  for _, team in pairs(self.teams) do
+    for _, player in pairs(team) do
+	  func(player)
+	end
+  end
+end
+
+function Duel:SpawnPlayer(player, spawn)
+  local hero = player:GetAssignedHero()
+
+  self.states[player.id] = Duel:GetPlayerState(hero)
+  Duel:ResetPlayerState(hero)
+
+  self.zone.addPlayer(player.id)
+  FindClearSpaceForUnit(hero, spawn, true)
+  self.MoveCameraToPlayer(player.id, hero)
+
+  hero:SetRespawnsDisabled(true)
+end
+
+function Duel:ResetPlayerState (player)
+  local hero = player:GetAssignedHero()
+
+  if not hero:IsAlive() then
+    hero:RespawnHero(false,false,false)
+  end
+
+  hero:SetHealth(hero:GetMaxHealth())
+  hero:SetMana(hero:GetMaxMana())
+
+  for abilityIndex = 0,hero:GetAbilityCount() do
+    local ability = hero:GetAbilityByIndex(abilityIndex)
+    if ability ~= nil then
+      ability:EndCooldown()
+    end
+  end
+end
+
+function Duel:GetPlayerState(hero)
+  local state = {
+    location = hero:GetAbsOrigin(),
+    abilityCount = hero:GetAbilityCount(),
+    maxAbility = 0,
+    abilities = {},
+    hp = hero:GetHealth(),
+    mana = hero:GetMana()
+  }
+
+  for abilityIndex = 0,state.abilityCount-1 do
+    local ability = hero:GetAbilityByIndex(abilityIndex)
+    if ability ~= nil then
+      state.maxAbility = abilityIndex
+      state.abilities[abilityIndex] = {
+        cooldown = ability:GetCooldownTimeRemaining()
+      }
+    end
+  end
+  return state
+end
+
+function Duel:RestorePlayerState(player)
+  local hero = player:GetAssignedHero()
+  local s = self.states[player.id]
+
+  hero.SetAbsOrigin(s.location)
+  Duel:MoveCameraToPlayer(player.id, hero)
+
+  if s.hp > 0 then
+    hero.SetHealth(s.hp)
+  end
+  hero:setMana(s.mana)
+
+  for abilityIndex = 0,state.maxAbility-1 do
+	local ability = hero:GetAbilityByIndex(abilityIndex)
+	if ability ~= nil then
+	  ability:StartCooldown(state.abilities[abilityIndex].cooldown)
+	end
+  end
+  self.zone.removePlayer(player.id)
+end
+
+function Duel:MoveCameraToPlayer (playerId, entity)
+  PlayerResource:SetCameraTarget(playerId, entity)
+
+  Timers:CreateTimer(1, function ()
+    PlayerResource:SetCameraTarget(playerId, nil)
+  end)
+end
+
+function Duel:CheckDuelStatus (keys)
+  if keys.killed:IsReincarnating() then
+    return
+  end
+
+  local playerId = keys.killed:GetPlayerOwnerID()
+  local foundIt = false
+
+  Duel:Map(function (player)
+    if foundIt or player.id ~= playerId then
+      return
+    end
+    foundIt = true
+    local scoreIndex = player.team
+    DebugPrint('Found dead player on team ' .. player.team)
+
+    self.scores[scoreIndex] = Duel.scores[scoreIndex] - 1
+
+    if self.scores[scoreIndex] <= 0 then
+      Duel:EndDuel()
+    end
+  end)
+end
+
+function Duel:EndDuel()
+  Duel:Map(Duel:RestorePlayerState)
+  self.over = true
+  --Need to unsubscribe from event manager!
+end
+
+
+
