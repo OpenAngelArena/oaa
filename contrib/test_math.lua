98a9b45eb5f76739d5ddc42afb46fcd95e025ba9
Chronophylos <admiral.chronophylos@web.de>
2017-03-23 19:32:19 +0100

    Added math.lua Test Cases

---
+++ contrib/test_math.lua
@@ -0,0 +1,55 @@
+luaunit = require('contrib/luaunit')
+require('game/scripts/vscripts/libraries/math')
+
+-- Test math.doLinesIntersect(a, b, c, d)
+TestMath = {}
+
+function TestMath:setUp()
+  print("Testing doLinesIntersect")
+  self.P1 = { x = 0, y = 0 }
+  self.P2 = { x = 1, y = 1 }
+  self.P3 = { x = 0, y = 1 }
+  self.P4 = { x = 1, y = 0 }
+  self.P5 = { x = -1, y = 1 }
+  self.P6 = { x = 1, y = -1 }
+  self.P7 = { x = 0, y = -1 }
+  self.P8 = { x = -1, y = 0 }
+  self.P9 = { x = -1, y = -1 }
+end
+
+function TestMath:test1()
+  print("with a simple intersection at (0,0)")
+  local isIntersecting, intersection = math.doLinesIntersect(self.P5, self.P6, self.P9, self.P2)
+  luaunit.assertTrue(isIntersecting)
+  luaunit.assertEquals(intersection, self.P1)
+end
+
+function TestMath:test2()
+  print("with parallel not touching lines")
+  local isIntersecting, intersection = math.doLinesIntersect(self.P5, self.P2, self.P9, self.P6)
+  luaunit.assertFalse(isIntersecting)
+end
+
+function TestMath:test3()
+  print("with weird not touching lines")
+  local isIntersecting, intersection = math.doLinesIntersect(self.P9, self.P1, self.P7, self.P6)
+  luaunit.assertFalse(isIntersecting)
+end
+
+function TestMath:test4()
+  print("with lines touching at the end")
+  local isIntersecting, intersection = math.doLinesIntersect(self.P5, self.P3, self.P2, self.P3)
+  luaunit.assertTrue(isIntersecting)
+  luaunit.assertEquals(intersection, self.P1)
+end
+
+function TestMath:test5()
+  print("with the same lines")
+  local isIntersecting, intersection = math.doLinesIntersect(self.P1, self.P2, self.P1, self.P2)
+  luaunit.assertTrue(isIntersecting)
+  luaunit.assertEquals(intersection, self.P1)
+end
+
+local runner = luaunit.LuaUnit.new()
+runner:setOutputType("tap")
+os.exit( runner:runSuite() )
