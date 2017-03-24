7884e1fd6c69cf8a267adf8110edcafc9a067ebd
Evan W <evan.white36@gmail.com>
2017-03-24 19:15:38 -0400

    stoneskin delay

--- game/scripts/vscripts/items/item_stoneskin.lua
+++ game/scripts/vscripts/items/item_stoneskin.lua
@@ -2,11 +2,13 @@ require( "libraries/Timers" )	--needed for the timers.
 
 function modifier_stoneskin_toggle(keys)
   if not keys.caster:HasModifier("modifier_item_stoneskin_stone_armor") and keys.ability:IsCooldownReady() then
+    --Apply modifier branch
 
     cooldown = keys.Cooldown
-    keys.ability:EndCooldown()
-    keys.ability:StartCooldown(keys.Delay + cooldown)
-
+    keys.ability:StartCooldown(keys.Delay + cooldown) --Adds delay and cooldown after delay so the player has
+                                                      --the cooldown indication that the item is working.
+                                                      -- *Want* eventually add a particle effect
+    --Delay timer
     Timers:CreateTimer({
       endTime = keys.Delay,
       callback = function()
@@ -14,6 +16,8 @@ function modifier_stoneskin_toggle(keys)
       end
     })
   else
+
+    --Remove modifier Branch
     if keys.ability:IsCooldownReady() then
       keys.caster:RemoveModifierByName("modifier_item_stoneskin_stone_armor")
     end
