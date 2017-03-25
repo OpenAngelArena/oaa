da8a75841d6cb93e3ad544991cfe963b818d3e30
darklordabc <curtisjdubois@hotmail.com>
2017-03-26 09:22:32 +1100

    fixspawn cheat addded

--- game/scripts/vscripts/libraries/chatcommand.lua
+++ game/scripts/vscripts/libraries/chatcommand.lua
@@ -119,12 +119,22 @@ function ChatCommand:OnPlayerChat(keys)
 	    end)
 
 	  -- Remove fog of war on the map, revealing everything
-	   elseif string.find(text, "-nofog") then
+	  elseif string.find(text, "-nofog") then
 	    GameRules:GetGameModeEntity():SetFogOfWarDisabled(true)
 
 	  -- Bring back the fog of war
 	  elseif string.find(text, "-fog") then
 	    GameRules:GetGameModeEntity():SetFogOfWarDisabled(false)
+
+	  -- Bring back the fog of war
+	  elseif string.find(text, "-fixspawn") then
+	     for playerID=0,24-1 do
+	      local hero = PlayerResource:GetSelectedHeroEntity(playerID)
+	      if hero ~= nil and IsValidEntity(hero) then
+	      	hero:AddNewModifier(caster, ability, "modifier_chen_test_of_faith_teleport", {duration = 1})
+	      end
+	    end
+
 
 	  -- Force start of a duel
 	  elseif string.find(text, "-startduel") then
