20fafa47d345f8da92d530025093e000cffe43a9
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-03-25 09:55:55 +0800

    Update Nature's Call and refactor talent checking to use a function (#311)
    * Add HasLearnedAbility method to CDOTA_BaseNPC class
    
    * Change Demonic Conversion to use HasLearnedAbility
    
    * Update Nature's Call to work with 7.04 +4 Treants talent
    
    Currently just spawns +4 normal Treants at all ability levels.
    
    * Fix reference to variable that was removed in Demonic Conversion
    
    * Change Nature's Call to use AbilitySpecial for extra Treant spawn amount

---
+++ game/scripts/vscripts/libraries/basenpc.lua
@@ -0,0 +1,14 @@
+--[[ Extension functions for CDOTA_BaseNPC
+
+-HasLearnedAbility(abilityName)
+  Checks if the unit has at least one point in ability abilityName.
+  Primarily for checking if talents have been learned.
+--]]
+
+function CDOTA_BaseNPC:HasLearnedAbility(abilityName)
+  local ability = self:FindAbilityByName(abilityName)
+  if ability then
+    return ability:GetLevel() > 0
+  end
+  return false
+end
