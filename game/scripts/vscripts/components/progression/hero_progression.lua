9ad0dd100ebc0ef114639233624a56a0b2da83b5
marengo_hue <mokonamodokidesu@gmail.com>
2017-03-25 16:45:59 +0300

    Added levelling pattern override for invoker, extended the mechanism to support additional overrides

--- game/scripts/vscripts/components/progression/hero_progression.lua
+++ game/scripts/vscripts/components/progression/hero_progression.lua
@@ -1,48 +1,73 @@
 if HeroProgression == nil then
     HeroProgression = class({})
     Debug.EnabledModules['progression:*'] = true
+
+    ChatCommand:LinkCommand("-levelup", "OnLevelUpChatCmd", HeroProgression)
 end
 
-function HeroProgression:Init ()
-    HeroProgression = self
+function HeroProgression:RegisterCustomLevellingPatterns()
+  HeroProgression.customLevellingPatterns = {}
 
-    GameEvents:OnPlayerLevelUp(function (keys)
-      local player = EntIndexToHScript(keys.player)
-      local level = keys.level
-      local hero = player:GetAssignedHero()
+  HeroProgression.customLevellingPatterns['npc_dota_hero_invoker'] = (function(level)
+    -- Invoker gets all dem ability points
+    return true
+  end)
+end
 
-      self:ReduceStatGain(hero, level)
-      self:ProcessAbilityPointGain(hero, level)
+function HeroProgression:Init()
+  HeroProgression = self
+
+  self:RegisterCustomLevellingPatterns()
+  GameEvents:OnPlayerLevelUp(function (keys)
+    local player = EntIndexToHScript(keys.player)
+    local level = keys.level
+    local hero = player:GetAssignedHero()
+
+    self:ReduceStatGain(hero, level)
+    self:ProcessAbilityPointGain(hero, level)
   end)
 end
 
 function HeroProgression:ReduceStatGain(hero, level)
-    if level > 25 then
-        local div = (level - 25 + 12) / 12
+  if level > 25 then
+    local div = (level - 25 + 12) / 12
 
-        local gainStr = hero:GetStrengthGain()
-        local gainAgi = hero:GetAgilityGain()
-        local gainInt = hero:GetIntellectGain()
+    local gainStr = hero:GetStrengthGain()
+    local gainAgi = hero:GetAgilityGain()
+    local gainInt = hero:GetIntellectGain()
 
-        local newStr = gainStr / div
-        local newAgi = gainAgi / div
-        local newInt = gainInt / div
+    local newStr = gainStr / div
+    local newAgi = gainAgi / div
+    local newInt = gainInt / div
 
-        hero:ModifyStrength(newStr - gainStr)
-        hero:ModifyAgility(newAgi - gainAgi)
-        hero:ModifyIntellect(newInt - gainInt)
-    end
+    hero:ModifyStrength(newStr - gainStr)
+    hero:ModifyAgility(newAgi - gainAgi)
+    hero:ModifyIntellect(newInt - gainInt)
+  end
 end
 
-function HeroProgression:ShouldGetAnAbilityPoint(level)
-    -- After level 25 hero gets an additional skill point every 3 levels
+function HeroProgression:ShouldGetAnAbilityPoint(hero, level)
+  pattern = HeroProgression.customLevellingPatterns[hero:GetName()]
+  if pattern == nil then
+    -- After level 25 most heros get an additional skill point every 3 levels
     return level < 25 or math.fmod(level, 3) == 1
+  else
+    -- Hero levelling up has a custom levelling pattern
+    -- (e.g. Invoker who gets all the skillpoints every level)
+    return pattern(level)
+  end
 end
 
 function HeroProgression:ProcessAbilityPointGain(hero, level)
-    DebugPrint('Processing the ability point for ' .. hero:GetName() .. ' @ level ' .. level)
-    if not self:ShouldGetAnAbilityPoint(level) then
-        DebugPrint('...taken it away!')
-        hero:SetAbilityPoints(hero:GetAbilityPoints() - 1)
-    end
+  DebugPrint('Processing the ability point for ' .. hero:GetName() .. ' at level ' .. level)
+  if not self:ShouldGetAnAbilityPoint(hero, level) then
+    DebugPrint('...taken it away! (had ' .. hero:GetAbilityPoints() .. ' ability points)')
+    hero:SetAbilityPoints(hero:GetAbilityPoints() - 1)
+  end
+end
+
+function HeroProgression:OnLevelUpChatCmd(keys)
+  local hero = PlayerResource:GetSelectedHeroEntity(keys.playerid)
+  DebugPrint('Levelling up ' .. hero:GetName() .. ' now at level ' .. hero:GetLevel())
+  hero:HeroLevelUp(true)
 end
