2f182cf53915f0f6d0a948e4252798fce806fcef
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-04-06 23:16:22 +0800

    Change modifier_purgetester to grant damage immunity instead of reduction

--- game/scripts/vscripts/modifiers/modifier_purgetester.lua
+++ game/scripts/vscripts/modifiers/modifier_purgetester.lua
@@ -1,10 +1,12 @@
--- Modifier that grants 500% damage reduction. Mainly for protecting "purge tester" units, like in
+-- Modifier that grants complete damage immunity. Mainly for protecting "purge tester" units, like in
 -- items/reflex/postactive.lua
 modifier_purgetester = class({})
 
 function modifier_purgetester:DeclareFunctions()
   return {
-    MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE
+    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL,
+    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL,
+    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE
   }
 end
 
@@ -20,6 +22,14 @@ function modifier_purgetester:IsPurgeException()
   return false
 end
 
-function modifier_purgetester:GetModifierIncomingDamage_Percentage(keys)
-  return -500
+function modifier_purgetester:GetAbsoluteNoDamageMagical(keys)
+  return 1
+end
+
+function modifier_purgetester:GetAbsoluteNoDamagePhysical(keys)
+  return 1
+end
+
+function modifier_purgetester:GetAbsoluteNoDamagePure(keys)
+  return 1
 end
