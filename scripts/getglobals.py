bc374f5528232a9633ac19a58fad39e0cd65a7d2
Chronophylos <admiral.chronophylos@web.de>
2017-04-08 22:03:58 +0200

    WIP Global finder

--- scripts/getglobals.py
+++ scripts/getglobals.py
@@ -6,6 +6,7 @@ import sys
 import codecs
 from collections import OrderedDict
 from functools import partial
+import os
 
 
 ENCODING = "utf-8"
@@ -36,7 +37,6 @@ def getBlock(span):
 def cleanBlock(table, elem, nested):
     """Return a clean block."""
     block = dict()
-    print(elem)
     for row in table('tr'):
         key = row.find_next('td')
         if nested:
@@ -46,16 +46,8 @@ def cleanBlock(table, elem, nested):
     return block
 
 
-def display(output, blocks):
-    """Format and print blocks to output."""
-    for k, vs in blocks.items():
-        output.write('-- ' + k + '\n')
-        for v in vs:
-            output.write('"' + v + '",\n')
-
-
-def main():
-    """Main Function."""
+def getAPIRefs():
+    """Get API Refs from valve and return a block"""
     cleanConstBlock = partial(cleanBlock, elem='td', nested=False)
     cleanFuncBlock = partial(cleanBlock, elem='code', nested=True)
 
@@ -78,8 +70,81 @@ def main():
         elif "Function" in header:
             blocks[name] = cleanFuncBlock(table)
             blocks['Accessor'].append(name)
+
+    return blocks
+
+
+def getRequirePath(line):
+    for item in line.split('require'):
+        item = item.strip()
+        if not item:
+            continue
+        if item.startswith('(') and item.endswith(')'):
+            return item.strip('()"\'')
+
+
+def getRequired(filename):
+    result = list()
+    if not filename.endswith('.lua'):
+        return
+    with open(filename, 'r') as luafile:
+        for line in luafile:
+            if "require" not in line:
+                continue
+            required = getRequirePath(line)
+            if not required:
+                continue
+            required = os.path.join("game/scripts/vscripts/", required)
+            result.append(required)
+    return result
+
+
+def getVScriptGlobals():
+    """Go through game/scripts/vscripts and follow require."""
+    blocks = dict()
+    filestoadd = list()
+    filestocheck = list()
+    for _, _, filenames in os.walk("game/scripts/vscripts/"):
+        for filename in filenames:
+            filename = os.path.join("game/scripts/vscripts/", filename)
+            required = getRequired(filename)
+            filestoadd += required
+            filestocheck += required
+        break
+    while filestoadd:
+        for entry in filestoadd:
+            print(entry)
+            entry = os.path.join("game/scripts/vscripts/", entry)
+            required = getRequired(entry)
+            if not required:
+                continue
+            filestoadd += required
+            filestocheck += required
+            filestoadd.remove(entry)
+    print(repr(filestoadd))
+    print(repr(filestocheck))
+    return blocks
+
+
+def display(output, blocks):
+    """Format and print blocks to output."""
+    for k, vs in blocks.items():
+        output.write('-- ' + k + '\n')
+        for v in vs:
+            output.write('"' + v + '",\n')
+
+
+def main():
+    """Main Function."""
+    blocks = OrderedDict()
+    blocks = OrderedDict(blocks, **getAPIRefs())
+    blocks = OrderedDict(blocks, **getVScriptGlobals())
+
     with open('dump', 'w') as dump:
-      display(dump, blocks)
+        # display(dump, blocks)
+        pass
+
+    return 0
 
 
 if __name__ == '__main__':
