1bdf4ff2d5d1905f62297e4304ba12b812e2ff18
Chris Vickery <chrisinajar@gmail.com>
2017-04-10 10:41:26 -0700

    Change boss lists to be per-pit (#516)

--- game/scripts/vscripts/components/boss/bosses.lua
+++ game/scripts/vscripts/components/boss/bosses.lua
@@ -1,23 +1,39 @@
 Bosses = {
   -- tier 1
   {
-    "npc_dota_boss_tier_1"
+    "npc_dota_boss_tier_1",
+    "npc_dota_boss_tier_2",
+    "npc_dota_boss_tier_3",
+    "npc_dota_boss_tier_4",
+    "npc_dota_boss_tier_5",
+    "npc_dota_boss_tier_6"
   },
   -- tier 2
   {
-    "npc_dota_boss_tier_2"
+    "npc_dota_boss_tier_2",
+    "npc_dota_boss_charger",
+    "npc_dota_boss_tier_4",
+    "npc_dota_boss_tier_5",
+    "npc_dota_boss_tier_6"
   },
   -- tier 3
   {
-    "npc_dota_boss_tier_3"
+    "npc_dota_boss_charger",
+    "npc_dota_boss_tier_4",
+    "npc_dota_boss_tier_5",
+    "npc_dota_boss_tier_6"
   },
   -- tier 4
   {
-    "npc_dota_boss_charger"
+    "npc_dota_boss_charger",
+    "npc_dota_boss_tier_4",
+    "npc_dota_boss_tier_5",
+    "npc_dota_boss_tier_6"
   },
   -- tier 5
   {
-    "npc_dota_boss_tier_5"
+    "npc_dota_boss_tier_5",
+    "npc_dota_boss_tier_6"
   },
   -- tier 6
   {
