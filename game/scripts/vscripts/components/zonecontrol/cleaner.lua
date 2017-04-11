2d24c359a9b282e885d5cad24d16d7e17cd574ee
Chronophylos <admiral.chronophylos@web.de>
2017-04-12 01:06:53 +0200

    It does work now

--- game/scripts/vscripts/components/zonecontrol/cleaner.lua
+++ game/scripts/vscripts/components/zonecontrol/cleaner.lua
@@ -5,17 +5,23 @@ if ZoneCleaner  == nil then
   ZoneCleaner = class({})
 end
 
+-- https://moddota.com/forums/discussion/92/tutorial-proposal-document-the-baseclasses
 ZoneCleaner.ForbiddenEntities = {
-  "npc_dota_observer_wards",
-  "npc_dota_sentry_wards",
-  "npc_dota_venomancer_plague_ward_1",
-  "npc_dota_venomancer_plague_ward_2",
-  "npc_dota_venomancer_plague_ward_3",
-  "npc_dota_venomancer_plague_ward_4",
-  "npc_dota_venomancer_plague_ward_5",
-  "npc_dota_venomancer_plague_ward_6",
-  "npc_dota_techies_land_mine",
-  "npc_dota_techies_remote_mine",
+  --"npc_dota_observer_wards",
+  --"npc_dota_sentry_wards",
+  "npc_dota_ward_base",
+  "npc_dota_ward_base_truesight",
+  --"npc_dota_venomancer_plague_ward_1",
+  --"npc_dota_venomancer_plague_ward_2",
+  --"npc_dota_venomancer_plague_ward_3",
+  --"npc_dota_venomancer_plague_ward_4",
+  --"npc_dota_venomancer_plague_ward_5",
+  --"npc_dota_venomancer_plague_ward_6",
+  "npc_dota_venomancer_plagueward",
+  --"npc_dota_techies_land_mine",
+  --"npc_dota_techies_remote_mine",
+  "npc_dota_techies_mines",
+  "npc_dota_techies_minefield_sign",
   "npc_dota_broodmother_web",
   "npc_dota_techies_stasis_trap",
   "npc_dota_templar_assassin_psionic_trap",
@@ -25,50 +31,18 @@ ZoneCleaner.ForbiddenEntities = {
 }
 
 function ZoneCleaner:CleanZone(state)
-  DebugPrint('Cleaning Zone')
-  DebugDrawBox(state.origin, state.bounds.Mins, state.bounds.Maxs, 255, 100, 0, 0, 10)
+  DebugDrawBox(state.origin, state.bounds.Mins, state.bounds.Maxs, 255, 100, 0, 0, 30)
+  DebugDrawSphere(state.origin, Vector(255, 100, 0), 0, max(state.bounds.Maxs.x + state.bounds.Maxs.y, state.bounds.Mins.x + state.bounds.Mins.y), true, 30)
 
-  for _,entry in pairs(ZoneCleaner.ForbiddenEntities) do
-    DebugPrint('Searching for "' .. entry .. '"')
-    DebugPrintTable(Entities:FindAllByClassname(entry)) -- WHY DON'T YOU FUCKING FIND THIS SHIT?
-    DebugPrintTable(Entities:FindAllByName(entry)) -- I'M EVEN USING THE OTHER FUCKING FUNCTION
-    for _,entity in pairs(Entities:FindAllByClassname(entry)) do -- ARE THESE NO ENTITIES OR WHAT?
-      DebugPrint('Found ' .. entity:GetClassname())
-      if InArea(entity.GetAbsOrigin(), state.origin + state.bounds.Mins, state.origin + state.bounds.Maxs) then
-        DebugPrint('Found entity is in zone.')
-        DebugPrintTable(entity)
-        entity:RemoveSelf()
+  local entities = Entities:FindAllInSphere(state.origin, max(max(state.bounds.Mins.x, state.bounds.Maxs.x),max(state.bounds.Mins.y, state.bounds.Maxs.y)))
+
+  for _,entity in pairs(entities) do
+    for _,entry in pairs(ZoneCleaner.ForbiddenEntities) do
+      if not entity:IsNull() then
+        if entry == entity:GetName() then
+          entity:RemoveSelf()
+        end
       end
     end
   end
-end
-
-function ZoneCleaner:CleanZones (states)
-  DebugPrint('Cleaning multiple Zones')
-
-
-end
-
-function IsInArea (vector, mins, maxs)
-  if IsBetween(vector.x, mins.x, maxs.x) and
-     IsBetween(vector.y, mins.y, maxs.z) then
-    return true
-  end
-  return false
-end
-
-function IsInBox(vector, mins, maxs)
-  if IsBetween(vector.x, mins.x, maxs.x) and
-     IsBetween(vector.y, mins.y, maxs.z) and
-     IsBetween(vector.z, mins.z, maxs.y) then
-    return true
-  end
-  return false
-end
-
-function IsBetween (x, a, b)
-  if a > x and x < b or b > x and x < a then
-    return true
-  end
-  return false
 end
