bdf027c29cb2c756526886e6bbe31632c5b55fe0
Chris Vickery <chrisinajar@gmail.com>
2017-04-12 12:50:56 -0700

    Make sire octarine doesnt mess up when used with satanic

--- game/scripts/vscripts/modifiers/modifier_octarine_vampirism_buff.lua
+++ game/scripts/vscripts/modifiers/modifier_octarine_vampirism_buff.lua
@@ -59,7 +59,7 @@ function modifier_octarine_vampirism_buff:OnTakeDamage(params)
   local nHeroHeal = self.hero_lifesteal / 100
   local nCreepHeal = self.creep_lifesteal / 100
 
-  if hero:HasModifier("modifier_item_satanic_unholy") and hero:HasModifier("modifier_item_satanic_core") then
+  if self.hero_spellsteal_unholy and hero:HasModifier("modifier_item_satanic_unholy") and hero:HasModifier("modifier_item_satanic_core") then
     nHeroHeal = self.hero_spellsteal_unholy / 100
     nCreepHeal = self.creep_spellsteal_unholy / 100
   end
