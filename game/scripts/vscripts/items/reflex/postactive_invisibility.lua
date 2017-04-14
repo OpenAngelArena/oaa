e86b284e51218ab6c6e8a5dc9562486cb01f1ec2
Chris Vickery <chrisinajar@gmail.com>
2017-04-14 16:19:07 -0700

    Fix icons and test each ability

--- game/scripts/vscripts/items/reflex/postactive_invisibility.lua
+++ game/scripts/vscripts/items/reflex/postactive_invisibility.lua
@@ -10,8 +10,8 @@ function item_postactive_2a:OnToggle(keys)
   local caster = self:GetCaster()
   local shroud_duration = self:GetSpecialValueFor( "duration" )
 
-  hTarget:AddNewModifier( hTarget, self, "modifier_item_glimmer_cape_fade", { duration = shroud_duration } )
-  EmitSoundOn( "Item.GlimmerCape.Activate", hTarget )
+  caster:AddNewModifier( caster, self, "modifier_item_glimmer_cape_fade", { duration = shroud_duration } )
+  EmitSoundOn( "Item.GlimmerCape.Activate", caster )
 
   -- important else you can use while on CD every other time
   if self:GetToggleState() then
