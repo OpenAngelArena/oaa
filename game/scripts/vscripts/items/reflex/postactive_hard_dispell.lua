43ddb3d79129edd637e9a061f972921e27570c33
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-04-16 04:08:20 +0800

    Change the rest of the stun-castable Reflex Items to not be fake toggles (#550)
    * Change Postactive Reflex Items to not be fake toggles
    
    * Change Postactive 3b (Divine Enrage Crystal) to have strong dispel

--- game/scripts/vscripts/items/reflex/postactive_hard_dispell.lua
+++ game/scripts/vscripts/items/reflex/postactive_hard_dispell.lua
@@ -1,22 +1,9 @@
 
 item_postactive_3b = class({})
 
-function item_postactive_3b:ResetToggleOnRespawn()
-  return true
-end
-
-function item_postactive_3b:OnToggle(keys)
+function item_postactive_3b:OnSpellStart()
   local caster = self:GetCaster()
 
   -- void Purge(bool bRemovePositiveBuffs, bool bRemoveDebuffs, bool bFrameOnly, bool bRemoveStuns, bool bRemoveExceptions)
-  caster:Purge(false, true, false, true, false)
-
-  -- important else you can use while on CD every other time
-  if self:GetToggleState() then
-    self:ToggleAbility()
-  end
-
-  self:StartCooldown(self:GetCooldownTime())
-
-  return false
+  caster:Purge(false, true, false, true, true)
 end
