aa9331993233f31ce98b1ddbacf86dc99c94e7e4
Chris Vickery <chrisinajar@gmail.com>
2017-04-16 02:25:18 -0700

    Implement most of the reactive items (#565)
    Closes: #237
    Closes: #238
    Closes: #240
    
    * Implement 2a, most of 2b
    
    * Change invuln to be damage reduction, finish 2b
    
    * reactive 3a
    
    * Fix skip team set settings
    
    * Delte pointless old file
    
    * Fix tooltips for reactive items

---
+++ game/scripts/vscripts/modifiers/modifier_charge_replenisher.lua
@@ -0,0 +1,68 @@
+LinkLuaModifier( "modifier_charge_replenishing", "modifiers/modifier_charge_replenisher.lua", LUA_MODIFIER_MOTION_NONE )
+
+modifier_charge_replenisher = class({})
+
+function modifier_charge_replenisher:IsHidden()
+  return true
+end
+function modifier_charge_replenisher:IsDebuff()
+  return false
+end
+function modifier_charge_replenisher:IsPurgable()
+  return false
+end
+function modifier_charge_replenisher:OnCreated()
+  self:StartIntervalThink(0.1)
+end
+function modifier_charge_replenisher:OnRefresh()
+  self:StartIntervalThink(0.1)
+end
+function modifier_charge_replenisher:OnIntervalThink()
+  local caster = self:GetCaster()
+  local isReplenishing = caster:HasModifier("modifier_charge_replenishing")
+  if isReplenishing then
+    return
+  end
+
+  local ability = self:GetAbility()
+  local maxCharges = ability:GetSpecialValueFor( "max_charges" )
+  local duration = ability:GetSpecialValueFor( "charge_restore_time" )
+  if not ability.GetCurrentCharges then
+    -- this happens when it first starts up and i have no clue why
+    return
+  end
+  local charges = ability:GetCurrentCharges()
+
+  local ability = self:GetAbility()
+  if charges < maxCharges then
+    -- purely visual
+    caster:AddNewModifier( caster, ability, "modifier_charge_replenishing", { duration = duration } )
+  end
+end
+
+modifier_charge_replenishing = class({})
+
+function modifier_charge_replenishing:IsHidden()
+  return false
+end
+function modifier_charge_replenishing:IsDebuff()
+  return false
+end
+function modifier_charge_replenishing:IsPurgable()
+  return false
+end
+function modifier_charge_replenishing:OnCreated(keys)
+  self:StartIntervalThink(keys.duration)
+end
+function modifier_charge_replenishing:OnRefresh()
+  self:StartIntervalThink(keys.duration)
+end
+function modifier_charge_replenishing:OnIntervalThink()
+  local ability = self:GetAbility()
+  local maxCharges = ability:GetSpecialValueFor( "max_charges" )
+  local charges = ability:GetCurrentCharges()
+  if charges < maxCharges then
+    ability:SetCurrentCharges( ability:GetCurrentCharges() + 1 )
+  end
+  self:StartIntervalThink(-1)
+end
