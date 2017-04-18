431716f00776a18e8e83f2b541c7546d7761d6aa
Karmatic Neutral <karmaticneutral@gmail.com>
2017-04-18 11:06:03 -0600

    fix other bosses from my idiocy and attempt this

--- game/scripts/vscripts/units/shielder.lua
+++ game/scripts/vscripts/units/shielder.lua
@@ -1,4 +1,12 @@
 
+--[[ Taken from bb template
+if ShielderAI == nil then
+  DebugPrint ( 'creating new BossAI object' )
+  ShielderAI = class({})
+
+  Debug.EnabledModules['boss:*'] = false
+end]]
+
 LinkLuaModifier( "modifier_boss_phase_controller", "modifiers/modifier_boss_phase_controller", LUA_MODIFIER_MOTION_NONE )
 
 function Spawn (entityKeyValues)
@@ -13,23 +21,17 @@ function Spawn (entityKeyValues)
   phaseController:SetPhases({ 66, 33 })
   phaseController:SetAbilities({
     "boss_shielder_shield"
-})
+  })
 end
 
---[[Taken from bb template
-if BossAI == nil then
-  DebugPrint ( 'creating new BossAI object' )
-  BossAI = class({})
+--[[
+ShielderAI.IDLE = 1
+ShielderAI.AGRO = 2
+ShielderAI.LEASHING = 3
+ShielderAI.DEAD = 4
+]]
 
-  Debug.EnabledModules['boss:*'] = false
-end]]
-
-BossAI.IDLE = 1
-BossAI.AGRO = 2
-BossAI.LEASHING = 3
-BossAI.DEAD = 4
-
-function BossAI:Create (unit, options)
+function Create (unit, options)
   options = {}
   options.tier = 1
 
@@ -37,20 +39,20 @@ function BossAI:Create (unit, options)
     handle = unit,
     origin = unit:GetAbsOrigin(),
     leash = 1500,
-    agroDamage = 100 * options.tier,
+    agroDamage = 100,
     tier = 1,
     currentDamage = 0,
-    state = BossAI.IDLE,
-    customAgro = options.customAgro or false,
+    state = 1,
+    customAgro = true,
 
     deathEvent = Event()
   }
 
   unit:OnHurt(function (keys)
-    BossAI:HurtHandler(state, keys)
+    HurtHandler(state, keys)
   end)
   unit:OnDeath(function (keys)
-    BossAI:DeathHandler(state, keys)
+    DeathHandler(state, keys)
   end)
 
   unit:SetIdleAcquire(false)
@@ -61,22 +63,22 @@ function BossAI:Create (unit, options)
   }
 end
 
-function BossAI:HurtHandler (state, keys)
-  if state.state == BossAI.IDLE then
+function HurtHandler (state, keys)
+  if state.state == 1 then
     DebugPrint('Checking boss agro...')
     DebugPrintTable(keys)
 
     state.currentDamage = state.currentDamage + keys.damage
 
     if state.currentDamage > state.agroDamage then
-      BossAI:Agro(state, EntIndexToHScript(keys.entindex_attacker))
+      Agro(state, EntIndexToHScript(keys.entindex_attacker))
       state.currentDamage = 0
     end
-  elseif state.state == BossAI.AGRO then
+  elseif state.state == 2 then
   end
 end
 
-function BossAI:GiveItemToWholeTeam (item, teamId)
+function GiveItemToWholeTeam (item, teamId)
   for playerId = 0,19 do
     if PlayerResource:GetTeam(playerId) == teamId and PlayerResource:GetPlayer(playerId) ~= nil then
       local player = PlayerResource:GetPlayer(playerId)
@@ -87,9 +89,9 @@ function BossAI:GiveItemToWholeTeam (item, teamId)
   end
 end
 
-function BossAI:DeathHandler (state, keys)
+function DeathHandler (state, keys)
   DebugPrint('Handling death of boss ' .. state.tier)
-  state.state = BossAI.DEAD
+  state.state = 4
 
   state.handle = nil
   local killer = EntIndexToHScript(keys.entindex_attacker)
@@ -105,7 +107,7 @@ function BossAI:DeathHandler (state, keys)
     return
   end
 
-  BossAI:GiveItemToWholeTeam("item_upgrade_core", teamId)
+  GiveItemToWholeTeam("item_upgrade_core", teamId)
 
   for playerId = 0,19 do
     if PlayerResource:GetTeam(playerId) == teamId and PlayerResource:GetPlayer(playerId) ~= nil then
@@ -125,24 +127,24 @@ function BossAI:DeathHandler (state, keys)
   end
 end
 
-function BossAI:Agro (state, target)
+function Agro (state, target)
   if state.customAgro then
     DebugPrint('Running custom agro ai')
     return
   end
 
   Timers:CreateTimer(1, function ()
-    if state.state == BossAI.DEAD then
+    if state.state == 4 then
       return
     end
 
-    if not BossAI:Think(state) or state.state == BossAI.IDLE then
+    if not Think(state) or state.state == 1 then
       DebugPrint('Stopping think timer')
       return
     end
     return 1
   end)
-  state.state = BossAI.AGRO
+  state.state = 2
   state.agroTarget = target
 
   state.handle:SetIdleAcquire(true)
@@ -164,29 +166,28 @@ function BossAI:Agro (state, target)
   })
 end
 
-function BossAI:Think (state)
+function Think (state)
   local distance = (state.handle:GetAbsOrigin() - state.origin):Length()
   DebugPrint(distance)
 
   if distance > state.leash then
-    BossAI:Leash(state)
-  elseif distance < state.leash / 2 and state.state == BossAI.LEASHING then
-    state.state = BossAI.IDLE
+    Leash(state)
+  elseif distance < state.leash / 2 and state.state == 3 then
+    state.state = 1
     return false
-  elseif distance == 0 and state.state == BossAI.AGRO then
-    state.state = BossAI.IDLE
+  elseif distance == 0 and state.state == 2 then
+    state.state = 1
     return false
   end
 
   return true
 end
 
-function BossAI:Leash (state)
+function Leash (state)
   local difference = state.handle:GetAbsOrigin() - state.origin
   local location = state.origin + (difference / 8)
 
-  state.state = BossAI.LEASHING
-
+  state.state = 3
   state.handle:SetIdleAcquire(false)
   state.handle:SetAcquisitionRange(0)
 
