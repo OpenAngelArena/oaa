a4b2183796a42bfc685bc0f1114e47352713ef51
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-04-19 02:53:45 +0800

    Implement Preemptive 3c Reflex Item (Bubble Orb) (#594)
    Closes: #235
    
    * Partially implement Bubble Orb
    
    Blocks targeted spells and spell damage from the outside (also currently block maelstrom/mjollnir lightning). Doesn't block any spell effects yet.
    
    * Functionalize checking of caster position
    
    * Change Bubble Orb to use a pseudo-aura thinker
    
    * Implement Bubble Orb spell effect blocking for non-targeted spells
    
    * Fix Bubble Orb recipe
    
    * Fix typo in reflexfilters/bubble.lua
    
    * Add buff tooltip

---
+++ game/scripts/vscripts/components/reflexfilters/bubble.lua
@@ -0,0 +1,41 @@
+-- Component for handling the ModifierGained Filter used for spell effect blocking
+-- from the Postactive 3c Reflex Item (Bubble Orb)
+
+if not BubbleOrbFilter then
+  DebugPrint("Creating filter for Preemptive 3c (Bubble Orb)")
+  BubbleOrbFilter = class({})
+
+  Debug.EnabledModules["reflexfilters:bubble"] = true
+end
+
+function BubbleOrbFilter:Init()
+  FilterManager:AddFilter(FilterManager.ModifierGained, self, Dynamic_Wrap(self, "ModifierGainedFilter"))
+end
+
+function BubbleOrbFilter:ModifierGainedFilter(keys)
+  local caster = EntIndexToHScript(keys.entindex_caster_const)
+  local parent = EntIndexToHScript(keys.entindex_parent_const)
+  local bubbleModifierName = "modifier_item_preemptive_bubble_block"
+  local casterIsAlly = caster:GetTeamNumber() == parent:GetTeamNumber()
+  local parentHasBubbleModifier = parent:HasModifier(bubbleModifierName)
+  local bubbleModifiers = parent:FindAllModifiersByName(bubbleModifierName)
+  local casterIsInBubbles = false
+
+  function UnitIsInSpecificBubble(unit, bubbleModifier)
+    return bubbleModifier:UnitIsInBubble(unit)
+  end
+
+  if parentHasBubbleModifier then
+    casterIsInBubbles = reduce(operator.land, true, map(partial(UnitIsInSpecificBubble, caster), iter(bubbleModifiers)))
+  end
+
+  if not parentHasBubbleModifier or casterIsAlly or casterIsInBubbles then
+    if parentHasBubbleModifier and caster ~= parent then
+      DebugPrint(tostring(casterIsAlly))
+      DebugPrint(tostring(casterIsInBubbles))
+    end
+    return true
+  else
+    return false
+  end
+end
