aedc426dcae5498268f7dd0c1c03f6543e497298
Karmatic Neutral <karmaticneutral@gmail.com>
2017-04-17 21:21:46 -0600

    AI free shield boss

---
+++ game/scripts/vscripts/units/shielder.lua
@@ -0,0 +1,17 @@
+
+LinkLuaModifier( "modifier_boss_phase_controller", "modifiers/modifier_boss_phase_controller", LUA_MODIFIER_MOTION_NONE )
+
+function Spawn (entityKeyValues)
+  thisEntity:FindAbilityByName("boss_shielder_shield")
+
+  thisEntity:SetContextThink( "ShielderThink", partial(ShielderThink, thisEntity) , 1)
+  print("Starting AI for " .. thisEntity:GetUnitName() .. " " .. thisEntity:GetEntityIndex())
+
+  ABILITY_shield = thisEntity:FindAbilityByName("boss_shielder_shield")
+
+  local phaseController = thisEntity:AddNewModifier(thisEntity, ABILITY_shield, "modifier_boss_phase_controller", {})
+  phaseController:SetPhases({ 66, 33 })
+  phaseController:SetAbilities({
+    "boss_shielder_shield"
+})
+end
