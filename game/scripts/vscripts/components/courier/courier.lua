f4966020378ce5566eb5b26ceac617b22b133267
darklordabc <curtisjdubois@hotmail.com>
2017-04-18 12:30:55 +1000

    courier fix

--- game/scripts/vscripts/components/courier/courier.lua
+++ game/scripts/vscripts/components/courier/courier.lua
@@ -15,8 +15,25 @@ function Courier:Init ()
 end
 
 function Courier:SpawnCourier (hero)
-  DebugPrint("Creating Courier for Team " .. hero:GetTeamNumber())
-  courier = hero:AddItemByName('item_courier')
-  flying = hero:AddItemByName('item_flying_courier')
-  Courier.hasCourier[hero:GetTeamNumber()] = true
+  Timers:CreateTimer(1, function ()
+    DebugPrint("Creating Courier for Team " .. hero:GetTeamNumber())
+
+    -- Check if there is an item blocking slot 1, if so sell it
+    slot1Item = hero:GetItemInSlot(0)
+    if slot1Item then
+      hero:SellItem(slot1Item)
+    end
+
+    -- Create couriers and then cast them straight away
+    local courier = hero:AddItemByName('item_courier')
+    if courier then
+        hero:CastAbilityImmediately(courier, hero:GetPlayerID())
+    end
+    local flying = hero:AddItemByName('item_flying_courier')
+    if flying then
+        hero:CastAbilityImmediately(flying, hero:GetPlayerID())
+    end
+
+    Courier.hasCourier[hero:GetTeamNumber()] = true
+  end)
 end
