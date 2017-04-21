65f8b03daa0d3828d9e4b323cfefe89a2d0a5f34
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-04-22 00:41:34 +0800

    Rewrite Invincibility Shard as Lua item

---
+++ game/scripts/vscripts/items/reflex/reactive_invincibility.lua
@@ -0,0 +1,47 @@
+LinkLuaModifier("modifier_reactive_immunity", "items/reflex/reactive_invincibility.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_generic_bonus", "modifiers/modifier_generic_bonus.lua", LUA_MODIFIER_MOTION_NONE)
+
+item_reactive = class({})
+
+function item_reactive:GetIntrinsicModifierName()
+  return "modifier_generic_bonus"
+end
+
+function item_reactive:OnSpellStart()
+  local duration = self:GetSpecialValueFor("reactive_duration")
+  local caster = self:GetCaster()
+
+  caster:AddNewModifier(caster, self, "modifier_reactive_immunity", {duration = duration})
+end
+
+------------------------------------------------------------------------
+
+modifier_reactive_immunity = class({})
+
+function modifier_reactive_immunity:GetEffectName()
+  return "particles/items_fx/black_king_bar_avatar.vpcf"
+end
+
+function modifier_reactive_immunity:GetTexture()
+  return self:GetAbility():GetAbilityTextureName()
+end
+
+function modifier_reactive_immunity:DeclareFunctions()
+  return {
+    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL,
+    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL,
+    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE
+  }
+end
+
+function modifier_reactive_immunity:GetAbsoluteNoDamagePhysical()
+  return 1
+end
+
+function modifier_reactive_immunity:GetAbsoluteNoDamageMagical()
+  return 1
+end
+
+function modifier_reactive_immunity:GetAbsoluteNoDamagePure()
+  return 1
+end
