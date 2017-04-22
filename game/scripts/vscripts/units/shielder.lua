1d83aff710c8c534c77fd273c0d74aa34766c106
Karmatic Neutral <karmaticneutral@gmail.com>
2017-04-22 10:17:39 -0600

    Technically Working

--- game/scripts/vscripts/units/shielder.lua
+++ game/scripts/vscripts/units/shielder.lua
@@ -23,6 +23,20 @@ function ShielderThink (thisEntity)
   thisEntity:OnHurt(function (keys)
   HurtHandler(keys)
   end)
+  if not GLOBAL_origin then
+    GLOBAL_origin = thisEntity:GetAbsOrigin()
+  else
+    local distance = (GLOBAL_origin - thisEntity:GetAbsOrigin()):Length()
+    if distance > 1000 then
+      ExecuteOrderFromTable({
+        UnitIndex = thisEntity:entindex(),
+        OrderType = DOTA_UNIT_ORDER_MOVE_TO_POSITION,
+        Position = GLOBAL_origin, --Optional.  Only used when targeting the ground
+        Queue = 0 --Optional.  Used for queueing up abilities
+      })
+      return 5
+    end
+  end
 end
 
 function HurtHandler (keys)
@@ -65,7 +79,7 @@ function HurtHandler (keys)
     ExecuteOrderFromTable({
       UnitIndex = bossIndex,
       OrderType = DOTA_UNIT_ORDER_STOP,
-      Position = nul,
+      Position = nul;
       Queue = 0
       })
 
@@ -74,7 +88,7 @@ function HurtHandler (keys)
       -- OrderType = DOTA_UNIT_ORDER_ATTACK_TARGET,
       OrderType = DOTA_UNIT_ORDER_ATTACK_MOVE,
       Position = EntIndexToHScript(playerIndex):GetAbsOrigin(),
-      Queue = 0,
+      Queue = 1,
     })
   end
 end
