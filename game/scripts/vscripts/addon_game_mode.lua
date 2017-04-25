5e158d5050026c7e58d70e954c365cf10d110aee
Chronophylos <admiral.chronophylos@web.de>
2017-04-25 16:33:06 +0200

    Fixed Precaching

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -21,8 +21,7 @@ function Precache( context )
   PrecacheItemByNameSync("item_postactive", context)
   PrecacheItemByNameSync("item_preemptive_3c", context)
 
-  -- stopfightingyourself
-  PrecacheResource("particle", "particles/generic_gameplay/illusion_killed.vpcf", context)
+  PrecacheUnitByNameSync("npc_dota_boss_stopfightingyourself", context)
 
   -- Particles can be precached individually or by folder
   -- It it likely that precaching a single particle system will precache all of its children, but this may not be guaranteed
