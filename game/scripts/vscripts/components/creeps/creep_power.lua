3819563426623a6536964f8afab33f4dc5faa1f0
Haganeko <haganekko@gmail.com>
2017-04-28 01:47:54 -0300

    Tweak prog (#668)
    * tweaked creep progression, with reduced armor and increased damage
    
    * fixed smaller ancients being stronger
    
    * slightly redistributed XP and gold within camps. Total amounts per spawn still the same
    
    * Made centaurs tankier so they wouldn't be a weaker camp compared to wolves

--- game/scripts/vscripts/components/creeps/creep_power.lua
+++ game/scripts/vscripts/components/creeps/creep_power.lua
@@ -16,10 +16,10 @@ function CreepPower:GetPowerForMinute (minute)
 
   return {
     minute,                                   -- minute
-    (7.5 * ((minute / 100) ^ 4) + 15 * ((minute/100) ^ 3) - 0.45 * ((minute/100) ^ 2) + 0.3 * (minute/100)) + 1,   -- hp
+    (7.5 * ((minute / 100) ^ 4) + 15 * ((minute/100) ^ 3) - 0.45 * ((minute/100) ^ 2) + 0.30 * (minute/100)) + 1,   -- hp
     (7.5 * ((minute / 100) ^ 4) + 15 * ((minute/100) ^ 3) - 0.45 * ((minute/100) ^ 2) + 0.3 * (minute/100)) + 1,   -- mana
-    (30 * ((minute / 100) ^ 4) + 60 * ((minute/100) ^ 3) - 1.8 * ((minute/100) ^ 2) + 1.2 * (minute/100)) + 1,     -- damage
-    (minute / 30) ^ 3 + (minute / 20) ^ 2 + minute / 5 + 1,                                                        -- armor
+    (40 * ((minute / 100) ^ 4) + 80 * ((minute/100) ^ 3) - 2.4 * ((minute/100) ^ 2) + 1.6 * (minute/100)) + 1,     -- damage
+    (minute / 20) ^ 2 + minute / 7 + 1,       -- armor
     (minute / 2) + 1,                         -- gold
     ((21 * minute^2 - 19 * minute + 3002) / 3002) * self.numPlayersXPFactor * multFactor -- xp
   }
