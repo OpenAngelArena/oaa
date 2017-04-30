60919e5a9a1ba882e63c3847481ec7e338bd998f
KarmaticNeutral <karmaticneutral@gmail.com>
2017-04-30 02:17:11 -0600

    Make T1 be literally rosh with boss resistance (#680)
    * Make T1 be literally rosh with boss resistance
    
    * LiterallyFixed

--- game/scripts/vscripts/components/boss/bosses.lua
+++ game/scripts/vscripts/components/boss/bosses.lua
@@ -1,8 +1,8 @@
 Bosses = {
   -- tier 1
   {
+    "npc_dota_boss_tier_1",
     "npc_dota_boss_shielder",
-    "npc_dota_boss_twin",
     "npc_dota_boss_tier_3",
     "npc_dota_boss_tier_4",
     "npc_dota_boss_tier_5",
