640ffd7f02aded9477935db17d86e79e4d057a18
Haganeko <haganekko@gmail.com>
2017-04-30 17:49:07 -0300

    added notes to creep camps for the purpose of code documentation

--- game/scripts/vscripts/components/creeps/creep_types.lua
+++ game/scripts/vscripts/components/creeps/creep_types.lua
@@ -4,7 +4,7 @@ CreepTypes = {
   -- 1 "easy camp"
   {
     {                                         --HP    MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_kobold",               300,  0,    10,   1,    13,   54},
+      {"npc_dota_neutral_kobold",               300,  0,    10,   1,    13,   54}, --expected gold is  65 and XP is 240
       {"npc_dota_neutral_kobold",               300,  0,    10,   1,    13,   54},
       {"npc_dota_neutral_kobold_taskmaster",    600,  0,    18,   2,    22,   70},
       {"npc_dota_neutral_kobold_tunneler",      425,  0,    14,   2,    17,   62}
@@ -13,7 +13,7 @@ CreepTypes = {
     -- 2 "medium camp"
   {
     {                                         --HP    MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_harpy_storm",          750,  400,  40,   2,    24,   140},
+      {"npc_dota_neutral_harpy_storm",          750,  400,  40,   2,    24,   140}, --expected gold is 68 and XP is 360
       {"npc_dota_neutral_harpy_storm",          750,  400,  40,   2,    24,   140},
       {"npc_dota_neutral_harpy_scout",          550,  0,    50,   1,    20,    80},
     }
@@ -21,7 +21,7 @@ CreepTypes = {
     -- 3 "hard camp"
   {
     {                                         --HP    MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_centaur_khan",         2200, 400,  70,   3,    66,   120},
+      {"npc_dota_neutral_centaur_khan",         2200, 400,  70,   3,    66,   120}, --expected gold is 135 and XP is 300
       {"npc_dota_neutral_centaur_outrunner",    1500, 400,  50,   2,    46,   120},
       {"npc_dota_neutral_centaur_outrunner",    1500, 400,  50,   2,    46,   120},
     },
