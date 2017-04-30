506b7f1e4358b0c37fa96cf44c62a4a09da2101d
Haganeko <haganekko@gmail.com>
2017-04-30 03:12:42 -0300

    changed progression to buff 20~40 minutes, trying to keep lower minutes' multiplier the same

--- game/scripts/vscripts/components/creeps/creep_power.lua
+++ game/scripts/vscripts/components/creeps/creep_power.lua
@@ -16,10 +16,10 @@ function CreepPower:GetPowerForMinute (minute)
 
   return {
     minute,                                   -- minute
-    (7.5 * ((minute / 100) ^ 4) + 15 * ((minute/100) ^ 3) - 0.45 * ((minute/100) ^ 2) + 0.30 * (minute/100)) + 1,   -- hp
-    (7.5 * ((minute / 100) ^ 4) + 15 * ((minute/100) ^ 3) - 0.45 * ((minute/100) ^ 2) + 0.3 * (minute/100)) + 1,   -- mana
-    (40 * ((minute / 100) ^ 4) + 80 * ((minute/100) ^ 3) - 2.4 * ((minute/100) ^ 2) + 1.6 * (minute/100)) + 1,     -- damage
-    (minute / 20) ^ 2 + minute / 7 + 1,       -- armor
+    (45 * ((minute / 100) ^ 4) - 36 * ((minute/100) ^ 3) + 21 * ((minute/100) ^ 2) - 1.5 * (minute/100)) + 1,   -- hp
+    (45 * ((minute / 100) ^ 4) - 36 * ((minute/100) ^ 3) + 21 * ((minute/100) ^ 2) - 1.5 * (minute/100)) + 1,   -- mana
+    (240 * ((minute / 100) ^ 4) - 192 * ((minute/100) ^ 3) + 112 * ((minute/100) ^ 2) - 8 * (minute/100)) + 1,     -- damage
+    (minute / 20) ^ 2 + minute / 6 + 1,       -- armor
     (minute / 2) + 1,                         -- gold
     ((21 * minute^2 - 19 * minute + 3002) / 3002) * self.numPlayersXPFactor * multFactor -- xp
   }
