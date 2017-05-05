78dca4ca1b1d5c4dcdade327221cfcb608152b75
Nikolai Zimmermann <Chronophylos@users.noreply.github.com>
2017-05-05 19:10:28 +0200

    Added "Stop fighting yourself" Boss (#600)
    * Do I have a boss?
    
    * Added more Commands
    
    Added `-noend` Command: Increase require Points for Win to 9999999
    Added `-switchhero x` Command: Change your hero
    
    * Added "Stop fighting yourself" Boss internally called `Mirror`
    Added for now incomplete Mirror AI
    Added Mirror's 'Debuff Mirror' Ability
    Added Mirror's 'Dupe Heroes' Ability
    Added Mirror's 'Dupe Items' Ability
    
    * Buffed Mirror
    
    Made Mirror Ranged
    
    * Added Mirror
    
    * Disabled Debugging Lines
    
    * Added Attack Move Order as Fallback
    
    * Mirror AI: Added ability to cast Items in Inventory (untested)
    
    * Enabled Debugging
    
    * Renamed to stopfightingyourself
    
    * Added precaching when switching heroes
    
    * Disabled Debugging
    
    * Added stopfightingyourself (I hope this is the right file)
    
    * Fixed bug where cooldown gets reset on every attack
    
    * Refractoring to make code clearer
    
    * Added Loadout command
    
    * Arc warden was missing
    
    * Typos in the renaming process
    
    * Some simple tooltips
    
    * Added Mana to allow the use of items
    
    * Add fury swipes as allowed modifier
    
    * Added `contains` as an alias for `index`
    
    * Now printing lua version
    
    * Added Commment
    
    * Fixed Table lookup
    
    * Added Mana
    
    * Fixed OutgoingDamage
    
    * Reduced Mana
    
    * Added scepter command
    
    * Added new commands to help command
    
    * Neutrals don't need a courier
    
    * Bug Fix and Additions
    
    Fixed Bug where looking at a unit with custom modifier causes crash
    Added Stat Progression
    Added max illusion value
    
    * Added Stoneskin to blacklist
    
    * Fixed using wrong parameter for creating modifier
    
    * Increased incomig Damage
    
    * Casting abilities and using items
    
    and killing illusions
    
    * Disable Debugging
    
    * Fix spassing
    
    * Gave Point casing higher priority
    
    * Reduced AttackRange
    Decreased Scale
    
    * Forgot this in here
    
    * Don't Queue
    
    * Fixing spassing
    
    * Added taunt?
    Fixed OrderType
    
    * Added blacklist for rapier
    
    * Added bug comments
    
    * Fixed Illusions not getting deleted correctly
    
    * Fixed item blacklist
    
    * cleaning up
    
    * Crappy icons
    
    * Added IsNull Check before deleting illusions
    
    * Added audiovisual effects for illusions
    
    * Added delay for particles
    
    * Added Idle check
    Removed taunt
    
    * Queues
    
    * Fixes by Trildar
    
    * Changed Purgetester to check for strong dispels
    
    * There is literally no need to use fun
    Renamed a variable
    
    * Added GC
    
    * Added Item blacklist
    
    * Fixed Precaching
    
    * Lots of fixes
    Added Phases (without phase controller for now)
    
    * Better max illusion count
    Added CastFilterResultLocation
    
    * Used wrong function
    
    * Fixed TTs

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -23,6 +23,8 @@ function Precache( context )
   PrecacheItemByNameSync("item_postactive", context)
   PrecacheItemByNameSync("item_preemptive_3c", context)
 
+  PrecacheUnitByNameSync("npc_dota_boss_stopfightingyourself", context)
+
   -- Particles can be precached individually or by folder
   -- It it likely that precaching a single particle system will precache all of its children, but this may not be guaranteed
   --PrecacheResource("particle", "particles/econ/generic/generic_aoe_explosion_sphere_1/generic_aoe_explosion_sphere_1.vpcf", context)
