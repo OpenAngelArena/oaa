63074f824f40e0e239722d395174659ed38616c1
Chris Vickery <chrisinajar@gmail.com>
2017-05-07 14:38:38 -0700

    Dont show ability icons when you cant level up yet

--- content/panorama/scripts/custom_game/abilitylevels.js
+++ content/panorama/scripts/custom_game/abilitylevels.js
@@ -1,7 +1,10 @@
+/* global GameEvents, $, FindDotaHudElement */
+
 'use strict';
 
 (function () {
   GameEvents.Subscribe('ability_level_error', DisplayAbilityLevelError);
+  GameEvents.Subscribe('check_level_up', CheckLevelUpBubbles);
 }());
 
 function DisplayAbilityLevelError (data) {
@@ -10,4 +13,22 @@ function DisplayAbilityLevelError (data) {
 
   var errorData = {reason: 80, message: errorMessageText};
   GameEvents.SendEventClientSide('dota_hud_error_message', errorData);
+}
+
+function CheckLevelUpBubbles (data) {
+  var playerID = Players.GetLocalPlayer();
+  var entID = Players.GetPlayerHeroEntityIndex(playerID);
+  var level = Players.GetLevel(playerID);
+  var canLevelUp = data.canLevelUp;
+
+  var abilitiesPanel = FindDotaHudElement('abilities');
+  abilitiesPanel.Children().forEach(function (abilityPanel, i) {
+    if (data.level < canLevelUp[i + 1]) {
+      $.Schedule(0.1, function() {
+        abilityPanel.FindChildTraverse('LevelUpTab').style.opacity = 0;
+        abilityPanel.FindChildTraverse('LevelUpLight').style.opacity = 0;
+        abilityPanel.FindChildTraverse('LevelUpBurstFXContainer').style.opacity = 0;
+      });
+    }
+  });
 }
