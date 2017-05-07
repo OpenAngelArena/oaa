88545ec54884198a25e68a5570f361df67434f67
Chris Vickery <chrisinajar@gmail.com>
2017-05-07 14:55:23 -0700

    0.25.0

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -1,6 +1,6 @@
 -- This is the entry-point to your game mode and should be used primarily to precache models/particles/sounds/etc
 
-GAME_VERSION = "0.24.3"
+GAME_VERSION = "0.25.0"
 
 require('internal/util')
 require('gamemode')
