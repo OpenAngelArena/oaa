3d341f98e8c62e43e5848f4448e768b974008d9b
Chronophylos <admiral.chronophylos@web.de>
2017-05-21 17:20:51 +0200

    Fixed indents

--- scripts/clean-files.js
+++ scripts/clean-files.js
@@ -1,6 +1,15 @@
-var fs = require('fs');
-var path = require('path');
-var chalk = require('chalk');
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const readline = require('readline');
+
+function stringRepeat (string, num) {
+  var result = '';
+  for (var i = 0; i < num; i++) {
+    result += string;
+  }
+  return result;
+}
 
 var walk = function (directoryName, action) {
   fs.readdir(directoryName, function (err, files) {
@@ -43,14 +52,31 @@ function cleanTooltipFile (file) {
 }
 
 function cleanKVFile (file) {
+  if (file.indexOf('.md') > -1) {
+    return;
+  }
+
   console.log('Cleaning ' + chalk.green(file));
 
-  fs.readFile(file, 'utf8', function (err, data) {
-    if (err) {
-      return console.log(chalk.red(err));
-    }
-    var result = data.replace(/\t/g, '  ').replace(/\r\n/g, '\n');
+  var lineReader = readline.createInterface({
+    input: fs.createReadStream(file)
+  });
 
+  var indent = 0;
+  var result = '';
+  lineReader.on('line', (line) => {
+    line = line.replace(/\t/g, '  '); // replace tabs
+    if (line.indexOf('}') > -1) {
+      indent--;
+    }
+    line = line.replace(/^ +/g, stringRepeat('  ', indent)); // fix indent
+    if (line.indexOf('{') > -1) {
+      indent++;
+    }
+    result += line + '\n';
+    // lineReader.write(line);
+  });
+  lineReader.on('close', () => {
     fs.writeFile(file, result, 'utf8', function (err) {
       if (err) {
         return console.log(chalk.red(err));
