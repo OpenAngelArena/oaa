1b7dc8b349da5b3926428f034c9183ea22708bcc
KarmaticNeutral <karmaticneutral@gmail.com>
2017-05-22 13:29:11 -0600

    Should work this way. Will get many errors

--- game/scripts/vscripts/units/twin.lua
+++ game/scripts/vscripts/units/twin.lua
@@ -2,7 +2,7 @@ LinkLuaModifier( "modifier_boss_phase_controller", "modifiers/modifier_boss_phas
 
 local ABILITY_empathy = nil
 
-function TwinThink(twin)
+function TwinThink()
   thisEntity:OnHurt(HurtHandler)
 end
 
@@ -23,9 +23,12 @@ function Spawn (entityKeyValues) --luacheck: ignore Spawn
 end
 
 function HutHandler()
-  local target = FarthestHeroInRange(thisEntity:GetAbsOrigin(), 1000)
-
-    ExecuteOrderFromTable({
+  local twin = FindTwin()
+  if twin == false
+    return
+  end
+  local target = FarthestHeroInRange(twin:GetAbsOrigin(), 1000)
+  ExecuteOrderFromTable({
     UnitIndex = thisEntity:entindex(),
     OrderType = DOTA_UNIT_ORDER_ATTACK_TARGET,
     Position = target:GetAbsOrigin(),
@@ -33,9 +36,37 @@ function HutHandler()
   })
 end
 
+local function FindTwin()
+  local unitsInRange = FindUnitsInRadius(
+    DOTA_TEAM_NEUTRALS, 
+    self:GetAbsOrigin(), 
+    nil, 
+    2000, 
+    DOTA_UNIT_TARGET_TEAM_FRIENDLY,
+    DOTA_UNIT_TARGET_HERO,
+    DOTA_UNIT_TARGET_FLAG_NONE,
+    FIND_ANY_ORDER, 
+    false
+    )
+
+  if #unitsInRange == 0
+    return false
+  end
+
+  local i = 1
+  while i < #unitsInRange
+    if unitsInRange[i]:GetUnitName() == "npc_dota_boss_twin_dumb"
+      return unitsInRange[i]
+    end
+  end
+
+  return false
+end
+
 local function FarthestHeroInRange(position, range)
   return FindUnitsInRadius(
     DOTA_TEAM_NEUTRALS,
+    position,
     nil,
     range,
     DOTA_UNIT_TARGET_TEAM_ENEMY,
