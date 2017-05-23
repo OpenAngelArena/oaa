c845fdc09cea9ed0cd96fdb8a4c9544ee1e8e8a0
KarmaticNeutral <karmaticneutral@gmail.com>
2017-05-23 15:52:08 -0600

    Potential up preduel time w/o upping farm time (#835)
    * Potential up preduel w/ no farmtime
    
    * FIXED!
    
    FIXED!

--- game/scripts/vscripts/settings.lua
+++ game/scripts/vscripts/settings.lua
@@ -7,7 +7,7 @@ ALLOW_SAME_HERO_SELECTION = false       -- Should we let people select the same 
 
 CUSTOM_GAME_SETUP_TIME = 30.0           -- How long to show custom game setup? 0 disables
 HERO_SELECTION_TIME = 30.0              -- How long should we let people select their hero?
-PRE_GAME_TIME = 5.0                     -- How long after people select their heroes should the horn blow and the game start?
+PRE_GAME_TIME = 10.0                     -- How long after people select their heroes should the horn blow and the game start?
 POST_GAME_TIME = 60.0                   -- How long should we let people look at the scoreboard before closing the server automatically?
 TREE_REGROW_TIME = 60.0                 -- How long should it take individual trees to respawn after being cut down/destroyed?
 
