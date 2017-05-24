e04c1ce79454e2b44ce6f0cd278997e91a2203b2
Chris Vickery <chrisinajar@gmail.com>
2017-05-24 15:21:20 -0700

    0.31.0

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -1,6 +1,6 @@
 -- This is the entry-point to your game mode and should be used primarily to precache models/particles/sounds/etc
 
-GAME_VERSION = "0.30.1"
+GAME_VERSION = "0.31.0"
 CustomNetTables:SetTableValue("info", "version", { value = GAME_VERSION })
 
 require('internal/util')
