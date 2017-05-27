fcf5d9ca6a486c411613459390d6947f02eb9902
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-05-27 16:13:02 +0800

    Remove LinkLuaModifier for modifier_item_satanic_unholy
    LinkLuaModifier is unnecessary for built-in modifiers

--- game/scripts/vscripts/items/satanic_core.lua
+++ game/scripts/vscripts/items/satanic_core.lua
@@ -1,6 +1,5 @@
 LinkLuaModifier( "modifier_octarine_vampirism_buff", "modifiers/modifier_octarine_vampirism_buff.lua", LUA_MODIFIER_MOTION_NONE )
 LinkLuaModifier( "modifier_item_satanic_core", "items/satanic_core.lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_item_satanic_unholy", LUA_MODIFIER_MOTION_NONE )
 
 --------------------------------------------------------------------------------
 
