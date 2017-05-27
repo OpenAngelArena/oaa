7259122d6cdb6099f3839625b73c3de408f7227a
Haganeko <haganekko@gmail.com>
2017-05-27 17:18:22 -0300

    redistributed XP to account for creep spawn chance changes

--- game/scripts/vscripts/components/creeps/creep_types.lua
+++ game/scripts/vscripts/components/creeps/creep_types.lua
@@ -4,20 +4,20 @@ CreepTypes = {
   -- 1 "easy camp"
   {
     {                                              --HP   MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_kobold",                   280,    0,  10,   0.5,   14,  70}, --expected gold is  46 and XP is 240
-      {"npc_dota_neutral_kobold_tunneler",          480,    0,  12,    1,    22, 120}
+      {"npc_dota_neutral_kobold",                   280,    0,  10,   0.5,   16,  80}, --expected gold is  46 and XP is 240
+      {"npc_dota_neutral_kobold_tunneler",          480,    0,  12,    1,    25, 130}
     },
     {
-      {"npc_dota_neutral_kobold_taskmaster",        560,    0,  16,    1,    26, 140},
-      {"npc_dota_neutral_kobold",                   280,    0,  10,   0.5,   14,  70}
-    },
-    {                                              --HP   MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_kobold",                   280,    0,  10,   0.5,   14,  70}, --expected gold is  46 and XP is 240
-      {"npc_dota_neutral_kobold_tunneler",          480,    0,  12,    1,    22, 120}
+      {"npc_dota_neutral_kobold_taskmaster",        560,    0,  16,    1,    27, 150},
+      {"npc_dota_neutral_kobold",                   280,    0,  10,   0.5,   16,  80}
     },
     {
-      {"npc_dota_neutral_kobold_taskmaster",        560,    0,  16,    1,    26, 140},
-      {"npc_dota_neutral_kobold",                   280,    0,  10,   0.5,   14,  70}
+      {"npc_dota_neutral_kobold",                   280,    0,  10,   0.5,   16,  80},
+      {"npc_dota_neutral_kobold_tunneler",          480,    0,  12,    1,    25, 130}
+    },
+    {
+      {"npc_dota_neutral_kobold_taskmaster",        560,    0,  16,    1,    27, 150},
+      {"npc_dota_neutral_kobold",                   280,    0,  10,   0.5,   16,  80}
     },
     {
       {"npc_dota_neutral_ghost",                    480,    0,  12,    1,    31, 160},
@@ -31,7 +31,7 @@ CreepTypes = {
       {"npc_dota_neutral_harpy_storm",              560,  320,  24,    2,    35,  175},
       {"npc_dota_neutral_harpy_scout",              440,    0,  40,    1,    26,   75},
     },
-    {                                              --HP   MANA  DMG   ARM   GOLD  EXP
+    {
       {"npc_dota_neutral_harpy_storm",              560,  320,  24,    2,    35,  175}
     },
     {
@@ -45,30 +45,30 @@ CreepTypes = {
       {"npc_dota_neutral_centaur_khan",         1600, 400,   44,   3,   100,   120}, --expected gold is 168 and XP is 240
       {"npc_dota_neutral_centaur_outrunner",    1200, 240,   28,   2,    68,   120},
     },
-    {                                          --HP   MANA   DMG   ARM  GOLD   EXP
-      {"npc_dota_neutral_centaur_khan",         1600, 400,   44,   3,   100,   120},
-      {"npc_dota_neutral_centaur_outrunner",    1200, 240,   28,   2,    68,   120},
-    },
-    {                                          --HP   MANA   DMG   ARM  GOLD   EXP
-      {"npc_dota_neutral_centaur_khan",         1600, 400,   44,   3,   100,   120}, --expected gold is 168 and XP is 240
-      {"npc_dota_neutral_centaur_outrunner",    1200, 240,   28,   2,    68,   120},
-    },
-    {                                          --HP   MANA   DMG   ARM  GOLD   EXP
+    {
       {"npc_dota_neutral_centaur_khan",         1600, 400,   44,   3,   100,   120},
       {"npc_dota_neutral_centaur_outrunner",    1200, 240,   28,   2,    68,   120},
     },
     {
-      {"npc_dota_neutral_giant_wolf",            640, 160,   24,   3,    32,    88},
-      {"npc_dota_neutral_giant_wolf",            640, 160,   24,   3,    32,    88},
-      {"npc_dota_neutral_alpha_wolf",           1200, 480,   56,   5,   120,   108}
+      {"npc_dota_neutral_centaur_khan",         1600, 400,   44,   3,   100,   120},
+      {"npc_dota_neutral_centaur_outrunner",    1200, 240,   28,   2,    68,   120},
     },
     {
-      {"npc_dota_neutral_giant_wolf",            640, 160,   24,   3,    32,    88},
-      {"npc_dota_neutral_alpha_wolf",           1200, 480,   56,   5,   120,   108}
+      {"npc_dota_neutral_centaur_khan",         1600, 400,   44,   3,   100,   120},
+      {"npc_dota_neutral_centaur_outrunner",    1200, 240,   28,   2,    68,   120},
     },
     {
-      {"npc_dota_neutral_giant_wolf",            640, 160,   24,   3,    32,    88},
-      {"npc_dota_neutral_alpha_wolf",           1200, 480,   56,   5,   120,   108}
+      {"npc_dota_neutral_giant_wolf",            640, 160,   24,   3,    36,    90},
+      {"npc_dota_neutral_giant_wolf",            640, 160,   24,   3,    36,    90},
+      {"npc_dota_neutral_alpha_wolf",           1200, 480,   56,   5,   120,   120}
+    },
+    {
+      {"npc_dota_neutral_giant_wolf",            640, 160,   24,   3,    36,    90},
+      {"npc_dota_neutral_alpha_wolf",           1200, 480,   56,   5,   120,   120}
+    },
+    {
+      {"npc_dota_neutral_giant_wolf",            640, 160,   24,   3,    36,    90},
+      {"npc_dota_neutral_alpha_wolf",           1200, 480,   56,   5,   120,   120}
     },
     {
       {"npc_dota_neutral_custom_black_dragon",  1840,   0,   80,   3,   168,   240},
