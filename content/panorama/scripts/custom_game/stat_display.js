cd7007377a56847cb8739d3da5b9061c15b7ad58
Chronophylos <admiral.chronophylos@web.de>
2017-06-03 18:03:57 +0200

    UI Fixes Stopped Stat Display yelling around Move Version Display to the left and adjusted colors Removed Battle Pass Towers (not tested)

--- content/panorama/scripts/custom_game/stat_display.js
+++ content/panorama/scripts/custom_game/stat_display.js
@@ -1,22 +1,22 @@
-/* global $ CustomNetTables, Game */
+/* global $ CustomNetTables Game */
 
 (function () {
   CustomNetTables.SubscribeNetTableListener('stat_display', onStatChange);
 }());
 
 function onStatChange (table, key, data) {
-  for (var entry in data.value) {
+  /* for (var entry in data.value) {
     $.Msg(entry);
-  }
+  } */
   var playerID = Game.GetLocalPlayerID();
-  $.Msg('onStatChange:');
-  $.Msg(playerID + ' : ' + key + ' = ' + JSON.stringify(data.value, null, 2));
+  // $.Msg('onStatChange:');
+  // $.Msg(playerID + ' : ' + key + ' = ' + JSON.stringify(data.value, null, 2));
   UpdateStatDisplay(key, data.value[playerID] || 0);
 }
 
 function UpdateStatDisplay (name, value) {
   var display = $('#OAAStatDisplay');
 
-  $.Msg('Looking for QuickStatLabelValue of ' + name + 'Row');
+  // $.Msg('Looking for QuickStatLabelValue of ' + name + 'Row');
   display.FindChildTraverse(name + 'Row').FindChildTraverse('QuickStatLabelValue').text = value;
 }
