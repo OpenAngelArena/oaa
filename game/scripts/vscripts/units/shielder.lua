2f2cb33311fa88100c86d2f2a1b6a71616d70648
KarmaticNeutral <karmaticneutral@gmail.com>
2017-06-04 21:39:58 -0600

    Update shielder.lua

--- game/scripts/vscripts/units/shielder.lua
+++ game/scripts/vscripts/units/shielder.lua
@@ -9,10 +9,10 @@ function Spawn (entityKeyValues) --luacheck: ignore Spawn
   ABILITY_shield = thisEntity:FindAbilityByName("boss_shielder_shield")
 
 local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_spectre/spectre_desolate.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
-ParticleManager:SetParticleControl(particle, 0, target:GetAbsOrigin())
-ParticleManager:SetParticleControl(particle, 1, target:GetAbsOrigin())
-ParticleManager:SetParticleControl(particle, 2, target:GetAbsOrigin())
-ParticleManager:SetParticleControl(particle, 4, target:GetAbsOrigin())
+ParticleManager:SetParticleControl(particle, 0, self:GetAbsOrigin())
+ParticleManager:SetParticleControl(particle, 1, self:GetAbsOrigin())
+ParticleManager:SetParticleControl(particle, 2, self:GetAbsOrigin())
+ParticleManager:SetParticleControl(particle, 4, self:GetAbsOrigin())
 ParticleManager:SetParticleControl(particle, 5, Vector(nil,0,0))
 
   local phaseController = thisEntity:AddNewModifier(thisEntity, ABILITY_shield, "modifier_boss_phase_controller", {})
