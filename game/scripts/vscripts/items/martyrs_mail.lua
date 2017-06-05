e3f78df75fdcd2d58d83d7a09992579f44c55201
Nikolai Zimmermann <Chronophylos@users.noreply.github.com>
2017-06-05 18:39:18 +0200

    Fix Martyr's Mail Infinite Echo Damage
    I think that should fix #1029

--- game/scripts/vscripts/items/martyrs_mail.lua
+++ game/scripts/vscripts/items/martyrs_mail.lua
@@ -120,6 +120,11 @@ end
 function modifier_item_martyrs_mail_martyr_active:OnTakeDamage( kv )
 	if IsServer() then
 		local hCaster = self:GetParent()
+    
+    -- Prevent Infinite Loop
+    if kv.attacker == hCaster then
+      return
+    end
 
 		if kv.unit == hCaster then
 			local damageTable = {
