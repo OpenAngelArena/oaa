0c23cba7e9790acec7175e4f6d2a17aabe363387
Chronophylos <admiral.chronophylos@web.de>
2017-06-08 22:12:43 +0200

    Polish

--- game/scripts/vscripts/abilities/oaa_bristleback.lua
+++ game/scripts/vscripts/abilities/oaa_bristleback.lua
@@ -74,7 +74,7 @@ function bristleback_takedamage(params)
       end
     else
       -- Check if the reduced damage is lethal
-      if((params.unit:GetHealth() - (params.Damage * (1 - side_reduction_percentage))) >= 1) then
+      if (params.unit:GetHealth() - (params.Damage * (1 - side_reduction_percentage))) >= 1 then
         -- This is the actual "damage reduction".
         params.unit:SetHealth((params.Damage * side_reduction_percentage) + params.unit:GetHealth())
         -- Play the sound on Bristleback.
@@ -90,12 +90,12 @@ function bristleback_takedamage(params)
     end
   end
 
-  -- If the amount of damage taken since the last Quill Spray proc is equal to or exceeds what's defined as the threshold, release a Quill Spray.
-  if params.unit.quill_threshold_counter >= ability:GetSpecialValueFor("quill_release_threshold") then
-    -- This should be Quill Spray, but in case something weird like AD is going on, we'll check anyway.
-    local ability = params.unit:FindAbilityByName("bristleback_bristleback_oaa")
+  local quill_spray = params.unit:FindAbilityByName("bristleback_quill_spray")
 
-    ability:CastAbility()
+  -- If the amount of damage taken since the last Quill Spray proc is equal to or exceeds what's defined as the threshold, release a Quill Spray.
+  if params.unit.quill_threshold_counter >= ability:GetSpecialValueFor("quill_release_threshold") and quill_spray:IsCooldownReady() then
+    -- This should be Quill Spray, but in case something weird like AD is going on, we'll check anyway.
+    quill_spray:CastAbility()
 
     -- I'm not entirely sure if this is how Bristleback actually works, but this seems like a safe bet.
     params.unit.quill_threshold_counter = params.unit.quill_threshold_counter - 250.0
