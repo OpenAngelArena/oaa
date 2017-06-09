3c815f0ea8f2e0288c3773e49bdef17be870716f
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-06-10 02:32:41 +0800

    Move passive stats out of modifier_octarine_vampirism_applier

--- game/scripts/vscripts/modifiers/modifier_octarine_vampirism_applier.lua
+++ game/scripts/vscripts/modifiers/modifier_octarine_vampirism_applier.lua
@@ -2,43 +2,6 @@
 
 modifier_octarine_vampirism_applier = class(ModifierBaseClass)
 
-function modifier_octarine_vampirism_applier:DeclareFunctions()
-  return {
-    MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
-    MODIFIER_PROPERTY_HEALTH_BONUS,
-    MODIFIER_PROPERTY_MANA_BONUS,
-    MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE,
-    MODIFIER_PROPERTY_MANA_REGEN_PERCENTAGE,
-    MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT
-  }
-end
-
-function modifier_octarine_vampirism_applier:GetModifierConstantHealthRegen()
-  return self:GetAbility():GetSpecialValueFor('bonus_health_regen')
-end
-
-function modifier_octarine_vampirism_applier:GetModifierPercentageManaRegen()
-  return self:GetAbility():GetSpecialValueFor('bonus_mana_regen')
-end
-
-function modifier_octarine_vampirism_applier:GetModifierBonusStats_Intellect()
-  return self:GetAbility():GetSpecialValueFor('bonus_intelligence')
-end
-
-function modifier_octarine_vampirism_applier:GetModifierHealthBonus()
-  return self:GetAbility():GetSpecialValueFor('bonus_health')
-end
-
-function modifier_octarine_vampirism_applier:GetModifierManaBonus()
-  return self:GetAbility():GetSpecialValueFor('bonus_mana')
-end
-
-function modifier_octarine_vampirism_applier:GetModifierPercentageCooldown()
-  return self:GetAbility():GetSpecialValueFor('bonus_cooldown')
-end
-
---------------------------------------------------------------------------------
-
 function modifier_octarine_vampirism_applier:IsHidden()
   return true
 end
