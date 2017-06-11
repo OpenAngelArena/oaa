f4af3581e14366c8283d1c77f6fc61f980679f46
Chris Vickery <chrisinajar@gmail.com>
2017-06-10 23:54:41 -0700

    Stub functionality

--- game/scripts/vscripts/components/gold/hero_kills.lua
+++ game/scripts/vscripts/components/gold/hero_kills.lua
@@ -6,4 +6,32 @@ if HeroKillGold == nil then
 end
 
 function HeroKillGold:Init()
+  GameEvents:OnHeroKilled(partial(self.HeroDeathHandler, self))
+end
+
+function HeroKillGold:HeroDeathHandler (keys)
+  -- points code for reference
+  -- if keys.killer:GetTeam() ~= keys.killed:GetTeam() and not keys.killed:IsReincarnating() and keys.killed:GetTeam() ~= DOTA_TEAM_NEUTRALS then
+  --   self:AddPoints(keys.killer:GetTeam())
+  -- end
+  if keys.killer:GetTeam() == keys.killed:GetTeam() then
+    return
+  end
+  if keys.killed:IsReincarnating() then
+    return
+  end
+  if keys.killed:GetTeam() == DOTA_TEAM_NEUTRALS or keys.killer:GetTeam() == DOTA_TEAM_NEUTRALS then
+    return
+  end
+
+  local killerPlayer = keys.killer:GetPlayer()
+  if not killerPlayer then
+    return
+  end
+  local killedPlayer = keys.killed:GetPlayer()
+  if not killedPlayer then
+    return
+  end
+
+  DebugPrint('')
 end
