d72d1595d4825192d846dbd2c067a6186922e3fd
Nikolai Zimmermann <Chronophylos@users.noreply.github.com>
2017-06-11 02:44:13 +0200

    Rewrote Dagon and Added Spell Amp (#1093)
    * Rewrote Dagon and Added Spell Amp
    Added more Stats to Modifier Generic Bonus
    
    * Added `.lua` to `ScriptFile` Values
    
    * Fixed Spell Amp Level Count and Fixed Level 2+ not having Spell Amp
    
    * Fixed Tooltips
    
    * Now using imaGecko's Spell Amp Values
    
    * Freedom for Particle Indicies
    
    * Tooltips again
    
    * Made `DOTA_Item.Dagon5.Target` play for lvl 5 and above
    Thanks LuaCheck for saving me

--- game/scripts/vscripts/items/dev_dagon.lua
+++ game/scripts/vscripts/items/dev_dagon.lua
@@ -10,12 +10,12 @@ function item_devDagon:OnSpellStart()
 
   target:Kill(self, caster)
 
-  local particle1Name = "particles/items_fx/dagon.vpcf"
-  local particle1 = ParticleManager:CreateParticle(particle1Name, PATTACH_POINT_FOLLOW, caster)
-  ParticleManager:SetParticleControlEnt(particle1, 0, caster, PATTACH_POINT_FOLLOW, "attach_attack1", caster:GetOrigin(), true)
-  ParticleManager:SetParticleControlEnt(particle1, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetOrigin(), true)
-  ParticleManager:SetParticleControl(particle1, 2, Vector(2000, 0, 0))
-  ParticleManager:ReleaseParticleIndex(particle1)
+  local particleName = "particles/items_fx/dagon.vpcf"
+  local particle = ParticleManager:CreateParticle(particleName, PATTACH_POINT_FOLLOW, caster)
+  ParticleManager:SetParticleControlEnt(particle, 0, caster, PATTACH_POINT_FOLLOW, "attach_attack1", caster:GetOrigin(), true)
+  ParticleManager:SetParticleControlEnt(particle, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetOrigin(), true)
+  ParticleManager:SetParticleControl(particle, 2, Vector(2000))
 
-  EmitSoundOn("DOTA_Item.Dagon5.Target", target)
+  caster:EmitSound("DOTA_Item.Dagon.Activate")
+  target:EmitSound("DOTA_Item.Dagon5.Target")
 end
