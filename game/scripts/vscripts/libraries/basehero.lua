52e72d55395062faba9a0ddc966047da319eee59
Chris Vickery <chrisinajar@gmail.com>
2017-06-15 13:37:11 -0700

    Replace hero function with gold call API in case someone uses it

--- game/scripts/vscripts/libraries/basehero.lua
+++ game/scripts/vscripts/libraries/basehero.lua
@@ -2,3 +2,7 @@
 function CDOTA_BaseNPC_Hero:GetNetworth ()
   return GetNetworth(self)
 end
+
+function CDOTA_BaseNPC_Hero:ModifyGold (playerID, goldAmmt, reliable, nReason)
+  return Gold:ModifyGold(playerID, goldAmmt, reliable, nReason)
+end
