96a027cbe6493ca55cc38b46031b0d04e3919e3b
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-06-18 02:58:53 +0800

    Implement splinter shot Greater Phase Boots

--- game/scripts/vscripts/items/farming/greater_phase_boots.lua
+++ game/scripts/vscripts/items/farming/greater_phase_boots.lua
@@ -1,14 +1,23 @@
 item_greater_phase_boots = class(ItemBaseClass)
 
-LinkLuaModifier( "modifier_item_greater_phase_boots_active", "items/farming/greater_phase_boots.lua", LUA_MODIFIER_MOTION_NONE )
+--LinkLuaModifier( "modifier_item_greater_phase_boots_active", "items/farming/greater_phase_boots.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_item_greater_phase_boots_splinter_shot", "items/farming/greater_phase_boots.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_intrinsic_multiplexer", "modifiers/modifier_intrinsic_multiplexer.lua", LUA_MODIFIER_MOTION_NONE)
 
 --------------------------------------------------------------------------------
 
 function item_greater_phase_boots:GetIntrinsicModifierName()
-	-- we're not modifying the passive benefits at all
-	-- ( besides the numbers )
-	-- so we can just reuse the normal phase boot modifier
-	return "modifier_item_phase_boots"
+	return "modifier_intrinsic_multiplexer"
+end
+
+function item_greater_phase_boots:GetIntrinsicModifierNames()
+  return {
+    -- we're not modifying the passive benefits at all
+    -- ( besides the numbers )
+    -- so we can just reuse the normal phase boot modifier
+    "modifier_item_phase_boots",
+    "modifier_item_greater_phase_boots_splinter_shot"
+  }
 end
 
 --------------------------------------------------------------------------------
@@ -19,174 +28,275 @@ function item_greater_phase_boots:OnSpellStart()
 	-- play the sound
 	caster:EmitSound( "DOTA_Item.PhaseBoots.Activate" )
 
-	-- add the new phase modifier
-	caster:AddNewModifier( caster, self, "modifier_item_greater_phase_boots_active", { duration = self:GetSpecialValueFor( "phase_duration" ) } )
+	-- add the vanilla phase active modifier
+	caster:AddNewModifier( caster, self, "modifier_item_phase_boots_active", { duration = self:GetSpecialValueFor( "phase_duration" ) } )
+end
+
+function item_greater_phase_boots:OnProjectileHit(target, location)
+  if target and not target:IsNull() then
+    local caster = self:GetCaster()
+    -- Make the modifier reduce damage for the attack
+    self.splinterMod.doReduction = true
+    caster:PerformAttack(target, true, true, true, false, false, false, false)
+
+    -- Reset the damage reduction after the attack is done
+    self.splinterMod.doReduction = false
+  end
 end
 
 --------------------------------------------------------------------------------
 
-modifier_item_greater_phase_boots_active = class(ModifierBaseClass)
+modifier_item_greater_phase_boots_splinter_shot = class(ModifierBaseClass)
 
---------------------------------------------------------------------------------
-
-function modifier_item_greater_phase_boots_active:IsHidden()
-	return false
+function modifier_item_greater_phase_boots_splinter_shot:IsHidden()
+  return true
 end
 
-function modifier_item_greater_phase_boots_active:IsDebuff()
-	return false
+function modifier_item_greater_phase_boots_splinter_shot:IsPurgable()
+  return false
 end
 
-function modifier_item_greater_phase_boots_active:IsPurgable()
-	return false
+function modifier_item_greater_phase_boots_splinter_shot:DeclareFunctions()
+  return {
+    MODIFIER_EVENT_ON_ATTACK_LANDED,
+    MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE
+  }
 end
 
---------------------------------------------------------------------------------
-
-function modifier_item_greater_phase_boots_active:GetEffectName()
-	return "particles/items2_fx/phase_boots.vpcf"
+function modifier_item_greater_phase_boots_splinter_shot:OnCreated()
+  if IsServer() then
+    self:GetAbility().splinterMod = self
+  end
 end
 
---------------------------------------------------------------------------------
+modifier_item_greater_phase_boots_splinter_shot.OnRefresh = modifier_item_greater_phase_boots_splinter_shot.OnCreated
 
-function modifier_item_greater_phase_boots_active:OnCreated( event )
-	local spell = self:GetAbility()
+function modifier_item_greater_phase_boots_splinter_shot:OnAttackLanded(keys)
+  local parent = self:GetParent()
+  if keys.attacker == parent and not keys.no_attack_cooldown then
+    local ability = self:GetAbility()
 
-	self.moveSpd = spell:GetSpecialValueFor( "phase_movement_speed" )
-	self.moveSpdRange = spell:GetSpecialValueFor( "phase_movement_speed_range" )
-	self.dmgReduction = spell:GetSpecialValueFor( "phase_attack_outgoing" ) - 100
+    local units = FindUnitsInRadius(
+      parent:GetTeamNumber(),
+      keys.target:GetAbsOrigin(),
+      nil,
+      ability:GetSpecialValueFor("splinter_radius"),
+      DOTA_UNIT_TARGET_TEAM_ENEMY,
+      DOTA_UNIT_TARGET_ALL,
+      bit.bor(DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE, DOTA_UNIT_TARGET_FLAG_NO_INVIS),
+      FIND_ANY_ORDER,
+      false
+    )
+    -- Exclude the original attack target from list of units to splinter to
+    table.remove(units, index(keys.target, units))
+    -- Take the first splinter_number units to split to
+    units = take_n(ability:GetSpecialValueFor("splinter_number"), units)
 
-	if IsServer() then
-		-- set up the table that stores the targets already hit
-		self.hitTargets = {}
+    -- Default to Drow Ranger's projectile
+    local projectileName = "particles/units/heroes/hero_drow/drow_base_attack.vpcf"
+    local projectileSpeed = 1250
 
-		-- start thinking
-		-- we call OnIntervalThink to make it so it can go into effect immediately
-		-- as StartIntervalThink waits for the interval to pass first
-		self:OnIntervalThink()
-		self:StartIntervalThink( 1 / 30 )
-	end
+    if parent:IsRangedAttacker() then
+      projectileName = parent:GetRangedProjectileName()
+      projectileSpeed = parent:GetProjectileSpeed()
+    end
+
+    local function CreateSplinterProjectile(target)
+      local projectileData = {
+        Target = target,
+        Ability = ability,
+        EffectName = projectileName,
+        iMoveSpeed = projectileSpeed,
+        vSourceLoc = keys.target:GetAbsOrigin(),
+        bDrawsOnMinimap = false,
+        bDodgeable = true,
+        bIsAttack = true,
+        bVisibleToEnemies = true,
+        bReplaceExisting = false,
+        bProvidesVision = false
+      }
+      ProjectileManager:CreateTrackingProjectile(projectileData)
+    end
+
+    foreach(CreateSplinterProjectile, units)
+  end
 end
 
---------------------------------------------------------------------------------
+function modifier_item_greater_phase_boots_splinter_shot:GetModifierDamageOutgoing_Percentage( event )
+ local spell = self:GetAbility()
+ local parent = self:GetParent()
 
-function modifier_item_greater_phase_boots_active:OnRefresh( event )
-	local spell = self:GetAbility()
+ if self.doReduction then
+   return spell:GetSpecialValueFor( "splinter_attack_outgoing" ) - 100
+ end
 
-	self.moveSpd = spell:GetSpecialValueFor( "phase_movement_speed" )
-	self.moveSpdRange = spell:GetSpecialValueFor( "phase_movement_speed_range" )
-	self.dmgReduction = spell:GetSpecialValueFor( "phase_attack_outgoing" ) - 100
-
-	if IsServer() then
-		-- clear the tagets hit table on refresh
-		self.hitTargets = {}
-	end
-end
-
---------------------------------------------------------------------------------
-
-if IsServer() then
-	function modifier_item_greater_phase_boots_active:HasHitUnit( target )
-		for _, unit in pairs( self.hitTargets ) do
-			if unit == target then
-				return true
-			end
-		end
-
-		return false
-	end
-
---------------------------------------------------------------------------------
-
-	function modifier_item_greater_phase_boots_active:OnIntervalThink()
-		local parent = self:GetParent()
-		local spell = self:GetAbility()
-
-		-- find all enemy creeps in range
-		local units = FindUnitsInRadius(
-			parent:GetTeamNumber(),
-			parent:GetAbsOrigin(),
-			nil,
-			spell:GetSpecialValueFor( "phase_radius" ),
-			DOTA_UNIT_TARGET_TEAM_ENEMY,
-			DOTA_UNIT_TARGET_BASIC,
-			DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
-			FIND_ANY_ORDER,
-			false
-		)
-
-		-- tell this modifier to reduce the damage of any further attacks
-		-- aka, the instant attacks
-		self.doReduction = true
-
-		for _, unit in pairs( units ) do
-			-- we don't hit units that have already been hit by this cast
-			-- or those that are controlled by a player
-			if not unit:IsControllableByAnyPlayer() and not self:HasHitUnit( unit ) then
-				-- add the unit to the targets hit list
-				table.insert( self.hitTargets, unit )
-
-				-- do an instant attack with no projectile that applies procs
-				parent:PerformAttack( unit, true, true, true, false, false, false, true )
-
-				-- play the particle
-				local part = ParticleManager:CreateParticle( "particles/items/phase_divehit.vpcf", PATTACH_ABSORIGIN, unit )
-				ParticleManager:SetParticleControlEnt( part, 1, unit, PATTACH_POINT, "attach_hitloc", unit:GetAbsOrigin(), true )
-				ParticleManager:ReleaseParticleIndex( part )
-			end
-		end
-
-		-- undo the damage reduction, so it doesn't leak into actual attacks
-		self.doReduction = false
-	end
+ return 0
 end
 
 --------------------------------------------------------------------------------
+-- Old mini-Shukuchi Greater Phase Boots effect
+-- modifier_item_greater_phase_boots_active = class(ModifierBaseClass)
 
-function modifier_item_greater_phase_boots_active:CheckState()
-	local state = {
-		[MODIFIER_STATE_NO_UNIT_COLLISION] = true,
-	}
+-- --------------------------------------------------------------------------------
 
-	return state
-end
+-- function modifier_item_greater_phase_boots_active:IsHidden()
+-- 	return false
+-- end
 
---------------------------------------------------------------------------------
+-- function modifier_item_greater_phase_boots_active:IsDebuff()
+-- 	return false
+-- end
 
-function modifier_item_greater_phase_boots_active:DeclareFunctions()
-	local funcs = {
-		MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
-		MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE,
-	}
+-- function modifier_item_greater_phase_boots_active:IsPurgable()
+-- 	return false
+-- end
 
-	return funcs
-end
+-- --------------------------------------------------------------------------------
 
---------------------------------------------------------------------------------
+-- function modifier_item_greater_phase_boots_active:GetEffectName()
+-- 	return "particles/items2_fx/phase_boots.vpcf"
+-- end
 
-function modifier_item_greater_phase_boots_active:GetModifierMoveSpeedBonus_Percentage( event )
-	local spell = self:GetAbility()
-	local parent = self:GetParent()
+-- --------------------------------------------------------------------------------
 
-	if parent:IsRangedAttacker() then
-		return self.moveSpdRange or spell:GetSpecialValueFor( "phase_movement_speed_range" )
-	end
+-- function modifier_item_greater_phase_boots_active:OnCreated( event )
+-- 	local spell = self:GetAbility()
 
-	return self.moveSpd or spell:GetSpecialValueFor( "phase_movement_speed" )
-end
+-- 	self.moveSpd = spell:GetSpecialValueFor( "phase_movement_speed" )
+-- 	self.moveSpdRange = spell:GetSpecialValueFor( "phase_movement_speed_range" )
+-- 	self.dmgReduction = spell:GetSpecialValueFor( "phase_attack_outgoing" ) - 100
 
---------------------------------------------------------------------------------
+-- 	if IsServer() then
+-- 		-- set up the table that stores the targets already hit
+-- 		self.hitTargets = {}
 
-function modifier_item_greater_phase_boots_active:GetModifierDamageOutgoing_Percentage( event )
-	local spell = self:GetAbility()
-	local parent = self:GetParent()
+-- 		-- start thinking
+-- 		-- we call OnIntervalThink to make it so it can go into effect immediately
+-- 		-- as StartIntervalThink waits for the interval to pass first
+-- 		self:OnIntervalThink()
+-- 		self:StartIntervalThink( 1 / 30 )
+-- 	end
+-- end
 
-	if self.doReduction then
-		return self.dmgReduction or spell:GetSpecialValueFor( "phase_attack_outgoing" ) - 100
-	end
+-- --------------------------------------------------------------------------------
 
-	return 0
-end
+-- function modifier_item_greater_phase_boots_active:OnRefresh( event )
+-- 	local spell = self:GetAbility()
+
+-- 	self.moveSpd = spell:GetSpecialValueFor( "phase_movement_speed" )
+-- 	self.moveSpdRange = spell:GetSpecialValueFor( "phase_movement_speed_range" )
+-- 	self.dmgReduction = spell:GetSpecialValueFor( "phase_attack_outgoing" ) - 100
+
+-- 	if IsServer() then
+-- 		-- clear the tagets hit table on refresh
+-- 		self.hitTargets = {}
+-- 	end
+-- end
+
+-- --------------------------------------------------------------------------------
+
+-- if IsServer() then
+-- 	function modifier_item_greater_phase_boots_active:HasHitUnit( target )
+-- 		for _, unit in pairs( self.hitTargets ) do
+-- 			if unit == target then
+-- 				return true
+-- 			end
+-- 		end
+
+-- 		return false
+-- 	end
+
+-- --------------------------------------------------------------------------------
+
+-- 	function modifier_item_greater_phase_boots_active:OnIntervalThink()
+-- 		local parent = self:GetParent()
+-- 		local spell = self:GetAbility()
+
+-- 		-- find all enemy creeps in range
+-- 		local units = FindUnitsInRadius(
+-- 			parent:GetTeamNumber(),
+-- 			parent:GetAbsOrigin(),
+-- 			nil,
+-- 			spell:GetSpecialValueFor( "phase_radius" ),
+-- 			DOTA_UNIT_TARGET_TEAM_ENEMY,
+-- 			DOTA_UNIT_TARGET_BASIC,
+-- 			DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
+-- 			FIND_ANY_ORDER,
+-- 			false
+-- 		)
+
+-- 		-- tell this modifier to reduce the damage of any further attacks
+-- 		-- aka, the instant attacks
+-- 		self.doReduction = true
+
+-- 		for _, unit in pairs( units ) do
+-- 			-- we don't hit units that have already been hit by this cast
+-- 			-- or those that are controlled by a player
+-- 			if not unit:IsControllableByAnyPlayer() and not self:HasHitUnit( unit ) then
+-- 				-- add the unit to the targets hit list
+-- 				table.insert( self.hitTargets, unit )
+
+-- 				-- do an instant attack with no projectile that applies procs
+-- 				parent:PerformAttack( unit, true, true, true, false, false, false, true )
+
+-- 				-- play the particle
+-- 				local part = ParticleManager:CreateParticle( "particles/items/phase_divehit.vpcf", PATTACH_ABSORIGIN, unit )
+-- 				ParticleManager:SetParticleControlEnt( part, 1, unit, PATTACH_POINT, "attach_hitloc", unit:GetAbsOrigin(), true )
+-- 				ParticleManager:ReleaseParticleIndex( part )
+-- 			end
+-- 		end
+
+-- 		-- undo the damage reduction, so it doesn't leak into actual attacks
+-- 		self.doReduction = false
+-- 	end
+-- end
+
+-- --------------------------------------------------------------------------------
+
+-- function modifier_item_greater_phase_boots_active:CheckState()
+-- 	local state = {
+-- 		[MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+-- 	}
+
+-- 	return state
+-- end
+
+-- --------------------------------------------------------------------------------
+
+-- function modifier_item_greater_phase_boots_active:DeclareFunctions()
+-- 	local funcs = {
+-- 		MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE,
+-- 		MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE,
+-- 	}
+
+-- 	return funcs
+-- end
+
+-- --------------------------------------------------------------------------------
+
+-- function modifier_item_greater_phase_boots_active:GetModifierMoveSpeedBonus_Percentage( event )
+-- 	local spell = self:GetAbility()
+-- 	local parent = self:GetParent()
+
+-- 	if parent:IsRangedAttacker() then
+-- 		return self.moveSpdRange or spell:GetSpecialValueFor( "phase_movement_speed_range" )
+-- 	end
+
+-- 	return self.moveSpd or spell:GetSpecialValueFor( "phase_movement_speed" )
+-- end
+
+-- --------------------------------------------------------------------------------
+
+-- function modifier_item_greater_phase_boots_active:GetModifierDamageOutgoing_Percentage( event )
+-- 	local spell = self:GetAbility()
+-- 	local parent = self:GetParent()
+
+-- 	if self.doReduction then
+-- 		return self.dmgReduction or spell:GetSpecialValueFor( "phase_attack_outgoing" ) - 100
+-- 	end
+
+-- 	return 0
+-- end
 
 --------------------------------------------------------------------------------
 
