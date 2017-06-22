328bc4b5178038dd37e5a6cf9f985a537c0af4bf
VilkkuV <viljami.virtanen.VV@gmail.com>
2017-06-23 01:49:04 +0300

    little fixes

--- game/scripts/vscripts/items/urn_of_sorcery.lua
+++ game/scripts/vscripts/items/urn_of_sorcery.lua
@@ -1,9 +1,8 @@
 function urn_of_sorcery_on_ability_executed(keys)
 	if keys.caster:GetTeam() ~= keys.unit:GetTeam() and keys.caster:CanEntityBeSeenByMyTeam(keys.unit) then
 		local oldest_urn_of_sorcery = nil
-	
 		for i=0, 5, 1 do
-			local current_item = keys.caster:GetItemInSlot(i)	
+			local current_item = keys.caster:GetItemInSlot(i)
 			if current_item ~= nil and string.find(current_item:GetName(), "urn_of_sorcery") and current_item:GetCurrentCharges() < keys.MaxCharges then
 				if oldest_urn_of_sorcery == nil or current_item:GetEntityIndex() < oldest_urn_of_sorcery:GetEntityIndex() then
 					oldest_urn_of_sorcery = current_item
@@ -12,7 +11,7 @@ function urn_of_sorcery_on_ability_executed(keys)
 		end
 		if oldest_urn_of_sorcery ~= nil then
 			oldest_urn_of_sorcery:SetCurrentCharges(oldest_urn_of_sorcery:GetCurrentCharges() + 1)
-		end	
+		end
 	end
 end
 
