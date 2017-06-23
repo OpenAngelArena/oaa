7ad555cdcef610a2e52605ce78aef000e0a48823
Nikolai Zimmermann <Chronophylos@users.noreply.github.com>
2017-06-23 21:17:17 +0200

    Added Return to Shielder when hit on the Shielded Side (#1184)
    * Added Return to Shielder when hit on the shielded side
    
    * Refrection Flags
    
    * Copy paste is hard

--- game/scripts/vscripts/abilities/shielder/modifier_boss_shielder_shielded.lua
+++ game/scripts/vscripts/abilities/shielder/modifier_boss_shielder_shielded.lua
@@ -61,53 +61,71 @@ end
 
 function modifier_boss_shielder_shielded_buff:GetModifierIncomingDamage_Percentage(keys)
  --[[
-[   VScript              ]: process_procs: true
-[   VScript              ]: order_type: 0
-[   VScript              ]: issuer_player_index: 1982289334
-[   VScript              ]: fail_type: 0
-[   VScript              ]: damage_category: 0
-[   VScript              ]: reincarnate: false
-[   VScript              ]: distance: 0
-[   VScript              ]: gain: 98.332176208496
-[   VScript              ]: attacker: table: 0x00636d38
-[   VScript              ]: ranged_attack: false
-[   VScript              ]: record: 5
-[   VScript              ]: activity: -1
-[   VScript              ]: do_not_consume: false
-[   VScript              ]: damage_type: 4
-[   VScript              ]: heart_regen_applied: false
-[   VScript              ]: diffusal_applied: false
-[   VScript              ]: no_attack_cooldown: false
-[   VScript              ]: cost: 0
-[   VScript              ]: inflictor: table: 0x004bfe30
-[   VScript              ]: damage_flags: 0
-[   VScript              ]: original_damage: 650
-[   VScript              ]: ignore_invis: false
-[   VScript              ]: damage: 650
-[   VScript              ]: basher_tested: false
-[   VScript              ]: target: table: 0x00524320
+process_procs: true
+order_type: 0
+issuer_player_index: 1982289334
+fail_type: 0
+damage_category: 0
+reincarnate: false
+distance: 0
+gain: 98.332176208496
+attacker: hScript
+ranged_attack: false
+record: 5
+activity: -1
+do_not_consume: false
+damage_type: 4
+heart_regen_applied: false
+diffusal_applied: false
+no_attack_cooldown: false
+cost: 0
+inflictor: hScript
+damage_flags: 0
+original_damage: 650
+ignore_invis: false
+damage: 650
+basher_tested: false
+target: hScript
 ]]
 --  for k,v in pairs(keys) do
 --    print(k .. ': ' .. tostring(v))
 --  end
   local parent = self:GetParent()
-  local hero = keys.attacker
-  local angleCos = 0
+  local attacker = keys.attacker
+  local damage = keys.damage
+  local damage_type = keys.damage_type
+  local damage_flags = keys.damage_flags
+  local ability = self:GetAbility()
 
   --if hero and hero:IsRealHero and hero:IsRealHero() then
-  local attackOrigin = hero:GetAbsOrigin()
+  local attackOrigin = attacker:GetAbsOrigin()
   local parentOrigin = parent:GetAbsOrigin()
   local attackDirection = (attackOrigin - parentOrigin):Normalized()
   local parentFacing = (parent:GetForwardVector()):Normalized()
-  angleCos = parentFacing:Dot(attackDirection)
+  local angleCos = parentFacing:Dot(attackDirection)
   --end
   --DebugPrint(angleCos .. ' : ' .. self:GetAbility():GetSpecialValueFor("sheild_width"))
-  if (angleCos > (self:GetAbility():GetSpecialValueFor("sheild_width"))) then
-    local target = self:GetParent()
-    local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_spectre/spectre_desolate.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
-    ParticleManager:SetParticleControl(particle, 0, target:GetAbsOrigin())
-    ParticleManager:SetParticleControl(particle, 1, Vector(nil,0,0))
-    return 0 - self:GetAbility():GetSpecialValueFor("percent_damage_reduce")
+  if (angleCos > (self:GetAbility():GetSpecialValueFor("shield_width"))) then
+    -- Return Damage
+
+    if not bit.band(damage_flags, DOTA_DAMAGE_FLAG_REFLECTION) == DOTA_DAMAGE_FLAG_REFLECTION then
+      local damage_return = damage * (ability:GetSpecialValueFor("damage_return_pct"))
+      damage_flags = bit.bor(damage_flags, DOTA_DAMAGE_FLAG_REFLECTION)
+      ApplyDamage({
+        victim = attacker,
+        attacker = parent,
+        damage = damage_return,
+        damage_type = damage_type,
+        damage_flags = damage_flags,
+        ability = ability
+      })
+    end
+
+    local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_spectre/spectre_desolate.vpcf", PATTACH_ABSORIGIN_FOLLOW, parent)
+    ParticleManager:SetParticleControl(particle, 0, parent:GetAbsOrigin())
+    ParticleManager:SetParticleControl(particle, 1, Vector(nil, 0, 0))
+
+    return 0 - ability:GetSpecialValueFor("damage_reduction_pct")
   end
 
   return 0
