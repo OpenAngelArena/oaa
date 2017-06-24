c417c0b166a40c978ba631c0f40fc74d91f506a1
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-06-25 03:28:16 +0800

    Refresher Core tweaks (#1208)
    * Add charge refreshing to Refresher Core
    
    * Rework modifier_octarine_vampirism_buff to not use an aura applier

--- game/scripts/vscripts/items/satanic_core.lua
+++ game/scripts/vscripts/items/satanic_core.lua
@@ -1,5 +1,5 @@
 LinkLuaModifier( "modifier_intrinsic_multiplexer", "modifiers/modifier_intrinsic_multiplexer.lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_octarine_vampirism_applier", "modifiers/modifier_octarine_vampirism_applier.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_octarine_vampirism_buff", "modifiers/modifier_octarine_vampirism_buff.lua", LUA_MODIFIER_MOTION_NONE )
 LinkLuaModifier( "modifier_item_satanic_core", "items/satanic_core.lua", LUA_MODIFIER_MOTION_NONE )
 LinkLuaModifier( "modifier_satanic_core_unholy", "items/satanic_core.lua", LUA_MODIFIER_MOTION_NONE )
 
@@ -14,7 +14,7 @@ end
 function item_satanic_core:GetIntrinsicModifierNames()
   return {
     "modifier_item_satanic_core",
-    "modifier_octarine_vampirism_applier"
+    "modifier_octarine_vampirism_buff"
   }
 end
 
