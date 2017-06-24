30a0e0edc9e3708e67476a68d4c5ebc33c88a784
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-06-25 03:30:45 +0800

    Charge BKB reworks (#1163)
    * Rewrite Charge BKB as Lua item
    
    Now respects ProcsMagicStick for gaining charges. Also reworked charge decay to be a reset time like Fury Swipes.
    
    * Change Charge BKB charge reset time to 10 seconds at all levels
    
    * Add model_scale KV to Charge BKB
    
    This is the value used by the Avatar active effect for model scaling.
    
    * Actually make Charge BKB 2 to 4 Lua items
    
    * Removed unused 5th level values of Charge BKB
    
    * Fix Charge BKB tooltip
    
    * Rearrange stats in Charge BKB KV so that tooltip displays them in more conventional order
    
    * Change level 1 Charge BKB recipe cost to 1500
    
    * Rework Charge BKB build path
    
    Level 1 Charge BKB now builds from level 2 BKB with just Wand and a Recipe and the sidegrades work similarly. Higher level Charge BKBs now show upgrading from lower level Charge BKBs as the primary recipe.
    
    * Add info about sidegrade recipe for BKB to Charge BKB to tooltip
    
    * Add tooltips for Charge BKB recipe dummy items
    
    * Fix ItemCost KV for Charge BKB
    
    * Add description for Charge BKB dummy recipe tooltip
    
    * Rework use of dummy recipes in Charge BKB build path
    
    * Modify some CBKB conditionals to use variables instead of functions
    
    I'm not even sure why I made them functions in the first place.

--- game/scripts/vscripts/items/charge_bkb.lua
+++ game/scripts/vscripts/items/charge_bkb.lua
@@ -1,99 +1,125 @@
 --[[ ============================================================================================================
 	Charge BKB: Combines magic_wand functionality with BKB functionality, and charges decay with time.
 	Written by RamonNZ
-	Version 1.04
+	Version 1.05
 	Credit: Some original code from Rook
 	RamonNZ: The code below starts when you activate the BKB:
 	RamonNZ: Added basic purge on BKB start.
+  Trildar: Reworked as Lua item and refactored some code
 ================================================================================================================= ]]
+LinkLuaModifier("modifier_item_charge_bkb", "items/charge_bkb.lua", LUA_MODIFIER_MOTION_NONE)
 
-require( "libraries/Timers" )	--needed for the timers.
+item_charge_bkb = class(ItemBaseClass)
 
-function modifier_charge_bkb_on_spell_start(keys)
-
---RamonNZ: Wand Effect: Idea: May as well keep the small wand heal effect in addition to the BKB effect. Can be commented out if not wanted or just set to 0 in the kv.
-	local amount_to_restore = keys.ability:GetCurrentCharges() * keys.RestorePerCharge --RestorePerCharge
-	keys.caster:Heal(amount_to_restore, keys.caster)
-	keys.caster:GiveMana(amount_to_restore)
---RamonNZ: BKB Effect:
-	local modifier_duration = keys.ChargeImmunityTime*keys.ability:GetCurrentCharges()
--- Basic Purge:
-	local RemovePositiveBuffs = false
-	local RemoveDebuffs = true
-	local BuffsCreatedThisFrameOnly = false
-	local RemoveStuns = false
-	local RemoveExceptions = false
-	keys.caster:Purge(RemovePositiveBuffs, RemoveDebuffs, BuffsCreatedThisFrameOnly, RemoveStuns, RemoveExceptions)
-
-	print ("--> bkb spell immunity length = ", modifier_duration)
-	keys.caster:AddNewModifier(keys.caster, keys.ability, "modifier_charge_bkb_spell_immunity", {duration = modifier_duration})
-	keys.ability:ApplyDataDrivenModifier(keys.caster, keys.caster, "modifier_charge_bkb_spell_immunity", {duration = modifier_duration}) -- why did I do this twice? I'll figure it out later
-
-	local modified_cooldown = keys.ChargeCooldownTime*keys.ability:GetCurrentCharges() + keys.ability:GetCooldownTime()
-	keys.ability:SetCurrentCharges(0)
-	keys.ability:StartCooldown( modified_cooldown )
+function item_charge_bkb:GetIntrinsicModifierName()
+  return "modifier_item_charge_bkb"
 end
 
+function item_charge_bkb:OnSpellStart()
+  local caster = self:GetCaster()
+  --RamonNZ: Wand Effect: Idea: May as well keep the small wand heal effect in addition to the BKB effect. Can be commented out if not wanted or just set to 0 in the kv.
+  local amount_to_restore = self:GetCurrentCharges() * self:GetSpecialValueFor("restore_per_charge")
+  caster:Heal(amount_to_restore, caster)
+  caster:GiveMana(amount_to_restore)
+  --RamonNZ: BKB Effect:
+  local modifier_duration = self:GetSpecialValueFor("immunity_time_per_charge") * self:GetCurrentCharges()
+  -- Basic Purge:
+  local RemovePositiveBuffs = false
+  local RemoveDebuffs = true
+  local BuffsCreatedThisFrameOnly = false
+  local RemoveStuns = false
+  local RemoveExceptions = false
+  caster:Purge(RemovePositiveBuffs, RemoveDebuffs, BuffsCreatedThisFrameOnly, RemoveStuns, RemoveExceptions)
 
+  caster:AddNewModifier(caster, self, "modifier_black_king_bar_immune", {duration = modifier_duration})
+  EmitSoundOn("DOTA_Item.BlackKingBar.Activate", caster)
 
---[[ ============================================================================================================
-	RamonNZ: This code adds charges when abilities are used by enemies.
-	Known Bugs (by Rook - wand code): Because OnAbilityExecuted does not pass in information about the ability that was just executed, this code cannot use ProcsMagicStick() to determine if Magic Stick should gain a charge.  For now, every cast ability awards a charge.
-	RamonNZ: Fixed - Charges can be added by neutral abilities, just like wand
-================================================================================================================= ]]
-function modifier_charge_bkb_aura_on_ability_executed(keys)
-	if keys.caster:GetTeam() ~= keys.unit:GetTeam() and keys.caster:CanEntityBeSeenByMyTeam(keys.unit) then
-		 --Rook's code: Search for a Charge_BKB in the aura creator's inventory.  If there are multiple Charge_BKBs in the player's inventory, the oldest one that's not full receives a charge.
-		local oldest_unfilled_wand = nil
-
-		for i=0, 5, 1 do
-			local current_item = keys.caster:GetItemInSlot(i)
-			if current_item ~= nil and current_item:GetName() == "item_charge_bkb" and current_item:GetCurrentCharges() < keys.MaxCharges then
-				if oldest_unfilled_wand == nil or current_item:GetEntityIndex() < oldest_unfilled_wand:GetEntityIndex() then
-					oldest_unfilled_wand = current_item
-				end
-			end
-		end
-
-		--RamonNZ: Increment the Magic Wand's current charges by 1, but only if CurrentCharges are less than MaxCharges
-		if oldest_unfilled_wand ~= nil then
-			if oldest_unfilled_wand:GetCurrentCharges() < keys.MaxCharges then
-				oldest_unfilled_wand:SetCurrentCharges(oldest_unfilled_wand:GetCurrentCharges() + 1)
-				--RamonNZ: start the charges decay timer when a new charge is added
-				create_decay_timer(keys)
-			end
-		end
-	end
+  local modified_cooldown = self:GetSpecialValueFor("cooldown_time_per_charge") * self:GetCurrentCharges() + self:GetCooldown(self:GetLevel())
+  self:SetCurrentCharges(0)
+  self:StartCooldown( modified_cooldown )
 end
 
-
---[[ ============================================================================================================
-	RamonNZ: This is the decay timer - every charge added creates a one-shot timer based on ChargeDecayTime
-================================================================================================================= ]]
-function create_decay_timer(keys)
-	Timers:CreateTimer({
-	useGameTime = true,
-	endTime = keys.ChargeDecayTime,
-	callback = function()
-		if keys.ability:GetCurrentCharges() > 0 then
-			print ("--> -1 Charge_BKB charge")
-			keys.ability:SetCurrentCharges(keys.ability:GetCurrentCharges()-1)
-		end
-		return nil
-	end})
+function item_charge_bkb:StartChargeResetTimer()
+  Timers:CreateTimer(
+    "CBKB_" .. self:entindex(),
+    {
+      endTime = self:GetSpecialValueFor("charge_decay_time"),
+      callback = self.ResetCharges
+    },
+    self
+  )
 end
 
+function item_charge_bkb:ResetCharges()
+  self:SetCurrentCharges(0)
+end
 
---[[ ============================================================================================================
-	RamonNZ: This code creates a decay timer * every initial charge when item is created
-================================================================================================================= ]]
-function modifier_charge_bkb_on_created(keys)
-	for i=1, keys.ability:GetCurrentCharges() do
-		create_decay_timer(keys)
-	end
-	--Removed test timer to allow you to test in single game
---	  Timers:CreateTimer(function()
---     keys.ability:SetCurrentCharges(keys.ability:GetCurrentCharges()+1)
---      return 10.0
---   end)
+item_charge_bkb_2 = class(item_charge_bkb)
+item_charge_bkb_3 = class(item_charge_bkb)
+item_charge_bkb_4 = class(item_charge_bkb)
+
+------------------------------------------------------------------------
+
+modifier_item_charge_bkb = class(ModifierBaseClass)
+
+function modifier_item_charge_bkb:IsPurgable()
+  return false
+end
+
+function modifier_item_charge_bkb:IsHidden()
+  return true
+end
+
+function modifier_item_charge_bkb:GetAttributes()
+  return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+function modifier_item_charge_bkb:DeclareFunctions()
+  return {
+    MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
+    MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+    MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+    MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+    MODIFIER_EVENT_ON_ABILITY_FULLY_CAST
+  }
+end
+
+function modifier_item_charge_bkb:GetModifierPreAttack_BonusDamage()
+  return self:GetAbility():GetSpecialValueFor("bonus_damage")
+end
+
+function modifier_item_charge_bkb:GetModifierBonusStats_Strength()
+  return self:GetAbility():GetSpecialValueFor("bonus_strength")
+end
+
+function modifier_item_charge_bkb:GetModifierBonusStats_Agility()
+  return self:GetAbility():GetSpecialValueFor("bonus_agility")
+end
+
+function modifier_item_charge_bkb:GetModifierBonusStats_Intellect()
+  return self:GetAbility():GetSpecialValueFor("bonus_intellect")
+end
+
+-- Add charges when abilities are cast by nearby visible enemies
+function modifier_item_charge_bkb:OnAbilityFullyCast(keys)
+  local parent = self:GetParent()
+  local chargeBkb = self:GetAbility()
+  -- Only add charges for abilities cast by visible enemies
+  local filterResult = UnitFilter(keys.unit,
+    DOTA_UNIT_TARGET_TEAM_ENEMY,
+    bit.bor(DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_OTHER),
+    bit.bor(DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE, DOTA_UNIT_TARGET_FLAG_NO_INVIS),
+    parent:GetTeamNumber()
+  )
+
+  -- Only allow 1 Charge BKB to gain charges
+  local isFirstCBKBModifier = parent:FindModifierByName("modifier_item_charge_bkb") == self
+
+  local distanceToUnit = #(parent:GetAbsOrigin() - keys.unit:GetAbsOrigin())
+  local unitIsInRange = distanceToUnit <= chargeBkb:GetSpecialValueFor("charge_radius")
+
+  if filterResult == UF_SUCCESS and isFirstCBKBModifier and keys.ability:ProcsMagicStick() and unitIsInRange then
+    chargeBkb:SetCurrentCharges(chargeBkb:GetCurrentCharges() + 1)
+    chargeBkb:StartChargeResetTimer()
+  end
 end
