88cc023f06031232e0271182a1e3c84aa083e49e
SquawkyArctangent <arctangentpuckett@gmail.com>
2017-07-12 21:42:35 -0500

    Added a level 4 to Elder Dragon Form (#1268)
    * Fixed the degeneracy with lua bashes and illusions
    
    one line fixes woo
    
    * Added a level 4 to Elder Dragon Form
    
    Based on Baumi's idea, it allows EDF to be triggered on attack with
    heavily reduced duration.
    
    Also fixes a KV mistake which accidentally gave splash attack too much
    range.
    
    * Removed redundant line in EDF

--- game/scripts/vscripts/abilities/oaa_elder_dragon_form.lua
+++ game/scripts/vscripts/abilities/oaa_elder_dragon_form.lua
@@ -4,6 +4,28 @@ LinkLuaModifier( "modifier_dragon_knight_elder_dragon_form_oaa", "abilities/oaa_
 
 --------------------------------------------------------------------------------
 
+-- this should probably be moved elsewhere somewhere down the line
+-- probably somewhere where anything prng can access it
+dragon_knight_elder_dragon_form_oaa.prngTable = {}
+dragon_knight_elder_dragon_form_oaa.prngTable[5] = 0.038
+dragon_knight_elder_dragon_form_oaa.prngTable[10] = 0.01475
+dragon_knight_elder_dragon_form_oaa.prngTable[15] = 0.03221
+dragon_knight_elder_dragon_form_oaa.prngTable[20] = 0.0557
+dragon_knight_elder_dragon_form_oaa.prngTable[25] = 0.08475
+dragon_knight_elder_dragon_form_oaa.prngTable[30] = 0.11895
+dragon_knight_elder_dragon_form_oaa.prngTable[35] = 0.14628
+dragon_knight_elder_dragon_form_oaa.prngTable[40] = 0.18128
+dragon_knight_elder_dragon_form_oaa.prngTable[45] = 0.21867
+dragon_knight_elder_dragon_form_oaa.prngTable[50] = 0.25701
+dragon_knight_elder_dragon_form_oaa.prngTable[55] = 0.29509
+dragon_knight_elder_dragon_form_oaa.prngTable[60] = 0.33324
+dragon_knight_elder_dragon_form_oaa.prngTable[65] = 0.38109
+dragon_knight_elder_dragon_form_oaa.prngTable[70] = 0.42448
+dragon_knight_elder_dragon_form_oaa.prngTable[75] = 0.46134
+dragon_knight_elder_dragon_form_oaa.prngTable[80] = 0.50276
+
+--------------------------------------------------------------------------------
+
 -- this makes the ability passive when it hits level 5
 function dragon_knight_elder_dragon_form_oaa:GetBehavior()
 	if self:GetLevel() >= 5 then
@@ -62,9 +84,23 @@ end
 
 function dragon_knight_elder_dragon_form_oaa:GetIntrinsicModifierName()
 	-- adds the modifier in change of automatically transforming dk
-	-- when edf hits level 5
-	if self:GetLevel() >= 5 then
+	-- when edf hits level 5 actually no level 4
+	-- since now this is also in charge of rage
+	if self:GetLevel() >= 4 then
 		return "modifier_dragon_knight_elder_dragon_form_oaa"
+	end
+end
+
+--------------------------------------------------------------------------------
+
+function dragon_knight_elder_dragon_form_oaa:OnUpgrade()
+	-- we need to refresh the passive modifier to cause it to instantly
+	-- transform dk on upgrade
+	if self:GetLevel() >= 5 then
+		local caster = self:GetCaster()
+
+		-- adding it again works in the off chance the modifier is somehow removed
+		caster:AddNewModifier( caster, self, "modifier_dragon_knight_elder_dragon_form_oaa", {} )
 	end
 end
 
@@ -105,16 +141,40 @@ if IsServer() then
 		local parent = self:GetParent()
 		local spell = self:GetAbility()
 
-		-- apply all the edf modifiers
-		for _, modName in pairs( self.edfMods ) do
-			local mod = parent:FindModifierByName( modName )
+		-- apply all the edf modifiers on creation if level 5
+		if spell:GetLevel() >= 5 then
+			for _, modName in ipairs( self.edfMods ) do
+				local mod = parent:FindModifierByName( modName )
 
-			if not mod then
-				parent:AddNewModifier( parent, spell, modName, {} )
-			else
-				-- if dk already has 'em, just set their duration to "permanent"
-				-- so no special effect happens on level up if he's already transformed
-				mod:SetDuration( -1, true )
+				if not mod then
+					parent:AddNewModifier( parent, spell, modName, {} )
+				else
+					-- if dk already has 'em, just set their duration to "permanent"
+					-- so no special effect happens on level up if he's already transformed
+					mod:SetDuration( -1, true )
+				end
+			end
+		end
+	end
+
+--------------------------------------------------------------------------------
+
+	function modifier_dragon_knight_elder_dragon_form_oaa:OnRefresh( event )
+		local parent = self:GetParent()
+		local spell = self:GetAbility()
+
+		-- apply all the edf modifiers on creation if level 5
+		if spell:GetLevel() >= 5 then
+			for _, modName in pairs( self.edfMods ) do
+				local mod = parent:FindModifierByName( modName )
+
+				if not mod then
+					parent:AddNewModifier( parent, spell, modName, {} )
+				else
+					-- if dk already has 'em, just set their duration to "permanent"
+					-- so no special effect happens on level up if he's already transformed
+					mod:SetDuration( -1, true )
+				end
 			end
 		end
 	end
@@ -123,6 +183,7 @@ if IsServer() then
 
 	function modifier_dragon_knight_elder_dragon_form_oaa:DeclareFunctions()
 		local funcs = {
+			MODIFIER_EVENT_ON_ATTACK_LANDED,
 			MODIFIER_EVENT_ON_RESPAWN,
 		}
 
@@ -131,12 +192,58 @@ if IsServer() then
 
 --------------------------------------------------------------------------------
 
-	function modifier_dragon_knight_elder_dragon_form_oaa:OnRespawn( event )
+	function modifier_dragon_knight_elder_dragon_form_oaa:OnAttackLanded( event )
 		local parent = self:GetParent()
 
-		if event.unit == parent then
+		if event.attacker == parent then
 			local spell = self:GetAbility()
 
+			if spell:GetLevel() == 4 then
+				-- no rage while broken
+				if parent:PassivesDisabled() then
+					return
+				end
+
+				local chance = spell:GetSpecialValueFor( "rage_chance" )
+
+				-- we're using the modifier's stack to store the amount of prng failures
+				-- this could be something else but since this modifier is hidden anyway ...
+				local prngMult = self:GetStackCount() + 1
+
+				-- compared prng to slightly less prng
+				if RandomFloat( 0.0, 1.0 ) <= ( spell.prngTable[chance] * prngMult ) then
+					-- reset failure count
+					self:SetStackCount( 0 )
+
+					local duration = spell:GetSpecialValueFor( "rage_duration" )
+
+					-- check if the ability is already active, and if so, grab the current
+					-- duration
+					local mod = parent:FindModifierByName( "modifier_dragon_knight_dragon_form" )
+
+					if mod then
+						duration = duration + mod:GetRemainingTime()
+					end
+
+					-- apply the edf modifiers with the new duration
+					for _, modName in pairs( self.edfMods ) do
+						parent:AddNewModifier( parent, spell, modName, { duration = duration } )
+					end
+				else
+					-- increment failure count
+					self:SetStackCount( prngMult )
+				end
+			end
+		end
+	end
+
+--------------------------------------------------------------------------------
+
+	function modifier_dragon_knight_elder_dragon_form_oaa:OnRespawn( event )
+		local parent = self:GetParent()
+		local spell = self:GetAbility()
+
+		if event.unit == parent and spell:GetLevel() >= 5 then
 			-- apply the edf modifiers on respawn
 			for _, modName in pairs( self.edfMods ) do
 				parent:AddNewModifier( parent, spell, modName, {} )
