3d8d40374390e297f4901425a1bad4c0dafeb167
Alex Tsernoh <alex@ehub.ee>
2017-08-12 13:16:01 +0300

    Replaced dualquotes with single

--- content/panorama/scripts/custom_game/music.js
+++ content/panorama/scripts/custom_game/music.js
@@ -1,18 +1,18 @@
 var musicPlaying = true;
-$.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass("MusicOn");
+$.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass('MusicOn');
 CustomNetTables.SubscribeNetTableListener('info', SetMusic);
 
 
 function ToggleMusic () {
     if (musicPlaying) {
         musicPlaying = false;
-        $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass("MusicOn");
-        $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass("MusicOff");
+        $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass('MusicOn');
+        $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass('MusicOff');
         //TURN OFF MUSIC(VOLUME)
     } else {
         musicPlaying = true;
-        $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass("MusicOff");
-        $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass("MusicOn");
+        $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass('MusicOff');
+        $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass('MusicOn');
         //TURN ON MUSIC(VOLUME)
     }
 }
@@ -20,7 +20,7 @@ function ToggleMusic () {
 // TESTED, data format is set as followed 
 // CustomNetTables:SetTableValue("info", "music", { title = "XD", subtitle = "XDD" })
 function SetMusic(table, key, data) {
-    if (key == "music");
+    if (key == 'music');
     $.GetContextPanel().FindChildTraverse('MusicTitle').text = data.title;
     $.GetContextPanel().FindChildTraverse('MusicSubTitle').text = data.subtitle;
 }
