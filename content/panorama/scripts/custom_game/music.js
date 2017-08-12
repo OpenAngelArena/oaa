1e480d104ab91e1fd3dc080ddfbb6895f0028ac2
Alex Tsernoh <alex@ehub.ee>
2017-08-12 13:11:54 +0300

    Paired JS with nettables, attached data format in commit description
    CustomNetTables:SetTableValue("info", "music", { title = "XD", subtitle = "XDD" })

--- content/panorama/scripts/custom_game/music.js
+++ content/panorama/scripts/custom_game/music.js
@@ -1,13 +1,26 @@
 var musicPlaying = true;
 $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass("MusicOn");
+CustomNetTables.SubscribeNetTableListener('info', SetMusic);
+
+
 function ToggleMusic () {
     if (musicPlaying) {
         musicPlaying = false;
         $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass("MusicOn");
         $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass("MusicOff");
+        //TURN OFF MUSIC(VOLUME)
     } else {
         musicPlaying = true;
         $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass("MusicOff");
         $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass("MusicOn");
+        //TURN ON MUSIC(VOLUME)
     }
+}
+
+// TESTED, data format is set as followed 
+// CustomNetTables:SetTableValue("info", "music", { title = "XD", subtitle = "XDD" })
+function SetMusic(table, key, data) {
+    if (key == "music");
+    $.GetContextPanel().FindChildTraverse('MusicTitle').text = data.title;
+    $.GetContextPanel().FindChildTraverse('MusicSubTitle').text = data.subtitle;
 }
