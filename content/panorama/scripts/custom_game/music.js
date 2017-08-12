57ee673f3fbb2f55204744ff266eee2f8714278a
Alex Tsernoh <alex@ehub.ee>
2017-08-12 13:21:35 +0300

    Formatting according to editorconfig

--- content/panorama/scripts/custom_game/music.js
+++ content/panorama/scripts/custom_game/music.js
@@ -3,24 +3,24 @@ $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass('MusicOn');
 CustomNetTables.SubscribeNetTableListener('info', SetMusic);
 
 
-function ToggleMusic () {
-    if (musicPlaying) {
-        musicPlaying = false;
-        $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass('MusicOn');
-        $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass('MusicOff');
-        //TURN OFF MUSIC(VOLUME)
-    } else {
-        musicPlaying = true;
-        $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass('MusicOff');
-        $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass('MusicOn');
-        //TURN ON MUSIC(VOLUME)
-    }
+function ToggleMusic() {
+  if (musicPlaying) {
+    musicPlaying = false;
+    $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass('MusicOn');
+    $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass('MusicOff');
+    //TURN OFF MUSIC(VOLUME)
+  } else {
+    musicPlaying = true;
+    $.GetContextPanel().FindChildTraverse('ToggleMusic').RemoveClass('MusicOff');
+    $.GetContextPanel().FindChildTraverse('ToggleMusic').AddClass('MusicOn');
+    //TURN ON MUSIC(VOLUME)
+  }
 }
 
-// TESTED, data format is set as followed 
+// TESTED, data format is set as followed
 // CustomNetTables:SetTableValue("info", "music", { title = "XD", subtitle = "XDD" })
 function SetMusic(table, key, data) {
-    if (key == 'music');
-    $.GetContextPanel().FindChildTraverse('MusicTitle').text = data.title;
-    $.GetContextPanel().FindChildTraverse('MusicSubTitle').text = data.subtitle;
+  if (key == 'music');
+  $.GetContextPanel().FindChildTraverse('MusicTitle').text = data.title;
+  $.GetContextPanel().FindChildTraverse('MusicSubTitle').text = data.subtitle;
 }
