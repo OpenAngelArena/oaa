70ecaaea0d61ebce7c19892afedc722e604a3e27
Alex Tsernoh <alex@ehub.ee>
2017-08-13 10:07:19 +0300

    Do not start stats if you spectate

--- content/panorama/scripts/custom_game/stat_display.js
+++ content/panorama/scripts/custom_game/stat_display.js
@@ -1,22 +1,26 @@
 /* global $ CustomNetTables Game Players */
 
 (function () {
-  CustomNetTables.SubscribeNetTableListener('stat_display_player', onPlayerStatChange);
-  CustomNetTables.SubscribeNetTableListener('stat_display_team', onTeamStatChange);
+  if (Game.GetLocalPlayerID() != -1) {
+    CustomNetTables.SubscribeNetTableListener('stat_display_player', onPlayerStatChange);
+    CustomNetTables.SubscribeNetTableListener('stat_display_team', onTeamStatChange);
+  } else {
+    $.GetContextPanel().FindChildTraverse('OAAStatDisplay').GetParent().RemoveAndDeleteChildren();
+  }
 }());
 
-function onPlayerStatChange (table, key, data) {
+function onPlayerStatChange(table, key, data) {
   var playerID = Game.GetLocalPlayerID();
   UpdateStatDisplay(key, data.value[playerID] || 0);
 }
 
-function onTeamStatChange (table, key, data) {
+function onTeamStatChange(table, key, data) {
   var playerID = Game.GetLocalPlayerID();
   var teamID = Players.GetTeam(playerID);
   UpdateStatDisplay(key, data.value[teamID] || 0);
 }
 
-function UpdateStatDisplay (name, value) {
+function UpdateStatDisplay(name, value) {
   var display = $('#OAAStatDisplay');
 
   display.FindChildTraverse(name + 'Row').FindChildTraverse('QuickStatLabelValue').text = value;
