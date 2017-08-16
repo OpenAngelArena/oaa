34540a22464ce5ae698785fb39398d586c455b56
Alex Tsernoh <alex@ehub.ee>
2017-08-16 18:07:59 +0300

    WOW

--- content/panorama/scripts/custom_game/hero_selection.js
+++ content/panorama/scripts/custom_game/hero_selection.js
@@ -1,1 +1,16 @@
-"use strict";
+/* global Players, $, GameEvents */
+
+var console = {
+  log: $.Msg.bind($)
+};
+
+if (typeof module !== 'undefined' && module.exports) {
+  module.exports = SelectHero;
+}
+
+function SelectHero () {
+  FindDotaHudElement('MainContent').style.transform = 'scaleX(15) scaleY(15)';
+  FindDotaHudElement('MainContent').style.opacity = '0';
+  FindDotaHudElement('StrategyContent').style.transform = 'scaleX(1) scaleY(1)';
+  FindDotaHudElement('StrategyContent').style.opacity = '1';
+}
