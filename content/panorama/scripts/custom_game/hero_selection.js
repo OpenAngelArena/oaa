c47b0f6828bffe399aeb85f75fc752e7e8fe674c
Alex Tsernoh <alex@ehub.ee>
2017-08-17 11:43:17 +0300

    map marker

--- content/panorama/scripts/custom_game/hero_selection.js
+++ content/panorama/scripts/custom_game/hero_selection.js
@@ -9,8 +9,18 @@ if (typeof module !== 'undefined' && module.exports) {
 }
 
 function SelectHero () {
-  FindDotaHudElement('MainContent').style.transform = 'scaleX(15) scaleY(15)';
+  FindDotaHudElement('MainContent').style.transform = 'scaleX(10) scaleY(10)';
   FindDotaHudElement('MainContent').style.opacity = '0';
   FindDotaHudElement('StrategyContent').style.transform = 'scaleX(1) scaleY(1)';
   FindDotaHudElement('StrategyContent').style.opacity = '1';
+  FindDotaHudElement('PregameBG').style.opacity = '0.15';
+
+
+
+  var bossMarkers = ['Boss1r', 'Boss1d', 'Boss2r', 'Boss2d', 'Boss3r', 'Boss3d', 'Boss4r', 'Boss4d', 'Boss5r', 'Boss5d'];
+
+  bossMarkers.forEach(function(element) {
+    FindDotaHudElement(element).style.transform = 'translateY(0)';
+    FindDotaHudElement(element).style.opacity = '1';
+  });
 }
