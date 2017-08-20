e926ef7b29c966880345850c7614c1b5658a1933
Alex Tsernoh <alex@ehub.ee>
2017-08-20 22:40:55 +0300

    DIE

--- content/panorama/scripts/custom_game/hero_selection.js
+++ content/panorama/scripts/custom_game/hero_selection.js
@@ -37,54 +37,54 @@ function onPlayerStatChange (table, key, data) {
           currentstat = intelligenceholder;
           break;
       }
-      var newelement = $.CreatePanel('RadioButton', currentstat, key);
-      newelement.group = 'HeroChoises';
-      newelement.SetPanelEvent('onactivate', ( function(newkey) { return function() { PreviewHero(newkey); }}(key)) );
-      var newimage = $.CreatePanel('DOTAHeroImage', newelement, '');
-      newimage.hittest = false;
-      newimage.AddClass('HeroCard');
-      newimage.heroname = key;
+      var newhero = $.CreatePanel('RadioButton', currentstat, key);
+      newhero.group = 'HeroChoises';
+      newhero.SetPanelEvent('onactivate', (function (newkey) { return function () { PreviewHero(newkey); }; }(key)));
+      var newheroimage = $.CreatePanel('DOTAHeroImage', newhero, '');
+      newheroimage.hittest = false;
+      newheroimage.AddClass('HeroCard');
+      newheroimage.heroname = key;
     }
   } else if (key === 'data' && data != null) {
     var length = Object.keys(data).length;
-    if (panelscreated != length) {
+    if (panelscreated !== length) {
       var teamdire = FindDotaHudElement('TeamDire');
       var teamradiant = FindDotaHudElement('TeamRadiant');
       panelscreated = length;
       teamdire.RemoveAndDeleteChildren();
       teamradiant.RemoveAndDeleteChildren();
-      for (var key in data) {
-        if (data.hasOwnProperty(key)) {
+      for (var nkey in data) {
+        if (data.hasOwnProperty(nkey)) {
           var currentteam = null;
-          switch(data[key].team) {
+          switch (data[nkey].team) {
             case 2:
               currentteam = teamradiant;
               break;
             case 3:
               currentteam = teamdire;
               break;
-          };
+          }
           var newelement = $.CreatePanel('Panel', currentteam, '');
           newelement.AddClass('Player');
-          var newimage = $.CreatePanel('DOTAHeroImage', newelement, data[key].steamid);
+          var newimage = $.CreatePanel('DOTAHeroImage', newelement, data[nkey].steamid);
           newimage.hittest = false;
           newimage.AddClass('PlayerImage');
-          newimage.heroname = data[key].selectedhero;
+          newimage.heroname = data[nkey].selectedhero;
           var newlabel = $.CreatePanel('DOTAUserName', newelement, '');
           newlabel.AddClass("PlayerLabel");
-          //I DO NOT KNOW WHY, BUT IT MESSES ID UP SOMEHOW BY +-3 WTF???
-          newlabel.steamid = data[key].steamid;
+          // I DO NOT KNOW WHY, BUT IT MESSES ID UP SOMEHOW BY +-3 WTF???
+          newlabel.steamid = data[nkey].steamid;
         }
       }
     } else {
-      for (var key in data) {
-        if (data.hasOwnProperty(key)) {
-          var currentplayer = FindDotaHudElement(data[key].steamid);
-          currentplayer.heroname = data[key].selectedhero;
+      for (var ind in data) {
+        if (data.hasOwnProperty(ind)) {
+          var currentplayer = FindDotaHudElement(data[ind].steamid);
+          currentplayer.heroname = data[ind].selectedhero;
         }
       }
     }
-  } else if (key == 'time' && data != null) {
+  } else if (key === 'time' && data != null) {
     if (data['time'] > -1) {
       FindDotaHudElement('TimeLeft').text = data['time'];
       FindDotaHudElement('GameMode').text = data['mode'];
@@ -96,7 +96,7 @@ function onPlayerStatChange (table, key, data) {
   }
 }
 
-function PreviewHero(name) {
+function PreviewHero (name) {
   if (!herolocked) {
     var preview = FindDotaHudElement('HeroPreview');
     preview.RemoveAndDeleteChildren();
@@ -105,9 +105,8 @@ function PreviewHero(name) {
   }
 }
 
-
 function SelectHero () {
-  if (!herolocked && selectedhero != 'empty') {
+  if (!herolocked && selectedhero !== 'empty') {
     herolocked = true;
     GameEvents.SendCustomGameEventToServer('hero_selected', {
       hero: selectedhero
@@ -117,7 +116,7 @@ function SelectHero () {
   }
 }
 
-function GoToStrategy() {
+function GoToStrategy () {
   FindDotaHudElement('MainContent').style.transform = 'translateX(0) translateY(100%)';
   FindDotaHudElement('MainContent').style.opacity = '0';
   FindDotaHudElement('StrategyContent').style.transform = 'scaleX(1) scaleY(1)';
@@ -126,11 +125,10 @@ function GoToStrategy() {
 
   var bossMarkers = ['Boss1r', 'Boss1d', 'Boss2r', 'Boss2d', 'Boss3r', 'Boss3d', 'Boss4r', 'Boss4d', 'Boss5r', 'Boss5d', 'Duel1', 'Duel2'];
 
-  bossMarkers.forEach(function(element) {
+  bossMarkers.forEach(function (element) {
     FindDotaHudElement(element).style.transform = 'translateY(0)';
     FindDotaHudElement(element).style.opacity = '1';
   });
-
 
   FindDotaHudElement('MainContent').GetParent().style.opacity = 0;
   FindDotaHudElement('MainContent').GetParent().style.transform = 'scaleX(2) scaleY(2) translateY(25%)';
