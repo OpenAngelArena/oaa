189600822e4ffbc2fba3defd97e0cadf72798d75
Richard Clutterbuck <rwclutterbuck@outlook.com>
2017-08-23 23:28:35 +0100

    Merge branch 'Cave+Map_Update' of https://github.com/ArchOAA/oaa into Cave+Map_Update
    accidentally clicked undo

--- content/panorama/scripts/custom_game/game_info.js
+++ content/panorama/scripts/custom_game/game_info.js
@@ -1,7 +1,11 @@
-/* global FindDotaHudElement, GameEvents */
+/* global FindDotaHudElement GameEvents Game */
 
 (function () {
-  GameEvents.Subscribe('game_rules_state_change', MoveGameInfo);
+  if (Game.GetLocalPlayerID() !== -1) {
+    GameEvents.Subscribe('game_rules_state_change', MoveGameInfo);
+  } else {
+    $.GetContextPanel().FindChildTraverse('GameInfoButton').GetParent().RemoveAndDeleteChildren();
+  }
 }());
 
 function MoveGameInfo () {
