314529bf92fcf7fe384be2c74f9f9b26b8b36f15
SquawkyArctangent <arctangentpuckett@gmail.com>
2017-08-27 21:51:44 -0500

    Reimplement Greater Tranquils as roaming midas boots (#1370)
    * Changed Greater Tranquil Boots to be something
    
    Builds up charges by moving, attacking with 100 charges will instantly
    kill a creep for bonus gold.
    
    KV Notes: bonus_xp is fully functional. max_charges is for the amount of
    charges that can be stored at once, not the ones needed to proc the
    midas-esque effect - it's meant to allow buffering some charges so you
    don't have to hit a creep immediately at 100 charges. Both of these
    should probably have tooltip changes if they're changed.
    
    * don't forget your accessors kids

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -26,6 +26,7 @@ function Precache( context )
   PrecacheItemByNameSync("item_stoneskin", context)
   PrecacheItemByNameSync("item_greater_phase_boots", context)
   PrecacheItemByNameSync("item_greater_power_treads", context)
+  PrecacheItemByNameSync("item_greater_tranquil_boots", context)
   PrecacheItemByNameSync("item_dagon", context)
   PrecacheItemByNameSync("item_manta_1", context)
 
