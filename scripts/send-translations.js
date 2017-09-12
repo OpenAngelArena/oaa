5e8b3e534637145cac1601e19bdf0849898ea35d
Chris Vickery <chrisinajar@gmail.com>
2017-09-12 08:13:51 -0700

    Use https for transifex data upload

--- scripts/send-translations.js
+++ scripts/send-translations.js
@@ -45,7 +45,7 @@ request.get({
   // fs.writeFileSync('./i18n.json', JSON.stringify(englishStrings, null, 2));
   // curl -i -L --user username:password -F file=@path_to_the_file -X PUT http://www.transifex.com/api/2/project/documentation/resource/api_doc/content/
   request.put({
-    url: 'http://www.transifex.com/api/2/project/open-angel-arena/resource/addon_english/content/',
+    url: 'https://www.transifex.com/api/2/project/open-angel-arena/resource/addon_english/content/',
     auth: {
       user: process.env.TRANSIFEX_USER,
       pass: process.env.TRANSIFEX_PASSWORD
