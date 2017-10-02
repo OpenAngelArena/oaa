e9f01b0d3632023c9a881b47c101607ba4e67d46
Satsaa <32210043+Satsaa@users.noreply.github.com>
2017-10-02 09:34:54 +0300

    Enchancements to Map
    Gameplay is overrated...
    -Revised dire arena and swapped arenas
    -Copied things from default map because they look nice
    -More grass
    -Texture fixes
    -Made sure the main shopkeepers are different and assumed arena shopkeepers as twins
    -Lifted area behind bosses where no one goes for vision advantage
    
    vpk file size 29,795 -> 29,993 KB
    
    -Removed background from music ui for clean look
    
    Also the ranged twin should no longer heal when the melee is dead. Additionally allowed twins healths to differ a bit.

--- game/scripts/vscripts/abilities/twin/modifier_boss_twin_twin_empathy.lua
+++ game/scripts/vscripts/abilities/twin/modifier_boss_twin_twin_empathy.lua
@@ -25,14 +25,13 @@ function modifier_boss_twin_twin_empathy_buff:OnIntervalThink()
   local master = self:GetCaster()
   local twin = self:GetParent()
 
-	if twin:IsAlive() then
-	  if twin:GetHealth() < master:GetHealth() then
+	if twin:IsAlive() and master:IsAlive() then
+	  if twin:GetHealth() < master:GetHealth() - 200 then
       twin:SetHealth(master:GetHealth())
-    end
-    if twin:GetHealth() > master:GetHealth() then
+    elseif twin:GetHealth() - 200 > master:GetHealth() then
       master:SetHealth(twin:GetHealth())
     end
-	end
+  end
 
   self:StartIntervalThink(self:GetAbility():GetSpecialValueFor( "heal_timer" ))
 end
