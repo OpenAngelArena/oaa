5264754f7e11ab5e49c74d5c9c5bec33cf1d3519
Soo Kuo-Yi <ksoo11@student.monash.edu>
2017-11-04 13:17:07 +0800

    Glaives of Wisdom fixes for 7.07 (#1448)
    * Update Glaives of Wisdom KV for 7.07
    
    * Fix Silencer's + Glaives of Wisdom damage Talent
    
    * Fix Silencer's + Intelligence Steal Talent
    
    * Fix(?) manual casting of Glaives of Wisdom not working

--- game/scripts/vscripts/abilities/oaa_glaives_of_wisdom.lua
+++ game/scripts/vscripts/abilities/oaa_glaives_of_wisdom.lua
@@ -57,7 +57,9 @@ end
 function modifier_oaa_glaives_of_wisdom:OnAttackStart(keys)
   local parent = self:GetParent()
   local ability = self:GetAbility()
-  if keys.attacker == parent and (keys.gain == 0 or ability:GetAutoCastState()) and ability:IsOwnersManaEnough() and keys.target.IsMagicImmune and (not keys.target:IsMagicImmune() or parent:HasScepter()) then
+  -- Manual cast of attack modifier appears to make the damage_type 1115000
+  -- May or may not work properly in practice
+  if keys.attacker == parent and (keys.damage_type == 1115000 or ability:GetAutoCastState()) and ability:IsOwnersManaEnough() and keys.target.IsMagicImmune and (not keys.target:IsMagicImmune() or parent:HasScepter()) then
     -- Set projectile
     parent:SetRangedProjectileName("particles/units/heroes/hero_silencer/silencer_glaives_of_wisdom.vpcf")
   end
@@ -73,9 +75,9 @@ end
 function modifier_oaa_glaives_of_wisdom:OnAttack(keys)
   local parent = self:GetParent()
   local ability = self:GetAbility()
-  -- keys.gain ~= keys.gain is to check if it is NaN which seems to always be the case when
-  -- an attack modifier ability is cast manually
-  if keys.attacker == parent and (keys.gain ~= keys.gain or ability:GetAutoCastState()) and ability:IsOwnersManaEnough() and keys.target.IsMagicImmune and (not keys.target:IsMagicImmune() or parent:HasScepter()) then
+  -- Manual cast of attack modifier appears to make the damage_type 2
+  -- May or may not work properly in practice
+  if keys.attacker == parent and (keys.damage_type == 2 or ability:GetAutoCastState()) and ability:IsOwnersManaEnough() and keys.target.IsMagicImmune and (not keys.target:IsMagicImmune() or parent:HasScepter()) then
     -- Enable proc for this attack record number
     self.procRecords[keys.record] = true
     -- Using attack modifier abilities doesn't actually fire any cast events so we have to spend the mana here
@@ -87,12 +89,19 @@ function modifier_oaa_glaives_of_wisdom:OnAttackLanded(keys)
   local parent = self:GetParent()
   if keys.attacker == parent and self.procRecords[keys.record] then
     local ability = self:GetAbility()
-    local bonusDamage = parent:GetIntellect() * ability:GetSpecialValueFor("intellect_damage_pct") / 100
+    local bonusDamagePct = ability:GetSpecialValueFor("intellect_damage_pct") / 100
     local player = parent:GetPlayerOwner()
 
-    if parent:HasScepter() and keys.target:IsSilenced() then
-      bonusDamage = bonusDamage * ability:GetSpecialValueFor("scepter_damage_multiplier")
+    -- Check for +20% Glaive damage Talent
+    if parent:HasLearnedAbility("special_bonus_unique_silencer_3") then
+      bonusDamagePct = bonusDamagePct + parent:FindAbilityByName("special_bonus_unique_silencer_3"):GetSpecialValueFor("value") / 100
     end
+
+    if parent:HasScepter() and keys.target:IsSilenced() then
+      bonusDamagePct = bonusDamagePct * ability:GetSpecialValueFor("scepter_damage_multiplier")
+    end
+
+    local bonusDamage = parent:GetIntellect() * bonusDamagePct
 
     local damageTable = {
       victim = keys.target,
