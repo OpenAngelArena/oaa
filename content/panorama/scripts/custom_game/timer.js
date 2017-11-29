7281f0c519d891622950086a87b5dcc4bd02cc2e
Chris Vickery <chrisinajar@gmail.com>
2017-11-28 21:27:19 -0800

    Formatting

--- content/panorama/scripts/custom_game/timer.js
+++ content/panorama/scripts/custom_game/timer.js
@@ -1,5 +1,6 @@
+/* global CustomNetTables */
 
-(function() {
+(function () {
   CustomNetTables.SubscribeNetTableListener('timer', UpdateClock);
   UpdateClock(null, 'data', CustomNetTables.GetTableValue('timer', 'data'));
 }());
@@ -8,12 +9,12 @@ function UpdateClock (table, name, data) {
   if (!data || data.time === undefined) {
     return;
   }
-  $("#TimeHider").style.visibility = 'visible';
+  $('#TimeHider').style.visibility = 'visible';
 
-  $("#GameTime").text = formatTime(data.time)
-  var dayTime = $("#DayTime");
-  var nightTime = $("#NightTime");
-  var nightstalkerNight = $("#NightstalkerNight");
+  $('#GameTime').text = formatTime(data.time);
+  var dayTime = $('#DayTime');
+  var nightTime = $('#NightTime');
+  var nightstalkerNight = $('#NightstalkerNight');
   dayTime.style.visibility = 'collapse';
   nightTime.style.visibility = 'collapse';
   nightstalkerNight.style.visibility = 'collapse';
@@ -29,9 +30,9 @@ function UpdateClock (table, name, data) {
 function formatTime (time) {
   var seconds = time % 60;
   if (seconds >= 0) {
-    return [Math.floor(time/60), seconds < 10 ? '0' + seconds : seconds].join(':')
+    return [Math.floor(time / 60), seconds < 10 ? '0' + seconds : seconds].join(':');
   } else {
     seconds = Math.abs(seconds);
-    return ['-' + Math.floor(time/60), seconds < 10 ? '0' + seconds : seconds].join(':')
+    return ['-' + Math.floor(time / 60), seconds < 10 ? '0' + seconds : seconds].join(':');
   }
 }
