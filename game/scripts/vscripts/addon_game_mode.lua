286e1f479af44b94d74044736f0a3ec745b5b83a
carlosrpg <carlos.h.rodrigues.a@gmail.com>
2017-12-06 20:09:06 -0200

    Add SiltBreaker Ogre Tank Boss (#1506)
    * First implementation for SiltBreaker Ogre Tank Boss
    
    The models and particles NEEDS to be at that place because the main model seems to reference them from itself and I don't know how to change it for now.
    
    Most the code is copied from dungeon dota_addon
    
    * Fix unit tests
    
    add tooltip and Ids to fix unit test
    
    * Fix Code standard
    
    * Fuck Valve white spaces
    
    * remove white line
    
    * Fix Code Review Requests
    
    * Add Ogre Boss Minions
    
    * Remove Spawn test (wrong commit) and remove global variable
    
    * Move precache line into seer section
    
    * Add missing precompiled files
    
    This is a temporary solution. The ideal would be do decompile everything and put it in content folder.
    Should be testable now.
    
    * Configure ogre boss
    
    * Replacing precompiled particles and sound by its decompiled parts
    
    Could not decompile vmat_c and vmdl_c.
    The ok should be green to go.
    Please make sure you are downloading every file because the .gitignore needed changes

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -54,10 +54,15 @@ function Precache( context )
   PrecacheUnitByNameSync("npc_dota_boss_simple_6", context)
   PrecacheUnitByNameSync("npc_dota_boss_simple_7", context)
   PrecacheUnitByNameSync("npc_dota_boss_stopfightingyourself", context)
+  PrecacheUnitByNameSync("npc_dota_creature_ogre_tank_boss", context)
 
   -- Ambient Sounds
   PrecacheResource("soundfile", "soundevents/ambient/doors.vsndevts", context)
   PrecacheResource("soundfile", "soundevents/music/music.vsndevts", context)
+
+  PrecacheResource("soundfile", "soundevents/game_sounds_creeps.vsndevts", context)
+  PrecacheResource("soundfile", "soundevents/bosses/game_sounds_dungeon_enemies.vsndevts", context)
+  PrecacheResource("soundfile", "soundevents/game_sounds_heroes/game_sounds_phantom_assassin.vsndevts", context)
 
   -- precache all hero econ folders
   -- this makes immortals and stuff work
