25d317eaa343af659719017713820f5bd5827ea4
Haganeko <haganekko@gmail.com>
2017-12-07 16:38:48 -0200

    attempt at fixing lua snafu

--- game/scripts/vscripts/items/allied_cyclone.lua
+++ game/scripts/vscripts/items/allied_cyclone.lua
@@ -15,7 +15,7 @@ function allied_cyclone_Purge(keys)
 	if keys.caster:GetTeam() == keys.target:GetTeam() then
 		keys.target:Purge(false,true,false,false,false)
   else
-    if keys.target:TriggerSpellAbsorb(self) then
+    if keys.target:TriggerSpellAbsorb(keys.ability) then
       return
     end
     keys.target:Purge(true, false, false, false, false)
