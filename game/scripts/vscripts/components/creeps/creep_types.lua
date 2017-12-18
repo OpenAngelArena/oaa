84f7ab24d4e1127e81f6e20684ac0f0154b701eb
Haganeko <haganekko@gmail.com>
2017-12-18 16:04:05 -0200

    Nerf creeps a tiny tiny amount (#1622)
    * reduced chance of Centaurs
    
    * added more creeps options to hard camp
    
    * slightly reduced hard camp HP
    
    * added more creeps options to ancient camp
    
    * slightly reduced ancient camp HP

--- game/scripts/vscripts/components/creeps/creep_types.lua
+++ game/scripts/vscripts/components/creeps/creep_types.lua
@@ -38,48 +38,49 @@ CreepTypes = {
     -- 3 "hard camp"
   {
     {                                              --HP   MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_custom_big_horse",        1200,  400,  30,    2,    90,  31}, --expected gold is 140 and XP is 62
-      {"npc_dota_neutral_custom_small_horse",       800,  240,  20,    1,    50,  31},
+      {"npc_dota_neutral_custom_big_horse",        1100,  400,  30,    2,    90,  31}, --expected gold is 140 and XP is 62
+      {"npc_dota_neutral_custom_small_horse",       700,  240,  20,    1,    50,  31},
     },
     {
-      {"npc_dota_neutral_custom_big_horse",        1200,  400,  30,    2,    90,  31},
-      {"npc_dota_neutral_custom_small_horse",       800,  240,  20,    1,    50,  31},
-    },
-    {
-      {"npc_dota_neutral_custom_big_horse",        1200,  400,  30,    2,    90,  31},
-      {"npc_dota_neutral_custom_small_horse",       800,  240,  20,    1,    50,  31},
-    },
-    {
-      {"npc_dota_neutral_custom_big_horse",        1200,  400,  30,    2,    90,  31},
-      {"npc_dota_neutral_custom_small_horse",       800,  240,  20,    1,    50,  31},
-    },
-    {
-      {"npc_dota_neutral_custom_small_pupper",      640,  160,  15,    1,    30,  21},
-      {"npc_dota_neutral_custom_small_pupper",      420,  160,  15,    1,    30,  21},
-      {"npc_dota_neutral_custom_big_pupper",        800,  480,  35,    3,   100,  34}
+      {"npc_dota_neutral_custom_big_horse",        1100,  400,  30,    2,    90,  31},
+      {"npc_dota_neutral_custom_small_horse",       700,  240,  20,    1,    50,  31},
     },
     {
       {"npc_dota_neutral_custom_small_pupper",      420,  160,  15,    1,    30,  21},
-      {"npc_dota_neutral_custom_big_pupper",        800,  480,  35,    3,   100,  34}
+      {"npc_dota_neutral_custom_small_pupper",      420,  160,  15,    1,    30,  21},
+      {"npc_dota_neutral_custom_big_pupper",        700,  480,  35,    3,   100,  34}
     },
     {
       {"npc_dota_neutral_custom_small_pupper",      420,  160,  15,    1,    30,  21},
-      {"npc_dota_neutral_custom_big_pupper",        800,  480,  35,    3,   100,  34}
+      {"npc_dota_neutral_custom_big_pupper",        700,  480,  35,    3,   100,  34}
+    },
+    {
+      {"npc_dota_neutral_custom_small_pupper",      420,  160,  15,    1,    30,  21},
+      {"npc_dota_neutral_custom_big_pupper",        700,  480,  35,    3,   100,  34}
+    },
+    {
+      {"npc_dota_neutral_satyr_tickster",           300,  160,  10,    1,    20,  11},
+      {"npc_dota_neutral_satyr_soulstealer",        400,  480,  20,    1,    40,  20},
+      {"npc_dota_neutral_satyr_hellcaller",         600,  480,  30,   1.5,   80,  31}
     }
   },
    -- 4 "ancient camp"
   {
     {                                               --HP  MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_granite_golem",           1600,    0,  50,    3,   166,  44}, --expected gold is 332 and XP is 86
-      {"npc_dota_neutral_rock_golem",              1200,    0,  40,    2,    83,  20},
-      {"npc_dota_neutral_rock_golem",              1200,    0,  40,    2,    83,  20}
+      {"npc_dota_neutral_granite_golem",           1500,    0,  50,    3,   166,  44}, --expected gold is 332 and XP is 86
+      {"npc_dota_neutral_rock_golem",              1100,    0,  40,    2,    83,  20},
+      {"npc_dota_neutral_rock_golem",              1100,    0,  40,    2,    83,  20}
     },
     {
-      {"npc_dota_neutral_granite_golem",           1600,    0,  50,    3,   166,  44},
-      {"npc_dota_neutral_granite_golem",           1600,    0,  40,    3,   166,  44}
+      {"npc_dota_neutral_granite_golem",           1500,    0,  50,    3,   166,  44},
+      {"npc_dota_neutral_granite_golem",           1500,    0,  40,    3,   166,  44}
     },
     {
-      {"npc_dota_neutral_custom_black_dragon",     1840,    0,  80,    3,   332,  86}
+      {"npc_dota_neutral_prowler_acolyte",          900,    0,  30,    1,   112,  29},
+      {"npc_dota_neutral_prowler_shaman",          1200,    0,  60,    2,   220,  57}
+    },
+    {
+      {"npc_dota_neutral_custom_black_dragon",     1800,    0,  80,    3,   332,  86}
     }
   }
 }
