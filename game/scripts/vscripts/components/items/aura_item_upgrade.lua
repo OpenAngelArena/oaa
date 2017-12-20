d5b745a948d4b0947353b767a8ff19322b3f85d7
carlosrpg <carlos.h.rodrigues.a@gmail.com>
2017-12-20 16:00:11 -0200

    "Fix" aura upgrade items (#1615)
    * Create Modifier to remove upgraded item and readd it 1 second later
    
    * Add Aura upgrade modifier only for items with aura
    
    * Implement CodeReview Suggestions
    
    * Fix LinkLuaModifier

---
+++ game/scripts/vscripts/components/items/aura_item_upgrade.lua
@@ -0,0 +1,16 @@
+
+LinkLuaModifier( "modifier_aura_item_upgrade", "modifiers/modifier_aura_item_upgrade.lua", LUA_MODIFIER_MOTION_NONE )
+
+GameEvents:OnItemCombined(function (keys)
+    -- The playerID of the hero who is buying something
+    local plyID = keys.PlayerID
+    if not plyID then return end
+    local player = PlayerResource:GetPlayer(plyID)
+
+    -- The name of the item purchased
+    local itemName = keys.itemname
+
+    local hero = player:GetAssignedHero()
+    local hthinker = CreateModifierThinker( hero, nil , "modifier_aura_item_upgrade", { ItemName = itemName, PlayerId = plyID}, hero:GetOrigin(), hero:GetTeamNumber(), false )
+end)
+
