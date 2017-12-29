78f97a76d97bbc597345a970a6cbf9b0e9a8e87a
Chris Vickery <chrisinajar@gmail.com>
2017-12-29 03:10:15 -0800

    Add reserve time to captains mode

--- game/scripts/vscripts/settings.lua
+++ game/scripts/vscripts/settings.lua
@@ -17,6 +17,7 @@ SCAN_REVEAL_DURATION = 14
 CAPTAINS_MODE_CAPTAIN_TIME = 20           -- how long players have to claim the captain chair
 CAPTAINS_MODE_PICK_BAN_TIME = 30          -- how long you have to do each pick/ban
 CAPTAINS_MODE_HERO_PICK_TIME = 20         -- time to choose which hero you're going to play
+CAPTAINS_MODE_RESERVE_TIME = 130          -- total bonus time that can be used throughout any selection
 
 -- Game timings
 PREGAME_TIME = 10
