2d39877b7e4784145fe91b1c1d3716725d71ec4e
Chris Vickery <chrisinajar@gmail.com>
2017-12-29 03:54:15 -0800

    Add reserve time to captains mode (#1666)

--- game/scripts/vscripts/components/heroselection/cmpickorder.lua
+++ game/scripts/vscripts/components/heroselection/cmpickorder.lua
@@ -4,9 +4,10 @@ cmpickorder = {
   ["currentstage"] = 0,
   ["captainradiant"] = "empty",
   ["captaindire"] = "empty",
+  ["reserveradiant"] = CAPTAINS_MODE_RESERVE_TIME,
+  ["reservedire"] = CAPTAINS_MODE_RESERVE_TIME,
 
   ["order"] = {
-
     {
       ["type"] = "Ban",
       ["side"] = DOTA_TEAM_GOODGUYS,
