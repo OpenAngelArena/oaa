0a71f2910576e77dfb801c73ae513671f7408f91
Chris Vickery <chrisinajar@gmail.com>
2017-12-29 10:22:24 -0800

    Give golden bottles to tournament winners (#1667)
    * Give golden bottles to tournament winners
    
    * Keep colors forever
    
    * Give baumi an arcane rune

--- game/scripts/vscripts/items/bottle.lua
+++ game/scripts/vscripts/items/bottle.lua
@@ -1,8 +1,33 @@
 LinkLuaModifier("modifier_bottle_regeneration", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_bottle_texture_tracker", "items/bottle.lua", LUA_MODIFIER_MOTION_NONE)
+
+--------------------------------------------------------------------------------
+
+local special_bottles = {
+  -- haste rune devs
+  [7131038] = 1, -- chrisinajar
+  [43305444] = 3, -- baumi
+  -- golden tournament winners
+  -- OTA except for nael
+  [124585666] = 2,
+  [75435056] = 2,
+  [136897804] = 2,
+  [57898114] = 2
+}
+
+local bonusNames = {
+  'haste',
+  'bounty',
+  'arcane'
+}
 
 --------------------------------------------------------------------------------
 
 item_infinite_bottle = class(ItemBaseClass)
+
+function item_infinite_bottle:GetIntrinsicModifierName()
+  return "modifier_bottle_texture_tracker"
+end
 
 function item_infinite_bottle:OnSpellStart()
   local restore_time = self:GetSpecialValueFor("restore_time")
@@ -20,7 +45,47 @@ function item_infinite_bottle:OnSpellStart()
 end
 
 function item_infinite_bottle:GetAbilityTextureName()
+  if self.bonus then
+    return "bottle_" .. self.bonus
+  end
+  if self.mod and not self.mod:IsNull() then
+    local stacks = self.mod:GetStackCount()
+    if stacks > 0 then
+      self.bonus = bonusNames[self.mod:GetStackCount()]
+      return "bottle_" .. self.bonus
+    end
+  end
   return "item_bottle"
 end
 
 --------------------------------------------------------------------------------
+
+Debug:EnableDebugging()
+
+modifier_bottle_texture_tracker = class(ModifierBaseClass)
+
+function modifier_bottle_texture_tracker:OnCreated()
+  local parent = self:GetParent()
+  local item = self:GetAbility()
+  item.mod = self
+
+  if IsServer() then
+    local playerID = parent:GetPlayerOwnerID()
+    local steamid = PlayerResource:GetSteamAccountID(playerID)
+    DebugPrint(steamid)
+
+    self:SetStackCount(0)
+
+    if special_bottles[steamid] then
+      self:SetStackCount(special_bottles[steamid])
+    end
+  end
+end
+
+function modifier_bottle_texture_tracker:GetAttributes()
+  return MODIFIER_ATTRIBUTE_MULTIPLE
+end
+
+function modifier_bottle_texture_tracker:IsHidden()
+  return true
+end
