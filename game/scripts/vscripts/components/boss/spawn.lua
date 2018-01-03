da9e3f84e73f53e75eb99984cd2a2516eab456e2
MasterGordon <gologo.x@gmail.com>
2018-01-03 20:12:14 +0100

    Update spawn.lua

--- game/scripts/vscripts/components/boss/spawn.lua
+++ game/scripts/vscripts/components/boss/spawn.lua
@@ -110,13 +110,13 @@ function BossSpawner:SpawnBoss (pit, boss, bossTier, isProtected)
   local core = CreateItem("item_upgrade_core", bossHandle, bossHandle)
 
   if(bossTier==1) then
-    core = CreateItem("item_upgrade_core2", bossHandle, bossHandle)
+    core = CreateItem("item_upgrade_core_2", bossHandle, bossHandle)
   end
   if(bossTier==2) then
-    core = CreateItem("item_upgrade_core3", bossHandle, bossHandle)
+    core = CreateItem("item_upgrade_core_3", bossHandle, bossHandle)
   end
   if(bossTier>=3) then
-    core = CreateItem("item_upgrade_core4", bossHandle, bossHandle)
+    core = CreateItem("item_upgrade_core_4", bossHandle, bossHandle)
   end
 
   local resistance = bossHandle:FindAbilityByName("boss_resistance")
