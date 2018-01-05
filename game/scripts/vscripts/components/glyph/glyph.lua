76d908b62801390a31118688cd6cdb59783673af
Soo Kuo-Yi <jaydonsky@gmail.com>
2018-01-06 04:48:56 +0800

    Expose vanilla Scan component duration in settings.lua

--- game/scripts/vscripts/components/glyph/glyph.lua
+++ game/scripts/vscripts/components/glyph/glyph.lua
@@ -125,7 +125,7 @@ function Glyph:CastScan(playerID, keys)
   local position = Vector(keys.position_x, keys.position_y, keys.position_z)
 
   CreateModifierThinker( hero, nil, "modifier_scan_true_sight_thinker", {duration = SCAN_REVEAL_DURATION}, position, hero:GetTeamNumber(), false )
-  CreateModifierThinker( hero, nil, "modifier_radar_thinker", {duration = 8}, position, hero:GetTeamNumber(), false )
+  CreateModifierThinker( hero, nil, "modifier_radar_thinker", {duration = SCAN_DURATION}, position, hero:GetTeamNumber(), false )
 
   self:ResetScanCooldown(playerID)
 
