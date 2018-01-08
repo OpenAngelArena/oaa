ad6414d5f634f96801a573c8d9175a68486e724e
Soo Kuo-Yi <ksoo11@student.monash.edu>
2018-01-09 04:26:09 +0800

    Fix Drums and Greater Greaves aura upgrading (#1737)
    * Fix aura item upgrade not running for Drums of Endurance
    
    * Make aura item upgrade run for level 1 Greater Guardian Greaves
    
    Since GGG could be upgraded from regular Guardian Greaves and uses the same modifier.

--- game/scripts/vscripts/modifiers/modifier_aura_item_upgrade.lua
+++ game/scripts/vscripts/modifiers/modifier_aura_item_upgrade.lua
@@ -20,9 +20,9 @@ if IsServer() then
   function modifier_aura_item_upgrade:OnCreated( kv )
     local auraItems =
     {
-      "item_greater_guardian_greaves_",
+      "item_greater_guardian_greaves",
       "item_greater_travel_boots_",
-      "item_ancient_jangoo_of_endurance_",
+      "item_ancient_janggo_of_endurance_",
       "item_assault_",
       "item_crimson_pipe_",
       "item_helm_of_the_dominator_",
