15288c5ed295490e3f0df35aed2d5a72b49b6a3a
imaGecko <florian2204@yahoo.de>
2018-01-11 20:09:43 +0100

    init

--- game/scripts/vscripts/components/boss/bosses.lua
+++ game/scripts/vscripts/components/boss/bosses.lua
@@ -4,9 +4,16 @@ Bosses = {
     "npc_dota_boss_tier_1",
     "npc_dota_boss_twin",
     "npc_dota_creature_ogre_tank_boss",
-    "npc_dota_boss_tier_4",
-    "npc_dota_creature_temple_guardian_spawner",
-    "npc_dota_boss_tier_6"
+    {
+      "npc_dota_boss_tier_4",
+      "npc_dota_creature_spider_boss_1",
+      "npc_dota_boss_stopfightingyourself_1"
+    },
+    {
+      "npc_dota_boss_tier_6",
+      "npc_dota_creature_spider_boss_2",
+      "npc_dota_boss_stopfightingyourself_2"
+    },
   },
   -- tier 1
   {
@@ -22,14 +29,24 @@ Bosses = {
     },
     {
       "npc_dota_creature_ogre_tank_boss",
-      "npc_dota_boss_charger",
+      "npc_dota_boss_charger"
     },
     {
       "npc_dota_boss_tier_4",
-      "npc_dota_creature_spider_boss"
+      "npc_dota_creature_spider_boss_1",
+      "npc_dota_creature_temple_guardian_1",
+      "npc_dota_boss_stopfightingyourself_1"
     },
-    "npc_dota_creature_temple_guardian_spawner",
-    "npc_dota_boss_stopfightingyourself"
+    {
+      "npc_dota_boss_stopfightingyourself_t5"
+      "npc_dota_boss_charger_t5"
+      "npc_dota_boss_twin_t5",
+      "npc_dota_boss_shielder_t5",
+      "npc_dota_creature_lycan_boss_t5"
+      "npc_dota_creature_ogre_tank_boss_t5",
+      "npc_dota_creature_spider_boss_t5",
+      "npc_dota_creature_temple_guardian_t5",
+    },
   },
   -- tier 1
   {
@@ -45,14 +62,20 @@ Bosses = {
     },
     {
       "npc_dota_creature_ogre_tank_boss",
-      "npc_dota_boss_charger",
+      "npc_dota_boss_charger"
     },
     {
       "npc_dota_boss_tier_4",
-      "npc_dota_creature_spider_boss"
+      "npc_dota_creature_spider_boss_1",
+      "npc_dota_creature_temple_guardian_1",
+      "npc_dota_boss_stopfightingyourself_1"
     },
-    "npc_dota_creature_temple_guardian_spawner",
-    "npc_dota_boss_stopfightingyourself"
+    {
+      "npc_dota_boss_tier_6",
+      "npc_dota_creature_spider_boss_2",
+      "npc_dota_creature_temple_guardian_2",
+      "npc_dota_boss_stopfightingyourself_2"
+    },
   },
   -- tier 2
   {
@@ -63,14 +86,20 @@ Bosses = {
     },
     {
       "npc_dota_creature_ogre_tank_boss",
-      "npc_dota_boss_charger",
+      "npc_dota_boss_charger"
     },
     {
       "npc_dota_boss_tier_4",
-      "npc_dota_creature_spider_boss"
+      "npc_dota_creature_spider_boss_1",
+      "npc_dota_creature_temple_guardian_1",
+      "npc_dota_boss_stopfightingyourself_1"
     },
-    "npc_dota_creature_temple_guardian_spawner",
-    "npc_dota_boss_stopfightingyourself"
+    {
+      "npc_dota_boss_tier_6",
+      "npc_dota_creature_spider_boss_2",
+      "npc_dota_creature_temple_guardian_2",
+      "npc_dota_boss_stopfightingyourself_2"
+    },
   },
   -- tier 3
   {
@@ -80,10 +109,16 @@ Bosses = {
     },
     {
       "npc_dota_boss_tier_4",
-      "npc_dota_creature_spider_boss"
+      "npc_dota_creature_spider_boss_1",
+      "npc_dota_creature_temple_guardian_1",
+      "npc_dota_boss_stopfightingyourself_1"
     },
-    "npc_dota_creature_temple_guardian_spawner",
-    "npc_dota_boss_stopfightingyourself"
+    {
+      "npc_dota_boss_tier_6",
+      "npc_dota_creature_spider_boss_2",
+      "npc_dota_creature_temple_guardian_2",
+      "npc_dota_boss_stopfightingyourself_2"
+    },
   },
   -- unused
   {
