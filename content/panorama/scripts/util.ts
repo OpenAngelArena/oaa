661837a9cbd81d9cf4a936d0df23a72f8b9b8957
Soo Kuo-Yi <jaydonsky@gmail.com>
2018-01-16 14:21:22 +0800

    Port Panorama util.js to TypeScript
    In the process, changed GetDotaHud function to throw an exception when it can't find the root Hud panel instead of silently returning `undefined`.
    Also added ColorToHexCode and ColoredText utility functions.

--- content/panorama/scripts/util.ts
+++ content/panorama/scripts/util.ts
@@ -1,28 +1,32 @@
 /* global $, GameUI, Entities, Players */
 'use strict';
+
 /*
   Author:
     Angel Arena Blackstar
   Credits:
     Angel Arena Blackstar
 */
+
 if (module !== undefined && module.exports) {
   module.exports.FindDotaHudElement = FindDotaHudElement;
   module.exports.ColorToHexCode = ColorToHexCode;
   module.exports.ColoredText = ColoredText;
 }
-var HudNotFoundException = /** @class */ (function () {
-  function HudNotFoundException (message) {
-    this.message = message;
+
+class HudNotFoundException {
+  constructor(readonly message: string) {
   }
-  return HudNotFoundException;
-}());
+}
+
 var PlayerTables = GameUI.CustomUIConfig().PlayerTables;
-function FindDotaHudElement (id) {
+
+function FindDotaHudElement(id: string): Panel {
   return GetDotaHud().FindChildTraverse(id);
 }
-function GetDotaHud () {
-  var p = $.GetContextPanel();
+
+function GetDotaHud() {
+  var p: Panel | null = $.GetContextPanel();
   while (p !== null && p.id !== 'Hud') {
     p = p.GetParent();
   }
@@ -32,15 +36,18 @@ function GetDotaHud () {
     return p;
   }
 }
+
 /**
  * Takes an integer and returns a hex code string of the color represented by the integer
  */
-function ColorToHexCode (color) {
-  var red = (color & 0xff).toString(16);
-  var green = ((color & 0xff00) >> 8).toString(16);
-  var blue = ((color & 0xff0000) >> 16).toString(16);
+function ColorToHexCode(color: number): string {
+  let red = (color & 0xff).toString(16);
+  let green = ((color & 0xff00) >> 8).toString(16);
+  let blue = ((color & 0xff0000) >> 16).toString(16);
   return '#' + red + green + blue;
 }
-function ColoredText (colorCode, text) {
-  return '<font color="' + colorCode + '">' + text + '</font>';
+
+function ColoredText(colorCode: string, text: string): string {
+  return `<font color="${colorCode}">${text}</font>`
 }
+// SNIPPET END
