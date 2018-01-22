0e1300f87261375afbed8be163cd6696b46baf8f
ThatMusicWriter <35638904+ThatMusicWriter@users.noreply.github.com>
2018-01-22 11:37:46 -0500

    Cave Rune Double Damage LUA
    This one is a little rough; I may need to add dispel data and such.

---
+++ game/scripts/vscripts/items/cave_rune_hasted_doubledamage.lua
@@ -0,0 +1,22 @@
+LinkLuaModifier("modifier_rune_haste", LUA_MODIDIER_MOTION_NONE)
+LinkLuaModifier("modifier_rune_doubledamage", LUA_MODIDIER_MOTION_NONE)
+
+item_cave_rune_hasted_doubledamage = class(ItemBaseClass)
+
+function item_cave_rune_hasted_doubledamage:OnSpellStart()
+	local caster = self:Getcaster()
+
+	caster:AddNewModifier(caster, self, "modifier_rune_haste", {duration = 30.0})
+
+function item_cave_rune_hasted_doubledamage:OnSpellStart()
+	caster:AddNewModifier(caster, self, "modifier_rune_doubledamage", {duration = 30.0})
+
+	caster:RemoveItem(self)
+	if self:GetCurrentCharges() - 1 <= 0 then
+    caster:RemoveItem(self)
+  else
+    self:SetCurrentCharges(self:GetCurrentCharges() - 1)
+
+	caster:EmitSound( "DOTA_Item.BlackKingBar.Activate" )
+
+end
