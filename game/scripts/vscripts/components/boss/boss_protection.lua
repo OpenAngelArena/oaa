d48950df3f9e6d14d81dd8e342075d75bbe155bb
carlos_rpg <carlos.h.rodrigues.a@gmail.com>
2018-01-23 00:09:51 -0200

    Add Code review changes

--- game/scripts/vscripts/components/boss/boss_protection.lua
+++ game/scripts/vscripts/components/boss/boss_protection.lua
@@ -1,116 +1,107 @@
 -- Component for handling the ModifierGained Filter used to block silence and stuns on Silt Bosses
+require("internal/util")
 
 if not BossProtectionFilter then
   DebugPrint("Creating filter for Preemptive protect from stun")
+
   BossProtectionFilter = class({})
 
   BossProtectionFilter.SilenceSpells = {
-    'bloodseeker_blood_bath',
-    'death_prophet_silence',
-    'disruptor_static_storm',
-    'doom_bringer_doom',
-    'drow_ranger_silence',
-    'earth_spirit_geomagnetic_grip',
-    'enigma_black_hole',
-    'legion_commander_duel',
-    'lone_druid_savage_roar',
-    'night_stalker_crippling_fear',
-    'puck_waning_rift',
-    'riki_smoke_screen',
-    'silencer_global_silence',
-    'silencer_last_word',
-    'skywrath_mage_ancient_seal',
-    'techies_suicide',
-    'viper_nethertoxin'
+    bloodseeker_blood_bath = true,
+    death_prophet_silence = true,
+    disruptor_static_storm = true,
+    doom_bringer_doom = true,
+    drow_ranger_silence = true,
+    earth_spirit_geomagnetic_grip = true,
+    enigma_black_hole = true,
+    legion_commander_duel = true,
+    lone_druid_savage_roar = true,
+    night_stalker_crippling_fear = true,
+    puck_waning_rift = true,
+    riki_smoke_screen = true,
+    silencer_global_silence = true,
+    silencer_last_word = true,
+    skywrath_mage_ancient_seal = true,
+    techies_suicide = true,
+    viper_nethertoxin = true
   }
 
   BossProtectionFilter.SilenceItems = {
-    'item_bloodthorn',
-    'item_orchid'
+    item_bloodthorn = true,
+    item_orchid = true
   }
 
-  -- uses unique modifiers to apply stun
+  -- uses unique modifiers to apply stun (not modifier_bashed)
   BossProtectionFilter.UniqueBashSpell = {
-    'faceless_void_time_lock',
-    'faceless_void_time_lock_oaa',
-    'spirit_breaker_greater_bash'
+    faceless_void_time_lock = true,
+    faceless_void_time_lock_oaa = true,
+    spirit_breaker_greater_bash = true
   }
 
-  -- uses unique modifiers to apply stun
+  -- uses unique modifiers to apply stun (not modifier_stunned)
   BossProtectionFilter.UniqueStunSpells = {
-    'ancient_apparition_cold_feet',
-    'bane_nightmare',
-    'bane_fiends_grip',
-    'batrider_flaming_lasso',
-    'beastmaster_primal_roar',
-    'rattletrap_power_cogs',
-    'dark_seer_vacuum',
-    'earth_spirit_petrify',
-    'earthshaker_fissure',
-    'elder_titan_echo_stomp',
-    'enigma_black_hole',
-    'faceless_void_chronosphere',
-    'invoker_cold_snap',
-    'invoker_tornado',
-    'jakiro_ice_path',
-    'kunkka_torrent',
-    'lion_impale',
-    'magnataur_skewer',
-    'medusa_mystic_snake',
-    'medusa_stone_gaze',
-    'monkey_king_boundless_strike',
-    'morphling_adaptive_strike_str',
-    'naga_siren_song_of_the_siren',
-    'necrolyte_reapers_scythe',
-    'nyx_assassin_spiked_carapace',
-    'nyx_assassin_impale',
-    'obsidian_destroyer_astral_imprisonment',
-    'pangolier_gyroshell',
-    'pudge_meat_hook',
-    'pudge_dismember',
-    'sandking_burrowstrike',
-    'shadow_demon_disruption',
-    'shadow_shaman_shackles',
-    'brewmaster_storm_cyclone',
-    'storm_spirit_electric_vortex',
-    'tidehunter_ravage',
-    'tiny_avalanche',
-    'tiny_toss',
-    'tusk_walrus_punch',
-    'tusk_walrus_kick',
-    'windrunner_shackleshot',
-    'winter_wyvern_cold_embrace',
-    'winter_wyvern_winters_curse'
+    ancient_apparition_cold_feet = true,
+    bane_nightmare = true,
+    bane_fiends_grip = true,
+    batrider_flaming_lasso = true,
+    beastmaster_primal_roar = true,
+    rattletrap_power_cogs = true,
+    dark_seer_vacuum = true,
+    earth_spirit_petrify = true,
+    earthshaker_fissure = true,
+    elder_titan_echo_stomp = true,
+    enigma_black_hole = true,
+    faceless_void_chronosphere = true,
+    invoker_cold_snap = true,
+    invoker_tornado = true,
+    jakiro_ice_path = true,
+    kunkka_torrent = true,
+    lion_impale = true,
+    magnataur_skewer = true,
+    medusa_mystic_snake = true,
+    medusa_stone_gaze = true,
+    monkey_king_boundless_strike = true,
+    morphling_adaptive_strike_str = true,
+    naga_siren_song_of_the_siren = true,
+    necrolyte_reapers_scythe = true,
+    nyx_assassin_spiked_carapace = true,
+    nyx_assassin_impale = true,
+    obsidian_destroyer_astral_imprisonment = true,
+    pangolier_gyroshell = true,
+    pudge_meat_hook = true,
+    pudge_dismember = true,
+    sandking_burrowstrike = true,
+    shadow_demon_disruption = true,
+    shadow_shaman_shackles = true,
+    brewmaster_storm_cyclone = true,
+    storm_spirit_electric_vortex = true,
+    tidehunter_ravage = true,
+    tiny_avalanche = true,
+    tiny_toss = true,
+    tusk_walrus_punch = true,
+    tusk_walrus_kick = true,
+    windrunner_shackleshot = true,
+    winter_wyvern_cold_embrace = true,
+    winter_wyvern_winters_curse = true
   }
 
   BossProtectionFilter.UniqueStunItems = {
-    'item_allied_cyclone'
+    item_allied_cyclone = true
   }
 
   BossProtectionFilter.HexItems = {
-    'item_sheepstick'
+    item_sheepstick = true
   }
 
   BossProtectionFilter.HexSpells = {
-    'lion_voodoo',
-    'shadow_shaman_voodoo'
+    lion_voodoo = true,
+    shadow_shaman_voodoo = true
   }
-
 
 end
 
 function BossProtectionFilter:Init()
   FilterManager:AddFilter(FilterManager.ModifierGained, self, Dynamic_Wrap(self, "ModifierGainedFilter"))
-end
-
-function BossProtectionFilter:TableContainValue(table, value)
-  for key, val in ipairs(table) do
-    if val == value then
-      print("Found :" .. value )
-      return true
-    end
-  end
-  return false
 end
 
 function BossProtectionFilter:ModifierGainedFilter(keys)
@@ -136,9 +127,9 @@ function BossProtectionFilter:ModifierGainedFilter(keys)
 
   -- protected boss should never be bashed or silenced
   if keys.name_const == 'modifier_bashed'
-      or BossProtectionFilter:TableContainValue(BossProtectionFilter.UniqueBashSpell, keys.abilityName)
-      or BossProtectionFilter:TableContainValue(BossProtectionFilter.SilenceSpells, keys.abilityName)
-      or BossProtectionFilter:TableContainValue(BossProtectionFilter.SilenceItems, keys.abilityName) then
+      or BossProtectionFilter.UniqueBashSpell[keys.abilityName]
+      or BossProtectionFilter.SilenceSpells[keys.abilityName]
+      or BossProtectionFilter.SilenceItems[keys.abilityName] then
 
     print("Bash/Silence blocked!")
     return false
@@ -147,13 +138,13 @@ function BossProtectionFilter:ModifierGainedFilter(keys)
   -- if boss has active protection block all stuns
   if parent:HasModifier("modifier_siltbreaker_boss_protection") then
     if keys.name_const == 'modifier_stunned'
-        or BossProtectionFilter:TableContainValue(BossProtectionFilter.UniqueStunSpells, keys.abilityName)
-        or BossProtectionFilter:TableContainValue(BossProtectionFilter.UniqueStunItems, keys.abilityName)
-        or BossProtectionFilter:TableContainValue(BossProtectionFilter.HexSpells, keys.abilityName)
-        or BossProtectionFilter:TableContainValue(BossProtectionFilter.HexItems, keys.abilityName) then
+        or BossProtectionFilter.UniqueStunSpells[keys.abilityName]
+        or BossProtectionFilter.UniqueStunItems[keys.abilityName]
+        or BossProtectionFilter.HexSpells[keys.abilityName]
+        or BossProtectionFilter.HexItems[keys.abilityName] then
 
       print("Stun blocked!")
-      return true
+      return false
     end
   end
 
