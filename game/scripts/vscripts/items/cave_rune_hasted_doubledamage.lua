215de8ef30418937293b9797f68d6b38f3dfb7e0
ThatMusicWriter <pjfitzy1120@gmail.com>
2018-01-25 23:11:06 -0500

    Revert "Moriah fix"
    This reverts commit c96e3361139273173021a1273107cfe406122738.

---
+++ game/scripts/vscripts/items/cave_rune_hasted_doubledamage.lua
@@ -0,0 +1,12 @@
+item_cave_rune_hasted_doubledamage = class(ItemBaseClass)
+
+
+function item_cave_rune_hasted_doubledamage:OnSpellStart()
+	local caster = self:Getcaster()
+
+	caster:AddNewModifier(caster, self, "modifier_rune_haste_doubledamage", {duration = self:GetSpecialValueFor("duration")})
+
+	caster:EmitSound( "sounds/items/rune_haste.vsnd" )
+
+	caster:RemoveItem(self)
+end
