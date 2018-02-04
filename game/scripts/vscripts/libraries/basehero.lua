01e90d37f80ed458fa0ba8fda128cf1d7b6e8de8
Soo Kuo-Yi <jaydonsky@gmail.com>
2018-02-04 22:51:25 +0800

    Add wrapper for hero:AddExperience
    This is to make it go through the ModifyXP filter since for some reason Valve apparently didn't make it do so.

--- game/scripts/vscripts/libraries/basehero.lua
+++ game/scripts/vscripts/libraries/basehero.lua
@@ -7,6 +7,18 @@ function CDOTA_BaseNPC_Hero:ModifyGold (playerID, goldAmmt, reliable, nReason)
   return Gold:ModifyGold(playerID, goldAmmt, reliable, nReason)
 end
 
+CDOTA_BaseNPC_Hero.UnfilteredAddExperience = CDOTA_BaseNPC_Hero.UnfilteredAddExperience or CDOTA_BaseNPC_Hero.AddExperience
+function CDOTA_BaseNPC_Hero:AddExperience(flXP, nReason, bApplyBotDifficultyScaling, bIncrementTotal)
+  local eventData = {
+    experience = flXP,
+    player_id_const = self:GetPlayerOwnerID(),
+    reason_const = nReason
+  }
+  if FilterManager:RunFilterForType(eventData, FilterManager.ModifyExperience) then
+    self:UnfilteredAddExperience(flXP, nReason, bApplyBotDifficultyScaling, bIncrementTotal)
+  end
+end
+
 function CDOTA_BaseNPC_Hero:GetBaseRangedProjectileName()
   if not IsServer() then
     return ""
