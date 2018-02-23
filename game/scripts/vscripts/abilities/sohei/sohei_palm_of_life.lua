b5235587d8ca7ece623918256411fcb0121bded1
SquawkyArctangent <arctangentpuckett@gmail.com>
2018-02-22 18:55:37 -0600

    hugeass sohei refactor + recode
    prepare for conflicts
    
    - Refactored basically the entire code, including organizing ability
    code into separate files.
    - Dash and Guard should no longer be castable when they shouldn't; they
    were previously, but they tried to fake not being casted by refunding
    cooldown and mana cost, but stuff like Nether Ward and Magic Wand would
    still react to them.
    - Guard now blocks more than one spell, no longer makes Sohei
    unkillable, and Sohei will now properly avoid ranged attack damage
    entirely instead of healing it back ( previously ranged attack damage
    would stick during Ice Blast / Covfefe due to the method used )
    - Modifiers that move units ( Dash, Guard talent knockback, Momentum
    knockback ) are now proper motion controllers and thus will interact
    properly with other motion controllers.
    - Momentum now checks for unpathable terrain instead of just terrain
    that's of a different height.
    - Momentum attacks will always do both the critical damage and the
    knockback, instead of sometimes only performing the knockback in rare
    situations.
    - Momentum now has cooldown support; it will neither build charges while
    on cooldown nor can it proc while on cooldown. This means
    trigger_distance can be set to 0 and it'll operate strictly on its
    cooldown, while also allowing for its cooldown and trigger_distance to
    operate in conjuction.
    - Dash now has a "momentum_cd_refund" AbilitySpecial, which will cause
    value% of momentum's current cooldown to be immediately refunded when
    Dash is cast.
    - Flurry of Blows is now a channel.
    - New Aghs effect: Adds a new ability, Palm of Life, which uses a Dash
    charge to dash directly to an ally at 600 range. If Momentum is ready
    upon cast, it is discharged to heal the target for 10% of Sohei's
    current health when he reaches them.

---
+++ game/scripts/vscripts/abilities/sohei/sohei_palm_of_life.lua
@@ -0,0 +1,233 @@
+sohei_palm_of_life = class( AbilityBaseClass )
+
+LinkLuaModifier( "modifier_sohei_palm_of_life_movement", "abilities/sohei/sohei_palm_of_life.lua", LUA_MODIFIER_MOTION_HORIZONTAL )
+
+--------------------------------------------------------------------------------
+
+function sohei_palm_of_life:CastFilterResultTarget( target )
+	local caster = self:GetCaster()
+
+	if caster == target then
+		return UF_FAIL_CUSTOM
+	end
+
+	local ufResult = UnitFilter(
+		target,
+		DOTA_UNIT_TARGET_HERO,
+		DOTA_UNIT_TARGET_TEAM_FRIENDLY,
+		DOTA_UNIT_TARGET_FLAG_NONE,
+		caster:GetTeamNumber()
+	)
+
+	return ufResult
+end
+
+--------------------------------------------------------------------------------
+
+function sohei_palm_of_life:GetCustomCastErrorTarget( target )
+	if self:GetCaster() == target then
+		return "#dota_hud_error_cant_cast_on_self"
+	end
+
+	return ""
+end
+
+--------------------------------------------------------------------------------
+
+function sohei_palm_of_life:OnHeroCalculateStatBonus()
+	local caster = self:GetCaster()
+
+	if caster:HasScepter() then
+		self:SetHidden( false )
+		if self:GetLevel() <= 0 then
+			self:SetLevel( 1 )
+		end
+	else
+		self:SetHidden( true )
+	end
+end
+
+--------------------------------------------------------------------------------
+
+if IsServer() then
+	function sohei_palm_of_life:OnSpellStart()
+		local caster = self:GetCaster()
+		local modifier_charges = caster:FindModifierByName( "modifier_sohei_dash_charges" )
+
+		if modifier_charges and not modifier_charges:IsNull() then
+			-- Perform the dash if there is at least one charge remaining
+			if modifier_charges:GetStackCount() >= 1 then
+				modifier_charges:SetStackCount( modifier_charges:GetStackCount() - 1 )
+			end
+		end
+
+		-- i commented on this in guard but
+		-- faking not casting is really just not a great solution
+		-- especially if something breaks due to dev fault and suddenly a bread and butter ability isn't
+		-- usable
+		-- so let's instead give the player some let in this regard and let 'em dash anyway
+		local target = self:GetCursorTarget()
+		local speed = self:GetSpecialValueFor( "dash_speed" )
+		local treeRadius = self:GetSpecialValueFor( "tree_radius" )
+		local duration = self:GetSpecialValueFor( "max_duration" )
+		local endDistance = self:GetSpecialValueFor( "end_distance" )
+		local doHeal = 0
+
+		local modMomentum = caster:FindModifierByName( "modifier_sohei_momentum_passive" )
+		local spellMomentum = caster:FindAbilityByName( "sohei_momentum" )
+
+		if ( modMomentum and modMomentum:IsMomentumReady() ) and ( spellMomentum and spellMomentum:IsCooldownReady() ) then
+			doHeal = 1
+		end
+
+		caster:RemoveModifierByName( "modifier_sohei_palm_of_life_movement" )
+		caster:RemoveModifierByName( "modifier_sohei_dash_movement" )
+		caster:EmitSound( "DOTA_Item.ForceStaff.Activate" )
+		caster:EmitSound( "Sohei.Dash" )
+		caster:StartGesture( ACT_DOTA_RUN )
+		caster:AddNewModifier( caster, self, "modifier_sohei_palm_of_life_movement", {
+			duration = duration,
+			target = target:entindex(),
+			tree_radius = treeRadius,
+			speed = speed,
+			endDistance = endDistance,
+			doHeal = doHeal
+		} )
+	end
+end
+
+--------------------------------------------------------------------------------
+
+-- Dash movement modifier
+modifier_sohei_palm_of_life_movement = class( ModifierBaseClass )
+
+--------------------------------------------------------------------------------
+
+function modifier_sohei_palm_of_life_movement:IsDebuff()
+	return false
+end
+
+function modifier_sohei_palm_of_life_movement:IsHidden()
+	return true
+end
+
+function modifier_sohei_palm_of_life_movement:IsPurgable()
+	return false
+end
+
+function modifier_sohei_palm_of_life_movement:IsStunDebuff()
+	return false
+end
+
+function modifier_sohei_palm_of_life_movement:GetPriority()
+	return DOTA_MOTION_CONTROLLER_PRIORITY_MEDIUM
+end
+
+--------------------------------------------------------------------------------
+
+function modifier_sohei_palm_of_life_movement:CheckState()
+	local state = {
+		[MODIFIER_STATE_NO_UNIT_COLLISION] = true,
+	}
+
+	return state
+end
+
+--------------------------------------------------------------------------------
+
+if IsServer() then
+	function modifier_sohei_palm_of_life_movement:OnCreated( event )
+		-- Movement parameters
+		local parent = self:GetParent()
+		self.target = EntIndexToHScript( event.target )
+		self.speed = event.speed
+		self.tree_radius = event.tree_radius
+		self.endDistance = event.endDistance
+		self.doHeal = event.doHeal > 0
+
+		if self:ApplyHorizontalMotionController() == false then
+			self:Destroy()
+			return
+		end
+
+		-- Trail particle
+		local trail_pfx = ParticleManager:CreateParticle( "particles/econ/items/juggernaut/bladekeeper_omnislash/_dc_juggernaut_omni_slash_trail.vpcf", PATTACH_CUSTOMORIGIN, parent )
+		ParticleManager:SetParticleControl( trail_pfx, 0, parent:GetAbsOrigin() )
+		ParticleManager:SetParticleControl( trail_pfx, 1, (  self.target:GetAbsOrigin() - parent:GetAbsOrigin() ):Normalized() * 300 )
+		ParticleManager:ReleaseParticleIndex( trail_pfx )
+	end
+
+--------------------------------------------------------------------------------
+
+	function modifier_sohei_palm_of_life_movement:OnDestroy()
+		local parent = self:GetParent()
+
+		parent:FadeGesture( ACT_DOTA_RUN )
+		parent:RemoveHorizontalMotionController( self )
+		ResolveNPCPositions( parent:GetAbsOrigin(), 128 )
+	end
+
+--------------------------------------------------------------------------------
+
+	function modifier_sohei_palm_of_life_movement:UpdateHorizontalMotion( parent, deltaTime )
+		local parentOrigin = parent:GetAbsOrigin()
+		local targetOrigin = self.target:GetAbsOrigin()
+		local dA = parentOrigin
+		dA.z = 0
+		local dB = targetOrigin
+		dB.z = 0
+		local direction = ( dB - dA ):Normalized()
+
+		local tickSpeed = self.speed * deltaTime
+		tickSpeed = math.min( tickSpeed, self.endDistance )
+		local tickOrigin = parentOrigin + ( tickSpeed * direction )
+
+		parent:SetAbsOrigin( tickOrigin )
+		parent:FaceTowards( targetOrigin )
+
+		GridNav:DestroyTreesAroundPoint( tickOrigin, self.tree_radius, false )
+
+		local distance = parent:GetRangeToUnit( self.target )
+
+		if distance <= self.endDistance then
+			if self.doHeal then
+				-- do the heal
+				local spell = self:GetAbility()
+				local healAmount = parent:GetHealth() * ( spell:GetSpecialValueFor( "hp_as_heal" ) / 100 )
+
+				self.target:Heal( healAmount, parent )
+
+				self.target:EmitSound( "Hero_Omniknight.Purification" )
+
+				local part = ParticleManager:CreateParticle( "particles/units/heroes/hero_omniknight/omniknight_purification.vpcf", PATTACH_ABSORIGIN_FOLLOW, self.target )
+				ParticleManager:SetParticleControl( part, 1, Vector( self.target:GetModelRadius(), 1, 1 ) )
+				ParticleManager:ReleaseParticleIndex( part )
+
+				SendOverheadEventMessage( nil, 10, self.target, healAmount, nil )
+
+				-- undo momentum charge
+				local modMomentum = parent:FindModifierByName( "modifier_sohei_momentum_passive" )
+
+				if modMomentum and modMomentum:IsMomentumReady() then
+					modMomentum:SetStackCount( 0 )
+				end
+
+				local spellMomentum = parent:FindAbilityByName( "sohei_momentum" )
+
+				if spellMomentum then
+					spellMomentum:EndCooldown()
+					spellMomentum:UseResources( true, true, true )
+				end
+			end
+
+			-- end it alllllll
+			self:Destroy()
+		end
+	end
+
+--------------------------------------------------------------------------------
+	
+	function modifier_sohei_palm_of_life_movement:OnHorizontalMotionInterrupted()
+		self:Destroy()
+	end
+end
