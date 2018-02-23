19185541f171b91f48f27a2928e4cf390a5201d1
Soo Kuo-Yi <jaydonsky@gmail.com>
2018-02-24 00:23:21 +0800

    Duel refactor stage 1
    Split up duels.lua into more modules and refactor some of the code. Notable changes off the top of my head include:
    * Storing Duel zones into an array and using loops to avoid repetition
    * Store item cooldowns with the handles as keys and the cooldowns as values and loop over them to restore (cause that function chain thing was seriously hard to understand)
    * Rewrite summon finding code, in SafeTeleportAll, in functional style (tried to use GetAdditionalOwnedUnits as well, but it's not usable in this case due to the reasons stated in the comment)

---
+++ game/scripts/vscripts/components/duels/teleport.lua
@@ -0,0 +1,75 @@
+-- This module contains functions for teleporting heroes including their summons
+-- and with handling for special cases where heroes might be in an exiled state
+
+local export = {}
+
+local function SafeTeleport(unit, location, maxDistance)
+  if unit:FindModifierByName("modifier_life_stealer_infest") then
+    DebugPrint("Found LS infesting.")
+    local ability = assert(unit:FindAbilityByName("life_stealer_consume"), 'Missing Ability "life_stealer_consume"')
+    assert(ability:IsActivated(), 'Ability is not activated')
+    ExecuteOrderFromTable({
+      UnitIndex = unit:entindex(),
+      OrderType = DOTA_UNIT_ORDER_CAST_NO_TARGET,
+      AbilityIndex = ability:entindex(), --Optional.  Only used when casting abilities
+      Queue = 0 --Optional.  Used for queueing up abilities
+    })
+  end
+  if unit:IsOutOfGame() then
+    unit:RemoveModifierByName("modifier_obsidian_destroyer_astral_imprisonment_prison")
+    unit:RemoveModifierByName("modifier_riki_tricks_of_the_trade") -- TODO: Check name
+  end
+  location = GetGroundPosition(location, unit)
+  FindClearSpaceForUnit(unit, location, true)
+  Timers:CreateTimer(0.1, function()
+    if not unit or unit:IsNull() then
+      return
+    end
+    local distance = (location - unit:GetAbsOrigin()):Length2D()
+    if distance > maxDistance then
+      SafeTeleport(unit, location, maxDistance)
+    end
+  end)
+end
+
+local function SafeTeleportAll(mainUnit, location, maxDistance)
+  SafeTeleport(mainUnit, location, maxDistance)
+  local playerAdditionalUnits
+  -- GetAdditionalOwnedUnits is unsuitable here as it apparently only returns hero-like units, like Lone Druid's Bear and such.
+  -- It definitely does not return most normal unit summons, including Necronomicon and Broodmother Spiderlings.
+  -- if mainUnit.GetAdditionalOwnedUnits then
+  --   playerAdditionalUnits = mainUnit:GetAdditionalOwnedUnits() or {} -- assign empty table instead of nil so iter can be called without errors
+  -- else
+  playerAdditionalUnits = FindUnitsInRadius(mainUnit:GetTeam(),
+                                            mainUnit:GetAbsOrigin(),
+                                            nil,
+                                            FIND_UNITS_EVERYWHERE,
+                                            DOTA_UNIT_TARGET_TEAM_FRIENDLY,
+                                            bit.bor(DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_BASIC),
+                                            DOTA_UNIT_TARGET_FLAG_PLAYER_CONTROLLED,
+                                            FIND_ANY_ORDER,
+                                            false)
+  playerAdditionalUnits = playerAdditionalUnits or {} -- assign empty table instead of nil so iter can be called without errors
+  playerAdditionalUnits = iter(playerAdditionalUnits):filter(function (unit)
+    return unit:GetPlayerOwnerID() == mainUnit:GetPlayerOwnerID()
+  end)
+  -- end
+
+  iter(playerAdditionalUnits)
+    :filter(CallMethod("HasMovementCapability"))
+    :foreach(function (unit)
+      SafeTeleport(unit, location, maxDistance)
+    end)
+end
+
+-- Test SafeTeleport function
+local function TestSafeTeleport(keys)
+  local hero = PlayerResource:GetSelectedHeroEntity(keys.playerid)
+  SafeTeleportAll(hero, Vector(0, 0, 0), 150)
+end
+
+ChatCommand:LinkCommand("-test_tp", TestSafeTeleport, nil)
+export.SafeTeleport = SafeTeleport
+export.SafeTeleportAll = SafeTeleportAll
+
+return export
