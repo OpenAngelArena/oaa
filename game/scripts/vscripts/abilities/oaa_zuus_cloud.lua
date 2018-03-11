6f0144f0298c922f32a803237a2ae8dd05850a60
OTAJumperjay <34005014+OTAJumperjay@users.noreply.github.com>
2018-03-11 17:42:55 +0100

    pleasure treatment for trildar

--- game/scripts/vscripts/abilities/oaa_zuus_cloud.lua
+++ game/scripts/vscripts/abilities/oaa_zuus_cloud.lua
@@ -6,7 +6,7 @@ function zuus_cloud:OnSpellStart()
   local hCloud = CreateUnitByName( "npc_dota_zeus_cloud", self:GetCursorPosition(), true, caster, caster, caster:GetTeamNumber() )
   hCloud:SetOwner( self:GetCaster() )
   hCloud:SetControllableByPlayer( self:GetCaster():GetPlayerOwnerID(), false )
-  hCloud:AddNewModifier( caster, self, "modifier_zuus_cloud_oaa", { Interval = self:GetSpecialValueFor( "cloud_bolt_interval" ), Radius = self:GetSpecialValueFor( "cloud_radius" ) } )
+  hCloud:AddNewModifier( caster, self, "modifier_zuus_cloud_oaa", nil)
   hCloud:AddNewModifier( caster, self, "modifier_kill", { duration = self:GetSpecialValueFor( "cloud_duration" ) } )
   FindClearSpaceForUnit( hCloud, self:GetCursorPosition(), true )
 end
@@ -31,10 +31,9 @@ end
 
 function modifier_zuus_cloud_oaa:OnCreated( kv )
   if IsServer() then
-    self.Interval = kv.Interval
-    self.Radius = kv.Radius
+    local radius = self:GetAbility():GetSpecialValueFor( "cloud_radius" )
     self.cloud_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_zeus/zeus_cloud.vpcf", PATTACH_ABSORIGIN, self:GetParent())
-    ParticleManager:SetParticleControl(self.cloud_particle, 1, Vector(self.Radius, 1, 1))
+    ParticleManager:SetParticleControl(self.cloud_particle, 1, Vector(radius, 1, 1))
 
     ParticleManager:SetParticleControlEnt(self.cloud_particle, 2, self:GetParent(), PATTACH_POINT_FOLLOW, "attach_hitloc", self:GetParent():GetAbsOrigin(), true)
     self:StartIntervalThink( 0.1 )
@@ -44,20 +43,22 @@ end
 
 function modifier_zuus_cloud_oaa:OnDestroy()
   if IsServer() then
-    ParticleManager:DestroyParticle( self.cloud_particle, false)
+    ParticleManager:DestroyParticle(self.cloud_particle, false)
     ParticleManager:ReleaseParticleIndex(self.cloud_particle)
-    self.cloud_particle= nil
+    self.cloud_particle = nil
   end
 end
 
 function modifier_zuus_cloud_oaa:OnIntervalThink()
-  if self.LastStrike == nil or GameRules:GetDOTATime(false, false) - self.LastStrike > self.Interval then
+  local interval = self:GetAbility():GetSpecialValueFor( "cloud_bolt_interval" )
+  local radius = self:GetAbility():GetSpecialValueFor( "cloud_radius" )
+  if self.LastStrike == nil or GameRules:GetDOTATime(false, false) - self.LastStrike > interval then
     local caster = self:GetCaster()
     local targets = FindUnitsInRadius(
       caster:GetTeamNumber(),
       self:GetParent():GetAbsOrigin(),
       nil,
-      self.Radius,
+      radius,
       DOTA_UNIT_TARGET_TEAM_ENEMY,
       DOTA_UNIT_TARGET_HERO,
       DOTA_UNIT_TARGET_FLAG_NONE,
@@ -69,7 +70,7 @@ function modifier_zuus_cloud_oaa:OnIntervalThink()
         caster:GetTeamNumber(),
         self:GetParent():GetAbsOrigin(),
         nil,
-        self.Radius,
+        radius,
         DOTA_UNIT_TARGET_TEAM_ENEMY,
         DOTA_UNIT_TARGET_CREEP,
         DOTA_UNIT_TARGET_FLAG_NONE,
@@ -88,22 +89,27 @@ end
 function modifier_zuus_cloud_oaa:CastLightningBolt(target)
   local caster = self:GetCaster()
   local parent = self:GetParent()
-  local ability = caster:FindAbilityByName( 'zuus_lightning_bolt' )
-  local radius = ability:GetSpecialValueFor("spread_aoe")
+  local lightning_bolt_ability = caster:FindAbilityByName("zuus_lightning_bolt")
   local sight_radius =  0
   if GameRules:IsDaytime() then
-    sight_radius = ability:GetSpecialValueFor("sight_radius_day")
+    sight_radius = lightning_bolt_ability:GetSpecialValueFor("sight_radius_day")
   else
-    sight_radius = ability:GetSpecialValueFor("sight_radius_night")
+    sight_radius = lightning_bolt_ability:GetSpecialValueFor("sight_radius_night")
   end
-  local sight_duration = ability:GetSpecialValueFor("sight_duration")
+  local sight_duration = lightning_bolt_ability:GetSpecialValueFor("sight_duration")
 
-  if ability:GetLevel() > 0 then
+  if lightning_bolt_ability:GetLevel() > 0 then
     AddFOWViewer(caster:GetTeam(), target:GetAbsOrigin(), sight_radius, sight_duration, false)
 
-    -- Checks if the target has been set yet
-    target:AddNewModifier(caster, ability, "modifier_stunned", {Duration = 0.1})
-    ApplyDamage({victim = target, attacker = caster, damage = ability:GetAbilityDamage(), damage_type = ability:GetAbilityDamageType()})
+    local talent = caster:FindAbilityByName("special_bonus_unique_zeus_3")
+    local ministun_duration = 0.1
+
+    if talent ~= nil and talent:GetLevel() > 0 then
+      ministun_duration = ministun_duration + talent:GetSpecialValueFor("value")
+    end
+
+    target:AddNewModifier(caster, lightning_bolt_ability, "modifier_stunned", {Duration = ministun_duration})
+    ApplyDamage({victim = target, attacker = parent, damage = lightning_bolt_ability:GetAbilityDamage(), damage_type = lightning_bolt_ability:GetAbilityDamageType()})
     -- Renders the particle on the sigil
     local particle = ParticleManager:CreateParticle("particles/units/heroes/hero_zeus/zeus_cloud_strike.vpcf", PATTACH_POINT_FOLLOW, self:GetParent())
 
