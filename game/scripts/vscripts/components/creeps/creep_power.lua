9913742f79e0aa623aae4ef923fc432d3cf46de0
Haganeko <haganekko@gmail.com>
2018-03-11 01:59:09 -0300

    make kills worth less early.

--- game/scripts/vscripts/components/creeps/creep_power.lua
+++ game/scripts/vscripts/components/creeps/creep_power.lua
@@ -34,6 +34,22 @@ function CreepPower:GetBasePowerForMinute (minute, multFactor)
     (0 * ((minute / 100) ^ 4) - 0 * ((minute/100) ^ 3) + 30 * ((minute/100) ^ 2) + 3 * (minute/100)) + 1,   -- mana
     (0 * ((minute / 100) ^ 4) - 0 * ((minute/100) ^ 3) + 60 * ((minute/100) ^ 2) + 6 * (minute/100)) + 1,     -- damage
     (0 * (minute / 26) ^ 2 + minute / 6) + 1,       -- armor
+    ((0 * minute ^ 2 + 6*2 * minute + 7*15)/(6*15)) * self.BootGoldFactor,                      -- gold
+    ((9 * minute ^ 2 + 17 * minute + 607) / 607) * self.numPlayersXPFactor * multFactor -- xp
+  }
+end
+
+function CreepPower:GetBaseCavePowerForMinute (minute, multFactor)
+  if minute == 0 then
+    return {   0,        1.0,      1.0,      1.0,      1.0,      1.0 * self.BootGoldFactor,      1.0 * self.numPlayersXPFactor}
+  end
+
+  return {
+    minute,                                   -- minute
+    (0 * ((minute / 100) ^ 4) - 0 * ((minute/100) ^ 3) + 30 * ((minute/100) ^ 2) + 3 * (minute/100)) + 1,   -- hp
+    (0 * ((minute / 100) ^ 4) - 0 * ((minute/100) ^ 3) + 30 * ((minute/100) ^ 2) + 3 * (minute/100)) + 1,   -- mana
+    (0 * ((minute / 100) ^ 4) - 0 * ((minute/100) ^ 3) + 60 * ((minute/100) ^ 2) + 6 * (minute/100)) + 1,     -- damage
+    (0 * (minute / 26) ^ 2 + minute / 6) + 1,       -- armor
     ((0 * minute ^ 2 + 2 * minute + 15)/(15)) * self.BootGoldFactor,                      -- gold
     ((9 * minute ^ 2 + 17 * minute + 607) / 607) * self.numPlayersXPFactor * multFactor -- xp
   }
