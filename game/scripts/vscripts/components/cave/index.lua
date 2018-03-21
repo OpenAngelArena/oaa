1c7292fecb89289e25654f7c4cf00934c4c0119a
Warpdragon <artfulgamers@gmail.com>
2018-03-21 14:28:35 -0400

    fixed offsides. shrunk map. added runes/shrines.

--- game/scripts/vscripts/components/cave/index.lua
+++ game/scripts/vscripts/components/cave/index.lua
@@ -1,5 +1,5 @@
+require('components/cave/protection')
 if GetMapName() ~= "oaa_test" then
   require('components/cave/cave_types')
   require('components/cave/cave')
-  require('components/cave/protection')
 end
