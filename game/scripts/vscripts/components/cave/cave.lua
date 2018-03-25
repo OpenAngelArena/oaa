03f795b416245999ac6c8af4f2ca5455ef614894
Haganeko <haganekko@gmail.com>
2018-03-25 00:13:27 -0300

    get rid of exponential XP

--- game/scripts/vscripts/components/cave/cave.lua
+++ game/scripts/vscripts/components/cave/cave.lua
@@ -142,12 +142,6 @@ function CaveHandler:SpawnCreepInRoom (room, properties, teamID, roomID)
   -- get random position
   local randPosition = room:GetAbsOrigin() + RandomVector(RandomFloat(10, 300))
 
-  --EXP BOUNTY
-  local minutes = math.floor(GameRules:GetGameTime() / 60)
-  if minutes > 60 then
-    properties.exp = properties.exp * 1.5^(minutes - 60)
-  end
-
   local creep = CreateUnitByName(
     properties.name, -- name
     randPosition, -- location
