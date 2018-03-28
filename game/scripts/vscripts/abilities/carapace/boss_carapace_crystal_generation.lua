d7c3c2c824ffbd2827e69fcb1ea303bf30dd94da
Yahnich <y.slagmeulder@hotmail.com>
2018-03-28 18:25:21 +0200

    lua start, untested

--- game/scripts/vscripts/abilities/carapace/boss_carapace_crystal_generation.lua
+++ game/scripts/vscripts/abilities/carapace/boss_carapace_crystal_generation.lua
@@ -1,1 +1,18 @@
 boss_carapace_crystal_generation = class(AbilityBaseClass)
+
+function boss_carapace_crystal_generation:GetIntrinsicModifierName()
+	return "modifier_boss_carapace_crystal_generation"
+end
+
+modifier_boss_carapace_crystal_generation = class({})
+LinkLuaModifier("modifier_boss_carapace_crystal_generation", "abilities/carapace/boss_carapace_crystal_generation", LUA_MODIFIER_MOTION_NONE)
+
+function modifier_boss_carapace_crystal_generation:OnCreated()
+	self.dmg_threshold = self:GetAbility():GetSpecialValueFor("health_threshold")
+	self.start_crystals = self:GetAbility():GetSpecialValueFor("starting_crystals")
+	if IsServer() then
+		for i = 1, self.start_crystals do
+			self:GetAbility():CreateCrystal(health)
+		end
+	end
+end
--- game/scripts/vscripts/abilities/carapace/boss_carapace_headbutt.lua
+++ game/scripts/vscripts/abilities/carapace/boss_carapace_headbutt.lua
@@ -1,3 +1,13 @@
 LinkLuaModifier("modifier_boss_carapace_headbutt_slow", "abilities/carapace/boss_carapace_headbutt.lua", LUA_MODIFIER_MOTION_NONE)
 
 boss_carapace_headbutt = class(AbilityBaseClass)
+
+function boss_carapace_headbutt:OnSpellStart()
+	local caster = self:GetCaster()
+	
+	local distance = self:GetSpecialValueFor("distance")
+	local radius = self:GetSpecialValueFor("radius")
+	Timers:CreateTimer(function()
+		return 0
+	end)
+end
