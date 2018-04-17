559c7db9df0d6a2e7f203136dfe782ec023df311
genious97 <robbiep1@hotmail.com>
2018-04-16 20:01:13 -0400

    requested changes

--- game/scripts/vscripts/items/sonic.lua
+++ game/scripts/vscripts/items/sonic.lua
@@ -1,30 +1,14 @@
-item_sonic = class(ItemBaseClass)
+item_sonic = class(TransformationBaseClass)
 
 LinkLuaModifier("modifier_item_sonic", "items/sonic.lua", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_sonic_fly", "modifiers/modifier_sonic_fly.lua", LUA_MODIFIER_MOTION_NONE)
 
 function item_sonic:GetIntrinsicModifierName()
+  return "modifier_item_sonic"
+end
+
+function item_sonic:GetTransformtionModifierName()
   return "modifier_sonic_fly"
-end
-
-function item_sonic:OnToggle()
-  local caster = self.GetCaster()
-  local state = self.GetToggleState()
-  local ability = self.GetAbility()
-
-  if state == true then
-    caster.AddNewModifier(caster, self, "modifier_sonic_fly", {})
-  else
-    caster.RemoveModifierByName("modifier_sonic_fly")
-  end
-  ability:StartCooldown(ability:GetCooldown(ability:GetLevel()))
-end
-
-function item_sonic:OnOwnerDied()
-  local owner = self.GetOwner()
-  if owner.HasModifier("modifier_sonic_fly") then
-    owner.RemoveModifierByName("modifier_sonic_fly")
-  end
 end
 
 modifier_item_sonic = class(ModifierBaseClass)
