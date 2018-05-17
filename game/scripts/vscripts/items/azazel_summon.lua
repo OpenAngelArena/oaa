385cf582f4f90b51b4342212af682661322673b3
Firetoad <fcalife@gmail.com>
2018-05-16 22:40:29 -0300

    Azazel Purchasables - Elixiers (#2156)
    * Implementing #2110
    
    * Added summon KV and extra abilities
    
    Assumed 300 MS for farmer and 1.0 turn rate for all summons.
    
    * Added azazel summon item entries and basic functionality
    
    * Finished implementing #2111
    
    - Used "funny" names for the summons, just change them if you don't like them or think they're not clear enough.
    - Summon stats are tied to their actual stat block in npc_units_custom; the values in the item just change tooltips.
    - The Farmer's cleave stats are tied to the ability's KV, not the item's; again, the item's KVs just change tooltips.
    
    If the whole KV/tooltip thing is a bother, it can be changed, but I thought it was outside the scope of the issue, since the current behavior is easier to implement and consistent with (almost) every vanilla summon ability in the game.
    
    * travis
    
    * missing tooltip for modifier "dota_tooltip_modifier_azazel_summon_farmer_innate"
     - it's a hidden modifier that needs no tooltip.
    * item/ability ID conflicts
     - fixed
    * "cost is set correctly in kv for item_elixier_sustain_4"
     - I assume this means incorrectly. The items all cost 100g (recipe) + their core's cost (1500/2500/3500/4500) so I don't see the error here, but i set everything to 100 anyway, assuming cores have no real value.
    * "[item] has reverse compatible upgrade [core x]"
     - If I understood correctly, this is intended.
    * blah blah whitespace
     - fixed
    * blah unused variable
     - fixed
    
    Also made the items temporarily purchasable in the regular shop.
    
    * seems like travis' "next free ID" is a fat old lie
    
    fixed conflicting item/ability IDs
    
    * test
    
    test
    
    * Fix Travis

---
+++ game/scripts/vscripts/items/azazel_summon.lua
@@ -0,0 +1,95 @@
+-- Azazel's Summons
+-- by Firetoad, April 17th, 2018
+
+--------------------------------------------------------------------------------
+
+LinkLuaModifier("modifier_azazel_summon_farmer_innate", "items/azazel_summon.lua", LUA_MODIFIER_MOTION_NONE)
+
+--------------------------------------------------------------------------------
+
+azazel_summon = class(ItemBaseClass)
+
+function azazel_summon:OnSpellStart()
+  if IsServer() then
+    local caster = self:GetCaster()
+
+    caster:EmitSound("DOTA_Item.Necronomicon.Activate")
+
+    -- Destroy any existing summons tied to this caster
+    if caster.azazel_summon then
+      caster.azazel_summon:Kill(nil, caster)
+    end
+
+    -- Summon parameters
+    local summon_position = caster:GetAbsOrigin() + caster:GetForwardVector() * 100
+    local summon_name = self:GetAbilityName()
+    summon_name = "npc_dota_"..summon_name:sub(6)
+
+    -- Summon the creature
+    GridNav:DestroyTreesAroundPoint(summon_position, 128, false)
+    caster.azazel_summon = CreateUnitByName(summon_name, summon_position, true, caster, caster, caster:GetTeam())
+    caster.azazel_summon:SetControllableByPlayer(caster:GetPlayerID(), true)
+    caster.azazel_summon:AddNewModifier(caster, self, "modifier_kill", {duration = self:GetSpecialValueFor("summon_duration")})
+
+    -- Level up any relevant abilities
+    if string.find(summon_name, "farmer") then
+      caster.azazel_summon:AddAbility("azazel_summon_farmer_innate"):SetLevel(self:GetLevel())
+    elseif string.find(summon_name, "scout") then
+      caster.azazel_summon:AddAbility("azazel_scout_permanent_invisibility"):SetLevel(1)
+    end
+
+    self:SpendCharge()
+  end
+end
+
+--------------------------------------------------------------------------------
+
+item_azazel_summon_farmer_1 = azazel_summon
+item_azazel_summon_farmer_2 = azazel_summon
+item_azazel_summon_farmer_3 = azazel_summon
+item_azazel_summon_farmer_4 = azazel_summon
+item_azazel_summon_scout_1 = azazel_summon
+item_azazel_summon_scout_2 = azazel_summon
+item_azazel_summon_scout_3 = azazel_summon
+item_azazel_summon_scout_4 = azazel_summon
+item_azazel_summon_tank_1 = azazel_summon
+item_azazel_summon_tank_2 = azazel_summon
+item_azazel_summon_tank_3 = azazel_summon
+item_azazel_summon_tank_4 = azazel_summon
+
+--------------------------------------------------------------------------------
+
+azazel_summon_farmer_innate = class(AbilityBaseClass)
+
+function azazel_summon_farmer_innate:GetIntrinsicModifierName()
+  return "modifier_azazel_summon_farmer_innate"
+end
+
+--------------------------------------------------------------------------------
+
+modifier_azazel_summon_farmer_innate = class(ModifierBaseClass)
+
+function modifier_azazel_summon_farmer_innate:IsHidden() return true end
+function modifier_azazel_summon_farmer_innate:IsPurgable() return false end
+function modifier_azazel_summon_farmer_innate:IsDebuff() return false end
+
+function modifier_azazel_summon_farmer_innate:DeclareFunctions()
+  local funcs = {
+    MODIFIER_PROPERTY_PHYSICAL_CONSTANT_BLOCK,
+    MODIFIER_EVENT_ON_ATTACK_LANDED
+  }
+  return funcs
+end
+
+function modifier_azazel_summon_farmer_innate:GetModifierPhysical_ConstantBlock()
+  return self:GetAbility():GetSpecialValueFor("damage_block")
+end
+
+function modifier_azazel_summon_farmer_innate:OnAttackLanded(keys)
+  if IsServer() then
+    if keys.attacker == self:GetParent() and (not keys.target:IsBuilding()) then
+      local ability = self:GetAbility()
+      DoCleaveAttack(self:GetParent(), keys.target, ability, keys.damage * ability:GetSpecialValueFor("cleave_pct") * 0.01, ability:GetSpecialValueFor("cleave_start_radius"), ability:GetSpecialValueFor("cleave_end_radius"), ability:GetSpecialValueFor("cleave_length"), "particles/units/heroes/hero_sven/sven_spell_great_cleave.vpcf")
+    end
+  end
+end
