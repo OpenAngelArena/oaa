da40985a7cbd6ef39f385d04e6ca3505d9b042a8
carlos_rpg <carlos.h.rodrigues.a@gmail.com>
2018-05-25 23:34:59 -0300

    3.5.7

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -1,6 +1,6 @@
 -- This is the entry-point to your game mode and should be used primarily to precache models/particles/sounds/etc
 
-GAME_VERSION = "3.5.6"
+GAME_VERSION = "3.5.7"
 
 -- Setup the main logger
 require('internal/logging')
