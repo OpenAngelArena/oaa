OpenAngelArena/oaa

commit cfbb38cd20f8265388651582175476ec15185f86

Implement creep bounty gold reduction on some farming boots (#1748)
* Add CallMethod utility function

* Add compose utility function

* Add OnEntityFatalDamage event

This event fires when an entity's health drops to 0 after taking damage. It's effectively an early OnEntityKilled that allows doing things like setting bounty values prior to the actual death event of the entity.

* Code and tooltips for creep gold bounty reduction effect

* Rework how Hand of Midas grants its bonus gold

To avoid interaction with creep gold bounty reduction

* Add creep bounty gold reduction to Greater Arcane Boots

* Add creep bounty gold reduction to Greater Tranquil Boots

* Add creep bounty gold reduction to Greater Boots of Travel

* Copy Greater Tranquil Boots icons to spellicons

* Fix Greater Tranquil Boots not breaking

* Add CallMethod and compose to luacheck globals

* Fix hand of midas recipe

game/scripts/vscripts/items/farming/greater_tranquil_boots.lua
@@ -2,6 +2,8 @@ item_greater_tranquil_boots = class(ItemBaseClass)
 
 LinkLuaModifier( "modifier_item_greater_tranquil_boots", "items/farming/greater_tranquil_boots.lua", LUA_MODIFIER_MOTION_NONE )
 LinkLuaModifier( "modifier_item_greater_tranquil_boots_sap", "items/farming/greater_tranquil_boots.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_intrinsic_multiplexer", "modifiers/modifier_intrinsic_multiplexer.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier( "modifier_creep_bounty", "items/farming/modifier_creep_bounty.lua", LUA_MODIFIER_MOTION_NONE )
 
 --------------------------------------------------------------------------------
 
@@ -20,7 +22,14 @@ end
 --------------------------------------------------------------------------------
 
 function item_greater_tranquil_boots:GetIntrinsicModifierName()
-	return "modifier_item_greater_tranquil_boots"
+	return "modifier_intrinsic_multiplexer"
+end
+
+function item_greater_tranquil_boots:GetIntrinsicModifierNames()
+  return {
+    "modifier_item_greater_tranquil_boots",
+    "modifier_creep_bounty"
+  }
 end
 
 --------------------------------------------------------------------------------
@@ -59,7 +68,7 @@ function modifier_item_greater_tranquil_boots:OnCreated( event )
 	self.interval = spell:GetSpecialValueFor( "check_interval" )
 
 	if IsServer() then
-		self:SetDuration( spell:GetCooldownTime(), true )
+		self:SetDuration( spell:GetCooldownTimeRemaining(), true )
 
 		self:StartIntervalThink( self.interval )
 	end
@@ -243,7 +252,7 @@ if IsServer() then
 				return
 			end
 
-			spell:UseResources( false, false, true )
+			spell:StartCooldown(spell:GetCooldown(spell:GetLevel()))
 
 			-- seriously, this is easy
 

.luacheckrc
@@ -126,6 +126,10 @@ read_globals = { -- these globals can only be accessed.
 "operator",
 "op",
 
+-- functional
+"CallMethod",
+"compose",
+
 -- util
 "split",
 "regexsplit",

commit b630b4988b65b4bfe3b2a53dbdcd447d5a45c700

Fix Greater Tranquil Boots not doing anything
Seems like Valve changed the behaviour of SetDuration so that setting it to 0 immediately destroys the modifier even when DestroyOnExpire is false #ThanksValve. Either that, or this has been broken since #1748, but I'm certain I tested it, so that's quite unlikely.

game/scripts/vscripts/items/farming/greater_tranquil_boots.lua
@@ -71,8 +71,13 @@ function modifier_item_greater_tranquil_boots:OnCreated( event )
 
 	self.interval = spell:GetSpecialValueFor( "check_interval" )
 
-	if IsServer() then
-		self:SetDuration( spell:GetCooldownTimeRemaining(), true )
+  if IsServer() then
+		local cdRemaining = spell:GetCooldownTimeRemaining()
+    -- Break for any remaining duration (e.g. if item was dropped and picked up)
+    -- Have to check for 0 because setting duration to 0 apparently destroys the modifier even with DestroyOnExpire false
+    if cdRemaining > 0 then
+      self:SetDuration( cdRemaining, true )
+    end
 
 		self:StartIntervalThink( self.interval )
 	end
@@ -102,8 +107,13 @@ function modifier_item_greater_tranquil_boots:OnRefresh( event )
 
 	self.interval = spell:GetSpecialValueFor( "check_interval" )
 
-	if IsServer() then
-		self:SetDuration( spell:GetCooldownTime(), true )
+  if IsServer() then
+    local cdRemaining = spell:GetCooldownTimeRemaining()
+    -- Break for any remaining duration (e.g. if item was dropped and picked up)
+    -- Have to check for 0 because setting duration to 0 apparently destroys the modifier even with DestroyOnExpire false
+    if cdRemaining > 0 then
+      self:SetDuration( cdRemaining, true )
+    end
 
 		self:StartIntervalThink( self.interval )
 	end