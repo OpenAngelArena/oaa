7d9a72e2709643e1b7499d1fcd9c2f1f7021bdd1
SquawkyArctangent <arctangentpuckett@gmail.com>
2018-07-22 18:33:51 -0500

    Fix Viper Strike Aghanims and Linken's/Lotus (#2299)
    * Fixed Viper Strike piercing Linken's and Lotus
    
    also minor tooltip untypo
    
    * Fixed Viper Strike's Aghs being non-functional
    
    i don't know how i completely forgot about this

--- game/scripts/vscripts/abilities/oaa_viper_strike.lua
+++ game/scripts/vscripts/abilities/oaa_viper_strike.lua
@@ -4,6 +4,38 @@ LinkLuaModifier( "modifier_viper_viper_strike_silence", "abilities/oaa_viper_str
 
 --------------------------------------------------------------------------------
 
+function viper_viper_strike_oaa:GetCastRange( loc, target )
+	local caster = self:GetCaster()
+
+	if caster:HasScepter() then
+		return self:GetSpecialValueFor( "cast_range_scepter" )
+	end
+
+	return self.BaseClass.GetCastRange( self, loc, target )
+end
+
+function viper_viper_strike_oaa:GetManaCost( level )
+	local caster = self:GetCaster()
+
+	if caster:HasScepter() then
+		return self:GetSpecialValueFor( "mana_cost_scepter" )
+	end
+
+	return self.BaseClass.GetManaCost( self, level )
+end
+
+function viper_viper_strike_oaa:GetCooldown( level )
+	local caster = self:GetCaster()
+
+	if caster:HasScepter() then
+		return self:GetSpecialValueFor( "cooldown_scepter" )
+	end
+
+	return self.BaseClass.GetCooldown( self, level )
+end
+
+--------------------------------------------------------------------------------
+
 function viper_viper_strike_oaa:OnAbilityPhaseStart()
 	local caster = self:GetCaster()
 	local originCaster = caster:GetAbsOrigin()
@@ -33,7 +65,7 @@ function viper_viper_strike_oaa:OnProjectileHit_ExtraData( target, loc, data )
 	-- make sure the projectile actually hit the target
 	-- and the target is still valid
 	-- before applying the rest of the effects
-	if target and UnitFilter( target, self:GetAbilityTargetTeam(), self:GetAbilityTargetType(), self:GetAbilityTargetFlags(), caster:GetTeamNumber() ) then
+	if target and UnitFilter( target, self:GetAbilityTargetTeam(), self:GetAbilityTargetType(), self:GetAbilityTargetFlags(), caster:GetTeamNumber() ) and not target:TriggerSpellAbsorb( self ) then
 		-- play the sound
 		target:EmitSound( "Hero_Viper.ViperStrike.Target" )
 
