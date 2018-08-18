336a6a6046d6d71f33d2c09793d0f7de3b747a36
Chris Vickery <chrisinajar@gmail.com>
2018-08-18 13:09:17 -0700

    Ward stack stuff

--- game/scripts/vscripts/items/ward_stack.lua
+++ game/scripts/vscripts/items/ward_stack.lua
@@ -1,22 +1,117 @@
-LinkLuaModifier( "modifier_item_ward_stack", "items/ward_stack.lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_item_ward_stack_aura", "items/ward_stack.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_item_ward_stack", "items/ward_stack.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_ward_stack_observers", "items/ward_stack.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_ward_stack_sentries", "items/ward_stack.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_ward_stack_aura", "items/ward_stack.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier('modifier_ward_invisibility', 'modifiers/modifier_ward_invisibility.lua', LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_intrinsic_multiplexer", "modifiers/modifier_intrinsic_multiplexer.lua", LUA_MODIFIER_MOTION_NONE)
 
 item_ward_stack = class(ItemBaseClass)
 
--- active effect!
-function item_ward_stack:OnSpellCast (keys)
-  Debug:EnableDebugging()
-  DebugPrintTable(keys)
-  DebugPrint('ward stack things')
+Debug:EnableDebugging()
+
+local WARD_TYPE_SENTRY = 1
+local WARD_TYPE_OBSERVER = 2
+
+local function wardTypeToString (ward)
+  if ward == WARD_TYPE_SENTRY then
+    return "sentry"
+  elseif ward == WARD_TYPE_OBSERVER then
+    return "observer"
+  end
+  return "unknown"
 end
 
-function item_ward_stack:GetIntrinsicModifierName ()
-  return "modifier_item_ward_stack"
+if IsServer() then
+  -- active effect!
+  function item_ward_stack:Setup ()
+    if not self.wardType then
+      self.wardType = WARD_TYPE_SENTRY
+    end
+    if not self.sentryCount then
+      self.sentryCount = 0
+    end
+    if not self.observerCount then
+      self.sentryCount = 0
+    end
+  end
+  function item_ward_stack:OnSpellStart ()
+    self::Setup()
+    local unit = false
+    if not self:GetCursorTargetingNothing() then
+      unit = self:GetCursorTarget()
+    end
+    local caster = self:GetCaster()
+    local target = self:GetCursorPosition()
+    local wardType = wardTypeToString(self.wardType)
+
+    if unit == caster then
+      return self:ToggleType()
+    end
+
+    if self[wardType .. 'Count'] == 0 then
+      self:ToggleType()
+      wardType = wardTypeToString(self.wardType)
+      if self[wardType .. 'Count'] == 0 then
+        return
+      end
+    end
+
+    local ward = CreateUnitByName("npc_dota_" .. wardType .. "_wards", target, true, nil, caster, caster:GetTeam())
+    ward:AddNewModifier(ward, nil, "modifier_kill", { duration = self:GetSpecialValueFor(wardType .. '_duration') })
+    ward:AddNewModifier(ward, nil, "modifier_ward_invisibility", { invisible = true })
+
+    self[wardType .. 'Count'] = self[wardType .. 'Count'] - 1
+    if self[wardType .. 'Count'] == 0 then
+      self:ToggleType()
+    end
+    -- ward
+  end
+
+  function item_ward_stack:ToggleType ()
+    self::Setup()
+    local newVal = self.wardType % 2 + 1
+    if self[wardTypeToString(newVal) .. 'Count'] == 0 then
+      return
+    end
+    self.wardType = newVal
+  end
+
+  function item_ward_stack:GetIntrinsicModifierName ()
+    return "modifier_intrinsic_multiplexer"
+  end
+
+  function item_ward_stack:GetIntrinsicModifierNames ()
+    return {
+      "modifier_item_ward_stack",
+      "modifier_item_ward_stack_observers",
+      "modifier_item_ward_stack_sentries",
+    }
+  end
+end
+
+function item_ward_stack:CastFilterResultTarget (unit)
+  if unit == self:GetCaster() then
+    return UF_SUCCESS
+  end
+  return UF_FAIL_INVALID_LOCATION
 end
 
 item_ward_stack_2 = item_ward_stack
 item_ward_stack_3 = item_ward_stack
 item_ward_stack_4 = item_ward_stack
+
+--------------------------------------------------------------------------
+-- observer/sentry count in status bar
+--------------------------------------------------------------------------
+modifier_item_ward_stack_observers = class(ModifierBaseClass)
+modifier_item_ward_stack_sentries = class(ModifierBaseClass)
+
+function modifier_item_ward_stack_observers:GetTexture()
+  return "item_observer_ward"
+end
+function modifier_item_ward_stack_sentries:GetTexture()
+  return "item_sentry_ward"
+end
 
 --------------------------------------------------------------------------
 -- modifier_item_ward_stack
@@ -56,9 +151,9 @@ end
 -- passive stats
 function modifier_item_ward_stack:DeclareFunctions()
   return {
-    MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT
-    MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS
-    MODIFIER_PROPERTY_HEALTH_BONUS
+    MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT,
+    MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
+    MODIFIER_PROPERTY_HEALTH_BONUS,
     MODIFIER_PROPERTY_MANA_BONUS
   }
 end
