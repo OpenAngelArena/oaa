5e25992c802eb65f59bea48178d97c0e24ca81fe
Chris Vickery <chrisinajar@gmail.com>
2018-09-11 18:26:45 -0700

    4.0.1

--- game/scripts/vscripts/addon_game_mode.lua
+++ game/scripts/vscripts/addon_game_mode.lua
@@ -1,6 +1,6 @@
 -- This is the entry-point to your game mode and should be used primarily to precache models/particles/sounds/etc
 
-GAME_VERSION = "4.0.0"
+GAME_VERSION = "4.0.1"
 
 -- Setup the main logger
 require('internal/logging')
