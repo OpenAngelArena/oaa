5afbddc6d4e689192a487f9f76dcd5c387b395cf
Soo Kuo-Yi <ksoo11@student.monash.edu>
2018-11-23 06:58:16 +0800

    Fix semistandard issues

--- content/panorama/scripts/custom_game/armor_resist_value.js
+++ content/panorama/scripts/custom_game/armor_resist_value.js
@@ -1,30 +1,30 @@
-/* global GameEvents, FindDotaHudElement */
+/* global GameEvents, Entities, FindDotaHudElement */
 'use strict';
 (function () {
-  var stats_tooltip_region = FindDotaHudElement('stats_tooltip_region');
+  var statsTooltipRegion = FindDotaHudElement('stats_tooltip_region');
   // Create a child panel to occupy the same space so that we can listen for mouse over events
-  var hook_panel = $.CreatePanel('Panel', stats_tooltip_region, 'stats_tooltip_region_hook');
-  hook_panel.SetPanelEvent('onmouseover' /* ON_MOUSE_OVER */, function () { return $.Schedule(0.1, UpdateTooltipPhysicalResistanceValue); });
-  hook_panel.style.width = '100%';
-  hook_panel.style.height = '100%';
+  var hookPanel = $.CreatePanel('Panel', statsTooltipRegion, 'stats_tooltip_region_hook');
+  hookPanel.SetPanelEvent('onmouseover' /* ON_MOUSE_OVER */, function () { return $.Schedule(0.1, UpdateTooltipPhysicalResistanceValue); });
+  hookPanel.style.width = '100%';
+  hookPanel.style.height = '100%';
   GameEvents.Subscribe('dota_portrait_unit_stats_changed', UpdateAltDisplayPhysicalResistanceValue);
   GameEvents.Subscribe('dota_portrait_unit_modifiers_changed', UpdateAltDisplayPhysicalResistanceValue);
   GameEvents.Subscribe('dota_player_update_hero_selection', UpdateAltDisplayPhysicalResistanceValue);
   GameEvents.Subscribe('dota_player_update_selected_unit', UpdateAltDisplayPhysicalResistanceValue);
 }());
-function UpdateTooltipPhysicalResistanceValue() {
-  var physical_resist_label = FindDotaHudElement('PhysicalResist');
-  if (physical_resist_label != null) {
-    var unit_armor = Entities.GetPhysicalArmorValue(Players.GetLocalPlayerPortraitUnit());
-    var physical_resistance = Math.round(0.05 * unit_armor / (1 + 0.05 * unit_armor) * 100);
-    physical_resist_label.text = physical_resistance + '%';
+function UpdateTooltipPhysicalResistanceValue () {
+  var physicalResistLabel = FindDotaHudElement('PhysicalResist');
+  if (physicalResistLabel != null) {
+    var unitArmor = Entities.GetPhysicalArmorValue(Players.GetLocalPlayerPortraitUnit());
+    var physicalResistance = Math.round(0.05 * unitArmor / (1 + 0.05 * unitArmor) * 100);
+    physicalResistLabel.text = physicalResistance + '%';
   }
 }
-function UpdateAltDisplayPhysicalResistanceValue() {
-  var physical_resist_label = FindDotaHudElement('PhysicalDamageResist');
-  if (physical_resist_label != null) {
-    var unit_armor = Entities.GetPhysicalArmorValue(Players.GetLocalPlayerPortraitUnit());
-    var physical_resistance = Math.round(0.05 * unit_armor / (1 + 0.05 * unit_armor) * 100);
-    physical_resist_label.text = physical_resistance + '%';
+function UpdateAltDisplayPhysicalResistanceValue () {
+  var physicalResistLabel = FindDotaHudElement('PhysicalDamageResist');
+  if (physicalResistLabel != null) {
+    var unitArmor = Entities.GetPhysicalArmorValue(Players.GetLocalPlayerPortraitUnit());
+    var physicalResistance = Math.round(0.05 * unitArmor / (1 + 0.05 * unitArmor) * 100);
+    physicalResistLabel.text = physicalResistance + '%';
   }
 }
--- content/panorama/scripts/custom_game/armor_resist_value.ts
+++ content/panorama/scripts/custom_game/armor_resist_value.ts
@@ -1,13 +1,13 @@
-/* global GameEvents, FindDotaHudElement */
+/* global GameEvents, Entities, FindDotaHudElement */
 'use strict';
 
 (function () {
-  let stats_tooltip_region = FindDotaHudElement('stats_tooltip_region');
+  let statsTooltipRegion = FindDotaHudElement('stats_tooltip_region');
   // Create a child panel to occupy the same space so that we can listen for mouse over events
-  let hook_panel = $.CreatePanel('Panel', stats_tooltip_region, 'stats_tooltip_region_hook');
-  hook_panel.SetPanelEvent(PanelEvent.ON_MOUSE_OVER, () => $.Schedule(0.1, UpdateTooltipPhysicalResistanceValue));
-  hook_panel.style.width = '100%';
-  hook_panel.style.height = '100%';
+  let hookPanel = $.CreatePanel('Panel', statsTooltipRegion, 'stats_tooltip_region_hook');
+  hookPanel.SetPanelEvent(PanelEvent.ON_MOUSE_OVER, () => $.Schedule(0.1, UpdateTooltipPhysicalResistanceValue));
+  hookPanel.style.width = '100%';
+  hookPanel.style.height = '100%';
 
   GameEvents.Subscribe('dota_portrait_unit_stats_changed', UpdateAltDisplayPhysicalResistanceValue);
   GameEvents.Subscribe('dota_portrait_unit_modifiers_changed', UpdateAltDisplayPhysicalResistanceValue);
@@ -15,20 +15,20 @@
   GameEvents.Subscribe('dota_player_update_selected_unit', UpdateAltDisplayPhysicalResistanceValue);
 }())
 
-function UpdateTooltipPhysicalResistanceValue() {
-  let physical_resist_label = <LabelPanel>FindDotaHudElement('PhysicalResist');
-  if (physical_resist_label != null) {
-    let unit_armor = Entities.GetPhysicalArmorValue(Players.GetLocalPlayerPortraitUnit());
-    let physical_resistance = Math.round(0.05 * unit_armor / (1 + 0.05 * unit_armor) * 100);
-    physical_resist_label.text = physical_resistance + '%';
+function UpdateTooltipPhysicalResistanceValue () {
+  let physicalResistLabel = <LabelPanel>FindDotaHudElement('PhysicalResist');
+  if (physicalResistLabel != null) {
+    let unitArmor = Entities.GetPhysicalArmorValue(Players.GetLocalPlayerPortraitUnit());
+    let physicalResistance = Math.round(0.05 * unitArmor / (1 + 0.05 * unitArmor) * 100);
+    physicalResistLabel.text = physicalResistance + '%';
   }
 }
 
-function UpdateAltDisplayPhysicalResistanceValue() {
-  let physical_resist_label = <LabelPanel>FindDotaHudElement('PhysicalDamageResist');
-  if (physical_resist_label != null) {
-    let unit_armor = Entities.GetPhysicalArmorValue(Players.GetLocalPlayerPortraitUnit());
-    let physical_resistance = Math.round(0.05 * unit_armor / (1 + 0.05 * unit_armor) * 100);
-    physical_resist_label.text = physical_resistance + '%';
+function UpdateAltDisplayPhysicalResistanceValue () {
+  let physicalResistLabel = <LabelPanel>FindDotaHudElement('PhysicalDamageResist');
+  if (physicalResistLabel != null) {
+    let unitArmor = Entities.GetPhysicalArmorValue(Players.GetLocalPlayerPortraitUnit());
+    let physicalResistance = Math.round(0.05 * unitArmor / (1 + 0.05 * unitArmor) * 100);
+    physicalResistLabel.text = physicalResistance + '%';
   }
 }
