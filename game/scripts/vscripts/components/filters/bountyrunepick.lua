7459e7f89222a11bd2d2fe60cd13cf74eb4b16e3
Chris Vickery <chrisinajar@gmail.com>
2019-01-06 12:48:25 -0800

    Fix bounty rewards and remove initial creep spawn delay

--- game/scripts/vscripts/components/filters/bountyrunepick.lua
+++ game/scripts/vscripts/components/filters/bountyrunepick.lua
@@ -13,11 +13,19 @@ function BountyRunePick:Filter(filter_table)
   local vanilla_gold_bounty = filter_table.gold_bounty
   local playerID = filter_table.player_id_const
   local vanilla_xp_bounty = filter_table.xp_bounty
+  Debug:EnableDebugging()
 
   -- Game time in seconds:
   local game_time = HudTimer:GetGameTime()
+  game_time = game_time - (game_time % 120)
   -- Game time in minutes:
-  game_time = game_time/60
+  game_time = math.floor(game_time / 60)
+  -- start at 1 minute instead of 0
+  if game_time < 1 then
+    DebugPrint('Using minute 1 rune instead')
+    game_time = 1
+  end
+  DebugPrint('Game time is ' .. tostring(game_time))
   -- Hero that picked up the rune
   local hero_with_rune = PlayerResource:GetSelectedHeroEntity(playerID)
   -- Team that picked up the rune
