39d35f2316c5c92436f14e4e84bbaddb11021a35
Darko V <darko1290@gmail.com>
2019-03-24 19:52:47 +0100

    Alpha Version

--- game/scripts/vscripts/abilities/oaa_wukongs_command.lua
+++ game/scripts/vscripts/abilities/oaa_wukongs_command.lua
@@ -16,16 +16,11 @@ end
 function monkey_king_wukongs_command_oaa:GetAOERadius()
   local caster = self:GetCaster()
 
-  if caster:HasTalent("special_bonus_unique_monkey_king_6") then
-    return caster:FindTalentValue("special_bonus_unique_monkey_king_6", "value")
-  end
-
-  return self:GetSpecialValueFor("second_radius")
+  return math.max(caster:FindTalentValue("special_bonus_unique_monkey_king_6", "value"), self:GetSpecialValueFor("second_radius"))
 end
 
 function monkey_king_wukongs_command_oaa:OnSpellStart()
   local caster = self:GetCaster()
-  --local playerID = caster:GetPlayerID()
   local center = self:GetCursorPosition()
   local unit_name = "npc_dota_monkey_clone_oaa"
 
@@ -44,53 +39,58 @@ function monkey_king_wukongs_command_oaa:OnSpellStart()
     units_on_third_ring = caster:FindTalentValue("special_bonus_unique_monkey_king_6", "value2")
     self.active_radius = third_ring_radius
   end
-
-  self:RemoveMonkeys()
+  -- Particle
+  --"particles/units/heroes/hero_monkey_king/monkey_king_fur_army_cast.vpcf"
+  -- Sound
+  EmitSoundOn("Hero_MonkeyKing.FurArmy", caster)
+   -- Remove previos instance of Wukongs Command
+  if caster.monkeys_thinker and not caster.monkeys_thinker:IsNull() then
+    caster.monkeys_thinker:Destroy()
+  end
+  -- Thinker
+  CreateModifierThinker(caster, self, "modifier_wukongs_command_oaa_thinker", {duration = self:GetSpecialValueFor("duration")}, center, caster:GetTeamNumber(), false)
   -- Inner Ring:
   self:CreateMonkeyRing(unit_name, first_ring, caster, center, first_ring_radius)
   -- Outer Ring:
   self:CreateMonkeyRing(unit_name, second_ring, caster, center, second_ring_radius)
   -- Extra Ring with the talent:
   self:CreateMonkeyRing(unit_name, third_ring, caster, center, third_ring_radius)
-
-  CreateModifierThinker(caster, self, "modifier_wukongs_command_oaa_thinker", {duration = self:GetSpecialValueFor("duration")}, center, caster:GetTeamNumber(), false)
-  EmitSoundOn("Hero_MonkeyKing.FurArmy", caster)
-
 end
 
 function monkey_king_wukongs_command_oaa:CreateMonkeyRing(unit_name, number, caster, center, radius)
   if number == 0 or radius == 0 then
     return
   end
-
+  --"particles/units/heroes/hero_monkey_king/monkey_king_furarmy_ring.vpcf"
   local top_direction = Vector(0,1,0)
   local top_point = center + top_direction*radius
-  local top_monkey = CreateUnitByName(unit_name, top_point, true, caster, caster:GetOwner(), caster:GetTeam())
+  local top_monkey = CreateUnitByName(unit_name, top_point, false, caster, caster:GetOwner(), caster:GetTeam())
   top_monkey:SetOwner(caster)
   top_monkey:AddNewModifier(caster, self, "modifier_monkey_clone_oaa", {})
-
   local angle_degrees = 360/number
-  for i=1,number-1 do
-    local direction = RotatePosition(center, QAngle(0,i*angle_degrees,0), top_direction)
-    local point = center + direction*radius
-    local monkey = CreateUnitByName(unit_name, point, true, caster, caster:GetOwner(), caster:GetTeam())
+  for i=1, number-1 do
+    -- Rotate a point around center for angle_degrees to get a new point
+    local point = RotatePosition(center, QAngle(0,i*angle_degrees,0), top_point)
+    local monkey = CreateUnitByName(unit_name, point, false, caster, caster:GetOwner(), caster:GetTeam())
     monkey:SetOwner(caster)
     monkey:AddNewModifier(caster, self, "modifier_monkey_clone_oaa", {})
   end
 end
 
-function monkey_king_wukongs_command_oaa:RemoveMonkeys()
-  local caster = self:GetCaster()
+function monkey_king_wukongs_command_oaa:RemoveMonkeys(caster)
   local unit_name = "npc_dota_monkey_clone_oaa"
   -- Find all monkeys belonging to the caster on the map and kill them
-  local allied_units = FindUnitsInRadius(caster:GetTeam(), Vector(0, 0, 0), nil, FIND_UNITS_EVERYWHERE, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_BASIC, bit.bor(DOTA_UNIT_TARGET_FLAG_NOT_ILLUSIONS, DOTA_UNIT_TARGET_FLAG_INVULNERABLE, DOTA_UNIT_TARGET_FLAG_OUT_OF_WORLD), FIND_ANY_ORDER, false)
+  local allied_units = FindUnitsInRadius(caster:GetTeamNumber(), Vector(0, 0, 0), nil, FIND_UNITS_EVERYWHERE, DOTA_UNIT_TARGET_TEAM_FRIENDLY, DOTA_UNIT_TARGET_BASIC, bit.bor(DOTA_UNIT_TARGET_FLAG_NOT_ILLUSIONS, DOTA_UNIT_TARGET_FLAG_INVULNERABLE, DOTA_UNIT_TARGET_FLAG_OUT_OF_WORLD), FIND_ANY_ORDER, false)
   for _,unit in pairs(allied_units) do
     if unit:GetUnitName() == unit_name then
+      --"particles/units/heroes/hero_monkey_king/monkey_king_fur_army_destroy.vpcf"
       unit:AddNoDraw()
-      unit:AddNewModifier(unit, nil, "modifier_kill", { duration = 0.5 })
+      unit:AddNewModifier(unit, nil, "modifier_kill", {duration = 0.2})
     end
   end
+  -- Sounds
   StopSoundOn("Hero_MonkeyKing.FurArmy", caster)
+  EmitSoundOn("Hero_MonkeyKing.FurArmy.End", caster)
 end
 
 function monkey_king_wukongs_command_oaa:ProcsMagicStick()
@@ -111,6 +111,10 @@ end
 
 function modifier_wukongs_command_oaa_thinker:IsPurgable()
   return false
+end
+
+function modifier_wukongs_command_oaa_thinker:IsAura()
+  return true
 end
 
 function modifier_wukongs_command_oaa_thinker:GetModifierAura()
@@ -150,15 +154,15 @@ end
 
 function modifier_wukongs_command_oaa_thinker:OnCreated()
   local caster = self:GetCaster()
-
-  self:StartIntervalThink(0.2)
+  caster.monkeys_thinker = self
+  -- Start checking caster for the buff
+  self:StartIntervalThink(0.3)
 end
 
 function modifier_wukongs_command_oaa_thinker:OnIntervalThink()
   local caster = self:GetCaster()
-   local ability = self:GetAbility()
+  local ability = self:GetAbility()
   if not caster:HasModifier("modifier_wukongs_command_oaa_buff") then
-    ability:RemoveMonkeys()
     self:StartIntervalThink(-1)
     self:SetDuration(0.1, false)
   end
@@ -168,10 +172,17 @@ function modifier_wukongs_command_oaa_thinker:OnDeath(event)
   if IsServer() then
     if event.unit == self:GetCaster() then
       local ability = self:GetAbility()
-      ability:RemoveMonkeys()
       self:StartIntervalThink(-1)
       self:SetDuration(0.1, false)
     end
+  end
+end
+
+function modifier_wukongs_command_oaa_thinker:OnDestroy()
+  if IsServer() then
+    local caster = self:GetCaster()
+    local ability = self:GetAbility()
+    ability:RemoveMonkeys(caster)
   end
 end
 
@@ -180,7 +191,7 @@ end
 modifier_wukongs_command_oaa_buff = class(ModifierBaseClass)
 
 function modifier_wukongs_command_oaa_buff:IsHidden()
-  return true
+  return false
 end
 
 function modifier_wukongs_command_oaa_buff:IsDebuff()
@@ -193,7 +204,7 @@ end
 
 function modifier_wukongs_command_oaa_buff:DeclareFunctions()
   local funcs = {
-  MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
+    MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
   }
   return funcs
 end
@@ -221,54 +232,68 @@ end
 function modifier_monkey_clone_oaa:OnCreated()
   local caster = self:GetCaster()
   local parent = self:GetParent()
-  local ability = self:GetAbility
+  local ability = self:GetAbility()
 
-  local banned_items ={
+  local banned_items = {
     "item_abyssal_blade",
     "item_abyssal_blade_2",
     "item_abyssal_blade_3",
     "item_abyssal_blade_4",
-    "item_abyssal_blade_5"
+    "item_abyssal_blade_5",
+    "item_rapier",
+    "item_gem",
+    "item_courier"
   }
+  if IsServer() then
+    for item_slot = DOTA_ITEM_SLOT_1, DOTA_ITEM_SLOT_6 do
+      local item = caster:GetItemInSlot(item_slot)
+      if item then
+        local item_name = item:GetName()
+        local skip = false
 
-  for item_slot=DOTA_ITEM_SLOT_1, DOTA_ITEM_SLOT_9 do
-    local item = caster:GetItemInSlot(item_slot)
-    if item then
-      local item_name = item:GetName()
-      local skip = false
+        -- Don't add certain items like Abyssal Blade
+        for i= 1, #banned_items do
+          if item_name == banned_items[i] then
+            skip = true
+          end
+        end
 
-      -- Don't add certain items like Abyssal Blade
-      for i=1, #banned_items do
-        if item_name == banned_items[i] then
+        -- Dont add items with charges to avoid weird bugs
+        if item:RequiresCharges() then
           skip = true
         end
-      end
 
-      -- Dont add items with charges to avoid weird bugs
-      if item:RequiresCharges() then
-        skip = true
-      end
+        if not skip then
+          local new_item = CreateItem(item_name, parent, parent)
+          parent:AddItem(new_item)
+          new_item:SetStacksWithOtherOwners(true)
+          new_item:SetPurchaser(nil)
 
-      if not skip then
-        local new_item = CreateItem(item_name, parent, parent)
-        parent:AddItem(new_item)
-        new_item:SetStacksWithOtherOwners(true)
-        new_item:SetPurchaser(nil)
-
-        if new_item:IsToggle() and item:GetToggleState() then
-          new_item:ToggleAbility()
+          if new_item:IsToggle() and item:GetToggleState() then
+            new_item:ToggleAbility()
+          end
         end
       end
     end
-  end
 
-  parent:SetBaseDamageMax(caster:GetBaseDamageMax())
-	parent:SetBaseDamageMin(caster:GetBaseDamageMin())
-  --parent:SetBaseAttackTime(ability:GetSpecialValueFor("bat"))    --caster:GetBaseAttackTime()
-	--parent:SetHasInventory(false)
-	parent:SetCanSellItems(false)
-  parent:SetNeverMoveToClearSpace(true)
-  parent:AttackNoEarlierThan(ability:GetSpecialValueFor("attack_interval"))  -- Setting attack interval
+    parent:SetBaseDamageMax(caster:GetBaseDamageMax())
+    parent:SetBaseDamageMin(caster:GetBaseDamageMin())
+    parent:SetNeverMoveToClearSpace(true)
+	  -- For attacking animations - requires Animations library
+    AddAnimationTranslate(parent, "attack_normal_range")
+  end
+end
+
+function modifier_monkey_clone_oaa:DeclareFunctions()
+  local funcs = {
+    MODIFIER_PROPERTY_FIXED_ATTACK_RATE,
+  }
+  return funcs
+end
+
+function modifier_monkey_clone_oaa:GetModifierFixedAttackRate(params)
+  local ability = self:GetAbility()
+  return ability:GetSpecialValueFor("attack_interval")
 end
 
 function modifier_monkey_clone_oaa:CheckState()
@@ -283,7 +308,7 @@ function modifier_monkey_clone_oaa:CheckState()
     [MODIFIER_STATE_NO_HEALTH_BAR] = true,
     [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
     [MODIFIER_STATE_NOT_ON_MINIMAP] = true,
-    --[MODIFIER_STATE_OUT_OF_GAME] = true,
-    }
+    [MODIFIER_STATE_OUT_OF_GAME] = true,
+  }
   return state
 end
