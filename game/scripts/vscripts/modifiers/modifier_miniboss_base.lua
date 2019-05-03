dde7686ec9b5b358de802bfe9efff4ae61cdcaa0
Jochnickel <47549862+Jochnickel@users.noreply.github.com>
2019-05-03 23:33:25 +0200

    buffs done

--- game/scripts/vscripts/modifiers/modifier_miniboss_base.lua
+++ game/scripts/vscripts/modifiers/modifier_miniboss_base.lua
@@ -1,30 +1,29 @@
-modifier_miniboss_good_buff = class({})
-print("inclued")
+modifier_miniboss_base = class(ModifierBaseClass)
 
-function modifier_miniboss_good_buff:IsHidden()		return false end
 
-function modifier_miniboss_good_buff:RemoveOnDeath()	return true end
+function modifier_miniboss_base:IsHidden()		return false end
 
-function modifier_miniboss_good_buff:GetTexture()
-	return "ursa_enrage"
-end
+function modifier_miniboss_base:RemoveOnDeath()	return true end
 
-function modifier_miniboss_good_buff:DeclareFunctions()
+function modifier_miniboss_base:GetTexture() return "death_prophet_carrion_swarm" end
+
+function modifier_miniboss_base:DeclareFunctions()
 	local funcs = {
 			MODIFIER_EVENT_ON_DEATH, -- OnDeath 
 			MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE, -- GetModifierMoveSpeedBonus_Percentage 
 			MODIFIER_PROPERTY_COOLDOWN_PERCENTAGE, -- GetModifierPercentageCooldown
 			MODIFIER_EVENT_ON_TAKEDAMAGE, -- OnTakeDamage 
-			MODIFIER_PROPERTY_HEALTH_REGEN_PERCENTAGE, -- GetModifierHealthRegenPercentage 
+			MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT, -- GetModifierConstantHealthRegen  
+			MODIFIER_EVENT_ON_ATTACK_LANDED, -- OnAttackLanded 
 	}
 	return funcs
 end
 
-function modifier_miniboss_good_buff:OnCreated( event )
+function modifier_miniboss_base:OnCreated( event )
 	if not IsServer() then return end
 end
 
-function modifier_miniboss_good_buff:OnDeath( event )
+function modifier_miniboss_base:OnDeath( event )
 	if (event.unit~=self:GetParent())
 	or (not event.attacker:IsRealHero()) then return end
 	print("hero killed buff")
