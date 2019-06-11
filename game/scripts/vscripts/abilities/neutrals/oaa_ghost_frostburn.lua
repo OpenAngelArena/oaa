707777ae06663f0e0c559eab6fbf8b1c7e9698b1
Darko V <darko1290@gmail.com>
2019-06-11 21:29:34 +0200

    Some work
    * Initial upload for Black Dragon custom Dragonhide aura
    * Update for Ghost Frostburn
    * Initial upload for Ghost Vortex
    * Initial upload of Centaur Stomp KV file

--- game/scripts/vscripts/abilities/neutrals/oaa_ghost_frostburn.lua
+++ game/scripts/vscripts/abilities/neutrals/oaa_ghost_frostburn.lua
@@ -1,66 +1,66 @@
-ghost_frostburn_oaa = class(ItemBaseClass)
+ghost_frostburn_oaa = class(AbilityBaseClass)
 
-LinkLuaModifier("modifier_frostburn_applier", "abilities/neutrals/oaa_ghost_frostburn.lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier("modifier_frostburn_effect", "abilities/neutrals/oaa_ghost_frostburn.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_frostburn_oaa_applier", "abilities/neutrals/oaa_ghost_frostburn.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_frostburn_oaa_effect", "abilities/neutrals/oaa_ghost_frostburn.lua", LUA_MODIFIER_MOTION_NONE )
 
 function ghost_frostburn_oaa:GetIntrinsicModifierName()
-  return "modifier_frostburn_applier"
+  return "modifier_frostburn_oaa_applier"
 end
 
 --------------------------------------------------------------------------------
 
-modifier_frostburn_applier = class(ModifierBaseClass)
+modifier_frostburn_oaa_applier = class(ModifierBaseClass)
 
-function modifier_frostburn_applier:IsHidden()
+function modifier_frostburn_oaa_applier:IsHidden()
   return true
 end
 
-function modifier_frostburn_applier:IsDebuff()
+function modifier_frostburn_oaa_applier:IsDebuff()
   return false
 end
 
-function modifier_frostburn_applier:IsPurgable()
+function modifier_frostburn_oaa_applier:IsPurgable()
   return false
 end
 
-function modifier_frostburn_applier:OnCreated()
+function modifier_frostburn_oaa_applier:OnCreated()
   self.heal_prevent_duration = self:GetAbility():GetSpecialValueFor("heal_prevent_duration")
 end
 
-function modifier_frostburn_applier:DeclareFunctions()
+function modifier_frostburn_oaa_applier:DeclareFunctions()
   local funcs = {
     MODIFIER_EVENT_ON_ATTACK_LANDED,
   }
   return funcs
 end
 
-function modifier_frostburn_applier:OnAttackLanded(event)
+function modifier_frostburn_oaa_applier:OnAttackLanded(event)
   if IsServer() then
     local attacker = event.attacker
     local target = event.target
     if attacker == self:GetParent() and not attacker:IsIllusion() and not target:IsMagicImmune() then
-      target:AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_frostburn_effect", {duration = self.heal_prevent_duration})
+      target:AddNewModifier(self:GetCaster(), self:GetAbility(), "modifier_frostburn_oaa_effect", {duration = self.heal_prevent_duration})
     end
   end
 end
 
 --------------------------------------------------------------------------------
 
-modifier_frostburn_effect = class(ModifierBaseClass)
+modifier_frostburn_oaa_effect = class(ModifierBaseClass)
 
-function modifier_frostburn_effect:IsHidden()
+function modifier_frostburn_oaa_effect:IsHidden()
   return false
 end
 
-function modifier_frostburn_effect:IsDebuff()
+function modifier_frostburn_oaa_effect:IsDebuff()
   return true
 end
 
-function modifier_frostburn_effect:IsPurgable()
+function modifier_frostburn_oaa_effect:IsPurgable()
   return false
 end
 
-function modifier_frostburn_effect:OnCreated()
+function modifier_frostburn_oaa_effect:OnCreated()
   if IsServer() then
     self.heal_prevent_percent = self:GetAbility():GetSpecialValueFor("heal_prevent_percent")
     self.duration = self:GetAbility():GetSpecialValueFor("heal_prevent_duration")
@@ -68,14 +68,14 @@ function modifier_frostburn_effect:OnCreated()
   end
 end
 
-function modifier_frostburn_effect:DeclareFunctions()
+function modifier_frostburn_oaa_effect:DeclareFunctions()
   local funcs = {
     MODIFIER_EVENT_ON_HEALTH_GAINED
   }
   return funcs
 end
 
-function modifier_frostburn_effect:OnHealthGained(event)
+function modifier_frostburn_oaa_effect:OnHealthGained(event)
   if IsServer() then
     -- Check that event is being called for the unit that self is attached to
     local parent = self:GetParent()
