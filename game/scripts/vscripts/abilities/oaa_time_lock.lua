3592b6ac6149c1337cdee7d6910b804d9da6a454
Darko V <darko1290@gmail.com>
2019-07-01 01:33:58 +0200

    Initial

--- game/scripts/vscripts/abilities/oaa_time_lock.lua
+++ game/scripts/vscripts/abilities/oaa_time_lock.lua
@@ -96,6 +96,9 @@ if IsServer() then
       -- use cooldown ( and mana, if necessary )
       spell:UseResources( true, true, true )
 
+      -- do another atttack that cannot miss after cd is started to prevent self-proccing
+      parent:PerformAttack(target, true, true, true, false, false, false, true)
+
       -- because talents are dumb we need to manually get its value
       local damageTalent = 0
 
