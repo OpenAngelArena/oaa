6ce006082fbb0aba71664db3146988a48caaf993
Darko V <darko1290@gmail.com>
2019-10-16 02:27:25 +0200

    Update creep_types.lua
    math is hard

--- game/scripts/vscripts/components/creeps/creep_types.lua
+++ game/scripts/vscripts/components/creeps/creep_types.lua
@@ -6,11 +6,11 @@ CreepTypes = {
     {                                              --HP   MANA  DMG   ARM   GOLD  EXP
       {"npc_dota_neutral_custom_small_wolf",        400,    0,  15,    1,    31,  30},
       {"npc_dota_neutral_custom_small_wolf",        400,    0,  15,    1,    31,  30},
-      {"npc_dota_neutral_custom_big_wolf",          600,  480,  35,    2,    85,  71}
+      {"npc_dota_neutral_custom_big_wolf",          600,  480,  35,    2,    85,  60}
     },
     {
-      {"npc_dota_neutral_custom_kobold_foreman",    560,  500,  16,    1,    40,  35},
-      {"npc_dota_neutral_custom_kobold_soldier",    480,    0,  12,    1,    36,  35},
+      {"npc_dota_neutral_custom_kobold_foreman",    560,  500,  16,    1,    40,  50},
+      {"npc_dota_neutral_custom_kobold_soldier",    480,    0,  12,    1,    36,  40},
       {"npc_dota_neutral_custom_kobold",            280,    0,  10,   0.5,   26,  30}
     },
   },
@@ -18,29 +18,31 @@ CreepTypes = {
   {
     {                                              --HP   MANA  DMG   ARM   GOLD  EXP
       {"npc_dota_neutral_custom_harpy_storm",       560,  500,  24,   1.2,   45,  82}, --expected gold is 75 and XP is 272
+      {"npc_dota_neutral_custom_harpy_storm",       560,  500,  24,   1.2,   45,  82},
       {"npc_dota_neutral_custom_harpy_scout",       440,    0,  40,   0.7,   40,  61},
       {"npc_dota_neutral_custom_harpy_scout",       440,    0,  40,   0.7,   40,  61}
     },
     {
-      {"npc_dota_neutral_custom_mud_golem",         480,    0,  12,    1,    33,  60},
-      {"npc_dota_neutral_custom_mud_golem",         480,    0,  12,    1,    33,  60}
+      {"npc_dota_neutral_custom_mud_golem",         480,    0,  12,    1,    33,  57},
+      {"npc_dota_neutral_custom_mud_golem",         480,    0,  12,    1,    33,  57}
     },
     {
       {"npc_dota_neutral_custom_blue_potato",       480,    0,  28,   1.3,   50,  61},
       {"npc_dota_neutral_custom_blue_potato",       480,    0,  28,   1.3,   50,  61},
+      {"npc_dota_neutral_custom_blue_tomato",       800,  500,  28,   1.3,   50,  82},
       {"npc_dota_neutral_custom_blue_tomato",       800,  500,  28,   1.3,   50,  82}
     }
   },
     -- 3 "hard camp"
   {
     {                                              --HP   MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_custom_ghost",             800,  400,  30,   1.5,   76,  61}, --expected gold is 113 and XP 121
-      {"npc_dota_neutral_custom_ghost",             800,  400,  30,   1.5,   76,  61}
+      {"npc_dota_neutral_custom_ghost",             800,  400,  30,   1.5,   76,  60}, --expected gold is 113 and XP 121
+      {"npc_dota_neutral_custom_ghost",             800,  400,  30,   1.5,   76,  60}
     },
     {
       {"npc_dota_neutral_custom_centaur_khan",      800,  400,  30,   1.5,   76,  61},
-      {"npc_dota_neutral_custom_small_centaur",     600,  240,  20,   0.8,   37,  60},
-      {"npc_dota_neutral_custom_small_centaur",     600,  240,  20,   0.8,   37,  60}
+      {"npc_dota_neutral_custom_small_centaur",     600,  240,  20,   0.8,   37,  30},
+      {"npc_dota_neutral_custom_small_centaur",     600,  240,  20,   0.8,   37,  30}
     },
     {
       {"npc_dota_neutral_satyr_trickster",          350,  160,  10,    1,    28,  27},
@@ -56,7 +58,7 @@ CreepTypes = {
       {"npc_dota_neutral_rock_golem",              1000,    0,  40,    1,    50,  38}
     },
     {
-      {"npc_dota_neutral_prowler_shaman",          1200,    0,  60,    2,   120, 100}
+      {"npc_dota_neutral_prowler_shaman",          1200,    0,  60,    2,   120, 151}
     },
     {
       {"npc_dota_neutral_black_dragon",            1700,    0,  80,    3,   199, 151}
