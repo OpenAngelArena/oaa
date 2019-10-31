d24562b219dd739bb68dee05127cd87b2273474e
Chris Vickery <chrisinajar@gmail.com>
2019-10-30 17:40:58 -0700

    Allow finals duels to be canceled by increasing the score limit (#2773)

--- game/scripts/vscripts/components/duels/duels.lua
+++ game/scripts/vscripts/components/duels/duels.lua
@@ -226,6 +226,7 @@ function Duels:StartDuel(options)
     DebugPrint ('There is already a duel running')
     return
   end
+  self.wasCanceled = false;
   options = options or {}
   if not options.firstDuel then
     Music:SetMusic(12)
@@ -237,17 +238,32 @@ function Duels:StartDuel(options)
   Notifications:TopToAll({text="#duel_imminent_warning", duration=math.min(DUEL_START_WARN_TIME, 5.0), replacement_map={seconds_to_duel = DUEL_START_WARN_TIME}})
   for index = 0,(DUEL_START_COUNTDOWN - 1) do
     Timers:CreateTimer(DUEL_START_WARN_TIME - DUEL_START_COUNTDOWN + index, function ()
+      if self.wasCanceled then
+        return
+      end
       Notifications:TopToAll({text=(DUEL_START_COUNTDOWN - index), duration=1.0})
     end)
   end
 
   Timers:CreateTimer(DUEL_START_WARN_TIME, function ()
+    if self.wasCanceled then
+      return
+    end
     Notifications:TopToAll({text="#duel_start", duration=3.0, style={color="red", ["font-size"]="110px"}})
     for _, zone in ipairs(self.zones) do
       ZoneCleaner:CleanZone(zone)
     end
     self:ActuallyStartDuel(options)
   end)
+end
+
+function Duels:CancelDuel ()
+  if self.currentDuel == DUEL_IS_STARTING and self.wasCanceled == false then
+    self.wasCanceled = true
+    Notifications:TopToAll({text="DUEL CANCELED", duration=3.0, style={color="red", ["font-size"]="110px"}})
+
+    self:CleanUpDuel()
+  end
 end
 
 function Duels:SplitDuelPlayers(options)
@@ -512,8 +528,8 @@ function Duels:GetNextDuelTime()
   return Duels.nextDuelTime
 end
 
-function Duels:EndDuel ()
-  if self.currentDuel == nil or type(self.currentDuel) == "number" then
+function Duels:CleanUpDuel ()
+  if self.currentDuel == nil then
     DebugPrint ('There is no duel running')
     return
   end
@@ -536,6 +552,15 @@ function Duels:EndDuel ()
     self.startDuelTimer = Timers:CreateTimer(60 - DUEL_START_WARN_TIME, partial(self.StartDuel, self))
   end)
 
+  self.currentDuel = nil
+end
+
+function Duels:EndDuel ()
+  if self.currentDuel == nil or type(self.currentDuel) == "number" then
+    DebugPrint ('There is no duel running')
+    return
+  end
+
   for playerId = 0,19 do
     for _, zone in ipairs(self.zones) do
       zone.removePlayer(playerId, false)
@@ -543,7 +568,7 @@ function Duels:EndDuel ()
   end
 
   local currentDuel = self.currentDuel
-  self.currentDuel = nil
+  self:CleanUpDuel()
 
   Timers:CreateTimer(0.1, function ()
     DebugPrint('Sending all players back!')
