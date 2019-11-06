4122d14ead45aa8ac0615349aeefc58781f15668
Darko V <darko1290@gmail.com>
2019-11-06 18:35:42 +0100

    Hand of Midas changes
    * Hand of Midas cd from 45 to 55.
    * Hand of Midas now kills a unit without changing its bounty to 0 (like Midas spark). Because of this change: killing Mud Golems with Hand of Midas will not make Mud Golems splits useless.
    * Total XP gained for killing the unit with Hand of Midas is the same.
    * Total gold gained for killing the unit with Hand of Midas is slightly better (most noticable in the early game when it matters). Thats one of the reasons why we nerf the cd on Midas.

--- game/scripts/vscripts/items/hand_of_midas.lua
+++ game/scripts/vscripts/items/hand_of_midas.lua
@@ -27,18 +27,20 @@ function item_hand_of_midas_1:OnSpellStart()
   local caster = self:GetCaster()
   local target = self:GetCursorTarget()
   local xpMult = self:GetSpecialValueFor("xp_multiplier")
-  local defaultGoldBounty = target:GetGoldBounty()
+  --local defaultMaxGoldBounty = target:GetMaximumGoldBounty()
+  --local defaultMinGoldBounty = target:GetMinimumGoldBounty()
   local defaultXPBounty = target:GetDeathXP()
-  local bonusGold = math.max(self:GetSpecialValueFor("bonus_gold"), defaultGoldBounty)
+  local bonusGold = self:GetSpecialValueFor("bonus_gold")
   local player = caster:GetPlayerOwner()
   local playerID = caster:GetPlayerOwnerID()
 
-  -- Particle
+  -- Midas Particle
   local midas_particle = ParticleManager:CreateParticle("particles/items2_fx/hand_of_midas.vpcf", PATTACH_ABSORIGIN_FOLLOW, target)
   ParticleManager:SetParticleControlEnt(midas_particle, 1, caster, PATTACH_POINT_FOLLOW, "attach_hitloc", caster:GetAbsOrigin(), false)
   ParticleManager:ReleaseParticleIndex(midas_particle)
 
   if player then
+    -- Overhead gold amount popup
     SendOverheadEventMessage(player, OVERHEAD_ALERT_GOLD, target, bonusGold, player)
     -- If the Hand of Midas user is a Spirit Bear or Arc Warden Temepest Double:
     if not caster:IsHero() or caster:IsTempestDouble() then
@@ -46,20 +48,20 @@ function item_hand_of_midas_1:OnSpellStart()
     end
   end
 
-  -- Sound
+  -- Midas Sound
   target:EmitSound("DOTA_Item.Hand_Of_Midas")
 
-  -- Give experience to the main hero
+  -- Give experience to the main hero; (xpMult-1) because we are NOT setting Death XP to 0 later
   if caster.AddExperience then
     caster:AddExperience(defaultXPBounty*(xpMult-1), DOTA_ModifyXP_CreepKill, false, false)
   end
 
-  -- Giving gold only to the player that used Hand of Midas
-  --PlayerResource:ModifyGold(playerID, bonusGold, true, DOTA_ModifyGold_CreepKill)
+  -- Giving only bonus gold as reliable gold to the player that used Hand of Midas
+  PlayerResource:ModifyGold(playerID, bonusGold, true, DOTA_ModifyGold_CreepKill)
 
-  --target:SetDeathXP(0) -- setting this to 0 will mess up OAA Mud Golems
-  target:SetMinimumGoldBounty(bonusGold) -- setting this to 0 will mess up OAA Mud Golems
-  target:SetMaximumGoldBounty(bonusGold) -- setting this to 0 will mess up OAA Mud Golems
+  --target:SetDeathXP(0)           -- setting this to 0 will mess up OAA Mud Golems
+  --target:SetMinimumGoldBounty(0) -- setting this to 0 will mess up OAA Mud Golems
+  --target:SetMaximumGoldBounty(0) -- setting this to 0 will mess up OAA Mud Golems
 
   target:Kill(self, caster)
 end
