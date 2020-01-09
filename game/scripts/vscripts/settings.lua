d44ac331c86774d7c43f9765d2fb772160cdaf25
Darko V <darko1290@gmail.com>
2020-01-09 02:31:12 +0100

    First Capture Point changes (#2821)
    * First capture point
    
    * First capture point now at 15 min.
    * First capture point not on highground.
    * First capture point now gives tier 2 core.
    
    * Update settings.lua
    
    * Fix spark scaling
    
    * Better solution?
    
    * First Capture Point spawns at 10 minutes.
    * Second at 20, Third at 30 and so on.
    * First Capture point gives tier 1 core again.
    
    Co-authored-by: Chris Vickery <chrisinajar@gmail.com>

--- game/scripts/vscripts/settings.lua
+++ game/scripts/vscripts/settings.lua
@@ -63,7 +63,7 @@ PREGAME_TIME = 35
 AP_GAME_TIME = 90
 
 -- Duels
-INITIAL_DUEL_DELAY = 35                 -- how long after the clock hits 0 should the initial duel start countind down
+INITIAL_DUEL_DELAY = 35                 -- how long after the clock hits 0 should the initial duel start counting down
 DUEL_START_WARN_TIME = 10               -- How many seconds to count down before each duel (added as a delay before the duel starts)
 DUEL_START_COUNTDOWN = 5                -- How many seconds to count down before each duel (added as a delay before the duel starts)
 DUEL_TIMEOUT = 90                       -- Time before the duel starts counting down to end in a stalemate
@@ -74,8 +74,15 @@ DUEL_RUNE_TIMER = 30                    -- how long until the highground object 
 DUEL_INTERVAL = 480                     -- time from duel ending until dnext duel countdown begins
 DUEL_START_PROTECTION_TIME = 2          -- duel start protection duration
 
+-- Sparks
+SPARK_LEVEL_1_TIME = 0                  -- just a placeholder so the other names make more sense
+SPARK_LEVEL_2_TIME = 240                -- 4 minutes
+SPARK_LEVEL_3_TIME = 900                -- 15 minutes
+SPARK_LEVEL_4_TIME = 1500               -- 25 minutes
+SPARK_LEVEL_5_TIME = 2100               -- 35 minutes
+
 -- CapturePoints
-INITIAL_CAPTURE_POINT_DELAY = 300       -- how long after the clock hits 0 should the CapturePoint duel start countind down
+INITIAL_CAPTURE_POINT_DELAY = 600       -- how long after the clock hits 0 should the initial Capture Point start counting down
 CAPTURE_FIRST_WARN = 60                 -- how many seconds before spawn of capture points the first ping on minimap will show
 CAPTURE_SECOND_WARN = 30                -- how many seconds before spawn of capture points the second ping on minimap will show
 CAPTURE_START_COUNTDOWN = 5             -- How many seconds to count down before each CapturePoint (added as a delay before the duel starts)
