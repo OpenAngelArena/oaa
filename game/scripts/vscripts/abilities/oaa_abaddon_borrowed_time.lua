d28ff2de646b39c764098372e545c86e895b3c61
Darko V <darko1290@gmail.com>
2020-02-10 00:10:06 +0100

    Initial version
    Reworked Abaddon Borrowed Time Scepter:
    - Redirects 40% of the damage done to allies in 900 radius back to Abaddon.

---
+++ game/scripts/vscripts/abilities/oaa_abaddon_borrowed_time.lua
@@ -0,0 +1,279 @@
+abaddon_borrowed_time_oaa = class(AbilityBaseClass)
+
+LinkLuaModifier("modifier_oaa_borrowed_time_passive", "abilities/oaa_abaddon_borrowed_time.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_oaa_borrowed_time_buff_caster", "abilities/oaa_abaddon_borrowed_time.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_oaa_borrowed_time_buff_ally", "abilities/oaa_abaddon_borrowed_time.lua", LUA_MODIFIER_MOTION_NONE)
+
+function abaddon_borrowed_time_oaa:GetIntrinsicModifierName()
+  return "modifier_oaa_borrowed_time_passive"
+end
+
+function abaddon_borrowed_time_oaa:OnSpellStart()
+  if IsServer() then
+    local caster = self:GetCaster()
+    local buff_duration = self:GetSpecialValueFor("duration")
+
+    if caster:HasScepter() then
+      buff_duration = self:GetSpecialValueFor("duration_scepter")
+    end
+
+    -- Strong Dispel
+    caster:Purge(false, true, false, true, true)
+
+    caster:AddNewModifier(caster, self, "modifier_oaa_borrowed_time_buff_caster", {duration = buff_duration})
+
+    --local responses = {"abaddon_abad_borrowedtime_02","abaddon_abad_borrowedtime_03","abaddon_abad_borrowedtime_04","abaddon_abad_borrowedtime_05","abaddon_abad_borrowedtime_06","abaddon_abad_borrowedtime_07","abaddon_abad_borrowedtime_08","abaddon_abad_borrowedtime_09","abaddon_abad_borrowedtime_10","abaddon_abad_borrowedtime_11"}
+    --if not caster:EmitCasterSound("npc_dota_hero_abaddon", responses, 50, DOTA_CAST_SOUND_FLAG_BOTH_TEAMS, nil, nil) then
+      --caster:EmitCasterSound("npc_dota_hero_abaddon", {"abaddon_abad_borrowedtime_01"}, 1, DOTA_CAST_SOUND_FLAG_BOTH_TEAMS, nil, nil)
+    --end
+
+    -- Sound
+    caster:EmitSound("Hero_Abaddon.BorrowedTime")
+  end
+end
+
+function abaddon_borrowed_time_oaa:ProcsMagicStick()
+  return true
+end
+
+function abaddon_borrowed_time_oaa:OnUnStolen()
+  local caster = self:GetCaster()
+  local modifier = caster:FindModifierByName("modifier_oaa_borrowed_time_passive")
+  if modifier then
+    caster:RemoveModifierByName("modifier_oaa_borrowed_time_passive")
+  end
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_oaa_borrowed_time_passive = class(ModifierBaseClass)
+
+function modifier_oaa_borrowed_time_passive:IsHidden()
+  return true
+end
+
+function modifier_oaa_borrowed_time_passive:IsDebuff()
+  return false
+end
+
+function modifier_oaa_borrowed_time_passive:IsPurgable()
+  return false
+end
+
+function modifier_oaa_borrowed_time_passive:AllowIllusionDuplicate()
+  return false
+end
+
+function modifier_oaa_borrowed_time_passive:OnCreated()
+  if IsServer() then
+    self.hp_threshold = self:GetAbility():GetSpecialValueFor("hp_threshold")
+    -- Check if we need to auto cast immediately
+    self:CheckHealthToTrigger()
+	end
+end
+
+function modifier_oaa_borrowed_time_passive:CheckHealthToTrigger()
+  local parent = self:GetParent()
+  local ability = self:GetAbility()
+
+	-- Check for ability state, if parent has break debuff
+  if not ability:IsHidden() and ability:IsCooldownReady() and not parent:PassivesDisabled() and parent:IsAlive() and not parent:IsIllusion() then
+    local hp_threshold = self.hp_threshold
+    local current_hp = parent:GetHealth()
+    if current_hp <= hp_threshold then
+      parent:CastAbilityImmediately(ability, parent:GetPlayerID())
+    end
+  end
+end
+
+function modifier_oaa_borrowed_time_passive:DeclareFunctions()
+  local funcs = {
+    MODIFIER_EVENT_ON_TAKEDAMAGE
+  }
+
+  return funcs
+end
+
+function modifier_oaa_borrowed_time_passive:OnTakeDamage(event)
+  if IsServer() then
+    local parent = self:GetParent()
+
+    -- Do nothing if damage has HP removal flag
+    if bit.band(event.damage_flags, DOTA_DAMAGE_FLAG_HPLOSS) == DOTA_DAMAGE_FLAG_HPLOSS then
+      return
+    end
+
+    if parent == event.unit then
+      self:CheckHealthToTrigger()
+    end
+  end
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_oaa_borrowed_time_buff_caster = class(ModifierBaseClass)
+
+function modifier_oaa_borrowed_time_buff_caster:IsHidden() -- needs tooltip
+  return false
+end
+
+function modifier_oaa_borrowed_time_buff_caster:IsDebuff()
+  return false
+end
+
+function modifier_oaa_borrowed_time_buff_caster:IsPurgable()
+  return false
+end
+
+function modifier_oaa_borrowed_time_buff_caster:GetEffectName()
+  return "particles/units/heroes/hero_abaddon/abaddon_borrowed_time.vpcf"
+end
+
+function modifier_oaa_borrowed_time_buff_caster:GetEffectAttachType()
+  return PATTACH_ABSORIGIN_FOLLOW
+end
+
+function modifier_oaa_borrowed_time_buff_caster:GetStatusEffectName()
+  return "particles/status_fx/status_effect_abaddon_borrowed_time.vpcf"
+end
+
+function modifier_oaa_borrowed_time_buff_caster:StatusEffectPriority()
+  return 15
+end
+
+function modifier_oaa_borrowed_time_buff_caster:DeclareFunctions()
+  local funcs = {
+    MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE
+  }
+
+  return funcs
+end
+
+function modifier_oaa_borrowed_time_buff_caster:IsAura()
+  return self:GetParent():HasScepter()
+end
+
+function modifier_oaa_borrowed_time_buff_caster:GetModifierAura()
+  return "modifier_oaa_borrowed_time_buff_ally"
+end
+
+function modifier_oaa_borrowed_time_buff_caster:GetAuraSearchType()
+  return DOTA_UNIT_TARGET_HERO
+end
+
+function modifier_oaa_borrowed_time_buff_caster:GetAuraSearchTeam()
+  return DOTA_UNIT_TARGET_TEAM_FRIENDLY
+end
+
+function modifier_oaa_borrowed_time_buff_caster:GetAuraRadius()
+  return self:GetAbility():GetSpecialValueFor("redirect_range_scepter")
+end
+
+function modifier_oaa_borrowed_time_buff_caster:GetAuraEntityReject(hEntity)
+  -- Do not apply aura to target
+  if hEntity == self:GetParent() or hEntity:HasModifier("modifier_oaa_borrowed_time_buff_caster") then
+    return true
+  end
+
+  return false
+end
+
+function modifier_oaa_borrowed_time_buff_caster:GetModifierIncomingDamage_Percentage(kv)
+  if IsServer() then
+    local parent 	= self:GetParent()
+
+    -- Show borrowed time heal particle
+    local heal_particle = ParticleManager:CreateParticle("particles/units/heroes/hero_abaddon/abaddon_borrowed_time_heal.vpcf", PATTACH_ABSORIGIN_FOLLOW, parent)
+    local target_vector = parent:GetAbsOrigin()
+    ParticleManager:SetParticleControl(heal_particle, 0, target_vector)
+    ParticleManager:SetParticleControl(heal_particle, 1, target_vector)
+    ParticleManager:ReleaseParticleIndex(heal_particle)
+
+    --  Heal instead of damage (damage after reductions, not original damage)
+    parent:Heal(kv.damage, parent)
+
+    return -9999999
+  end
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_oaa_borrowed_time_buff_ally = class(ModifierBaseClass)
+
+function modifier_oaa_borrowed_time_buff_ally:IsHidden() -- needs tooltip
+  return false
+end
+
+function modifier_oaa_borrowed_time_buff_ally:IsDebuff()
+  return false
+end
+
+function modifier_oaa_borrowed_time_buff_ally:IsPurgable()
+  return false
+end
+
+function modifier_oaa_borrowed_time_buff_ally:DeclareFunctions()
+  local funcs = {
+    MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE
+  }
+
+  return funcs
+end
+
+--[[
+function modifier_oaa_borrowed_time_buff_ally:OnCreated()
+  if IsServer() then
+    local caster = self:GetCaster()
+    local parent = self:GetParent()
+
+    local parent_origin = parent:GetAbsOrigin()
+    local particle_name = "particles/econ/courier/courier_hyeonmu_ambient/courier_hyeonmu_ambient_trail_steam.vpcf"
+
+    -- Body steam particle
+    local particle = ParticleManager:CreateParticle(particle_name, PATTACH_ABSORIGIN, parent)
+    ParticleManager:SetParticleControlEnt(particle, 0, parent, PATTACH_POINT_FOLLOW, "attach_hitloc", parent_origin, true)
+    self:AddParticle(particle, false, false, -1, false, false)
+
+    -- Weapon particle
+    particle = ParticleManager:CreateParticle(particle_name, PATTACH_ABSORIGIN, parent)
+    ParticleManager:SetParticleControlEnt(particle, 0, parent, PATTACH_POINT_FOLLOW, "attach_attack1", parent_origin, true)
+    self:AddParticle(particle, false, false, -1, false, false)
+  end
+end
+]]
+
+function modifier_oaa_borrowed_time_buff_ally:GetModifierIncomingDamage_Percentage(kv)
+  if IsServer() then
+    local caster = self:GetCaster()
+    local parent = self:GetParent()
+    local ability = self:GetAbility()
+
+    -- Don't redirect damage with hp removal flag
+    if bit.band(kv.damage_flags, DOTA_DAMAGE_FLAG_HPLOSS) == DOTA_DAMAGE_FLAG_HPLOSS then
+      return 0
+    end
+
+    -- Don't redirect damage if damaged unit is not the parent
+    if kv.unit ~= parent then
+      return 0
+    end
+
+    local redirect_pct = ability:GetSpecialValueFor("damage_redirect_scepter")
+    local redirect_damage =	kv.damage * (redirect_pct/100)
+
+    local damage_table = {}
+    damage_table.attacker = kv.attacker
+    damage_table.damage_type = kv.damage_type or DAMAGE_TYPE_PURE
+    damage_table.ability = ability
+    damage_table.damage = redirect_damage
+    damage_table.victim = caster
+
+    -- Redirect the damage to Abaddon (caster)
+    if caster:HasModifier("modifier_oaa_borrowed_time_buff_caster") and redirect_damage > 0 then
+      ApplyDamage(damage_table)
+    end
+
+    --Block the amount of damage required.
+    return -(redirect_pct)
+	end
+end
