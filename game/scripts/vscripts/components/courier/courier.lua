280c384afaf57973eab259dfdc807d4e269bfdd6
Darko V <darko1290@gmail.com>
2020-03-12 01:52:49 +0100

    Update courier mechanics (#2855)

--- game/scripts/vscripts/components/courier/courier.lua
+++ game/scripts/vscripts/components/courier/courier.lua
@@ -10,7 +10,13 @@ end
 function Courier:Init ()
   Courier.hasCourier = {}
   LinkLuaModifier("modifier_custom_courier_stuff", "components/courier/courier.lua", LUA_MODIFIER_MOTION_NONE)
-  GameEvents:OnHeroInGame(Courier.SpawnCourier)
+  Courier.enableCustomCourier = false -- if you want custom couriers just set this to true
+  if Courier.enableCustomCourier then
+    GameEvents:OnHeroInGame(Courier.SpawnCourier)
+  else
+    GameRules:GetGameModeEntity():SetFreeCourierModeEnabled(true)
+    ListenToGameEvent("npc_spawned", Dynamic_Wrap(Courier, 'OnNpcSpawned'), self)
+  end
 end
 
 function Courier.SpawnCourier(hero)
@@ -28,26 +34,37 @@ function Courier.SpawnCourier(hero)
   Timers:CreateTimer(1, function ()
     DebugPrint("Creating Courier for Hero " .. hero:GetUnitName())
 
-	local playerID = hero:GetPlayerOwnerID()
+    local playerID = hero:GetPlayerOwnerID()
     -- Create a courier
-  local courier_unit = CreateUnitByName("npc_dota_courier", hero:GetAbsOrigin(), true, hero, hero, hero:GetTeamNumber())
-	courier_unit:SetOwner(hero)
-	courier_unit:SetControllableByPlayer(playerID, true)
-	courier_unit:SetMoveCapability(DOTA_UNIT_CAP_MOVE_FLY)
-	--courier_unit:SetBaseMoveSpeed(380)                                       -- doesnt work
-	--courier_unit:SetOriginalModel("models/props_gameplay/donkey_wings.vmdl") -- not needed
-	courier_unit:RemoveAbility("courier_burst")
-	courier_unit:RemoveAbility("courier_shield")
-	courier_unit:RemoveAbility("courier_go_to_enemy_secretshop")
-	courier_unit:RemoveAbility("courier_go_to_sideshop")
-	courier_unit:RemoveAbility("courier_go_to_sideshop2")
-	--courier_unit:RemoveAbility("nothing")   -- it doesnt give an error even if it didnt find an ability with that name
-	courier_unit:AddNewModifier(hero, nil, "modifier_custom_courier_stuff", {})
+    local courier_unit = CreateUnitByName("npc_dota_courier", hero:GetAbsOrigin(), true, hero, hero, hero:GetTeamNumber())
+    courier_unit:SetOwner(hero)
+    courier_unit:SetControllableByPlayer(playerID, true)
+    courier_unit:SetMoveCapability(DOTA_UNIT_CAP_MOVE_FLY)
+    courier_unit:RemoveAbility("courier_burst")
+    courier_unit:RemoveAbility("courier_shield")
+    courier_unit:RemoveAbility("courier_go_to_sideshop")
+    courier_unit:RemoveAbility("courier_go_to_sideshop2")
+    --courier_unit:RemoveAbility("nothing")   -- it doesnt give an error even if it didnt find an ability with that name
+    courier_unit:AddNewModifier(hero, nil, "modifier_custom_courier_stuff", {})
 
-	Courier.hasCourier[hero] = true
-
+    Courier.hasCourier[hero] = true
   end)
 end
+
+function Courier:OnNpcSpawned(keys)
+  local npc
+  if keys.entindex then
+    npc = EntIndexToHScript(keys.entindex)
+  end
+  if not npc then
+    return
+  end
+  if npc:IsCourier() then
+    npc:AddNewModifier(npc, nil, "modifier_custom_courier_stuff", {})
+  end
+end
+
+---------------------------------------------------------------------------------------------------
 
 modifier_custom_courier_stuff = class({})
 
@@ -64,16 +81,23 @@ function modifier_custom_courier_stuff:RemoveOnDeath()
 end
 
 function modifier_custom_courier_stuff:DeclareFunctions()
-  local funcs = {
-    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL,
-    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL,
-    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE,
-    MODIFIER_PROPERTY_EXTRA_HEALTH_BONUS,
-    MODIFIER_PROPERTY_MOVESPEED_LIMIT,
-    MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE,
-    MODIFIER_PROPERTY_VISUAL_Z_DELTA,
-    MODIFIER_EVENT_ON_TAKEDAMAGE
-  }
+  local funcs = {}
+  if Courier.enableCustomCourier then
+    funcs = {
+      MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL,
+      MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PHYSICAL,
+      MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE,
+      MODIFIER_PROPERTY_EXTRA_HEALTH_BONUS,
+      MODIFIER_PROPERTY_MOVESPEED_LIMIT,
+      MODIFIER_PROPERTY_MOVESPEED_ABSOLUTE,
+      MODIFIER_PROPERTY_VISUAL_Z_DELTA,
+      MODIFIER_EVENT_ON_TAKEDAMAGE
+    }
+  else
+    funcs = {
+      MODIFIER_PROPERTY_TOTAL_CONSTANT_BLOCK,
+    }
+  end
   return funcs
 end
 
@@ -95,12 +119,12 @@ end
 
 function modifier_custom_courier_stuff:CheckState()
   local state = {
-    [MODIFIER_STATE_FLYING] = true,
     [MODIFIER_STATE_NO_UNIT_COLLISION] = true,
     [MODIFIER_STATE_BLIND] = true,
-    --[MODIFIER_STATE_INVULNERABLE] = true,
-    --[MODIFIER_STATE_NO_HEALTH_BAR] = true
   }
+  if Courier.enableCustomCourier then
+    state[MODIFIER_STATE_FLYING] = true
+  end
   return state
 end
 
@@ -134,4 +158,12 @@ end
 
 function modifier_custom_courier_stuff:GetVisualZDelta()
   return 220
+end
+
+function modifier_custom_courier_stuff:GetModifierTotal_ConstantBlock(keys)
+  -- Block damage only from neutrals
+  if keys.attacker:GetTeamNumber() == DOTA_TEAM_NEUTRALS then
+    return keys.damage
+  end
+  return 0
 end
