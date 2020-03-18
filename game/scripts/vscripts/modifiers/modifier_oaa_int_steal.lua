900522ee8611dc84e46b7b3ea050174cd4f596e3
Darko V <darko1290@gmail.com>
2020-03-18 22:07:36 +0100

    Silencer
    * Permanent Intelligence steal is still innate.
    * Improved tooltips for Glaives of Wisdom and Permanent Intelligence steal.
    * Fixed being able to steal INT from Tempest Double.

--- game/scripts/vscripts/modifiers/modifier_oaa_int_steal.lua
+++ game/scripts/vscripts/modifiers/modifier_oaa_int_steal.lua
@@ -19,25 +19,26 @@ function modifier_oaa_int_steal:OnDeath(keys)
   local ability = self:GetAbility()
   local stealRange = ability:GetLevelSpecialValueFor("steal_range", math.max(1, ability:GetLevel()))
   local stealAmount = ability:GetLevelSpecialValueFor("steal_amount", math.max(1, ability:GetLevel()))
+  local unit = keys.unit
   local filterResult = UnitFilter(
-    keys.unit,
+    unit,
     DOTA_UNIT_TARGET_TEAM_ENEMY,
     DOTA_UNIT_TARGET_HERO,
     bit.bor(DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, DOTA_UNIT_TARGET_FLAG_INVULNERABLE, DOTA_UNIT_TARGET_FLAG_DEAD),
     parent:GetTeamNumber()
   )
-  local isWithinRange = #(keys.unit:GetAbsOrigin() - parent:GetAbsOrigin()) <= stealRange
+  local isWithinRange = #(unit:GetAbsOrigin() - parent:GetAbsOrigin()) <= stealRange
 
   -- Check for +2 Int Steal Talent
   if parent:HasLearnedAbility("special_bonus_unique_silencer_2") then
     stealAmount = stealAmount + parent:FindAbilityByName("special_bonus_unique_silencer_2"):GetSpecialValueFor("value")
   end
 
-  if filterResult == UF_SUCCESS and (keys.attacker == parent or isWithinRange) and parent:IsRealHero() and parent:IsAlive() and keys.unit:IsRealHero() and not keys.unit:IsClone() then
-    local oldIntellect = keys.unit:GetBaseIntellect()
-    keys.unit:SetBaseIntellect(math.max(1, oldIntellect - stealAmount))
-    keys.unit:CalculateStatBonus()
-    local intellectDifference = oldIntellect - keys.unit:GetBaseIntellect()
+  if filterResult == UF_SUCCESS and (keys.attacker == parent or isWithinRange) and parent:IsRealHero() and parent:IsAlive() and unit:IsRealHero() and not unit:IsClone() and not unit:IsTempestDouble() then
+    local oldIntellect = unit:GetBaseIntellect()
+    unit:SetBaseIntellect(math.max(1, oldIntellect - stealAmount))
+    unit:CalculateStatBonus()
+    local intellectDifference = oldIntellect - unit:GetBaseIntellect()
     parent:ModifyIntellect(intellectDifference)
     self:SetStackCount(self:GetStackCount() + intellectDifference)
 
@@ -47,7 +48,7 @@ function modifier_oaa_int_steal:OnDeath(keys)
     ParticleManager:ReleaseParticleIndex(plusIntParticle)
 
     local minusIntParticleName = "particles/units/heroes/hero_silencer/silencer_last_word_victim_count.vpcf"
-    local minusIntParticle = ParticleManager:CreateParticle(minusIntParticleName, PATTACH_OVERHEAD_FOLLOW, keys.unit)
+    local minusIntParticle = ParticleManager:CreateParticle(minusIntParticleName, PATTACH_OVERHEAD_FOLLOW, unit)
     ParticleManager:SetParticleControl(minusIntParticle, 1, Vector(10 + intellectDifference, 0, 0))
     ParticleManager:ReleaseParticleIndex(minusIntParticle)
   end
