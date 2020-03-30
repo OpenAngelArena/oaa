3bf5ebc54632e351ee6365f164ede6b2458ecc96
Darko V <darko1290@gmail.com>
2020-03-30 02:36:05 +0200

    Update ai_simple.lua

--- game/scripts/vscripts/units/ai_simple.lua
+++ game/scripts/vscripts/units/ai_simple.lua
@@ -39,17 +39,7 @@ function SimpleBossThink()
       --thisEntity:SetAcquisitionRange(128) -- It seems this is not helping
       local nearby_enemies = FindUnitsInRadius(thisEntity:GetTeamNumber(), thisEntity.spawn_position, nil, SIMPLE_BOSS_LEASH_SIZE, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, bit.bor(DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE), FIND_CLOSEST, false)
       if #nearby_enemies == 0 then
-        nearby_enemies = FindUnitsInRadius(thisEntity:GetTeamNumber(), thisEntity.spawn_position, nil, 3*SIMPLE_BOSS_LEASH_SIZE, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, bit.bor(DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE), FIND_CLOSEST, false)
-      end
-      -- Filter out couriers and invisible units
-      if #nearby_enemies ~= 0 then
-        for i=1,#nearby_enemies do
-          if nearby_enemies[i] then
-            if nearby_enemies[i]:IsCourier() or nearby_enemies[i]:IsInvisible() then
-              table.remove(nearby_enemies,i)
-            end
-          end
-        end
+        nearby_enemies = FindUnitsInRadius(thisEntity:GetTeamNumber(), thisEntity.spawn_position, nil, 3*SIMPLE_BOSS_LEASH_SIZE, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_CLOSEST, false)
       end
       -- Find nearest enemy that a boss can attack and assign it as aggro_target
       local nearest_enemy
@@ -64,7 +54,7 @@ function SimpleBossThink()
           Queue = 0,
         })
       end
-      thisEntity.aggro_target = nearest_enemy
+      --thisEntity.aggro_target = nearest_enemy
       thisEntity.state = SIMPLE_AI_STATE_AGGRO
     else
       -- Check if the boss was messed around with displacing abilities (Force Staff for example)
@@ -78,23 +68,35 @@ function SimpleBossThink()
     -- Check how far did the boss go from the spawn position
     if (thisEntity:GetAbsOrigin() - thisEntity.spawn_position):Length2D() > SIMPLE_BOSS_LEASH_SIZE then
       -- Remove aggro_target if boss goes outside of leash range
-      if thisEntity.aggro_target then
-        thisEntity.aggro_target = nil
+      --if thisEntity.aggro_target then
+        --thisEntity.aggro_target = nil
+      --end
+      thisEntity.aggro_target = thisEntity:GetAggroTarget()
+      print(thisEntity:GetAggroTarget())
+      if thisEntity.aggro_target and not thisEntity.aggro_target:IsNull() then
+        print(thisEntity.aggro_target:GetUnitName())
+        if (thisEntity.aggro_target:GetAbsOrigin() - thisEntity.spawn_position):Length2D() > SIMPLE_BOSS_LEASH_SIZE then
+          if thisEntity.aggro_target:GetAttackRange() <= SIMPLE_BOSS_LEASH_SIZE then
+            thisEntity.aggro_target = nil
+          end
+        end
       end
       -- Find units that attacked the boss outside of leash range (Sniper and other high attack range units)
-      local enemies = FindUnitsInRadius(thisEntity:GetTeamNumber(), thisEntity.spawn_position, nil, 3*SIMPLE_BOSS_LEASH_SIZE, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, bit.bor(DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, DOTA_UNIT_TARGET_FLAG_FOW_VISIBLE), FIND_CLOSEST, false)
-      if #enemies ~=0 then
-        for i=1,#enemies do
+      --[[
+      local enemies = FindUnitsInRadius(thisEntity:GetTeamNumber(), thisEntity.spawn_position, nil, 3*SIMPLE_BOSS_LEASH_SIZE, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_ALL, DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, FIND_CLOSEST, false)
+      if #enemies ~= 0 then
+        for i = 1, #enemies do
           local enemy = enemies[i]
-          if enemy:IsAttackingEntity(thisEntity) and enemy:GetAttackRange() > SIMPLE_BOSS_LEASH_SIZE then
+          if enemy:IsAttackingEntity(thisEntity) and enemy:GetAttackRange() >= SIMPLE_BOSS_LEASH_SIZE then
             thisEntity.aggro_target = enemy
             break
           end
         end
       else
         -- No enemies so no need for aggro_target
-        thisEntity.aggro_target = nil
+        --thisEntity.aggro_target = nil
       end
+      ]]
     end
 
     -- Check if aggro_target exists
@@ -105,12 +107,12 @@ function SimpleBossThink()
         return 1
       end
       -- Check if aggro_target is dead or attack immune (ethereal) or a courier or invisible
-      if thisEntity.aggro_target:IsAlive() and not thisEntity.aggro_target:IsAttackImmune() and not thisEntity.aggro_target:IsCourier() and not thisEntity.aggro_target:IsInvisible() then
-        thisEntity:MoveToTargetToAttack(thisEntity.aggro_target)
-      else
+      --if thisEntity.aggro_target:IsAlive() and not thisEntity.aggro_target:IsAttackImmune() and not thisEntity.aggro_target:IsCourier() and not thisEntity.aggro_target:IsInvisible() then
+        --thisEntity:MoveToTargetToAttack(thisEntity.aggro_target)
+      --else
         -- Don't try to attack dead units, attack-immune units, invisible units or couriers
-        thisEntity.aggro_target = nil
-      end
+        --thisEntity.aggro_target = nil
+      --end
     else
       -- Boss goes back angry (attack-moving to the spawn position) because he can't attack
       ExecuteOrderFromTable({
