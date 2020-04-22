3d53b159dd742156e9bf332b18b007e8aa03de38
Darko V <darko1290@gmail.com>
2020-04-23 01:03:28 +0200

    Update armor_resist_value.js

--- content/panorama/scripts/custom_game/armor_resist_value.js
+++ content/panorama/scripts/custom_game/armor_resist_value.js
@@ -18,11 +18,12 @@ function UpdateTooltipPhysicalResistanceValue () {
   var unit = Players.GetLocalPlayerPortraitUnit();
   if (physicalResistLabel != null) {
     var unitArmor = Entities.GetPhysicalArmorValue(unit);
-	if (HasModifier(unit, 'modifier_legacy_armor')) {
-      var physicalResistance = Math.round(0.05 * unitArmor / (1 + 0.05 * Math.abs(unitArmor)) * 100);
+    var physicalResistance;
+    if (HasModifier(unit, 'modifier_legacy_armor')) {
+      physicalResistance = Math.round(0.05 * unitArmor / (1 + 0.05 * Math.abs(unitArmor)) * 100);
       physicalResistLabel.text = physicalResistance + '%';
     } else {
-      var physicalResistance = Math.round(0.052 * unitArmor / (0.9 + 0.048 * Math.abs(unitArmor)) * 100);
+      physicalResistance = Math.round(0.052 * unitArmor / (0.9 + 0.048 * Math.abs(unitArmor)) * 100);
       physicalResistLabel.text = physicalResistance + '%';
     }
   }
@@ -32,11 +33,12 @@ function UpdateAltDisplayPhysicalResistanceValue () {
   var unit = Players.GetLocalPlayerPortraitUnit();
   if (physicalResistLabel != null) {
     var unitArmor = Entities.GetPhysicalArmorValue(unit);
-	if (HasModifier(unit, 'modifier_legacy_armor')) {
-      var physicalResistance = Math.round(0.05 * unitArmor / (1 + 0.05 * Math.abs(unitArmor)) * 100);
+    var physicalResistance;
+    if (HasModifier(unit, 'modifier_legacy_armor')) {
+      physicalResistance = Math.round(0.05 * unitArmor / (1 + 0.05 * Math.abs(unitArmor)) * 100);
       physicalResistLabel.text = physicalResistance + '%';
     } else {
-      var physicalResistance = Math.round(0.052 * unitArmor / (0.9 + 0.048 * Math.abs(unitArmor)) * 100);
+      physicalResistance = Math.round(0.052 * unitArmor / (0.9 + 0.048 * Math.abs(unitArmor)) * 100);
       physicalResistLabel.text = physicalResistance + '%';
     }
   }
