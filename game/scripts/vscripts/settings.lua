71a62de63bd236929bbafe6fafc652899f76c6dc
Darko V <darko1290@gmail.com>
2020-06-29 20:33:18 +0200

    CREEP_BOUNTY_SHARE_PERCENT
    the percentage of the creep's bounty that's given to shared allies increased from 35% to 40%.

--- game/scripts/vscripts/settings.lua
+++ game/scripts/vscripts/settings.lua
@@ -104,7 +104,7 @@ INITIAL_CREEP_DELAY = 1                 -- number of seconds to wait before spaw
 BOTTLE_DESPAWN_TIME = 60                -- Time until Bottles despawn
 CREEP_POWER_MAX = 1.5                   -- the total max power creeps will get stacked up to (1 = 100%)
 CREEP_BOUNTY_SHARE_RADIUS = 1500        -- the radius in which creep bounty is shared with allies
-CREEP_BOUNTY_SHARE_PERCENT = 35         -- the percentage of the creep's bounty that's given to shared allies
+CREEP_BOUNTY_SHARE_PERCENT = 40         -- the percentage of the creep's bounty that's given to shared allies
 CREEP_BOUNTY_BONUS_PERCENT_CLEAVE = 15  -- the bonus percentage of the creep's bounty that's given to those that kill with Cleave Spark
 CREEP_BOUNTY_BONUS_PERCENT_POWER = 30   -- the bonus percentage of the creep's bounty that's given to those that kill with Power Spark
 
