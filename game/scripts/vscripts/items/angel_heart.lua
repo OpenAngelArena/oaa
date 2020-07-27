2e15694f2fe2afb988b60fd4e19d96e70389c169
Darko V <darko1290@gmail.com>
2020-07-28 01:47:52 +0200

    New Neutral Item + Removing Book of the Dead
    * New neutral tier 1 item: Angel Heart
    Passive non-consumable. Provides all stats, hp regen and mana cost/mana loss reduction. Has the icon of Ocean Heart.

---
+++ game/scripts/vscripts/items/angel_heart.lua
@@ -0,0 +1,69 @@
+LinkLuaModifier("modifier_item_angel_heart_passive", "items/angel_heart.lua", LUA_MODIFIER_MOTION_NONE)
+
+item_angel_heart = class(ItemBaseClass)
+
+function item_angel_heart:GetIntrinsicModifierName()
+  return "modifier_item_angel_heart_passive"
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_item_angel_heart_passive = class(ModifierBaseClass)
+
+function modifier_item_angel_heart_passive:IsHidden()
+  return true
+end
+function modifier_item_angel_heart_passive:IsDebuff()
+  return false
+end
+function modifier_item_angel_heart_passive:IsPurgable()
+  return false
+end
+
+function modifier_item_angel_heart_passive:OnCreated()
+  local ability = self:GetAbility()
+  if ability and not ability:IsNull() then
+    self.stats = ability:GetSpecialValueFor("bonus_all_stats")
+    self.regen = ability:GetSpecialValueFor("bonus_hp_regen")
+    self.mana_cost_reduction = ability:GetSpecialValueFor("mana_cost_reduction_pct")
+  end
+end
+
+function modifier_item_angel_heart_passive:OnRefresh()
+  local ability = self:GetAbility()
+  if ability and not ability:IsNull() then
+    self.stats = ability:GetSpecialValueFor("bonus_all_stats")
+    self.regen = ability:GetSpecialValueFor("bonus_hp_regen")
+    self.mana_cost_reduction = ability:GetSpecialValueFor("mana_cost_reduction_pct")
+  end
+end
+
+function modifier_item_angel_heart_passive:DeclareFunctions()
+  return {
+    MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
+    MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
+    MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
+    MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT,
+    MODIFIER_PROPERTY_MANACOST_PERCENTAGE_STACKING,
+  }
+end
+
+function modifier_item_angel_heart_passive:GetModifierBonusStats_Strength()
+  return self.stats or self:GetAbility():GetSpecialValueFor("bonus_all_stats")
+end
+
+function modifier_item_angel_heart_passive:GetModifierBonusStats_Agility()
+  return self.stats or self:GetAbility():GetSpecialValueFor("bonus_all_stats")
+end
+
+function modifier_item_angel_heart_passive:GetModifierBonusStats_Intellect()
+  return self.stats or self:GetAbility():GetSpecialValueFor("bonus_all_stats")
+end
+
+function modifier_item_angel_heart_passive:GetModifierConstantHealthRegen()
+  return self.regen or self:GetAbility():GetSpecialValueFor("bonus_hp_regen")
+end
+
+function modifier_item_angel_heart_passive:GetModifierPercentageManacostStacking()
+  return self.mana_cost_reduction or self:GetAbility():GetSpecialValueFor("mana_cost_reduction_pct")
+end
