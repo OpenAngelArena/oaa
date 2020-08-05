be21e079bf542896acd184d4dbe900ff78757fd9
Darko V <darko1290@gmail.com>
2020-08-05 18:16:26 +0200

    Underlord ult rework
    * Highway to Hell replaces with Dark Rift: CHANNELED - After channeling for 2 seconds, Underlord teleports to the target location, stunning and damaging enemies in a 400 radius. Cast Range: 1200
    * Cancel/Close Highway replaced with Dark Abduction: Teleports Underlord and stunned enemies back to the inital Dark Lift cast location.

--- game/scripts/vscripts/abilities/oaa_dark_rift.lua
+++ game/scripts/vscripts/abilities/oaa_dark_rift.lua
@@ -1,313 +1,177 @@
-abyssal_underlord_dark_rift_oaa = class( AbilityBaseClass )
+abyssal_underlord_dark_rift_oaa = class(AbilityBaseClass)
 
-LinkLuaModifier( "modifier_abyssal_underlord_dark_rift_oaa_timer", "abilities/oaa_dark_rift.lua", LUA_MODIFIER_MOTION_NONE )
-LinkLuaModifier( "modifier_abyssal_underlord_dark_rift_oaa_portal", "abilities/oaa_dark_rift.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_underlord_dark_rift_oaa_stun", "abilities/oaa_dark_rift.lua", LUA_MODIFIER_MOTION_NONE)
 
 --------------------------------------------------------------------------------
 
 function abyssal_underlord_dark_rift_oaa:GetAOERadius()
-	return self:GetSpecialValueFor( "radius" )
+  return self:GetSpecialValueFor("radius")
 end
-
---------------------------------------------------------------------------------
 
 function abyssal_underlord_dark_rift_oaa:GetCastAnimation()
-	return ACT_DOTA_CAST_ABILITY_4
+ return ACT_DOTA_CAST_ABILITY_4
 end
 
---------------------------------------------------------------------------------
-
-function abyssal_underlord_dark_rift_oaa:GetAssociatedPrimaryAbilities()
+function abyssal_underlord_dark_rift_oaa:GetAssociatedSecondaryAbilities()
 	return "abyssal_underlord_cancel_dark_rift_oaa"
 end
 
---------------------------------------------------------------------------------
-
 function abyssal_underlord_dark_rift_oaa:OnUpgrade()
-	local caster = self:GetCaster()
+  local abilityLevel = self:GetLevel()
+  local sub_ability = self:GetCaster():FindAbilityByName("abyssal_underlord_cancel_dark_rift_oaa")
 
-	local spell = caster:FindAbilityByName( self:GetAssociatedPrimaryAbilities() )
-
-	if spell then
-		-- if the spell hasn't be upgraded yet
-		-- init the disabled state
-		if spell:GetLevel() then
-			spell:SetActivated( false )
-		end
-
-		-- upgrade the subspell
-		spell:SetLevel( self:GetLevel() )
+	-- Check to not enter a level up loop
+  if sub_ability and sub_ability:GetLevel() ~= abilityLevel then
+		sub_ability:SetLevel(abilityLevel)
 	end
 end
-
---------------------------------------------------------------------------------
 
 function abyssal_underlord_dark_rift_oaa:OnSpellStart()
-	local caster = self:GetCaster()
-	local originCaster = caster:GetAbsOrigin()
-	local duration = self:GetSpecialValueFor( "portal_duration" )
-	local pos = self:GetCursorPosition()
-	local team = caster:GetTeamNumber()
-	local minRange = self:GetSpecialValueFor( "minimum_range" )
-	local vectorTarget = pos - originCaster
+  local caster = self:GetCaster()
+  local originCaster = caster:GetAbsOrigin()
+  local pos = self:GetCursorPosition()
+  local minRange = self:GetSpecialValueFor("minimum_range")
+  local vectorTarget = pos - originCaster
 
-	-- if the target point is too close, push it out to minimum range
-	if vectorTarget:Length2D() < minRange then
-		pos = originCaster + ( vectorTarget:Normalized() * minRange )
-	end
+  -- if the target point is too close, push it out to minimum range
+  if vectorTarget:Length2D() < minRange then
+    pos = originCaster + ( vectorTarget:Normalized() * minRange )
+  end
 
-	local thinker1 = CreateModifierThinker( caster, self, "modifier_abyssal_underlord_dark_rift_oaa_portal", {
-		targetX = pos.x,
-		targetY = pos.y,
-	},
-	originCaster, team, false )
+  local radius = self:GetSpecialValueFor( "radius" )
+  self.originSecond = GetGroundPosition( Vector(pos.x, pos.y, 0), caster)
 
-	-- for the sake of sanity in regards to the cancel subspell
-	-- as well as a neat detail where you can keep track of the portals' duration
-	-- we'll add a timer modifier that actually handles the portals' duration
-	-- instead of, y'know, making the portals handle it themselves
-	local mod = caster:AddNewModifier( caster, self, "modifier_abyssal_underlord_dark_rift_oaa_timer", {
-		duration = duration,
-	} )
+  -- create portal particle on caster location
+  local partPortal1 = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abyssal_underlord_dark_rift_portal.vpcf", PATTACH_WORLDORIGIN, caster )
+  ParticleManager:SetParticleControl( partPortal1, 0, originCaster )
+  ParticleManager:SetParticleControl( partPortal1, 2, originCaster )
+  ParticleManager:SetParticleControl( partPortal1, 1, Vector( radius, 1, 1 ) )
 
-	-- link the modifiers together
-	-- CreateModifierThinker returns the thinker unit, not the modifier
-	mod.modPortal = thinker1:FindModifierByName( "modifier_abyssal_underlord_dark_rift_oaa_portal" )
+  -- create portal particle on destination
+  local partPortal2 = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abyssal_underlord_dark_rift_portal.vpcf", PATTACH_WORLDORIGIN, caster )
+  ParticleManager:SetParticleControl( partPortal2, 0, self.originSecond )
+  ParticleManager:SetParticleControl( partPortal2, 2, self.originSecond )
+  ParticleManager:SetParticleControl( partPortal2, 1, Vector( radius, 1, 1 ) )
+
+  -- play cast sounds
+  caster:EmitSound( "Hero_AbyssalUnderlord.DarkRift.Cast" )
+  EmitSoundOnLocationWithCaster( self.originSecond, "Hero_AbyssalUnderlord.DarkRift.Cast", caster )
+
+  -- Store the location of the caster on the caster itself for the sub ability
+  caster.original_cast_location = originCaster
+
+  -- Store particles on ability and caster itself
+  self.partPortal1 = partPortal1
+  self.partPortal2 = partPortal2
+  caster.partPortal1 = partPortal1
+  caster.partPortal2 = partPortal2
 end
 
---------------------------------------------------------------------------------
+function abyssal_underlord_dark_rift_oaa:OnChannelFinish(bInterrupted)
+  local caster = self:GetCaster()
 
-modifier_abyssal_underlord_dark_rift_oaa_timer = class( ModifierBaseClass )
+  -- Stop and remove stuff if interrupted and don't continue
+  if bInterrupted then
+    -- Stop sound
+    caster:StopSound( "Hero_AbyssalUnderlord.DarkRift.Cast" )
 
---------------------------------------------------------------------------------
+    -- Remove particles
+    if self.partPortal1 then
+      ParticleManager:DestroyParticle( self.partPortal1, false )
+		  ParticleManager:ReleaseParticleIndex( self.partPortal1 )
+      caster.partPortal1 = nil
+    end
+    if self.partPortal2 then
+      ParticleManager:DestroyParticle( self.partPortal2, false )
+      ParticleManager:ReleaseParticleIndex( self.partPortal2 )
+      caster.partPortal2 = nil
+    end
 
-function modifier_abyssal_underlord_dark_rift_oaa_timer:IsHidden()
+    caster.original_cast_location = nil
+
+    return
+  end
+
+  local originParent = caster:GetAbsOrigin()
+  local radius = self:GetSpecialValueFor("radius")
+
+  -- destroy all trees in portals
+  GridNav:DestroyTreesAroundPoint( originParent, radius, true )
+  GridNav:DestroyTreesAroundPoint( self.originSecond, radius, true )
+
+  -- play teleportation sounds
+  caster:EmitSound("Hero_AbyssalUnderlord.DarkRift.Aftershock")
+  EmitSoundOnLocationWithCaster( self.originSecond, "Hero_AbyssalUnderlord.DarkRift.Aftershock", caster )
+
+  -- emit warp particles
+  local part = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abbysal_underlord_darkrift_warp.vpcf", PATTACH_WORLDORIGIN, caster )
+  ParticleManager:SetParticleControl( part, 1, Vector( radius, 1, 1 ) )
+  ParticleManager:SetParticleControl( part, 2, originParent )
+
+  local part2 = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abbysal_underlord_darkrift_warp.vpcf", PATTACH_WORLDORIGIN, caster )
+  ParticleManager:SetParticleControl( part2, 1, Vector( radius, 1, 1 ) )
+  ParticleManager:SetParticleControl( part2, 2, self.originSecond )
+
+  local targetTeam = self:GetAbilityTargetTeam()
+  local targetType = self:GetAbilityTargetType()
+  local targetFlags = self:GetAbilityTargetFlags()
+
+  local units_in_portal = FindUnitsInRadius(caster:GetTeamNumber(), self.originSecond, nil, radius, targetTeam, targetType, targetFlags, FIND_ANY_ORDER, false)
+
+  -- Original stun duration
+  local duration = self:GetSpecialValueFor("stun_duration")
+
+  -- Teleport the caster
+  caster:SetAbsOrigin(self.originSecond)
+  FindClearSpaceForUnit(caster, self.originSecond, true)
+
+  -- Disjoint disjointable/dodgeable projectiles
+	ProjectileManager:ProjectileDodge(caster)
+
+  -- Teleportation particle
+  local part3 = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abbysal_underlord_darkrift_ambient_end.vpcf", PATTACH_ABSORIGIN_FOLLOW, caster )
+	ParticleManager:SetParticleControl( part3, 2, originParent )
+	ParticleManager:SetParticleControl( part3, 5, originParent )
+
+  local damageTable = {}
+  damageTable.attacker = caster
+  damageTable.damage = self:GetSpecialValueFor("damage")
+  damageTable.damage_type = self:GetAbilityDamageType()
+  damageTable.ability = self
+
+  -- Find all enemies and apply effects of the spell: stun them and damage them
+  for _, unit in pairs(units_in_portal) do
+    -- Status Resistance Fix
+    duration = unit:GetValueChangedByStatusResistance(duration)
+    unit:AddNewModifier(caster, self, "modifier_underlord_dark_rift_oaa_stun", {duration = duration} )
+    -- Apply damage
+    damageTable.victim = unit
+    ApplyDamage(damageTable)
+  end
+
+  -- Particles releasing
+  ParticleManager:ReleaseParticleIndex(part)
+  ParticleManager:ReleaseParticleIndex(part2)
+  ParticleManager:ReleaseParticleIndex(part3)
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_underlord_dark_rift_oaa_stun = class(ModifierBaseClass)
+
+
+function modifier_underlord_dark_rift_oaa_stun:IsHidden()
 	return false
 end
 
-function modifier_abyssal_underlord_dark_rift_oaa_timer:IsDebuff()
-	return false
-end
-
-function modifier_abyssal_underlord_dark_rift_oaa_timer:IsPurgable()
-	return false
-end
-
-function modifier_abyssal_underlord_dark_rift_oaa_timer:RemoveOnDeath()
-	return false
-end
-
-function modifier_abyssal_underlord_dark_rift_oaa_timer:GetAttributes()
-	return MODIFIER_ATTRIBUTE_MULTIPLE
-end
-
---------------------------------------------------------------------------------
-
-if IsServer() then
-	function modifier_abyssal_underlord_dark_rift_oaa_timer:OnCreated( event )
-		local caster = self:GetCaster()
-		local spell = self:GetAbility()
-
-		local spell2 = caster:FindAbilityByName( spell:GetAssociatedPrimaryAbilities() )
-
-		-- activate the sub spell
-		if spell2 then
-			spell2:SetActivated( true )
-		end
-	end
-
---------------------------------------------------------------------------------
-
-	function modifier_abyssal_underlord_dark_rift_oaa_timer:OnDestroy()
-		local caster = self:GetCaster()
-
-		-- if the linked portals exists, destroy them
-		if self.modPortal and not self.modPortal:IsNull() then
-			self.modPortal:Destroy()
-		end
-
-		local spell = self:GetAbility()
-		local spell2 = caster:FindAbilityByName( spell:GetAssociatedPrimaryAbilities() )
-
-		if spell2 then
-			-- don't deactivate subspell if caster has a second
-			-- instance of this modifier
-			if not caster:HasModifier( self:GetName() ) then
-				spell2:SetActivated( false )
-			end
-		end
-	end
-end
-
---------------------------------------------------------------------------------
-
-modifier_abyssal_underlord_dark_rift_oaa_portal = class( ModifierBaseClass )
-
---------------------------------------------------------------------------------
-
-function modifier_abyssal_underlord_dark_rift_oaa_portal:IsHidden()
+function modifier_underlord_dark_rift_oaa_stun:IsDebuff()
 	return true
 end
 
-function modifier_abyssal_underlord_dark_rift_oaa_portal:IsDebuff()
-	return false
+function modifier_underlord_dark_rift_oaa_stun:IsPurgable()
+	return true
 end
 
-function modifier_abyssal_underlord_dark_rift_oaa_portal:IsPurgable()
-	return false
-end
-
---------------------------------------------------------------------------------
-
-if IsServer() then
-	function modifier_abyssal_underlord_dark_rift_oaa_portal:OnCreated( event )
-		local parent = self:GetParent()
-		local caster = self:GetCaster()
-		local spell = self:GetAbility()
-
-		self.radius = spell:GetSpecialValueFor( "radius" )
-		self.originSecond = GetGroundPosition( Vector( event.targetX, event.targetY, 0 ), caster )
-		local originParent = parent:GetAbsOrigin()
-
-		-- create portal particles
-		self.partPortal1 = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abyssal_underlord_dark_rift_portal.vpcf", PATTACH_WORLDORIGIN, parent )
-		ParticleManager:SetParticleControl( self.partPortal1, 0, originParent )
-		ParticleManager:SetParticleControl( self.partPortal1, 2, originParent )
-		ParticleManager:SetParticleControl( self.partPortal1, 1, Vector( self.radius, 1, 1 ) )
-
-		self.partPortal2 = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abyssal_underlord_dark_rift_portal.vpcf", PATTACH_WORLDORIGIN, parent )
-		ParticleManager:SetParticleControl( self.partPortal2, 0, self.originSecond )
-		ParticleManager:SetParticleControl( self.partPortal2, 2, self.originSecond )
-		ParticleManager:SetParticleControl( self.partPortal2, 1, Vector( self.radius, 1, 1 ) )
-
-		-- play cast sounds
-		parent:EmitSound( "Hero_AbyssalUnderlord.DarkRift.Cast" )
-		EmitSoundOnLocationWithCaster( self.originSecond, "Hero_AbyssalUnderlord.DarkRift.Cast", parent )
-
-		-- destroy trees
-		GridNav:DestroyTreesAroundPoint( originParent, self.radius, true )
-		GridNav:DestroyTreesAroundPoint( self.originSecond, self.radius, true )
-
-		self:StartIntervalThink( spell:GetSpecialValueFor( "teleport_delay" ) )
-	end
-
---------------------------------------------------------------------------------
-
-	function modifier_abyssal_underlord_dark_rift_oaa_portal:OnIntervalThink()
-		local parent = self:GetParent()
-		local spell = self:GetAbility()
-		local originParent = parent:GetAbsOrigin()
-		local team = parent:GetTeamNumber()
-
-		-- destroy all trees in portals
-		GridNav:DestroyTreesAroundPoint( originParent, self.radius, true )
-		GridNav:DestroyTreesAroundPoint( self.originSecond, self.radius, true )
-
-		-- play teleporation sounds
-		parent:EmitSound( "Hero_AbyssalUnderlord.DarkRift.Aftershock" )
-		EmitSoundOnLocationWithCaster( self.originSecond, "Hero_AbyssalUnderlord.DarkRift.Aftershock", parent )
-
-		-- emit warp particles
-		local part = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abbysal_underlord_darkrift_warp.vpcf", PATTACH_WORLDORIGIN, parent )
-		ParticleManager:SetParticleControl( part, 1, Vector( self.radius, 1, 1 ) )
-		ParticleManager:SetParticleControl( part, 2, originParent )
-
-		part = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abbysal_underlord_darkrift_warp.vpcf", PATTACH_WORLDORIGIN, parent )
-		ParticleManager:SetParticleControl( part, 1, Vector( self.radius, 1, 1 ) )
-		ParticleManager:SetParticleControl( part, 2, self.originSecond )
-
-		local targetTeam = spell:GetAbilityTargetTeam()
-		local targetType = spell:GetAbilityTargetType()
-		local targetFlags = bit.bor( DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, DOTA_UNIT_TARGET_FLAG_PLAYER_CONTROLLED )--spell:GetAbilityTargetFlags()
-
-		-- find the units in both portals
-		local unitsPortal1 = FindUnitsInRadius(
-			team,
-			originParent,
-			nil,
-			self.radius,
-			targetTeam,
-			targetType,
-			targetFlags,
-			FIND_ANY_ORDER,
-			false
-		)
-
-		local unitsPortal2 = FindUnitsInRadius(
-			team,
-			self.originSecond,
-			nil,
-			self.radius,
-			targetTeam,
-			targetType,
-			targetFlags,
-			FIND_ANY_ORDER,
-			false
-		)
-
-		local unitsAll = {}
-
-		local function FindInTable( t, target )
-			for k, v in pairs( t ) do
-				if v == target then
-					return k
-				end
-			end
-
-			return nil
-		end
-
-		local function CheckRiftTeleport( unit )
-			return not FindInTable( unitsAll, unit ) and not unit:IsRooted() and ( not unit:IsOpposingTeam( parent:GetTeamNumber() ) or not unit:HasModifier( "modifier_fountain_aura_buff" ) )
-		end
-
-		-- if the unit hasn't been telported by a previous portal, retain its old position and put it at its new one
-		for _, unit in pairs( unitsPortal1 ) do
-			if CheckRiftTeleport( unit ) then
-				unit.tempOriginOld = unit:GetAbsOrigin()
-				local vectorOffset = unit:GetAbsOrigin() - originParent
-				unit:SetAbsOrigin( self.originSecond + vectorOffset )
-
-				table.insert( unitsAll, unit )
-			end
-		end
-
-		for _, unit in pairs( unitsPortal2 ) do
-			if CheckRiftTeleport( unit ) then
-				unit.tempOriginOld = unit:GetAbsOrigin()
-				local vectorOffset = unit:GetAbsOrigin() - self.originSecond
-				unit:SetAbsOrigin( originParent + vectorOffset )
-
-				table.insert( unitsAll, unit )
-			end
-		end
-
-		-- final touches, such as making sure units aren't in unpathable terrain
-
-		for _, unit in pairs( unitsAll ) do
-			FindClearSpaceForUnit( unit, unit:GetAbsOrigin(), true )
-			local originUnit = unit:GetAbsOrigin()
-
-			part = ParticleManager:CreateParticle( "particles/units/heroes/heroes_underlord/abbysal_underlord_darkrift_ambient_end.vpcf", PATTACH_ABSORIGIN_FOLLOW, unit )
-			ParticleManager:SetParticleControl( part, 2, unit.tempOriginOld )
-			ParticleManager:SetParticleControl( part, 5, unit.tempOriginOld )
-
-			unit.tempOriginOld = nil
-
-			-- interrupt the unit, so that channeled abilities won't keep channeling
-			unit:Interrupt()
-		end
-	end
-
---------------------------------------------------------------------------------
-
-	function modifier_abyssal_underlord_dark_rift_oaa_portal:OnDestroy()
-		ParticleManager:DestroyParticle( self.partPortal1, false )
-		ParticleManager:ReleaseParticleIndex( self.partPortal1 )
-
-		ParticleManager:DestroyParticle( self.partPortal2, false )
-		ParticleManager:ReleaseParticleIndex( self.partPortal2 )
-
-		UTIL_Remove( self:GetParent() )
-	end
+function modifier_underlord_dark_rift_oaa_stun:IsStunDebuff()
+  return true
 end
