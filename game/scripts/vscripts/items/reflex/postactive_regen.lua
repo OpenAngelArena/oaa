21c95a70f55400b9ccedd17d17875cc4e1b97bb7
Darko V <darko1290@gmail.com>
2020-09-25 22:00:24 +0200

    Regen Crystal rework
    * Bonus HP increased from 1200/2300/3400 to 1600/2500/3400.
    * Bonus STR reduced from 60/70/80 to 40/60/80.
    * Bonus HP regen increased from 30/40/50 to 300/400/500.
    * Regen Crystal active now gives 75% hp regen amp instead of flat 500/650/800 hp regen.

--- game/scripts/vscripts/items/reflex/postactive_regen.lua
+++ game/scripts/vscripts/items/reflex/postactive_regen.lua
@@ -1,4 +1,4 @@
-LinkLuaModifier( "modifier_item_postactive_regen", "items/reflex/postactive_regen.lua", LUA_MODIFIER_MOTION_NONE )
+LinkLuaModifier("modifier_item_postactive_regen", "items/reflex/postactive_regen.lua", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_generic_bonus", "modifiers/modifier_generic_bonus.lua", LUA_MODIFIER_MOTION_NONE)
 
 item_regen_crystal_1 = class(ItemBaseClass)
@@ -11,27 +11,87 @@ end
 
 function item_regen_crystal_1:OnSpellStart()
   local caster = self:GetCaster()
-  caster:AddNewModifier(caster, self, 'modifier_item_postactive_regen', {
-    duration = self:GetSpecialValueFor( "duration" )
-  })
+  caster:AddNewModifier(caster, self, "modifier_item_postactive_regen", {duration = self:GetSpecialValueFor("duration")})
 end
 
+function item_regen_crystal_1:ProcsMagicStick()
+  return false
+end
+
+---------------------------------------------------------------------------------------------------
 modifier_item_postactive_regen = class(ModifierBaseClass)
 
+function modifier_item_postactive_regen:IsHidden()
+  return false
+end
 
-function modifier_item_postactive_regen:OnCreated( kv )
+function modifier_item_postactive_regen:IsDebuff()
+  return false
+end
+
+function modifier_item_postactive_regen:IsPurgable()
+  return false
+end
+
+function modifier_item_postactive_regen:OnCreated()
+  local parent = self:GetParent()
   if IsServer() then
     if self.nPreviewFX == nil then
-      self.nPreviewFX = ParticleManager:CreateParticle( "particles/items/regen_crystal/regen_ambient.vpcf", PATTACH_ABSORIGIN_FOLLOW, self:GetParent() )
-      ParticleManager:SetParticleControlEnt( self.nPreviewFX, 0, self:GetParent(), PATTACH_ABSORIGIN_FOLLOW, nil, self:GetParent():GetOrigin(), true )
+      self.nPreviewFX = ParticleManager:CreateParticle("particles/items/regen_crystal/regen_ambient.vpcf", PATTACH_ABSORIGIN_FOLLOW, parent)
+      ParticleManager:SetParticleControlEnt(self.nPreviewFX, 0, parent, PATTACH_ABSORIGIN_FOLLOW, nil, parent:GetOrigin(), true)
     end
+  end
+  local ability = self:GetAbility()
+  local max_mana_to_hp_regen = 3
+  if ability and not ability:IsNull() then
+    self.hp_regen_amp = ability:GetSpecialValueFor("active_hp_regen_amp")
+    max_mana_to_hp_regen = ability:GetSpecialValueFor("max_mana_to_hp_regen")
+  end
+  local max_mana = parent:GetMaxMana()
+  self.bonus_hp_regen = max_mana*max_mana_to_hp_regen/100
+  if IsServer() and parent:IsHero() then
+    parent:CalculateStatBonus()
+  end
+  self:StartIntervalThink(0.5)
+end
+
+function modifier_item_postactive_regen:OnRefresh()
+  local parent = self:GetParent()
+  local ability = self:GetAbility()
+  local max_mana_to_hp_regen = 3
+  if ability and not ability:IsNull() then
+    self.hp_regen_amp = ability:GetSpecialValueFor("active_hp_regen_amp")
+    max_mana_to_hp_regen = ability:GetSpecialValueFor("max_mana_to_hp_regen")
+  end
+  local max_mana = parent:GetMaxMana()
+  self.bonus_hp_regen = max_mana*max_mana_to_hp_regen/100
+  if IsServer() and parent:IsHero() then
+    parent:CalculateStatBonus()
   end
 end
 
-function modifier_item_postactive_regen:OnDestroy(  )
+function modifier_item_postactive_regen:OnIntervalThink()
+  local parent = self:GetParent()
+  if not self.bonus_hp_regen then
+    self:ForceRefresh()
+    return
+  end
+
+  local ability = self:GetAbility()
+  local max_mana_to_hp_regen = 3
+  if ability and not ability:IsNull() then
+    max_mana_to_hp_regen = ability:GetSpecialValueFor("max_mana_to_hp_regen")
+  end
+  local max_mana = parent:GetMaxMana()
+  if self.bonus_hp_regen ~= max_mana*max_mana_to_hp_regen/100 then
+    self:ForceRefresh()
+  end
+end
+
+function modifier_item_postactive_regen:OnDestroy()
   if IsServer() then
-    if self.nPreviewFX ~= nil then
-      ParticleManager:DestroyParticle( self.nPreviewFX, false )
+    if self.nPreviewFX then
+      ParticleManager:DestroyParticle(self.nPreviewFX, false)
       ParticleManager:ReleaseParticleIndex(self.nPreviewFX)
       self.nPreviewFX = nil
     end
@@ -40,10 +100,15 @@ end
 
 function modifier_item_postactive_regen:DeclareFunctions()
   return {
-    MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT
+    MODIFIER_PROPERTY_HEALTH_REGEN_CONSTANT,
+    MODIFIER_PROPERTY_HP_REGEN_AMPLIFY_PERCENTAGE,
   }
 end
 
 function modifier_item_postactive_regen:GetModifierConstantHealthRegen()
-  return self:GetAbility():GetSpecialValueFor( "active_health_regen" )
+  return self.bonus_hp_regen
+end
+
+function modifier_item_postactive_regen:GetModifierHPRegenAmplify_Percentage()
+  return self.hp_regen_amp or self:GetAbility():GetSpecialValueFor("active_hp_regen_amp")
 end
