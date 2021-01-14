fbe1d94f3b38a7c1438f7955954a108b51a96915
Darko V <darko1290@gmail.com>
2021-01-14 02:00:43 +0100

    Sohei 2021 Update (#3044)
    * Dash and talent changes
    
    * Added shard check function to basenpc library.
    * Removed Scepter upgrade for Dash
    * Added Shard upgrade for Dash: "Sohei heals allies he dashed through for 100 plus 10% of his current health and pulls them along the way. Sohei and his allies are invulnerable during Dash and disjoint projectiles."
    * Dash speed is now based on Sohei's movement speed at the moment of the cast. Dash speed = 7 x movement speed.
    * Dash casting behavior changed: Sohei will no longer walk into range to cast Dash, he will always use Dash towards target location.
    * Dash (cast) range reduced from 600 to 575.
    * Level 15 talent Dash Invulnerability replaced with +20% Dash Slow.
    * Level 20 talent +2s Momentum Strike Stun replaced with +275 Dash Cast Range.
    * Level 25 talent +200 Flurry of Blows AoE with +2s Momentum Strike Stun.
    
    * Wholeness of Body
    
    * Mana cost increased from 85 to 80/85/90/95/100/105.
    * Bonus magic resistance increased from 90% to 100%
    * Base heal rescaled from 90/160/230/300/600/900 to 100/150/200/250/500/1000.
    
    * Momentum Strike
    
    * Cast Point increased by 0.05
    * Mana Cost increased by 5.
    * Attempted to make collision of a unit affected by Momentum Strike more smooth and better looking against trees, buildings, blockers and other units.
    
    * Flurry of Blows
    
    * Bonus damage is now applied only on the delayed instant attack and not on normal attacks made by Sohei during Flurry of Blows duration. This also means Momentum Strike will not do extra damage if used during Flurry of Blows.
    
    * Scepter rework and some other stuff
    
    * Dash: Sohei's health bar is now visible during Dash if he is vulnerable.
    * Dash: Attempted to make it more smooth when going over cliffs.
    * New Scepter Upgrade: Grants Quivering Palm - When activated, last attacked hero by Sohei will be damaged based on the difference between the hero's strength and Sohei's. Can only affect heroes attacked in the last 10 seconds. If there is no valid target, cooldown is reduced to 1 second. Quivering Palm debuff is dispellable. Damage is magical and its 100 + 20 x strength difference. - TEST VERSION
    * Flurry of Blows tooltip updates
    * Added Modelscale, BoundsHullName, AbilityTalentStart and VoiceFile to sohei.txt (hero kv file)
    * Level 10 talent changed from +50 Damage to +20 Strength
    
    * Forgot EndCooldown
    
    * Forgot RefundManaCost
    
    * Added particle
    
    * Update tooltip_sohei.txt
    
    * Apply small momentum strike knockback to Quivering Palm victim
    
    * Add sound and exit condition if illusion
    
    * Update tooltip_sohei.txt
    
    * Some sounds experiments
    
    * oopsies
    
    * Some balance
    
    * Momentum Strike crit damage increased from 150/180/210/240/270/300% to 150/190/230/270/310/350%.
    * Level 25 talent +120% Momentum Strike Crit Damage reduced to +100%.
    * Quivering Palm strength difference multiplier reduced from 20 to 15.

---
+++ game/scripts/vscripts/abilities/sohei/sohei_quivering_palm.lua
@@ -0,0 +1,255 @@
+sohei_quivering_palm = class(AbilityBaseClass)
+
+LinkLuaModifier("modifier_sohei_quivering_palm_passive", "abilities/sohei/sohei_quivering_palm.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_sohei_quivering_palm_debuff", "abilities/sohei/sohei_quivering_palm.lua", LUA_MODIFIER_MOTION_NONE)
+
+function sohei_quivering_palm:GetIntrinsicModifierName()
+  return "modifier_sohei_quivering_palm_passive"
+end
+
+function sohei_quivering_palm:OnHeroCalculateStatBonus()
+  local caster = self:GetCaster()
+
+  if caster:HasScepter() or self:IsStolen() then
+    self:SetHidden(false)
+    if self:GetLevel() <= 0 then
+      self:SetLevel(1)
+    end
+  else
+    self:SetHidden(true)
+  end
+end
+
+function sohei_quivering_palm:OnSpellStart()
+  local caster = self:GetCaster()
+
+  -- Find enemy heroes everywhere
+  local heroes = FindUnitsInRadius(
+    caster:GetTeamNumber(),
+    caster:GetAbsOrigin(),
+    nil,
+    FIND_UNITS_EVERYWHERE,
+    DOTA_UNIT_TARGET_TEAM_ENEMY,
+    DOTA_UNIT_TARGET_HERO,
+    DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
+    FIND_ANY_ORDER,
+    false
+  )
+
+  if #heroes < 1 then
+    --print("No enemy heroes on the map")
+    self:EndCooldown()
+    self:StartCooldown(1)
+    self:RefundManaCost()
+    return
+  end
+
+  local heroes_with_modifier = {}
+  for _, hero in pairs(heroes) do
+    if hero and not hero:IsNull() then
+      if hero:HasModifier("modifier_sohei_quivering_palm_debuff") then
+        table.insert(heroes_with_modifier, hero)
+      end
+    end
+  end
+
+  if #heroes_with_modifier < 1 then
+    --print("No heroes with the modifier")
+    self:EndCooldown()
+    self:StartCooldown(1)
+    self:RefundManaCost()
+    return
+  end
+
+  local passive = caster:FindModifierByName("modifier_sohei_quivering_palm_passive")
+  local last_attacked = passive.last_attacked_unit
+
+  if last_attacked and not last_attacked:IsNull() then
+    -- Check if target marked by the passive died or dispelled the debuff, if yes find a new one
+    if not last_attacked:IsAlive() or not last_attacked:FindModifierByNameAndCaster("modifier_sohei_quivering_palm_debuff", caster) then
+      last_attacked = self:FindLastAttackedWithDebuff(heroes_with_modifier, "modifier_sohei_quivering_palm_debuff")
+    end
+  else
+    -- Target marked by the passive doesn't exist, find a new one
+    last_attacked = self:FindLastAttackedWithDebuff(heroes_with_modifier, "modifier_sohei_quivering_palm_debuff")
+  end
+
+  self:QuiveringPalmEffect(last_attacked)
+end
+
+function sohei_quivering_palm:FindLastAttackedWithDebuff(candidates, debuff_name)
+  local last_attacked = nil
+  local current_time = GameRules:GetGameTime()
+  local caster = self:GetCaster()
+  local difference = self:GetSpecialValueFor("max_duration") or 10
+  for _, hero in pairs(candidates) do
+    if hero and not hero:IsNull() then
+      local debuff = hero:FindModifierByNameAndCaster(debuff_name, caster)
+      if debuff then
+        local debuff_creation_time = debuff:GetCreationTime()
+        if current_time - debuff_creation_time < difference then
+          difference = current_time - debuff_creation_time
+          last_attacked = hero
+        end
+      end
+    end
+  end
+
+  return last_attacked
+end
+
+function sohei_quivering_palm:QuiveringPalmEffect(victim)
+  if not victim then
+    --print("No valid target")
+    self:EndCooldown()
+    self:StartCooldown(1)
+    self:RefundManaCost()
+    return
+  end
+
+  if not victim:IsHero() then
+    --print("No valid target")
+    self:EndCooldown()
+    self:StartCooldown(1)
+    self:RefundManaCost()
+    return
+  end
+
+  local caster = self:GetCaster()
+
+  -- Sound
+  victim:EmitSound("Sohei.QuiveringPalm")
+
+  -- Kill illusions
+  if victim:IsIllusion() then
+    victim:Kill(self, caster)
+    return
+  end
+
+  -- Check if caster has Momentum Strike learned, if not apply regular knockback
+  local momentum_strike = caster:FindAbilityByName("sohei_momentum_strike")
+  if momentum_strike and momentum_strike:GetLevel() > 0 then
+    -- Knockback parameters
+    local distance = momentum_strike:GetSpecialValueFor("knockback_distance")/2
+    local speed = momentum_strike:GetSpecialValueFor("knockback_speed")/2
+    local duration = distance / speed
+    local collision_radius = momentum_strike:GetSpecialValueFor("collision_radius")
+
+    local direction = -victim:GetForwardVector() -- victim:GetAbsOrigin() - position_in_front_of_them
+    direction.z = 0
+    direction = direction:Normalized()
+
+    -- Apply Momentum Strike Knockback to the enemy
+    victim:RemoveModifierByName("modifier_sohei_momentum_strike_knockback")
+    victim:AddNewModifier(caster, momentum_strike, "modifier_sohei_momentum_strike_knockback", {
+      duration = duration,
+      distance = distance,
+      speed = speed,
+      collision_radius = collision_radius,
+      direction_x = direction.x,
+      direction_y = direction.y,
+    })
+  end
+
+  -- Calculate damage
+  local caster_str = caster:GetStrength()
+  local victim_str = victim:GetStrength()
+  local diff_multiplier = self:GetSpecialValueFor("str_diff_multiplier")
+  local bonus_damage = math.max((caster_str - victim_str) * diff_multiplier, 0)
+
+  local damage_table = {}
+  damage_table.attacker = caster
+  damage_table.damage_type = self:GetAbilityDamageType()
+  damage_table.ability = self
+  damage_table.damage = self:GetSpecialValueFor("base_damage") + bonus_damage
+  damage_table.victim = victim
+  ApplyDamage(damage_table)
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_sohei_quivering_palm_passive = class(ModifierBaseClass)
+
+function modifier_sohei_quivering_palm_passive:IsHidden()
+  return true
+end
+
+function modifier_sohei_quivering_palm_passive:IsDebuff()
+  return false
+end
+
+function modifier_sohei_quivering_palm_passive:IsPurgable()
+  return false
+end
+
+function modifier_sohei_quivering_palm_passive:RemoveOnDeath()
+  return false
+end
+
+function modifier_sohei_quivering_palm_passive:DeclareFunctions()
+  return {
+    MODIFIER_EVENT_ON_ATTACK_LANDED,
+  }
+end
+
+function modifier_sohei_quivering_palm_passive:OnAttackLanded(event)
+  local parent = self:GetParent()
+  local ability = self:GetAbility()
+  local attacker = event.attacker
+  local target = event.target
+
+  if attacker ~= parent then
+    return
+  end
+
+  -- Check if attacker exists
+  if not attacker or attacker:IsNull() then
+    return
+  end
+
+  -- Check if attacker is an illusion
+  if attacker:IsIllusion() then
+    return
+  end
+
+  -- Check if attacked entity exists
+  if not target or target:IsNull() then
+    return
+  end
+
+  -- Check if its an item, rune, or something weird
+  if target.GetUnitName == nil then
+    return
+  end
+
+  -- Check if its a hero or illusion of a hero
+  if not target:IsHero() then
+    return
+  end
+
+  -- Applying the debuff
+  target:AddNewModifier(attacker, ability, "modifier_sohei_quivering_palm_debuff", {duration = ability:GetSpecialValueFor("max_duration")})
+
+  -- Last attacked hero (can be illusion too)
+  self.last_attacked_unit = target
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_sohei_quivering_palm_debuff = class(ModifierBaseClass)
+
+function modifier_sohei_quivering_palm_debuff:IsHidden()
+  return false
+end
+
+function modifier_sohei_quivering_palm_debuff:IsDebuff()
+  return true
+end
+
+function modifier_sohei_quivering_palm_debuff:IsPurgable()
+  return true
+end
+
+function modifier_sohei_quivering_palm_debuff:RemoveOnDeath()
+  return true
+end
