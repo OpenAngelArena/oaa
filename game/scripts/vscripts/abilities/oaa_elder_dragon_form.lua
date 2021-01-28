936a1628810f3d84effd08db3764cd2b90b9c202
Darko V <darko1290@gmail.com>
2021-01-28 02:53:56 +0100

    Dragon Knight Elder Dragon Form Update (#3052)
    * Dragon Knight Ult update
    
    * Improved Elder Dragon Form description.
    * Fixed Elder Dragon Form tooltip on the picking screen not showing the correct value of Frostbite attack healing reduction.
    * Elder Dragon Form at level 5 and with Aghanim Scepter now becomes Permanent Dragon Form (level 6 Elder Dragon Form).
    * Fixed Elder Dragon Form cooldown and mana cost being 0 at level 5 without Aghanim Scepter.
    
    * npm test
    
    * More fixes
    
    * Fixed lvl 4 and 5 Dragon Form not being correct when scepter is bought late or when dropped.
    * Fixed Dragon Form projectile.
    * Simplified code: Everything is done in OnSpellStart()
    * Deleted code that is not needed.
    
    * Travis

--- game/scripts/vscripts/abilities/oaa_elder_dragon_form.lua
+++ game/scripts/vscripts/abilities/oaa_elder_dragon_form.lua
@@ -1,51 +1,59 @@
-dragon_knight_elder_dragon_form_oaa = class( AbilityBaseClass )
+dragon_knight_elder_dragon_form_oaa = class(AbilityBaseClass)
 
 LinkLuaModifier( "modifier_dragon_knight_elder_dragon_form_oaa", "abilities/oaa_elder_dragon_form.lua", LUA_MODIFIER_MOTION_NONE )
 LinkLuaModifier( "modifier_dragon_knight_max_level_oaa", "abilities/oaa_elder_dragon_form.lua", LUA_MODIFIER_MOTION_NONE )
 LinkLuaModifier( "modifier_dragon_knight_frostbite_debuff_oaa", "abilities/oaa_elder_dragon_form.lua", LUA_MODIFIER_MOTION_NONE )
 
---------------------------------------------------------------------------------
-
--- this makes the ability passive when it hits level 5
---[[
+-- this makes the ability passive when it hits level 5 and caster has scepter
 function dragon_knight_elder_dragon_form_oaa:GetBehavior()
-	if self:GetLevel() >= 5 then
+	if self:GetLevel() >= 5 and self:GetCaster():HasScepter() then
 		return DOTA_ABILITY_BEHAVIOR_PASSIVE
 	end
 
-	return self.BaseClass.GetBehavior( self )
+	return self.BaseClass.GetBehavior(self)
 end
-]]
-
---------------------------------------------------------------------------------
 
 -- this is meant to accompany the above, removing the mana cost and cooldown
 -- from the tooltip when it becomes passive
-function dragon_knight_elder_dragon_form_oaa:GetCooldown( level )
-	if self:GetLevel() >= 5 or level >= 5 then
+function dragon_knight_elder_dragon_form_oaa:GetCooldown(level)
+	local caster = self:GetCaster() or self:GetOwner()
+  if (self:GetLevel() >= 5 or level >= 5) and caster:HasScepter() then
 		return 0
 	end
 
-	return self.BaseClass.GetCooldown( self, level )
+	return self.BaseClass.GetCooldown(self, level)
 end
 
---------------------------------------------------------------------------------
-
-function dragon_knight_elder_dragon_form_oaa:GetManaCost( level )
-	if self:GetLevel() >= 5 or level >= 5 then
+function dragon_knight_elder_dragon_form_oaa:GetManaCost(level)
+	local caster = self:GetCaster() or self:GetOwner()
+  if (self:GetLevel() >= 5 or level >= 5) and caster:HasScepter() then
 		return 0
 	end
 
-	return self.BaseClass.GetManaCost( self, level )
+	return self.BaseClass.GetManaCost(self, level)
 end
-
---------------------------------------------------------------------------------
 
 function dragon_knight_elder_dragon_form_oaa:OnSpellStart()
   local caster = self:GetCaster()
   local level = self:GetLevel()
-  local duration = self:GetSpecialValueFor( "duration" )
-  local ability = caster:FindAbilityByName("dragon_knight_elder_dragon_form") or self
+  local duration = self:GetSpecialValueFor("duration")
+  local ability = caster:FindAbilityByName("dragon_knight_elder_dragon_form")
+
+  if ability then
+    if level >= 4 then
+      if caster:HasScepter() then
+        ability:SetLevel(3)
+      else
+        ability:SetLevel(4)
+      end
+    end
+  else
+    ability = self
+  end
+
+  if level >= 5 and caster:HasScepter() then
+    duration = -1
+  end
 
   -- apply the standard dragon form modifier ( for movespeed and the model change )
   caster:AddNewModifier( caster, ability, "modifier_dragon_knight_dragon_form", { duration = duration, } )
@@ -67,37 +75,21 @@ function dragon_knight_elder_dragon_form_oaa:OnSpellStart()
   end
 end
 
---------------------------------------------------------------------------------
---[[
 function dragon_knight_elder_dragon_form_oaa:GetIntrinsicModifierName()
-	-- adds the modifier in change of automatically transforming dk
-	-- when edf hits level 5 actually no level 4
-	-- since now this is also in charge of rage
-	if self:GetLevel() >= 4 then
-		return "modifier_dragon_knight_elder_dragon_form_oaa"
-	end
+  if self:GetLevel() >= 5 then
+    return "modifier_dragon_knight_elder_dragon_form_oaa"
+  end
 end
-]]
-
---------------------------------------------------------------------------------
 
 function dragon_knight_elder_dragon_form_oaa:OnUpgrade()
   local caster = self:GetCaster()
   local ability_level = self:GetLevel()
-  --[[
-  -- we need to refresh the passive modifier to cause it to instantly
-  -- transform dk on upgrade
-  if ability_level >= 5 then
-    -- adding it again works in the off chance the modifier is somehow removed
-    caster:AddNewModifier( caster, self, "modifier_dragon_knight_elder_dragon_form_oaa", {} )
-  end
-  ]]
+
   local vanilla_ability = caster:FindAbilityByName("dragon_knight_elder_dragon_form")
-  
   if not vanilla_ability then
     return
   end
-  
+
   if ability_level >= 4 then
     if caster:HasScepter() then
       vanilla_ability:SetLevel(3)
@@ -110,19 +102,9 @@ function dragon_knight_elder_dragon_form_oaa:OnUpgrade()
   vanilla_ability:SetLevel(ability_level)
 end
 
---------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
 
-modifier_dragon_knight_elder_dragon_form_oaa = class( ModifierBaseClass )
-
--- table of edf modifiers
-modifier_dragon_knight_elder_dragon_form_oaa.edfMods = {
-	"modifier_dragon_knight_dragon_form",
-	"modifier_dragon_knight_corrosive_breath",
-	"modifier_dragon_knight_splash_attack",
-	"modifier_dragon_knight_frost_breath",
-}
-
---------------------------------------------------------------------------------
+modifier_dragon_knight_elder_dragon_form_oaa = class(ModifierBaseClass)
 
 function modifier_dragon_knight_elder_dragon_form_oaa:IsHidden()
 	return true
@@ -140,123 +122,129 @@ function modifier_dragon_knight_elder_dragon_form_oaa:RemoveOnDeath()
 	return false
 end
 
---------------------------------------------------------------------------------
+function modifier_dragon_knight_elder_dragon_form_oaa:OnCreated()
+  if not IsServer() then
+    return
+  end
 
-if IsServer() then
-	function modifier_dragon_knight_elder_dragon_form_oaa:OnCreated( event )
-		local parent = self:GetParent()
-		local spell = self:GetAbility()
-
-		-- apply all the edf modifiers on creation if level 5
-		if spell:GetLevel() >= 5 then
-			for _, modName in ipairs( self.edfMods ) do
-				local mod = parent:FindModifierByName( modName )
-
-				if not mod then
-					parent:AddNewModifier( parent, spell, modName, {} )
-				else
-					-- if dk already has 'em, just set their duration to "permanent"
-					-- so no special effect happens on level up if he's already transformed
-					mod:SetDuration( -1, true )
-				end
-			end
-		end
-	end
-
---------------------------------------------------------------------------------
-
-	function modifier_dragon_knight_elder_dragon_form_oaa:OnRefresh( event )
-		local parent = self:GetParent()
-		local spell = self:GetAbility()
-
-		-- apply all the edf modifiers on creation if level 5
-		if spell:GetLevel() >= 5 then
-			for _, modName in pairs( self.edfMods ) do
-				local mod = parent:FindModifierByName( modName )
-
-				if not mod then
-					parent:AddNewModifier( parent, spell, modName, {} )
-				else
-					-- if dk already has 'em, just set their duration to "permanent"
-					-- so no special effect happens on level up if he's already transformed
-					mod:SetDuration( -1, true )
-				end
-			end
-		end
-	end
-
---------------------------------------------------------------------------------
-
-	function modifier_dragon_knight_elder_dragon_form_oaa:DeclareFunctions()
-		local funcs = {
-			MODIFIER_EVENT_ON_ATTACK_LANDED,
-			MODIFIER_EVENT_ON_RESPAWN,
-		}
-
-		return funcs
-	end
-
---------------------------------------------------------------------------------
-
-	function modifier_dragon_knight_elder_dragon_form_oaa:OnAttackLanded( event )
-		local parent = self:GetParent()
-
-		if event.attacker == parent and event.process_procs then
-			local spell = self:GetAbility()
-
-			if spell:GetLevel() == 4 then
-				-- no rage while broken
-				if parent:PassivesDisabled() then
-					return
-				end
-
-				local chance = spell:GetSpecialValueFor( "rage_chance" ) / 100
-
-				-- we're using the modifier's stack to store the amount of prng failures
-				-- this could be something else but since this modifier is hidden anyway ...
-				local prngMult = self:GetStackCount() + 1
-
-				-- compared prng to slightly less prng
-				if RandomFloat( 0.0, 1.0 ) <= ( PrdCFinder:GetCForP(chance) * prngMult ) then
-					-- reset failure count
-					self:SetStackCount( 0 )
-
-					local duration = spell:GetSpecialValueFor( "rage_duration" )
-
-					-- check if the ability is already active, and if so, grab the current
-					-- duration
-					local mod = parent:FindModifierByName( "modifier_dragon_knight_dragon_form" )
-
-					if mod then
-						duration = duration + mod:GetRemainingTime()
-					end
-
-					-- apply the edf modifiers with the new duration
-					for _, modName in pairs( self.edfMods ) do
-						parent:AddNewModifier( parent, spell, modName, { duration = duration } )
-					end
-				else
-					-- increment failure count
-					self:SetStackCount( prngMult )
-				end
-			end
-		end
-	end
-
---------------------------------------------------------------------------------
-
-	function modifier_dragon_knight_elder_dragon_form_oaa:OnRespawn( event )
-		local parent = self:GetParent()
-		local spell = self:GetAbility()
-
-		if event.unit == parent and spell:GetLevel() >= 5 then
-			-- apply the edf modifiers on respawn
-			for _, modName in pairs( self.edfMods ) do
-				parent:AddNewModifier( parent, spell, modName, {} )
-			end
-		end
-	end
+  self:StartIntervalThink(1)
 end
+
+function modifier_dragon_knight_elder_dragon_form_oaa:OnRefresh()
+  if not IsServer() then
+    return
+  end
+
+  self:OnIntervalThink()
+end
+
+function modifier_dragon_knight_elder_dragon_form_oaa:OnIntervalThink()
+  if not IsServer() then
+    return
+  end
+
+  local parent = self:GetParent()
+
+  if not parent or parent:IsNull() then
+    return
+  end
+
+  if parent:IsIllusion() then
+    return
+  end
+
+  local ability = self:GetAbility() or parent:FindAbilityByName("dragon_knight_elder_dragon_form_oaa")
+
+  if not ability or ability:IsNull() then
+    return
+  end
+
+  if ability:GetLevel() >= 5 then
+    local modifier = parent:FindModifierByName("modifier_dragon_knight_dragon_form")
+    if parent:HasScepter() then
+      if not modifier then
+        ability:OnSpellStart()
+      else
+        if modifier:GetDuration() ~= -1 then
+          ability:OnSpellStart()
+        end
+      end
+    else
+      -- Parent doesn't have scepter -> indirectly check if parent dropped/sold his scepter
+      -- by checking if has dragon form modifiers and by checking modifier durations
+      if not modifier then
+        -- Modifier doesn't exist and parent doesn't have scepter -> don't do anything
+        return
+      else
+        if modifier:GetDuration() == -1 then
+          -- Duration of the modifier is -1 (infinite) but parent doesn't have scepter
+          -- Reapply Dragon Form modifiers to give them normal duration
+          ability:OnSpellStart()
+        end
+      end
+    end
+  end
+end
+
+--[[
+function modifier_dragon_knight_elder_dragon_form_oaa:DeclareFunctions()
+  local funcs = {
+    --MODIFIER_EVENT_ON_ATTACK_LANDED,
+  }
+
+  return funcs
+end
+
+-- Rage chance - chance to transform into Dragon Form when attack lands
+function modifier_dragon_knight_elder_dragon_form_oaa:OnAttackLanded(event)
+  if not IsServer() then
+    return
+  end
+
+  local parent = self:GetParent()
+
+  if event.attacker == parent and event.process_procs then
+    local spell = self:GetAbility()
+
+    if spell:GetLevel() == 4 then
+      -- no rage while broken
+      if parent:PassivesDisabled() then
+        return
+      end
+
+      local chance = spell:GetSpecialValueFor( "rage_chance" ) / 100
+
+      -- we're using the modifier's stack to store the amount of prng failures
+      -- this could be something else but since this modifier is hidden anyway ...
+      local prngMult = self:GetStackCount() + 1
+
+      -- compared prng to slightly less prng
+      if RandomFloat( 0.0, 1.0 ) <= ( PrdCFinder:GetCForP(chance) * prngMult ) then
+        -- reset failure count
+        self:SetStackCount( 0 )
+
+        local duration = spell:GetSpecialValueFor( "rage_duration" )
+
+        -- check if the ability is already active, and if so, grab the current
+        -- duration
+        local mod = parent:FindModifierByName( "modifier_dragon_knight_dragon_form" )
+
+        if mod then
+          duration = duration + mod:GetRemainingTime()
+        end
+
+        -- apply the edf modifiers with the new duration
+        for _, modName in pairs( self.edfMods ) do
+          parent:AddNewModifier( parent, spell, modName, { duration = duration } )
+        end
+      else
+        -- increment failure count
+        self:SetStackCount( prngMult )
+      end
+    end
+  end
+end
+]]
 
 ---------------------------------------------------------------------------------------------------
 
@@ -274,6 +262,10 @@ function modifier_dragon_knight_max_level_oaa:IsPurgable()
   return false
 end
 
+function modifier_dragon_knight_max_level_oaa:RemoveOnDeath()
+  return true
+end
+
 function modifier_dragon_knight_max_level_oaa:DeclareFunctions()
   local funcs = {
     MODIFIER_EVENT_ON_ATTACK_LANDED,
@@ -281,6 +273,10 @@ function modifier_dragon_knight_max_level_oaa:DeclareFunctions()
   }
 
   return funcs
+end
+
+function modifier_dragon_knight_max_level_oaa:GetModifierMagicalResistanceBonus()
+  return 20
 end
 
 function modifier_dragon_knight_max_level_oaa:OnAttackLanded(event)
@@ -323,12 +319,6 @@ function modifier_dragon_knight_max_level_oaa:OnAttackLanded(event)
   target:AddNewModifier(parent, ability, "modifier_dragon_knight_frostbite_debuff_oaa", {duration = duration})
 end
 
-function modifier_dragon_knight_max_level_oaa:GetModifierMagicalResistanceBonus()
-  --local ability = self:GetAbility()
-  --return ability:GetLevelSpecialValueFor("magic_resistance", ability:GetLevel()-1)
-  return 20
-end
-
 ---------------------------------------------------------------------------------------------------
 
 modifier_dragon_knight_frostbite_debuff_oaa = class(ModifierBaseClass)
@@ -349,12 +339,12 @@ function modifier_dragon_knight_frostbite_debuff_oaa:GetEffectName()
   return "particles/items4_fx/spirit_vessel_damage.vpcf"
 end
 
-function modifier_dragon_knight_frostbite_debuff_oaa:OnCreated( kv )
-  self.heal_suppression_pct = self:GetAbility():GetSpecialValueFor( "heal_suppression_pct" )
+function modifier_dragon_knight_frostbite_debuff_oaa:OnCreated()
+  self.heal_suppression_pct = self:GetAbility():GetSpecialValueFor("heal_suppression_pct")
 end
 
-function modifier_dragon_knight_frostbite_debuff_oaa:OnRefresh( kv )
-  self.heal_suppression_pct = self:GetAbility():GetSpecialValueFor( "heal_suppression_pct" )
+function modifier_dragon_knight_frostbite_debuff_oaa:OnRefresh()
+  self.heal_suppression_pct = self:GetAbility():GetSpecialValueFor("heal_suppression_pct")
 end
 
 function modifier_dragon_knight_frostbite_debuff_oaa:DeclareFunctions()
