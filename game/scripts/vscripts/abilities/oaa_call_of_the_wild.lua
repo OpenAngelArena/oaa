579802e3f396f40bbf02374b6f6d6893a5304d9d
Darko V <darko1290@gmail.com>
2021-01-28 01:43:39 +0100

    Beastmaster Shard and Hawk rebalance
    * Hawk HP rescaled from 150/200/250/300/1250/2500 to 150/250/350/450/900/1800.
    * Hawk vision at OAA levels reduced from 1350/1800 to 1000/1100.
    * Hawk magic resistance at OAA levels increased from 0% to 25/50%
    * Added Aghanim Shard upgrade for Hawk: Increases magic resistance to 100% and gives Dive Bomb ability to Hawks.
    * Simplified the code for Hawks so their stats can be changed in the KV file.

--- game/scripts/vscripts/abilities/oaa_call_of_the_wild.lua
+++ game/scripts/vscripts/abilities/oaa_call_of_the_wild.lua
@@ -86,21 +86,58 @@ function beastmaster_call_of_the_wild_hawk_oaa:OnSpellStart()
 end
 
 function beastmaster_call_of_the_wild_hawk_oaa:SpawnHawk(caster, playerID, abilityLevel, duration, number_of_hawks)
+  local unit_name = "npc_dota_beastmaster_hawk_oaa"
+  local hawk_hp = self:GetLevelSpecialValueFor("hawk_hp", abilityLevel-1)
+  local hawk_armor = self:GetLevelSpecialValueFor("hawk_armor", abilityLevel-1)
+  local hawk_speed = self:GetLevelSpecialValueFor("hawk_speed", abilityLevel-1)
+  local hawk_vision = self:GetLevelSpecialValueFor("hawk_vision", abilityLevel-1)
+  local hawk_magic_resistance = self:GetLevelSpecialValueFor("hawk_magic_resistance", abilityLevel-1)
+  local hawk_gold_bounty = self:GetLevelSpecialValueFor("hawk_gold_bounty", abilityLevel-1)
 
-  local baseUnitName = "npc_dota_beastmaster_hawk"
-  local levelUnitName = baseUnitName .. "_" .. abilityLevel
+  if caster:HasShardOAA() then
+    hawk_magic_resistance = 100
+  end
 
   for i = 1, number_of_hawks do
     -- Spawn hawk and orient it to face the same way as the caster
-    local hawk = self:SpawnUnit(levelUnitName, caster, playerID, abilityLevel, duration, true)
+    local hawk = self:SpawnUnit(unit_name, caster, playerID, abilityLevel, duration, true)
 
     -- Create particle effects
     local particleName = "particles/units/heroes/hero_beastmaster/beastmaster_call_bird.vpcf"
     local particle1 = ParticleManager:CreateParticle(particleName, PATTACH_CUSTOMORIGIN, caster)
     ParticleManager:SetParticleControl(particle1, 0, hawk:GetOrigin())
     ParticleManager:ReleaseParticleIndex(particle1)
+
     -- Invisibility buff
     hawk:AddNewModifier(caster, self, "modifier_hawk_invisibility_oaa", {})
+
+    -- Fix stats of hawks
+    -- HP
+    hawk:SetBaseMaxHealth(hawk_hp)
+    hawk:SetMaxHealth(hawk_hp)
+    hawk:SetHealth(hawk_hp)
+
+    -- ARMOR
+    hawk:SetPhysicalArmorBaseValue(hawk_armor)
+
+    -- MOVEMENT SPEED
+    hawk:SetBaseMoveSpeed(hawk_speed)
+
+    -- VISION
+    hawk:SetDayTimeVisionRange(hawk_vision)
+    hawk:SetNightTimeVisionRange(hawk_vision)
+
+    -- Magic Resistance
+    hawk:SetBaseMagicalResistanceValue(hawk_magic_resistance)
+
+    -- GOLD BOUNTY
+    hawk:SetMaximumGoldBounty(hawk_gold_bounty)
+    hawk:SetMinimumGoldBounty(hawk_gold_bounty)
+
+    if caster:HasShardOAA() then
+      local dive_bomb = hawk:AddAbility("beastmaster_hawk_dive_oaa")
+      dive_bomb:SetLevel(1)
+    end
   end
 
   caster:EmitSound("Hero_Beastmaster.Call.Hawk")
