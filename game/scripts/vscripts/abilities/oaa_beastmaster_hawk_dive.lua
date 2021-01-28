f5edd0311604e3642b9a1fae215cf42ed23889f1
Darko V <darko1290@gmail.com>
2021-01-28 02:54:37 +0100

    Beastmaster Shard fix and Hawk rebalance (#3055)
    * Beastmaster Shard and Hawk rebalance
    
    * Hawk HP rescaled from 150/200/250/300/1250/2500 to 150/250/350/450/900/1800.
    * Hawk vision at OAA levels reduced from 1350/1800 to 1000/1100.
    * Hawk magic resistance at OAA levels increased from 0% to 25/50%
    * Added Aghanim Shard upgrade for Hawk: Increases magic resistance to 100% and gives Dive Bomb ability to Hawks.
    * Simplified the code for Hawks so their stats can be changed in the KV file.
    
    * Fixes
    
    * Increased damage of Dive Bomb from 175 to 500.

---
+++ game/scripts/vscripts/abilities/oaa_beastmaster_hawk_dive.lua
@@ -0,0 +1,83 @@
+beastmaster_hawk_dive_oaa = class(AbilityBaseClass)
+
+LinkLuaModifier( "modifier_hawk_dive_stun", "abilities/oaa_beastmaster_hawk_dive.lua", LUA_MODIFIER_MOTION_NONE )
+
+function beastmaster_hawk_dive_oaa:OnSpellStart()
+  local caster = self:GetCaster()
+  local target = self:GetCursorTarget()
+
+   -- Check if target and caster entities exist
+  if not target or not caster then
+    return
+  end
+
+  -- Check if target has spell block
+  if target:TriggerSpellAbsorb(self) then
+    return
+  end
+
+  local damage = self:GetSpecialValueFor("damage")
+  local duration = self:GetSpecialValueFor("stun_duration")
+
+  local damage_table = {}
+  damage_table.victim = target
+  damage_table.attacker = caster
+  damage_table.damage = damage
+  damage_table.damage_type = self:GetAbilityDamageType()
+  damage_table.ability = self
+
+  ApplyDamage(damage_table)
+
+  target:AddNewModifier(caster, self, "modifier_hawk_dive_stun", {duration = duration})
+
+  if caster and caster:IsAlive() then
+    caster:Kill(self, caster)
+  end
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_hawk_dive_stun = class(ModifierBaseClass)
+
+function modifier_hawk_dive_stun:IsHidden()
+  return true
+end
+
+function modifier_hawk_dive_stun:IsDebuff()
+  return true
+end
+
+function modifier_hawk_dive_stun:IsStunDebuff()
+  return true
+end
+
+function modifier_hawk_dive_stun:IsPurgable()
+  return true
+end
+
+function modifier_hawk_dive_stun:GetEffectName()
+  return "particles/generic_gameplay/generic_stunned.vpcf"
+end
+
+function modifier_hawk_dive_stun:GetEffectAttachType()
+  return PATTACH_OVERHEAD_FOLLOW
+end
+
+function modifier_hawk_dive_stun:DeclareFunctions()
+  local funcs = {
+    MODIFIER_PROPERTY_OVERRIDE_ANIMATION,
+  }
+
+  return funcs
+end
+
+function modifier_hawk_dive_stun:GetOverrideAnimation()
+  return ACT_DOTA_DISABLED
+end
+
+function modifier_hawk_dive_stun:CheckState()
+  local state = {
+    [MODIFIER_STATE_STUNNED] = true,
+  }
+  return state
+end
