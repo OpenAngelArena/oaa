e1908b19c880bb15100427b976473126d7e07ee7
Darko V <darko1290@gmail.com>
2021-02-08 00:33:11 +0100

    Part 2
    1) Nature's Prophet Nature's Call (Force of Nature) treants rebalanced:
    * Treants hp rescaled from 550/700/900/1200/1800/2700 to 550/650/750/850/1850/2850.
    * Treants damage rescaled from 38/54/74/98/152/235 to 24/32/40/48/148/248.
    * Treants magic resistance increased from 0% to 25%. (Greater Treants magic resistance unchanged)
    * Treants gold and xp bounties reduced at levels 1-4.
    * Treants hp regen reduced at levels 5-6.
    * Treants model scale increased at level 5-6.
    2) Attempting to fix Nature's Prophet shard's Greater Treant not having correct hp and damage.
    3) Nature's Prophet Wrath of Nature buffs:
    * Scepter damage increased from 135/230/325/610/895 to 150/250/350/650/1150.
    * Scepter Wrath of Nature no longer creates normal Treants when killing creeps with it.
    * Scepter Wrath of Nature no longer creates Greater Treants when killing illusions with it. (Killing real heroes still does).
    * Scepter Wrath of Nature Greater Treant spawn on death debuff is no longer dispellable.
    * Scepter Wrath of Nature now also applies root for 1.6 - 3.4 seconds. Duration scales from 1.6 up to 3.4 seconds at max bounce.
    4) Monkey King Primal Spring damage at OAA levels rescaled from 770/1190 to 700/1400.
    5) Puck Waning Rift damage at level 6 increased from 1000 to 1120.
    6) Puck Dream Coil Rapid Fire talent attack rate improved from 0.6 to 0.55s

--- game/scripts/vscripts/abilities/oaa_force_of_nature.lua
+++ game/scripts/vscripts/abilities/oaa_force_of_nature.lua
@@ -44,12 +44,14 @@ function furion_force_of_nature:OnSpellStart()
   local duration = self:GetSpecialValueFor( "duration" )
   local ability_level = self:GetLevel()
   -- Units to spawn for each ability level
-  local treant_names = {"npc_dota_furion_treant_1",
-                        "npc_dota_furion_treant_2",
-                        "npc_dota_furion_treant_3",
-                        "npc_dota_furion_treant_4",
-                        "npc_dota_furion_treant_5",
-                        "npc_dota_furion_treant_6"}
+  local treant_names = {
+    "npc_dota_furion_treant_1",
+    "npc_dota_furion_treant_2",
+    "npc_dota_furion_treant_3",
+    "npc_dota_furion_treant_4",
+    "npc_dota_furion_treant_5",
+    "npc_dota_furion_treant_6"
+  }
 
   local trees = GridNav:GetAllTreesAroundPoint( target_point, area_of_effect, true )
   local tree_count = #trees
