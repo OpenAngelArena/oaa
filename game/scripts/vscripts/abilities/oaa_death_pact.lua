60dcd61d9f936cb2cc2310eab2915354a5ba9754
Darko V <darko1290@gmail.com>
2021-03-24 06:34:04 +0100

    Fixes
    * Fixed bonus damage from Death Pact not being correct on the client when lvl 25 talent was taken.
    * All calculations for Death Pact are now server-only.

--- game/scripts/vscripts/abilities/oaa_death_pact.lua
+++ game/scripts/vscripts/abilities/oaa_death_pact.lua
@@ -26,10 +26,42 @@ function clinkz_death_pact_oaa:OnSpellStart()
 
   -- Apply the modifier that just displays duration and visual effects
   caster:AddNewModifier( caster, self, "modifier_clinkz_death_pact_effect_oaa", {duration = duration} )
+  
+  -- get KV variables
+  local healthPct = self:GetSpecialValueFor("health_gain_pct")
+  local healthMax = self:GetSpecialValueFor("health_gain_max")
+  local damagePct = self:GetSpecialValueFor("damage_gain_pct")
+  local damageMax = self:GetSpecialValueFor("damage_gain_max")
+
+  -- Talent that increases hp and dmg gain
+  local talent = caster:FindAbilityByName("special_bonus_clinkz_death_pact_oaa")
+  if talent then
+    if talent:GetLevel() > 0 then
+      healthPct = healthPct + talent:GetSpecialValueFor("value")
+      damagePct = damagePct + talent:GetSpecialValueFor("value2")
+      healthMax = healthMax + talent:GetSpecialValueFor("value3")
+      damageMax = damageMax + talent:GetSpecialValueFor("value4")
+    end
+  end
+
+  -- Calculate bonuses
+  local health = targetHealth * healthPct * 0.01
+  local damage = targetHealth * damagePct * 0.01
+
+  -- Cap the health bonus
+  if healthMax > 0 then
+    health = math.min(healthMax, health)
+  end
+
+  -- Cap the damage bonus
+  if damageMax > 0 then
+    damage = math.min(damageMax, damage)
+  end
 
   -- apply the new modifier which actually provides the stats
   -- then set its stack count to the amount of health the target had
-  caster:AddNewModifier( caster, self, "modifier_clinkz_death_pact_oaa", {duration = duration, stacks = targetHealth} )
+  local modifier = caster:AddNewModifier( caster, self, "modifier_clinkz_death_pact_oaa", {duration = duration, health = health} )
+  modifier:SetStackCount(damage)
 
   -- play the sounds
   caster:EmitSound( "Hero_Clinkz.DeathPact.Cast" )
@@ -61,122 +93,32 @@ function modifier_clinkz_death_pact_oaa:RemoveOnDeath()
   return false
 end
 
-function modifier_clinkz_death_pact_oaa:OnCreated( event )
+function modifier_clinkz_death_pact_oaa:OnCreated(event)
   local parent = self:GetParent()
   local spell = self:GetAbility()
 
-  -- this has to be done server-side because valve
   if IsServer() then
-    -- get the parent's (Clinkz) current health before applying anything
-    --self.parentHealth = parent:GetHealth()
+    self.health = event.health
 
-    -- set the modifier's stack count to the target's health, so that we
-    -- have access to it on the client
-    self:SetStackCount( event.stacks )
-  end
-
-  -- get KV variables
-  local healthPct = spell:GetSpecialValueFor( "health_gain_pct" )
-  local healthMax = spell:GetSpecialValueFor( "health_gain_max" )
-  local damagePct = spell:GetSpecialValueFor( "damage_gain_pct" )
-  local damageMax = spell:GetSpecialValueFor( "damage_gain_max" )
-
-  if IsServer() then
-    -- Talent that increases hp and dmg gain
-    local talent = parent:FindAbilityByName("special_bonus_clinkz_death_pact_oaa")
-    if talent then
-      if talent:GetLevel() > 0 then
-        healthPct = healthPct + talent:GetSpecialValueFor("value")
-        damagePct = damagePct + talent:GetSpecialValueFor("value2")
-        healthMax = healthMax + talent:GetSpecialValueFor("value3")
-        damageMax = damageMax + talent:GetSpecialValueFor("value4")
-      end
-    end
-  end
-
-  -- retrieve the stack count
-  local targetHealth = self:GetStackCount()
-
-  -- Calculate bonuses
-  self.health = targetHealth * healthPct * 0.01
-  self.damage = targetHealth * damagePct * 0.01
-
-  -- Cap the health bonus
-  if healthMax > 0 then
-    self.health = math.min( healthMax, self.health )
-  end
-
-  -- Cap the damage bonus
-  if damageMax > 0 then
-    self.damage = math.min( damageMax, self.damage )
-  end
-
-  if IsServer() then
     -- apply the new health and such
     parent:CalculateStatBonus(true)
 
     -- add the added health
-    --parent:SetHealth( self.parentHealth + self.health )
     parent:Heal(self.health, spell)
   end
 end
 
-function modifier_clinkz_death_pact_oaa:OnRefresh( event )
+function modifier_clinkz_death_pact_oaa:OnRefresh(event)
   local parent = self:GetParent()
   local spell = self:GetAbility()
 
-  -- this has to be done server-side because valve
   if IsServer() then
-    -- get the parent's current health before applying anything
-    --self.parentHealth = parent:GetHealth()
+    self.health = event.health
 
-    -- set the modifier's stack count to the target's health, so that we
-    -- have access to it on the client
-    self:SetStackCount( event.stacks )
-  end
-
-  -- get KV variables
-  local healthPct = spell:GetSpecialValueFor( "health_gain_pct" )
-  local healthMax = spell:GetSpecialValueFor( "health_gain_max" )
-  local damagePct = spell:GetSpecialValueFor( "damage_gain_pct" )
-  local damageMax = spell:GetSpecialValueFor( "damage_gain_max" )
-
-  if IsServer() then
-    -- Talent that increases hp and dmg gain
-    local talent = parent:FindAbilityByName("special_bonus_clinkz_death_pact_oaa")
-    if talent then
-      if talent:GetLevel() > 0 then
-        healthPct = healthPct + talent:GetSpecialValueFor("value")
-        damagePct = damagePct + talent:GetSpecialValueFor("value2")
-        healthMax = healthMax + talent:GetSpecialValueFor("value3")
-        damageMax = damageMax + talent:GetSpecialValueFor("value4")
-      end
-    end
-  end
-
-  -- retrieve the stack count
-  local targetHealth = self:GetStackCount()
-
-  -- Calculate bonuses
-  self.health = targetHealth * healthPct * 0.01
-  self.damage = targetHealth * damagePct * 0.01
-
-  -- Cap the health bonus
-  if healthMax > 0 then
-    self.health = math.min( healthMax, self.health )
-  end
-
-  -- Cap the damage bonus
-  if damageMax > 0 then
-    self.damage = math.min( damageMax, self.damage )
-  end
-
-  if IsServer() then
     -- apply the new health and such
     parent:CalculateStatBonus(true)
 
     -- add the added health
-    --parent:SetHealth( self.parentHealth + self.health )
     parent:Heal(self.health, spell)
   end
 end
@@ -192,11 +134,11 @@ function modifier_clinkz_death_pact_oaa:DeclareFunctions()
 end
 
 --function modifier_clinkz_death_pact_oaa:GetModifierBaseAttack_BonusDamage()
-  --return self.damage
+  --return self:GetStackCount()
 --end
 
 function modifier_clinkz_death_pact_oaa:GetModifierPreAttack_BonusDamage()
-  return self.damage
+  return self:GetStackCount()
 end
 
 function modifier_clinkz_death_pact_oaa:GetModifierExtraHealthBonus()
