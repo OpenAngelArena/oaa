760cdf05c5aaddda76789b4c9eb21a1cc8da5787
Darko V <darko1290@gmail.com>
2021-05-12 22:50:15 +0200

    Ogre Tank boss changes
    * Ogre Tank boss cannot be disabled (stunned, silenced etc.) during the jump.
    * Fixed blood particle on the killed units by the Ogre not showing on the killed units. (They were showing in the center of the map.)
    * Fixed Ogre Tank boss not reacting to creeps that attack him.
    * Improved Ogre Tank boss and Ogre Seer AI a little bit.
    * Ogre Tank boss will not react to snipers but Ogre Seers will. Why? Ogre Tanks are stupid so they will not react to snipers unless they are within 1200 range of him.
    * Ogre Tank boss abilities will no longer stun spell-immune units.

--- game/scripts/vscripts/abilities/siltbreaker/npc_dota_creature_ogre_tank_boss/ogre_tank_boss_melee_smash_tier5.lua
+++ game/scripts/vscripts/abilities/siltbreaker/npc_dota_creature_ogre_tank_boss/ogre_tank_boss_melee_smash_tier5.lua
@@ -23,16 +23,11 @@ end
 -----------------------------------------------------------------------------
 
 function ogre_tank_boss_melee_smash_tier5:OnSpellStart()
-  if IsServer() then
-    local caster = self:GetCaster()
-		caster:EmitSound("OgreTank.Grunt")
-		local flSpeed = self:GetSpecialValueFor( "base_swing_speed" ) / self:GetPlaybackRateOverride()
-		local vToTarget = self:GetCursorPosition() - caster:GetOrigin()
-		vToTarget = vToTarget:Normalized()
-		local vTarget = caster:GetOrigin() + vToTarget * self:GetCastRange( caster:GetOrigin(), nil )
-		local hThinker = CreateModifierThinker( caster, self, "modifier_ogre_tank_melee_smash_thinker", { duration = flSpeed }, vTarget, caster:GetTeamNumber(), false )
-	end
+  local caster = self:GetCaster()
+  caster:EmitSound("OgreTank.Grunt")
+  local flSpeed = self:GetSpecialValueFor( "base_swing_speed" ) / self:GetPlaybackRateOverride()
+  local vToTarget = self:GetCursorPosition() - caster:GetOrigin()
+  vToTarget = vToTarget:Normalized()
+  local vTarget = caster:GetOrigin() + vToTarget * self:GetCastRange( caster:GetOrigin(), nil )
+  local hThinker = CreateModifierThinker( caster, self, "modifier_ogre_tank_melee_smash_thinker", { duration = flSpeed }, vTarget, caster:GetTeamNumber(), false )
 end
-
------------------------------------------------------------------------------
-
