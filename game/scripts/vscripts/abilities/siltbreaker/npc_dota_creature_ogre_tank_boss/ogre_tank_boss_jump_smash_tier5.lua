0eb12594aacdb0bf38e55e2c743aeb9f80e22ab1
Darko V <darko1290@gmail.com>
2021-05-15 00:59:10 +0200

    More Random fixes
    1) Mini Roshan fixes:
    * Changed soundset to something less loud.
    * Hopefully fixed Mini Roshan aggro to normal neutral aggro.
    * Improved BAT from 4 to 2.5
    * Increased Attack Acquisition range from 150 to 300.
    * Increased mana from 0 to 300.
    * Increased mana regen from 0 to 1.
    * Reduced day/night vision from 1400 to 800.
    * Disabled inventory on Mini-Roshans.
    2) Improved Lycan boss Claw Lunge code.
    3) Lycan Boss Rupture Ball impact sound is now audible only on real heroes and not on creeps.
    4) Fixed Tier 5 Ogre Tank Boss Jump not working.

--- game/scripts/vscripts/abilities/siltbreaker/npc_dota_creature_ogre_tank_boss/ogre_tank_boss_jump_smash_tier5.lua
+++ game/scripts/vscripts/abilities/siltbreaker/npc_dota_creature_ogre_tank_boss/ogre_tank_boss_jump_smash_tier5.lua
@@ -1,17 +1,17 @@
 LinkLuaModifier("modifier_anti_stun_oaa", "modifiers/modifier_anti_stun_oaa.lua", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_ogre_tank_melee_smash_thinker", "modifiers/modifier_ogre_tank_melee_smash_thinker.lua", LUA_MODIFIER_MOTION_NONE)
 
-ogre_tank_boss_jump_smash = class( AbilityBaseClass )
+ogre_tank_boss_jump_smash_tier5  = class( AbilityBaseClass )
 
-function ogre_tank_boss_jump_smash:ProcsMagicStick()
+function ogre_tank_boss_jump_smash_tier5:ProcsMagicStick()
   return false
 end
 
-function ogre_tank_boss_jump_smash:GetPlaybackRateOverride()
+function ogre_tank_boss_jump_smash_tier5:GetPlaybackRateOverride()
   return self:GetSpecialValueFor("jump_speed") / 1.5 -- 0.9 for 1.8, 0.7 for 1.5
 end
 
-function ogre_tank_boss_jump_smash:OnAbilityPhaseStart()
+function ogre_tank_boss_jump_smash_tier5:OnAbilityPhaseStart()
   if IsServer() then
     local caster = self:GetCaster()
     local delay = self:GetCastPoint()
@@ -21,7 +21,7 @@ function ogre_tank_boss_jump_smash:OnAbilityPhaseStart()
   return true
 end
 
-function ogre_tank_boss_jump_smash:OnSpellStart()
+function ogre_tank_boss_jump_smash_tier5:OnSpellStart()
   local caster = self:GetCaster()
   local hThinker = CreateModifierThinker(caster, self, "modifier_ogre_tank_melee_smash_thinker", { duration = self:GetSpecialValueFor( "jump_speed") }, caster:GetOrigin(), caster:GetTeamNumber(), false)
 end
