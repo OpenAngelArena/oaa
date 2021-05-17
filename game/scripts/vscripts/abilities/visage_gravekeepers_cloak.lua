926e4ae5b80f681428f6918cb54089903bbe801d
lovesmuffins <46329466+lovesmuffins@users.noreply.github.com>
2021-05-17 13:13:49 -0500

    Update visage_gravekeepers_cloak.lua

--- game/scripts/vscripts/abilities/visage_gravekeepers_cloak.lua
+++ game/scripts/vscripts/abilities/visage_gravekeepers_cloak.lua
@@ -8,11 +8,19 @@ function visage_gravekeepers_cloak_oaa:GetIntrinsicModifierName()
   return "modifier_visage_gravekeepers_cloak_oaa"
 end
 
-if IsServer() then
-  function visage_gravekeepers_cloak_oaa:OnUpgrade()
-    local caster = self:GetCaster()
-    local mod = caster:FindModifierByName("modifier_visage_gravekeepers_cloak_oaa")
-    mod:SetStackCount(self:GetSpecialValueFor("max_layers"))
+function visage_gravekeepers_cloak_oaa:OnUpgrade()
+  local caster = self:GetCaster()
+  local mod = caster:FindModifierByName("modifier_visage_gravekeepers_cloak_oaa")
+  local max_layers = self:GetSpecialValueFor("max_layers")
+  -- Talent that increases number of layers
+  local talent = caster:FindAbilityByName("special_bonus_unique_visage_5")
+  if talent then
+    if talent:GetLevel() > 0 then
+      max_layers = max_layers + talent:GetSpecialValueFor("value")
+    end
+  end
+  if mod then
+    mod:SetStackCount(max_layers)
   end
 end
 
@@ -20,9 +28,20 @@ end
 
 modifier_visage_gravekeepers_cloak_oaa = class(ModifierBaseClass)
 
-if IsServer() then
-  function modifier_visage_gravekeepers_cloak_oaa:OnCreated()
-    self:SetStackCount(self:GetAbility():GetSpecialValueFor("max_layers"))
+
+function modifier_visage_gravekeepers_cloak_oaa:OnCreated()
+  if IsServer() then
+    local caster = self:GetCaster()
+    local ability = self:GetAbility()
+    local max_layers = ability:GetSpecialValueFor("max_layers")
+    -- Talent that increases number of layers
+    local talent = caster:FindAbilityByName("special_bonus_unique_visage_5")
+    if talent then
+      if talent:GetLevel() > 0 then
+        max_layers = max_layers + talent:GetSpecialValueFor("value")
+      end
+    end
+    self:SetStackCount(max_layers)
   end
 end
 
@@ -41,9 +60,18 @@ if IsServer() then
   end
 
   function modifier_visage_gravekeepers_cloak_oaa:IncreaseStacks()
+    local caster = self:GetCaster()
     local ability = self:GetAbility()
     local stackCount = self:GetStackCount()
-    if stackCount < ability:GetSpecialValueFor("max_layers") then
+    local max_layers = ability:GetSpecialValueFor("max_layers")
+    -- Talent that increases number of layers
+    local talent = caster:FindAbilityByName("special_bonus_unique_visage_5")
+    if talent then
+      if talent:GetLevel() > 0 then
+        max_layers = max_layers + talent:GetSpecialValueFor("value")
+      end
+    end
+    if stackCount < max_layers then
       self:SetStackCount(stackCount + 1)
     end
   end
@@ -52,14 +80,14 @@ if IsServer() then
     local ability = self:GetAbility()
     local caster = self:GetCaster()
     local stackCount = self:GetStackCount()
-    local damageReduction = math.min(100, ability:GetSpecialValueFor("damage_reduction") * stackCount)
+    local damageReduction = math.min(80, ability:GetSpecialValueFor("damage_reduction") * stackCount)
     local damageThreshold = ability:GetSpecialValueFor("minimum_damage")
     local recovery_time = ability:GetSpecialValueFor("recovery_time")
     -- Talent that decreases recovery time
     if caster:HasLearnedAbility("special_bonus_unique_visage_oaa_5") then
       recovery_time = recovery_time - caster:FindAbilityByName("special_bonus_unique_visage_oaa_5"):GetSpecialValueFor("value")
     end
-    if keys.attacker:GetTeam() ~= caster:GetTeam() and (keys.attacker:GetTeam() == DOTA_TEAM_GOODGUYS or keys.attacker:GetTeam() == DOTA_TEAM_BADGUYS) then
+    if keys.attacker:GetTeam() ~= caster:GetTeam() and keys.attacker:GetTeam() ~= DOTA_TEAM_NEUTRALS then
       if keys.damage > damageThreshold then
         self:DecreaseStacks()
         Timers:CreateTimer(recovery_time, function()
@@ -87,11 +115,11 @@ function modifier_visage_gravekeepers_cloak_oaa:IsAura()
 end
 
 function modifier_visage_gravekeepers_cloak_oaa:GetAuraSearchType()
-  return bit.bor(DOTA_UNIT_TARGET_ALL)
+  return DOTA_UNIT_TARGET_ALL
 end
 
 function modifier_visage_gravekeepers_cloak_oaa:GetAuraSearchFlags()
-  return bit.bor(DOTA_UNIT_TARGET_FLAG_PLAYER_CONTROLLED)
+  return DOTA_UNIT_TARGET_FLAG_PLAYER_CONTROLLED
 end
 
 function modifier_visage_gravekeepers_cloak_oaa:GetAuraSearchTeam()
@@ -122,7 +150,12 @@ end
 
 function modifier_visage_gravekeepers_cloak_oaa_aura:GetModifierTotal_ConstantBlock(keys)
   local caster = self:GetCaster()
-  local stackCount = caster:FindModifierByName('modifier_visage_gravekeepers_cloak_oaa'):GetStackCount()
-  local damageReduction = math.min(100, self:GetAbility():GetSpecialValueFor("damage_reduction") * stackCount)
-  return keys.damage * damageReduction / 100
+  local mod = caster:FindModifierByName("modifier_visage_gravekeepers_cloak_oaa")
+  if mod then
+    local stackCount = mod:GetStackCount()
+    local damageReduction = math.min(80, self:GetAbility():GetSpecialValueFor("damage_reduction") * stackCount)
+    return keys.damage * damageReduction / 100
+  end
+
+  return 0
 end
