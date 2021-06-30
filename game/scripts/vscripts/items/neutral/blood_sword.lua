b691295b27aab892be058abbfd0bb2177b696367
Darko V <darko1290@gmail.com>
2021-07-01 01:44:35 +0200

    Neutral items update (#3147)
    * Neutral Items changes (work in progress)
    
    * Max number of items per tier increased from 4 to 5.
    * Removed Fairy's Trinket
    * Reworked Dragon Scale - now deals 32 damage to the attacker every time the owner takes damage.
    * Removed Craggy Coat
    * Titan Sliver is now a tier 3 neutral item again.
    * Tier 4 neutral items now start dropping at 30 minutes instead at 35.
    * Penta-Edged Sword reworked into Blood Sword - similar stats, has an active that lets you proc your attack on any target (ally or enemy) and lifesteal from that attack. 25s cd. Cast range equal to owner's attack range.
    * Added new Tier 4 item - Rune Breaker - provides attack speed, armor, bonus dmg during duels and has an active that lets you purge buffs on enemies.
    * Removed Pirate Hat.
    * Creed of Omniscience reworked - no longer gives bonus attack range and bonus attack projectile speed, now provides bonus movement speed, bonus turn rate and bonus intelligence instead.
    * Demon Stone damage penalty reduced from -100 to -30.
    * Demon Stone bonus XP per minute increased from 100 to 1000.
    * Demon Stone now also has attack speed penatly of -30.
    * Demon Stone now has an active - summons a demon with true sight. 60s cd.
    
    * Tooltips and fixes
    
    * Added tooltips
    * Rune Breaker now also applies break for 4 seconds.
    * Dragon Scale return damage increased from 32 to 50.
    * Tweaked Demon Stone summon stats.
    * Demon Stone summon duration reduced from 50 to 40 seconds.
    
    * Update tooltip_rune_breaker.txt
    
    * Minor Fixes
    
    * Added a 0.5s cd to Dragon Scale proc
    * Minor indentation fixes
    
    * Demon Stone nerfs
    
    * Cooldown increased from 60 to 80 seconds.
    * Bonus XPPM reduced from 1000 to 666.
    * Damage penalty increased from -30 to -60.
    * Attack speed penalty increased from -30 to -35.
    * Demon's HP reduced from 1000 to 900.
    * Demon's Movement Speed reduced from 400 to 370.
    * Demon's Attack Range reduced from 200 to 150.
    * Demon's Gold and XP bounty increased from 100 to 150.
    * Demon's Day Vision range reduced from 1400 to 1200.
    * Demon's True-Sight range reduced from 1400/800 during day/night to constant 800.

---
+++ game/scripts/vscripts/items/neutral/blood_sword.lua
@@ -0,0 +1,159 @@
+LinkLuaModifier("modifier_item_blood_sword_passive", "items/neutral/blood_sword.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_blood_sword_lifesteal", "items/neutral/blood_sword.lua", LUA_MODIFIER_MOTION_NONE)
+
+item_blood_sword = class(ItemBaseClass)
+
+function item_blood_sword:GetCastRange(location, target)
+  return self:GetCaster():GetAttackRange()
+end
+
+function item_blood_sword:GetIntrinsicModifierName()
+  return "modifier_item_blood_sword_passive"
+end
+
+function item_blood_sword:OnSpellStart()
+  local caster = self:GetCaster()
+  local target = self:GetCursorTarget()
+
+  -- Don't do anything if target has Linken's effect
+  if target:TriggerSpellAbsorb(self) then
+    return
+  end
+
+  -- Add a lifesteal buff before the instant attack
+  local buff = caster:AddNewModifier(caster, self, "modifier_item_blood_sword_lifesteal", {})
+
+  -- Instant attack
+  caster:PerformAttack(target, true, true, true, false, false, false, true)
+
+  -- Remove lifesteal buff when instant attack is over
+  buff:Destroy()
+
+  -- Particle
+  local particle = ParticleManager:CreateParticle("particles/items3_fx/iron_talon_active.vpcf", PATTACH_ABSORIGIN, target)
+  ParticleManager:SetParticleControl(particle, 1, target:GetAbsOrigin())
+  ParticleManager:ReleaseParticleIndex(particle)
+
+  -- Sound
+  caster:EmitSound("DOTA_Item.IronTalon.Activate")
+end
+
+function item_blood_sword:ProcsMagicStick()
+  return false
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_item_blood_sword_passive = class(ModifierBaseClass)
+
+function modifier_item_blood_sword_passive:IsHidden()
+  return true
+end
+function modifier_item_blood_sword_passive:IsDebuff()
+  return false
+end
+function modifier_item_blood_sword_passive:IsPurgable()
+  return false
+end
+
+function modifier_item_blood_sword_passive:OnCreated()
+  local ability = self:GetAbility()
+  if ability and not ability:IsNull() then
+    self.dmg = ability:GetSpecialValueFor("bonus_damage")
+    self.attack_range_melee = ability:GetSpecialValueFor("bonus_attack_range_melee")
+  end
+end
+
+function modifier_item_blood_sword_passive:OnRefresh()
+  local ability = self:GetAbility()
+  if ability and not ability:IsNull() then
+    self.dmg = ability:GetSpecialValueFor("bonus_damage")
+    self.attack_range_melee = ability:GetSpecialValueFor("bonus_attack_range_melee")
+  end
+end
+
+function modifier_item_blood_sword_passive:DeclareFunctions()
+  return {
+    MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
+    MODIFIER_PROPERTY_ATTACK_RANGE_BONUS,
+  }
+end
+
+function modifier_item_blood_sword_passive:GetModifierPreAttack_BonusDamage()
+  return self.dmg or self:GetAbility():GetSpecialValueFor("bonus_damage")
+end
+
+function modifier_item_blood_sword_passive:GetModifierAttackRangeBonus()
+  if not self:GetParent():IsRangedAttacker() then
+    return self.attack_range_melee or self:GetAbility():GetSpecialValueFor("bonus_attack_range_melee")
+  end
+
+  return 0
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_item_blood_sword_lifesteal = class(ModifierBaseClass)
+
+function modifier_item_blood_sword_lifesteal:IsHidden()
+  return true
+end
+
+function modifier_item_blood_sword_lifesteal:IsDebuff()
+  return false
+end
+
+function modifier_item_blood_sword_lifesteal:IsPurgable()
+  return false
+end
+
+function modifier_item_blood_sword_lifesteal:DeclareFunctions()
+  local funcs = {
+    MODIFIER_EVENT_ON_ATTACK_LANDED,
+  }
+  return funcs
+end
+
+function modifier_item_blood_sword_lifesteal:OnAttackLanded(event)
+  if not IsServer() then
+    return
+  end
+
+  local parent = self:GetParent()
+  local ability = self:GetAbility()
+  local attacker = event.attacker
+  local damage = event.damage
+
+  if attacker ~= parent or damage < 0 then
+    return
+  end
+
+  if not attacker or attacker:IsNull() or not ability or ability:IsNull() then
+    return
+  end
+
+  if event.damage_category ~= DOTA_DAMAGE_CATEGORY_ATTACK then
+    return
+  end
+
+  local parent_team = parent:GetTeamNumber()
+  local target = event.unit
+
+  local ufResult = UnitFilter(
+    target,
+    DOTA_UNIT_TARGET_TEAM_BOTH,
+    bit.bor(DOTA_UNIT_TARGET_BASIC, DOTA_UNIT_TARGET_HERO),
+    bit.bor(DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, DOTA_UNIT_TARGET_FLAG_DEAD),
+    parent_team
+  )
+
+  local lifesteal_percent = ability:GetSpecialValueFor("active_lifesteal_percent")
+
+  if ufResult == UF_SUCCESS and lifesteal_percent > 0 and parent:IsAlive() then
+    parent:Heal(damage * lifesteal_percent * 0.01, parent)
+
+    local part = ParticleManager:CreateParticle("particles/generic_gameplay/generic_lifesteal.vpcf", PATTACH_ABSORIGIN_FOLLOW, parent)
+    ParticleManager:SetParticleControl(part, 0, parent:GetAbsOrigin())
+    ParticleManager:ReleaseParticleIndex(part)
+  end
+end
---
+++ game/scripts/vscripts/items/neutral/rune_breaker.lua
@@ -0,0 +1,143 @@
+LinkLuaModifier("modifier_item_rune_breaker_oaa_passive", "items/neutral/rune_breaker.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_rune_breaker_oaa_debuff", "items/neutral/rune_breaker.lua", LUA_MODIFIER_MOTION_NONE)
+
+item_rune_breaker_oaa = class(ItemBaseClass)
+
+function item_rune_breaker_oaa:GetIntrinsicModifierName()
+  return "modifier_item_rune_breaker_oaa_passive"
+end
+
+function item_rune_breaker_oaa:OnSpellStart()
+  local caster = self:GetCaster()
+  local target = self:GetCursorTarget()
+
+  -- Don't do anything if target has Linken's effect
+  if target:TriggerSpellAbsorb(self) then
+    return
+  end
+
+  -- Basic Dispel
+  local RemovePositiveBuffs = true
+  local RemoveDebuffs = false
+  local BuffsCreatedThisFrameOnly = false
+  local RemoveStuns = false
+  local RemoveExceptions = false
+
+  target:Purge(RemovePositiveBuffs, RemoveDebuffs, BuffsCreatedThisFrameOnly, RemoveStuns, RemoveExceptions)
+
+  -- Apply break debuff (duration is not affected by status resistance)
+  local debuff_duration = self:GetSpecialValueFor("duration")
+  target:AddNewModifier(caster, self, "modifier_item_rune_breaker_oaa_debuff", {duration = debuff_duration})
+
+  -- Particle
+  --local particle_name = ""
+  --local particle = ParticleManager:CreateParticle(particle_name, PATTACH_ABSORIGIN, target)
+  --ParticleManager:SetParticleControl(particle, 1, target:GetAbsOrigin())
+  --ParticleManager:ReleaseParticleIndex(particle)
+
+  -- Sound
+  --caster:EmitSound("")
+end
+
+function item_rune_breaker_oaa:ProcsMagicStick()
+  return false
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_item_rune_breaker_oaa_passive = class(ModifierBaseClass)
+
+function modifier_item_rune_breaker_oaa_passive:IsHidden()
+  return true
+end
+function modifier_item_rune_breaker_oaa_passive:IsDebuff()
+  return false
+end
+function modifier_item_rune_breaker_oaa_passive:IsPurgable()
+  return false
+end
+
+function modifier_item_rune_breaker_oaa_passive:OnCreated()
+  local ability = self:GetAbility()
+  if ability and not ability:IsNull() then
+    self.attack_speed = ability:GetSpecialValueFor("bonus_attack_speed")
+    self.armor = ability:GetSpecialValueFor("bonus_armor")
+    self.dmg = ability:GetSpecialValueFor("bonus_damage_during_duels")
+  end
+
+  if IsServer() then
+    self:StartIntervalThink(0)
+  end
+end
+
+function modifier_item_rune_breaker_oaa_passive:OnRefresh()
+  local ability = self:GetAbility()
+  if ability and not ability:IsNull() then
+    self.attack_speed = ability:GetSpecialValueFor("bonus_attack_speed")
+    self.armor = ability:GetSpecialValueFor("bonus_armor")
+    self.dmg = ability:GetSpecialValueFor("bonus_damage_during_duels")
+  end
+end
+
+function modifier_item_rune_breaker_oaa_passive:OnIntervalThink()
+  if Duels:IsActive() then
+    self:SetStackCount(1)
+  else
+    self:SetStackCount(2)
+  end
+end
+
+function modifier_item_rune_breaker_oaa_passive:DeclareFunctions()
+  return {
+    MODIFIER_PROPERTY_PHYSICAL_ARMOR_BONUS,
+    MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
+    MODIFIER_PROPERTY_BASEDAMAGEOUTGOING_PERCENTAGE,
+  }
+end
+
+function modifier_item_rune_breaker_oaa_passive:GetModifierPhysicalArmorBonus()
+  return self.armor or self:GetAbility():GetSpecialValueFor("bonus_armor")
+end
+
+function modifier_item_rune_breaker_oaa_passive:GetModifierAttackSpeedBonus_Constant()
+  return self.attack_speed or self:GetAbility():GetSpecialValueFor("bonus_attack_speed")
+end
+
+function modifier_item_rune_breaker_oaa_passive:GetModifierBaseDamageOutgoing_Percentage()
+  if self:GetStackCount() == 1 then
+    return self.dmg or self:GetAbility():GetSpecialValueFor("bonus_damage_during_duels")
+  end
+
+  return 0
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_item_rune_breaker_oaa_debuff = class(ModifierBaseClass)
+
+function modifier_item_rune_breaker_oaa_debuff:IsHidden()
+  return false
+end
+
+function modifier_item_rune_breaker_oaa_debuff:IsDebuff()
+  return true
+end
+
+function modifier_item_rune_breaker_oaa_debuff:IsPurgable()
+  return false
+end
+
+function modifier_item_rune_breaker_oaa_debuff:CheckState()
+  local state = {
+    [MODIFIER_STATE_PASSIVES_DISABLED] = true,
+  }
+  return state
+end
+
+function modifier_item_rune_breaker_oaa_debuff:GetEffectName()
+  return "particles/items3_fx/silver_edge.vpcf"
+end
+
+function modifier_item_rune_breaker_oaa_debuff:GetEffectAttachType()
+  return PATTACH_ABSORIGIN_FOLLOW
+end
