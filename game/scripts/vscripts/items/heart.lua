aabb090ec511b28ec0d1c815014c70dd3d1fa048
Darko V <darko1290@gmail.com>
2021-08-04 01:31:09 +0200

    oops

--- game/scripts/vscripts/items/heart.lua
+++ game/scripts/vscripts/items/heart.lua
@@ -217,12 +217,12 @@ function modifier_item_heart_oaa_active:OnCreated()
     self.bonus_damage = ability:GetSpecialValueFor("buff_bonus_base_damage")
   end
 
-  if IsServer() then
-    if self.nPreviewFX == nil then
-      self.nPreviewFX = ParticleManager:CreateParticle("", PATTACH_ABSORIGIN_FOLLOW, parent)
-      ParticleManager:SetParticleControlEnt(self.nPreviewFX, 0, parent, PATTACH_ABSORIGIN_FOLLOW, nil, parent:GetOrigin(), true)
-    end
-  end
+  -- if IsServer() then
+    -- if self.nPreviewFX == nil then
+      -- self.nPreviewFX = ParticleManager:CreateParticle("", PATTACH_ABSORIGIN_FOLLOW, parent)
+      -- ParticleManager:SetParticleControlEnt(self.nPreviewFX, 0, parent, PATTACH_ABSORIGIN_FOLLOW, nil, parent:GetOrigin(), true)
+    -- end
+  -- end
 
   if IsServer() and parent:IsHero() then
     parent:CalculateStatBonus(true)
@@ -242,15 +242,15 @@ function modifier_item_heart_oaa_active:OnRefresh()
   end
 end
 
-function modifier_item_heart_oaa_active:OnDestroy()
-  if IsServer() then
-    if self.nPreviewFX then
-      ParticleManager:DestroyParticle(self.nPreviewFX, false)
-      ParticleManager:ReleaseParticleIndex(self.nPreviewFX)
-      self.nPreviewFX = nil
-    end
-  end
-end
+-- function modifier_item_heart_oaa_active:OnDestroy()
+  -- if IsServer() then
+    -- if self.nPreviewFX then
+      -- ParticleManager:DestroyParticle(self.nPreviewFX, false)
+      -- ParticleManager:ReleaseParticleIndex(self.nPreviewFX)
+      -- self.nPreviewFX = nil
+    -- end
+  -- end
+-- end
 
 function modifier_item_heart_oaa_active:DeclareFunctions()
   return {
