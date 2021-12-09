43fba786892e4589d5d9f46bee57beb8f4333f1d
Darko V <darko1290@gmail.com>
2021-12-09 02:59:16 +0100

    Duel Runes Update (#3252)
    * Duel Runes update
    
    * Duel rune triggers/zones will no longer be active when duels are not in progress.
    * Disabled all modifier code related to duel runes working clientside.
    * Duel Rune counter will not tick if the unit on the trigger/zone is invulnerable or banished.
    * Triple Damage now requires 150 stacks instead of 130.
    * Triple Damage is now undispellable.
    * Triple Damage now dispels Double Damage buff to avoid confusion of having 2 buffs that basically do the same thing.
    * New Duel Rune: Arcane Rune, needs 130 stacks.
    * New Duel Rune: Super Sight, needs 200 stacks. Super Sight provides true sight and unobstructed vision.
    * Duel Runes duration reduced from 60 to 50 seconds.
    * Duel Runes counter is no longer reset when taking damage from neutrals.
    
    * Tooltips and particles
    
    * Updated Duel Runes tooltips - it's not high ground advantage in every duel arena.
    * Added particles to Triple Damage and Super Sight buffs.

---
+++ game/scripts/vscripts/modifiers/modifier_rune_hill_super_sight.lua
@@ -0,0 +1,74 @@
+
+modifier_rune_hill_super_sight = class(ModifierBaseClass)
+
+function modifier_rune_hill_super_sight:IsHidden()
+  return false
+end
+
+function modifier_rune_hill_super_sight:IsPurgable()
+  return false
+end
+
+function modifier_rune_hill_super_sight:GetEffectName()
+  return "particles/econ/courier/courier_greevil_white/courier_greevil_white_ambient_3.vpcf"
+end
+
+function modifier_rune_hill_super_sight:GetEffectAttachType()
+  return PATTACH_ABSORIGIN_FOLLOW
+end
+
+function modifier_rune_hill_super_sight:GetTexture()
+  return "item_gem"
+end
+
+function modifier_rune_hill_super_sight:OnCreated()
+  if not IsServer() then
+    return
+  end
+  self:StartIntervalThink(0.1)
+end
+
+function modifier_rune_hill_super_sight:OnIntervalThink()
+  if not IsServer() then
+    return
+  end
+
+  if not Duels:IsActive() then
+    self:StartIntervalThink(-1)
+    self:Destroy()
+    return
+  end
+end
+
+-- Flying Vision
+function modifier_rune_hill_super_sight:CheckState()
+  local state = {
+    [MODIFIER_STATE_FORCED_FLYING_VISION] = true,
+  }
+  return state
+end
+
+-- TrueSight part:
+function modifier_rune_hill_super_sight:IsAura()
+  return true
+end
+
+function modifier_rune_hill_super_sight:GetAuraSearchTeam()
+  return DOTA_UNIT_TARGET_TEAM_ENEMY
+end
+
+function modifier_rune_hill_super_sight:GetAuraSearchType()
+  return DOTA_UNIT_TARGET_ALL
+end
+
+function modifier_rune_hill_super_sight:GetAuraSearchFlags()
+  return bit.bor(DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES, DOTA_UNIT_TARGET_FLAG_INVULNERABLE)
+end
+
+function modifier_rune_hill_super_sight:GetModifierAura()
+  return "modifier_truesight"
+end
+
+function modifier_rune_hill_super_sight:GetAuraRadius()
+  return 800
+end
--- game/scripts/vscripts/modifiers/modifier_rune_hill_tripledamage.lua
+++ game/scripts/vscripts/modifiers/modifier_rune_hill_tripledamage.lua
@@ -1,6 +1,39 @@
 
 modifier_rune_hill_tripledamage = class(ModifierBaseClass)
 
+function modifier_rune_hill_tripledamage:IsHidden()
+  return false
+end
+
+function modifier_rune_hill_tripledamage:IsPurgable()
+  return false
+end
+
+function modifier_rune_hill_tripledamage:OnCreated()
+  if not IsServer() then
+    return
+  end
+  self:StartIntervalThink(0.1)
+end
+
+function modifier_rune_hill_tripledamage:OnIntervalThink()
+  if not IsServer() then
+    return
+  end
+
+  if not Duels:IsActive() then
+    self:StartIntervalThink(-1)
+    self:Destroy()
+    return
+  end
+
+  local parent = self:GetParent()
+
+  if parent:HasModifier("modifier_rune_doubledamage") then
+    parent:RemoveModifierByName("modifier_rune_doubledamage")
+  end
+end
+
 function modifier_rune_hill_tripledamage:DeclareFunctions()
   return {
     MODIFIER_PROPERTY_BASEDAMAGEOUTGOING_PERCENTAGE
@@ -8,7 +41,15 @@ function modifier_rune_hill_tripledamage:DeclareFunctions()
 end
 
 function modifier_rune_hill_tripledamage:GetModifierBaseDamageOutgoing_Percentage()
-  return 100
+  return 200
+end
+
+function modifier_rune_hill_tripledamage:GetEffectName()
+  return "particles/generic_gameplay/rune_doubledamage_owner.vpcf"
+end
+
+function modifier_rune_hill_tripledamage:GetEffectAttachType()
+  return PATTACH_ABSORIGIN_FOLLOW
 end
 
 function modifier_rune_hill_tripledamage:GetTexture()
