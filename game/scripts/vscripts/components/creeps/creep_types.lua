9f296fa9ebca40cc9bb400d21790353b81dddf3a
Darko V <darko1290@gmail.com>
2022-01-15 23:06:06 +0100

    Update creep_types.lua

--- game/scripts/vscripts/components/creeps/creep_types.lua
+++ game/scripts/vscripts/components/creeps/creep_types.lua
@@ -1,7 +1,7 @@
 -- These values are starting and minimum values for neutral creeps when 5vs5; values increase over time (check creep_power.lua)
 -- "creep name", Health, Mana, Damage, Armor, Gold Bounty, Exp Bounty
 CreepTypes = {
-  -- 1 "easy camp"
+  -- 1 "easy camp" (CreepMax is 10 for all easy camps)
   {
     {                                              --HP   MANA  DMG   ARM   GOLD  EXP
       {"npc_dota_neutral_custom_big_wolf",          480,  150,  35,   1.5,   30,  40}, -- expected gold is 70 and XP is 90
@@ -14,7 +14,7 @@ CreepTypes = {
       {"npc_dota_neutral_custom_kobold",            250,    0,  10,   0.5,   15,  25}
     },
   },
-    -- 2 "medium camp"
+    -- 2 "medium camp" (CreepMax is 8 for all medium camps)
   {
     {                                              --HP   MANA  DMG   ARM   GOLD  EXP
       {"npc_dota_neutral_custom_harpy_storm",       650,  300,  40,   1.3,   35,  82}, -- expected gold is 60 and XP is 143
@@ -29,7 +29,7 @@ CreepTypes = {
       {"npc_dota_neutral_custom_blue_potato",       400,    0,  35,   1.3,   25,  61}
     }
   },
-    -- 3 "hard camp"
+    -- 3 "hard camp" (CreepMax is 10 for all hard camps)
   {
     {                                              --HP   MANA  DMG   ARM   GOLD  EXP
       {"npc_dota_neutral_custom_ghost",             800,    0,  40,   1.5,   50,  50}, -- expected gold is 100 and XP 100
@@ -47,7 +47,7 @@ CreepTypes = {
       {"npc_dota_neutral_satyr_trickster",          300,  500,  10,    1,    20,  25}
     }
   },
-   -- 4 "ancient camp"
+   -- 4 "ancient camp" (CreepMax is 8 for ancient camps)
   {
     {                                               --HP  MANA  DMG   ARM   GOLD  EXP
       {"npc_dota_neutral_granite_golem",           1275,    0,  50,    2,   120,  120} -- expected gold is 120 and XP is 120
@@ -59,19 +59,19 @@ CreepTypes = {
       {"npc_dota_neutral_black_dragon",            1275,  500,  80,    3,   120, 120}
     }
   },
-   -- 5 "solo ancient corner camp"
+   -- 5 "solo ancient corner camp" (CreepMax is 1)
   {
     {
       {"npc_dota_neutral_custom_black_dragon",     1500,  300,  80,    3,   100, 150}
     }
   },
-   -- 6 "solo ancient mid camp"
+   -- 6 "solo ancient mid camp" (CreepMax is 1)
   {
     {
       {"npc_dota_mini_roshan",                     1500,  300,  80,    3,   120, 25}
     }
   },
-   -- 7 Solo Prowler
+   -- 7 "solo prowler - part of the ancient camp" (CreepMax is 1)
   {
     {
       {"npc_dota_neutral_prowler_shaman",          1275,  400,  80,    3,   100, 120}
