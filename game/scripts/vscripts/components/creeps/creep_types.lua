1640e4aa4d2c255f1ae2c21d5c0fd6225d025c7e
Darko V <darko1290@gmail.com>
2022-01-15 22:34:35 +0100

    Creep update
    * Mud Golems now spawn in pairs - prevents the chance for medium camps to give way too much experience if multiple golems spawn. (Adjusted Mud Golems gold and experience accordingly.)
    * Reduced experience bounty of Hard camps by 17%.
    * Ghosts no longer spawn in pairs. Now it only spawns one Ghost and 2 small ghost-like creeps without abilities. prevents the chance for hard camps to give way too much gold and experience if multiple ghosts spawn.
    * Reduced experience bounty of Ancient camps by 14%. (Prowler experience bounty reduced by 20%)
    * Mini Roshan experience bounty reduced by 84%.
    * Mini Roshan gold bounty increased by 20%.

--- game/scripts/vscripts/components/creeps/creep_types.lua
+++ game/scripts/vscripts/components/creeps/creep_types.lua
@@ -21,7 +21,8 @@ CreepTypes = {
       {"npc_dota_neutral_custom_harpy_scout",       400,    0,  30,     1,   25,  61}
     },
     {
-      {"npc_dota_neutral_custom_mud_golem",         650,    0,  35,    1,    30,  57} -- multiply gold value by 2 and xp value by 2.5 because they split
+      {"npc_dota_neutral_custom_mud_golem",         525,    0,  35,    1,    15,  29}, -- multiply gold value by 2 and xp value by 2.5 because they split
+      {"npc_dota_neutral_custom_mud_golem",         525,    0,  35,    1,    15,  29}
     },
     {
       {"npc_dota_neutral_custom_blue_tomato",       650,  300,  40,   1.3,   35,  82},
@@ -31,30 +32,31 @@ CreepTypes = {
     -- 3 "hard camp"
   {
     {                                              --HP   MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_custom_ghost",             800,    0,  35,   1.5,   50,  60}, -- expected gold is 100 and XP 120
-      {"npc_dota_neutral_custom_ghost",             800,    0,  35,   1.5,   50,  60}
+      {"npc_dota_neutral_custom_ghost",             800,    0,  40,   1.5,   50,  50}, -- expected gold is 100 and XP 100
+      {"npc_dota_neutral_fel_beast",                400,    0,  30,    1,    25,  25},
+      {"npc_dota_neutral_fel_beast",                400,    0,  30,    1,    25,  25}
     },
     {
-      {"npc_dota_neutral_custom_centaur_khan",      800,  300,  50,   1.5,   50,  60},
-      {"npc_dota_neutral_custom_small_centaur",     400,    0,  30,    1,    25,  30},
-      {"npc_dota_neutral_custom_small_centaur",     400,    0,  30,    1,    25,  30}
+      {"npc_dota_neutral_custom_centaur_khan",      800,  300,  50,   1.5,   50,  50},
+      {"npc_dota_neutral_custom_small_centaur",     400,    0,  30,    1,    25,  25},
+      {"npc_dota_neutral_custom_small_centaur",     400,    0,  30,    1,    25,  25}
     },
     {
-      {"npc_dota_neutral_satyr_hellcaller",         800,  400,  50,   1.5,   50,  53},
-      {"npc_dota_neutral_satyr_soulstealer",        500,  600,  30,    1,    30,  40},
-      {"npc_dota_neutral_satyr_trickster",          300,  500,  10,    1,    20,  27}
+      {"npc_dota_neutral_satyr_hellcaller",         800,  400,  50,   1.5,   50,  50},
+      {"npc_dota_neutral_satyr_soulstealer",        500,  600,  30,    1,    30,  30},
+      {"npc_dota_neutral_satyr_trickster",          300,  500,  10,    1,    20,  25}
     }
   },
    -- 4 "ancient camp"
   {
     {                                               --HP  MANA  DMG   ARM   GOLD  EXP
-      {"npc_dota_neutral_granite_golem",           1275,    0,  50,    2,   120,  140} -- expected gold is 120 and XP is 140
+      {"npc_dota_neutral_granite_golem",           1275,    0,  50,    2,   120,  120} -- expected gold is 120 and XP is 120
     },
     {
-      {"npc_dota_neutral_rock_golem",              1200,    0,  40,    1,    50,  140}
+      {"npc_dota_neutral_rock_golem",              1200,    0,  40,    1,    50,  120}
     },
     {
-      {"npc_dota_neutral_black_dragon",            1275,  500,  80,    3,   120, 140}
+      {"npc_dota_neutral_black_dragon",            1275,  500,  80,    3,   120, 120}
     }
   },
    -- 5 "solo ancient corner camp"
@@ -66,13 +68,13 @@ CreepTypes = {
    -- 6 "solo ancient mid camp"
   {
     {
-      {"npc_dota_mini_roshan",                     1500,  300,  80,    3,   100, 150}
+      {"npc_dota_mini_roshan",                     1500,  300,  80,    3,   120, 25}
     }
   },
    -- 7 Solo Prowler
   {
     {
-      {"npc_dota_neutral_prowler_shaman",          1275,  400,  80,    3,   100, 150}
+      {"npc_dota_neutral_prowler_shaman",          1275,  400,  80,    3,   100, 120}
     }
   }
 }
