ec4c9c97ec9965254e6cb362b9246483cab23eff
Darko V <darko1290@gmail.com>
2022-02-03 00:32:27 +0100

    Game options update (#3297)
    * Draft version
    
    * small update
    
    * Work in Progress
    
    * Minor tweaks
    
    * Update
    
    * Make everything visible
    * Add localization strings
    * Add icons to every modifier
    
    * nya

---
+++ game/scripts/vscripts/modifiers/funmodifiers/modifier_troll_switch_oaa.lua
@@ -0,0 +1,147 @@
+modifier_troll_switch_oaa = class(ModifierBaseClass)
+
+function modifier_troll_switch_oaa:IsHidden()
+  return false
+end
+
+function modifier_troll_switch_oaa:IsPurgable()
+  return true
+end
+
+function modifier_troll_switch_oaa:RemoveOnDeath()
+  return false
+end
+
+function modifier_troll_switch_oaa:OnCreated()
+  self.atkRange = 600
+  self.projectileSpeed = 900
+  self.bonus_health_per_lvl = 50
+  self.bonus_attack_speed_per_lvl = 5
+
+  if not IsServer() then
+    return
+  end
+
+  local parent = self:GetParent()
+
+  -- Check if parent has Berserkers Rage,
+  if parent:HasAbility("troll_warlord_berserkers_rage") then
+    return
+  end
+
+  if parent:IsRangedAttacker() then
+    -- Parent is ranged -> turn to melee
+    parent:SetAttackCapability(DOTA_UNIT_CAP_MELEE_ATTACK)
+    self.set_attack_capability = DOTA_UNIT_CAP_MELEE_ATTACK
+  elseif parent:HasAttackCapability() then
+    -- Parent is melee -> turn to ranged
+    parent:SetAttackCapability(DOTA_UNIT_CAP_RANGED_ATTACK)
+    -- Change attack projectile only if parent doesn't have Metamorphosis and Dragon Form
+    if not parent:HasAbility("dragon_knight_elder_dragon_form") and not parent:HasAbility("dragon_knight_elder_dragon_form_oaa") and not parent:HasAbility("terrorblade_metamorphosis") then
+      parent:SetRangedProjectileName("particles/base_attacks/ranged_tower_good.vpcf")
+    end
+    self.set_attack_capability = DOTA_UNIT_CAP_RANGED_ATTACK
+  else
+    self.set_attack_capability = DOTA_UNIT_CAP_NO_ATTACK
+  end
+
+  self:StartIntervalThink(0)
+end
+
+function modifier_troll_switch_oaa:OnIntervalThink()
+  if not IsServer() then
+    return
+  end
+
+  local parent = self:GetParent()
+
+  -- Check if parent has True Form
+  if parent:HasModifier("modifier_lone_druid_true_form") and self.set_attack_capability == DOTA_UNIT_CAP_MELEE_ATTACK and not parent:IsRangedAttacker() then
+    parent:SetAttackCapability(DOTA_UNIT_CAP_RANGED_ATTACK)
+    return
+  end
+
+  if self.set_attack_capability ~= parent:GetAttackCapability() and not parent:HasModifier("modifier_lone_druid_true_form") then
+    parent:SetAttackCapability(self.set_attack_capability)
+  end
+end
+
+function modifier_troll_switch_oaa:DeclareFunctions()
+  local funcs = {
+    MODIFIER_PROPERTY_ATTACK_RANGE_BONUS,
+    MODIFIER_PROPERTY_PROJECTILE_SPEED_BONUS,
+    MODIFIER_PROPERTY_HEALTH_BONUS,
+    MODIFIER_PROPERTY_ATTACKSPEED_BONUS_CONSTANT,
+  }
+
+  return funcs
+end
+
+
+function modifier_troll_switch_oaa:GetModifierAttackRangeBonus()
+  local parent = self:GetParent()
+  if parent:IsRangedAttacker() then
+    return self.atkRange
+  else
+    if self.ar_lock then
+      return 0
+    else
+      self.ar_lock = true
+      local attack_range = parent:GetAttackRange()
+      self.ar_lock = false
+      if attack_range > self.atkRange then
+        return (self.atkRange / 2) - attack_range
+      else
+        return (self.atkRange / 4) - attack_range
+      end
+    end
+  end
+
+  return 0
+end
+
+function modifier_troll_switch_oaa:GetModifierProjectileSpeedBonus()
+  local parent = self:GetParent()
+  if not IsServer() or parent:HasModifier("modifier_item_princes_knife") then
+    return 0
+  end
+
+  if self.ps_lock then
+    return 0
+  else
+    self.ps_lock = true
+    local projectile_speed = parent:GetProjectileSpeed()
+    self.ps_lock = false
+    if projectile_speed <= self.projectileSpeed then
+      return self.projectileSpeed - projectile_speed
+    --elseif projectile_speed > self.projectileSpeed then
+      --return self.projectileSpeed - projectile_speed
+    end
+  end
+
+  return 0
+end
+
+function modifier_troll_switch_oaa:GetModifierHealthBonus()
+  -- Bonus hp for melee heroes
+  local parent = self:GetParent()
+  if not parent:IsRangedAttacker() then
+    return self.bonus_health_per_lvl * parent:GetLevel()
+  end
+
+  return 0
+end
+
+function modifier_troll_switch_oaa:GetModifierAttackSpeedBonus_Constant()
+  -- Bonus attack speed for ranged heroes
+  local parent = self:GetParent()
+  if parent:IsRangedAttacker() then
+    return self.bonus_attack_speed_per_lvl * parent:GetLevel()
+  end
+
+  return 0
+end
+
+function modifier_troll_switch_oaa:GetTexture()
+  return "troll_warlord_berserkers_rage"
+end
