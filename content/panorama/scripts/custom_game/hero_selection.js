f005ab70a53c608c916848fa94463c33e1b96102
Darko V <darko1290@gmail.com>
2022-03-26 02:42:42 +0100

    GetPlayerName doesn't work

--- content/panorama/scripts/custom_game/hero_selection.js
+++ content/panorama/scripts/custom_game/hero_selection.js
@@ -74,10 +74,8 @@ var hilariousLoadingPhrases = [
   'Adding another overpowered custom hero',
   'Actually doing nothing',
   'Prolonging loading screen for dramatic effect',
-  'Begging for Oracle sets',
   'Crashing tournament finals',
   'Banning spectators on a hunch',
-  'Nerfing Tinker more',
   'Replacing all heroes with Oracle',
   'Losing self in the music the moment I owned it',
   'Practicing invincible ledgedash',
@@ -113,7 +111,7 @@ var hilariousLoadingPhrases = [
   'Unclaimed Runes will be destroyed in developer\'s pits of darkness',
   'You cannot block neutral creep camps from respawning at the minute mark. Griefers get rekt',
   'Scan reveals invisible enemy units and heroes but not wards',
-  'Aghanim\'s Shard cannot be purchased until the 15 minute mark',
+  'Aghanim\'s Shard cannot be purchased until the 12 minute mark',
   'Azazel\'s Scout, unit that can be purchased from the Azazel shop, has flying vision, true sight and 100% magic resistance',
   'You can sell core points for gold by right-clicking on the special item in the main shop',
   'Bosses start regenerating rapidly to full health if not damaged for 5 seconds',
@@ -122,7 +120,6 @@ var hilariousLoadingPhrases = [
   'Tooltips usually provide answers to all your questions. But also keep in mind that Valve is a small indie company and we are not obligated to fix all their errors',
   'Remember that this is only a game',
   'Never question Darkonius\' item builds',
-  'Lich can use Frost Shield on Techies mines',
   'Dark Seer can use Ion Shell on invulnerable allies',
   'Ten years since chrisinajar stream'
 ];
@@ -1109,8 +1106,13 @@ function SelectHero (hero) {
       CaptainSelectHero();
     } else {
       $.Msg('Selecting ' + newhero);
+      let playerId = Game.GetLocalPlayerID()
+      let playerName = Players.GetPlayerName(playerId);
+      let heroName = $.Localize('#' + newhero);
       GameEvents.SendCustomGameEventToServer('hero_selected', {
-        hero: newhero
+        hero: newhero,
+        player_name: playerName,
+        hero_name: heroName
       });
     }
   }
--- game/scripts/vscripts/components/heroselection/heroselection.lua
+++ game/scripts/vscripts/components/heroselection/heroselection.lua
@@ -371,18 +371,18 @@ function HeroSelection:RankedManager (event)
     end
     if choice == 'random' then
       choice = self:RandomHero()
-      local name = string.gsub(choice, "npc_dota_hero_", "") -- Cuts the npc_dota_hero_ prefix
-      GameRules:SendCustomMessage(tostring(PlayerResource:GetPlayerName(event.PlayerID)).." randomed "..name, 0, 0)
+      --local name = string.gsub(choice, "npc_dota_hero_", "") -- Cuts the npc_dota_hero_ prefix
+      --GameRules:SendCustomMessage(tostring(PlayerResource:GetPlayerName(event.PlayerID)).." randomed "..name, 0, 0)
     end
     if choice == 'forcerandom' then
       choice = self:ForceRandomHero(event.PlayerID)
-      local previewHero = self:GetPreviewHero(event.PlayerID)
-      local name = string.gsub(choice, "npc_dota_hero_", "") -- Cuts the npc_dota_hero_ prefix
-      if choice == previewHero then
-        GameRules:SendCustomMessage(tostring(PlayerResource:GetPlayerName(event.PlayerID)).." was forced to pick "..name, 0, 0)
-      else
-        GameRules:SendCustomMessage(tostring(PlayerResource:GetPlayerName(event.PlayerID)).." was forced to random "..name, 0, 0)
-      end
+      --local previewHero = self:GetPreviewHero(event.PlayerID)
+      --local name = string.gsub(choice, "npc_dota_hero_", "") -- Cuts the npc_dota_hero_ prefix
+      --if choice == previewHero then
+        --GameRules:SendCustomMessage(tostring(PlayerResource:GetPlayerName(event.PlayerID)).." was forced to pick "..name, 0, 0)
+      --else
+        --GameRules:SendCustomMessage(tostring(PlayerResource:GetPlayerName(event.PlayerID)).." was forced to random "..name, 0, 0)
+      --end
     end
     DebugPrint('Picking step ' .. rankedpickorder.currentOrder)
     if rankedpickorder.order[rankedpickorder.currentOrder].team ~= PlayerResource:GetTeam(event.PlayerID) then
@@ -965,15 +965,15 @@ function HeroSelection:HeroSelected (event)
     DebugPrint('Cheater...')
     return
   end
+  local player_name = tostring(event.player_name) or tostring(PlayerResource:GetPlayerName(event.PlayerID))
+  local hero_name = tostring(event.hero_name)
   if rankedpickorder.phase == 'bans' then
     if IsInToolsMode() then
-      local name = string.gsub(event.hero, "npc_dota_hero_", "") -- Cuts the npc_dota_hero_ prefix
-      GameRules:SendCustomMessage("Tools Mode: "..tostring(PlayerResource:GetPlayerName(event.PlayerID)).." banned "..name, 0, 0)
+      GameRules:SendCustomMessage("Tools Mode: "..player_name.." nominated "..hero_name.." to be banned.", 0, 0)
     end
   elseif rankedpickorder.phase == 'picking' then
     if event.hero ~= 'random' and event.hero ~= 'forcerandom' then
-      local name = string.gsub(event.hero, "npc_dota_hero_", "") -- Cuts the npc_dota_hero_ prefix
-      GameRules:SendCustomMessage(tostring(PlayerResource:GetPlayerName(event.PlayerID)).." picked "..name, 0, 0)
+      GameRules:SendCustomMessage(player_name.." picked "..hero_name, 0, 0)
     end
   end
   if HeroSelection.isBanning then
