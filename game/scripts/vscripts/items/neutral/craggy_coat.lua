1d9d191b73f8d1648280df51e486e4d8c5764841
Darko V <darko1290@gmail.com>
2022-05-23 20:49:32 +0200

    4)
    * Craggy Coat active: Coat Harden - Converts 50% of all received non-physical damage to physical damage. Lasts 5 seconds.
    * Tooltips

--- game/scripts/vscripts/items/neutral/craggy_coat.lua
+++ game/scripts/vscripts/items/neutral/craggy_coat.lua
@@ -79,62 +79,58 @@ end
 
 function modifier_item_craggy_coat_active:DeclareFunctions()
   return {
-    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_MAGICAL,
-    MODIFIER_PROPERTY_ABSOLUTE_NO_DAMAGE_PURE,
-    MODIFIER_EVENT_ON_TAKEDAMAGE,
+    MODIFIER_PROPERTY_TOTAL_CONSTANT_BLOCK,
   }
 end
 
-function modifier_item_craggy_coat_active:GetAbsoluteNoDamageMagical()
-  return 1
-end
-
-function modifier_item_craggy_coat_active:GetAbsoluteNoDamagePure()
-  return 1
-end
-
 if IsServer() then
-  function modifier_item_craggy_coat_active:OnTakeDamage(event)
+  function modifier_item_craggy_coat_active:GetModifierTotal_ConstantBlock(event)
     local parent = self:GetParent()
+    local ability = self:GetAbility()
     local attacker = event.attacker
-    local damaged_unit = event.unit
 
-    -- Check if attacker exists
-    if not attacker or attacker:IsNull() then
-      return
+    -- Check if attacker and ability exist
+    if not attacker or attacker:IsNull() or not ability or ability:IsNull() then
+      return 0
     end
 
-    -- Check if damaged entity exists
-    if not damaged_unit or damaged_unit:IsNull() then
-      return
-    end
-
-    -- Check if damaged entity has this modifier
-    if damaged_unit ~= parent then
-      return
-    end
-
-    local damage = event.original_damage
+    local original_dmg = event.original_damage
+    local dmg_after_reductions = event.damage
     local damage_type = event.damage_type
 
     -- Check if damage is somehow 0 or negative
-    if damage <= 0 then
-      return
+    if dmg_after_reductions <= 0 then
+      return 0
     end
 
+    -- Don't do anything if damage is already physical
     if damage_type == DAMAGE_TYPE_PHYSICAL then
-      return
+      return 0
     end
 
+    local conversion_pct = ability:GetSpecialValueFor("damage_conversion_pct")
+    local block_pct = 100 - conversion_pct
+
+    -- "Convert" a part of the original damage to physical
     local damage_table = {
       victim = parent,
       attacker = attacker,
-      damage = damage,
+      damage = original_dmg * conversion_pct / 100,
       damage_type = DAMAGE_TYPE_PHYSICAL,
       damage_flags = event.damage_flags,
       ability = event.inflictor,
     }
 
     ApplyDamage(damage_table)
+
+    -- Block part of the 'damage after reductions' (magic or pure) to mimic damage conversion
+    local block_amount = dmg_after_reductions * block_pct / 100
+
+    if block_amount > 0 then
+      -- Visual effect
+      SendOverheadEventMessage(nil, OVERHEAD_ALERT_BLOCK, parent, block_amount, nil)
+    end
+
+    return block_amount
   end
 end
