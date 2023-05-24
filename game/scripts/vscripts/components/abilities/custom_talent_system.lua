5372a001afea4845db6834055b7cb7ea95c6e39e
AitanBachrach <40442717+AitanBachrach@users.noreply.github.com>
2023-05-24 11:13:02 -0400

    Phylactery changes, Muerta nerf, Hoodwink Talent (#3510)
    Hoodwink:
    * Acorn Shot bounce count increased by 1 at all levels
    * -4 armor corruption talent moved to 15, overwrites bounce count talent
    * new +25% base damage talent at level 20
    
    Muerta:
    * Pierce the Veil cooldown increased from 75/60/45/40/35 to 75/70/65/60/55 seconds.
    
    Phylactery:
    * Slow duration increased from 1.5 to 1.5/1.75/2.0/2.25/2.5 seconds.
    * Slow amount increased from 50% to 50/60/70/80/90%.

--- game/scripts/vscripts/components/abilities/custom_talent_system.lua
+++ game/scripts/vscripts/components/abilities/custom_talent_system.lua
@@ -31,6 +31,9 @@ local abilities_with_custom_talents = {
   faceless_void_time_dilation = {
     radius = "special_bonus_unique_faceless_void_1_oaa",
   },
+  hoodwink_acorn_shot = {
+    base_damage_pct = "special_bonus_unique_hoodwink_1_oaa",
+  },
   invoker_emp = {
     damage_per_mana_pct = "special_bonus_unique_invoker_1_oaa",
   },
