16d91490303605113965627d6b9737faf126473e
Darko V <darko1290@gmail.com>
2023-07-11 21:33:47 +0200

    Power Spark
    * Power Spark damage block chance reduced from 75% to 60%.
    * Power Spark damage block value is now halved on ranged heroes and illusions. (Ranged summons still get the full value.)

--- game/scripts/vscripts/modifiers/sparks/modifier_spark_power.lua
+++ game/scripts/vscripts/modifiers/sparks/modifier_spark_power.lua
@@ -81,57 +81,8 @@ function modifier_spark_power:OnCreated()
   parent.power_spark_bonus = 0
   self:SetStackCount(0)
 
-  if IsServer() then
-    -- Current damage values
-    local base_damage_max = parent:GetBaseDamageMax()
-    local base_damage_min = parent:GetBaseDamageMin()
-    local current_primary_stat_value = parent:GetBonusDamageFromPrimaryStat()
-    local current_average_base_damage = (base_damage_max + base_damage_min)/2
-
-    -- Find primary attribute value at level 1 (starting_primary_stat_value)
-    local primary_attribute = parent:GetPrimaryAttribute()
-    local level = parent:GetLevel()
-    local starting_primary_stat_value
-    if primary_attribute == DOTA_ATTRIBUTE_STRENGTH then
-      starting_primary_stat_value = parent:GetBaseStrength() - (level - 1) * parent:GetStrengthGain()
-    elseif primary_attribute == DOTA_ATTRIBUTE_AGILITY then
-      starting_primary_stat_value = parent:GetBaseAgility() - (level - 1) * parent:GetAgilityGain()
-    elseif primary_attribute == DOTA_ATTRIBUTE_INTELLECT then
-      starting_primary_stat_value = parent:GetBaseIntellect() - (level - 1) * parent:GetIntellectGain()
-    elseif primary_attribute == DOTA_ATTRIBUTE_ALL then
-      starting_primary_stat_value = (parent:GetBaseStrength() - (level - 1) * parent:GetStrengthGain() + parent:GetBaseAgility() - (level - 1) * parent:GetAgilityGain() + parent:GetBaseIntellect() - (level - 1) * parent:GetIntellectGain())/3
-    end
-
-    -- Damage values at level 1 (starting base damage values)
-    local base_damage_level_1_max = base_damage_max - current_primary_stat_value + starting_primary_stat_value
-    local base_damage_level_1_min = base_damage_min - current_primary_stat_value + starting_primary_stat_value
-    local starting_average_base_damage = (base_damage_level_1_max + base_damage_level_1_min)/2
-
-    -- Bonus damage/block formula
-    local bonus = math.ceil(3188/61 + (4166 * current_average_base_damage - 7312 * starting_average_base_damage)/8723)
-    -- Change stack count
-    self:SetStackCount(bonus)
-  end
-
+  self:OnIntervalThink()
   self:StartIntervalThink(0.3)
-
-  --[[
-  -- Power Spark constants
-  self.creep_damage_melee = {40, 120, 200, 280, 360}
-  self.creep_damage_ranged = {40, 120, 200, 280, 360}
-  self.creep_damage_melee_illusion = {20, 60, 100, 140, 180}
-  self.creep_damage_ranged_illusion = {20, 60, 100, 140, 180}
-
-  -- Stack count is for tooltip only
-  if IsServer() then
-    local spark_level = self:GetSparkLevel()
-    if parent:IsRangedAttacker() then
-      self:SetStackCount(self.creep_damage_ranged[spark_level])
-    else
-      self:SetStackCount(self.creep_damage_melee[spark_level])
-    end
-  end
-  ]]
 end
 
 function modifier_spark_power:OnRefresh()
@@ -248,129 +199,86 @@ function modifier_spark_power_effect:OnCreated()
     return
   end
 
-  self.bonus = caster.power_spark_bonus
+  self.bonus = caster.power_spark_bonus or caster:GetModifierStackCount("modifier_spark_power", caster)
+  self.chance = 60
 end
 
 function modifier_spark_power_effect:OnRefresh()
-  local caster = self:GetCaster() or self:GetAuraOwner()
-
-  self.bonus = self.bonus or 0
-
-  if not caster or caster:IsNull() then
-    return
-  end
-
-  if caster:IsIllusion() then
-    return
-  end
-
-  self.bonus = caster.power_spark_bonus
+  self:OnCreated()
 end
 
-function modifier_spark_power_effect:GetModifierProcAttack_BonusDamage_Pure(event)
-  local parent = self:GetParent()
-  local target = event.target
+if IsServer() then
+  function modifier_spark_power_effect:GetModifierProcAttack_BonusDamage_Pure(event)
+    local parent = self:GetParent()
+    local target = event.target
 
-  -- To prevent crashes:
-  if not target then
-    return 0
-  end
-
-  if target:IsNull() then
-    return 0
-  end
-
-  -- Check for existence of GetUnitName method to determine if target is a unit or an item
-  -- items don't have that method -> nil; if the target is an item, don't continue
-  if target.GetUnitName == nil then
-    return 0
-  end
-
-  -- Don't affect buildings and wards
-  if target:IsTower() or target:IsBuilding() or target:IsOther() then
-    return 0
-  end
-
-  if not IsServer() then
-    return 0
-  end
-
-  -- don't damage non-neutrals
-  if not target:IsNeutralCreep(false) then
-    return 0
-  end
-
-  -- Power Spark variables
-  --[[
-  local creep_damage_melee = self.creep_damage_melee
-  local creep_damage_ranged = self.creep_damage_ranged
-  local creep_damage_melee_illusion = self.creep_damage_melee_illusion
-  local creep_damage_ranged_illusion = self.creep_damage_ranged_illusion
-
-  local spark_level = self:GetSparkLevel()
-
-  local damage = creep_damage_melee[spark_level]
-  if parent:IsRangedAttacker() then
-    damage = creep_damage_ranged[spark_level]
-  end
-
-  if parent:IsIllusion() then
-    damage = creep_damage_melee_illusion[spark_level]
-    if parent:IsRangedAttacker() then
-      damage = creep_damage_ranged_illusion[spark_level]
-    end
-  end
-  ]]
-  local damage = self.bonus
-  if parent:IsIllusion() or not parent:IsHero() then
-    damage = damage / 7
-  end
-  if damage > 0 then
-    SendOverheadEventMessage(parent, OVERHEAD_ALERT_MAGICAL_BLOCK, target, damage, parent)
-  end
-
-  return damage
-end
-
-function modifier_spark_power_effect:GetModifierPhysical_ConstantBlock(keys)
-  if not IsServer() then
-    return 0
-  end
-
-  local attacker = keys.attacker
-  if not attacker or attacker:IsNull() then
-    return 0
-  end
-
-  if attacker:GetTeamNumber() == DOTA_TEAM_NEUTRALS and not attacker:IsOAABoss() then
-    local block = self.bonus
-    -- True Random chance
-    --[[
-    if RandomInt(1, 100) <= 50 then
-      return block
-    end
-    ]]
-
-    if not self.damage_block_failures then
-      self.damage_block_failures = 0
+    -- To prevent crashes:
+    if not target or target:IsNull() then
+      return 0
     end
 
-    -- Get number of failures
-    local prngMult = self.damage_block_failures + 1
-
-    -- 50% chance for damage block; Pseudo-Random chance
-    if RandomFloat(0.0, 1.0) <= (PrdCFinder:GetCForP(0.75) * prngMult) then
-      -- Reset failure count
-      self.damage_block_failures = 0
-
-      return block
-    else
-      -- Increment number of failures
-      self.damage_block_failures = prngMult
+    -- Check for existence of GetUnitName method to determine if target is a unit or an item
+    -- items don't have that method -> nil; if the target is an item, don't continue
+    if target.GetUnitName == nil then
+      return 0
     end
+
+    -- Don't affect buildings and wards
+    if target:IsTower() or target:IsBuilding() or target:IsOther() then
+      return 0
+    end
+
+    -- Don't damage non-neutrals
+    if not target:IsNeutralCreep(false) then
+      return 0
+    end
+
+    local damage = self.bonus
+    if parent:IsIllusion() or not parent:IsHero() then
+      damage = damage / 7
+    end
+    if damage > 0 then
+      SendOverheadEventMessage(parent, OVERHEAD_ALERT_MAGICAL_BLOCK, target, damage, parent)
+    end
+
+    return damage
   end
 
-  return 0
+  function modifier_spark_power_effect:GetModifierPhysical_ConstantBlock(keys)
+    local attacker = keys.attacker
+    local parent = self:GetParent()
+    if not attacker or attacker:IsNull() then
+      return 0
+    end
+
+    if attacker:GetTeamNumber() == DOTA_TEAM_NEUTRALS and not attacker:IsOAABoss() then
+      local block = self.bonus
+
+      if not self.damage_block_failures then
+        self.damage_block_failures = 0
+      end
+
+      -- Get number of failures
+      local prngMult = self.damage_block_failures + 1
+
+      -- Pseudo-Random chance for damage block
+      if RandomFloat(0.0, 1.0) <= (PrdCFinder:GetCForP(self.chance/100) * prngMult) then
+        -- Reset failure count
+        self.damage_block_failures = 0
+
+        if parent:IsRangedAttacker() and parent:IsHero() then
+          return block / 2
+        else
+          return block
+        end
+      else
+        -- Increment number of failures
+        self.damage_block_failures = prngMult
+      end
+    end
+
+    return 0
+  end
 end
 
 function modifier_spark_power_effect:OnTooltip()
@@ -383,7 +291,13 @@ function modifier_spark_power_effect:OnTooltip()
 end
 
 function modifier_spark_power_effect:OnTooltip2()
-  return self.bonus
+  local parent = self:GetParent()
+  local block = self.bonus
+  if parent:IsRangedAttacker() and parent:IsHero() then
+    return block / 2
+  else
+    return block
+  end
 end
 
 function modifier_spark_power_effect:GetTexture()
