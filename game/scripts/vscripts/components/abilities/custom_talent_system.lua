57ec78c09d6c6778bf7558ee74f0ac0a5fc9ceee
Darko V <darko1290@gmail.com>
2023-08-09 23:56:46 +0200

    Spectre
    * Spectre Spectral Dagger cooldown reduced at OAA levels from 16 to 15/14 seconds.
    * Spectre Spectral Dagger bonus move speed increased at OAA levels from 22% to 23/24%.
    * Spectre Level 25 Talent +48% Haunt Illusion Damage changed to +40% Shadow Step and Haunt Illusion Damage. (made it custom so it will work for sure no matter what the tooltip says)

--- game/scripts/vscripts/components/abilities/custom_talent_system.lua
+++ game/scripts/vscripts/components/abilities/custom_talent_system.lua
@@ -69,6 +69,14 @@ local abilities_with_custom_talents = {
   skywrath_mage_arcane_bolt = {
     bolt_damage = {"special_bonus_unique_skywrath_1_oaa", "+"},
   },
+  spectre_haunt = {
+    illusion_damage_outgoing = {"special_bonus_unique_spectre_4_oaa", "+"},
+    tooltip_outgoing = {"special_bonus_unique_spectre_4_oaa", "+"},
+  },
+  spectre_haunt_single = {
+    illusion_damage_outgoing = {"special_bonus_unique_spectre_4_oaa", "+"},
+    tooltip_outgoing = {"special_bonus_unique_spectre_4_oaa", "+"},
+  },
   ursa_fury_swipes = {
     bonus_reset_time = {"special_bonus_unique_ursa_1_oaa", "+"},
   },
