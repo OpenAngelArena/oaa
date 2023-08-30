ed1432d7bddba2f4e3953705628136b4361dee1b
Darko V <darko1290@gmail.com>
2023-08-30 23:46:47 +0200

    Frostkitten Bottle (#3555)

--- game/scripts/vscripts/items/bottle.lua
+++ game/scripts/vscripts/items/bottle.lua
@@ -62,7 +62,7 @@ local bonusNames = {
   'custom/bottles/bottle_satsaa',
   'custom/bottles/bottle_hastedd',
   'custom/bottles/bottle_nate',
-
+  'custom/bottles/bottle_frostkitten', --60
 }
 
 --------------------------------------------------------------------------------
