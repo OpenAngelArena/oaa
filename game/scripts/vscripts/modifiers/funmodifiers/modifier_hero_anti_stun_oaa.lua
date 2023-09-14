4ff2f6cc21052137918ae163bf55ab6e738bf763
Darko V <darko1290@gmail.com>
2023-09-14 04:33:39 +0200

    Hotfix
    * White Queen doesn't provide damage reduction anymore.
    * Duelist damage reduction reduced from 35% to 15%.
    * Smurf no longer provides damage reduction and evasion.
    * Smurf model scale increased by 20%.
    * Smurf aoe decrease reduced from 50% to 25%.

--- game/scripts/vscripts/modifiers/funmodifiers/modifier_hero_anti_stun_oaa.lua
+++ game/scripts/vscripts/modifiers/funmodifiers/modifier_hero_anti_stun_oaa.lua
@@ -18,48 +18,15 @@ function modifier_hero_anti_stun_oaa:RemoveOnDeath()
   return false
 end
 
-function modifier_hero_anti_stun_oaa:DeclareFunctions()
-  return {
-    MODIFIER_PROPERTY_INCOMING_DAMAGE_PERCENTAGE,
-  }
-end
-
-function modifier_hero_anti_stun_oaa:GetModifierIncomingDamage_Percentage()
-  local parent = self:GetParent()
-  local current_ability = parent:GetCurrentActiveAbility()
-  if (current_ability and current_ability:IsInAbilityPhase()) or parent:IsChanneling() then
-    return -50
-  end
-  return 0
-end
-
-function modifier_hero_anti_stun_oaa:GetPriority()
-  return MODIFIER_PRIORITY_SUPER_ULTRA + 20000
-end
-
 function modifier_hero_anti_stun_oaa:CheckState()
   local parent = self:GetParent()
   local current_ability = parent:GetCurrentActiveAbility()
   if (current_ability and current_ability:IsInAbilityPhase()) or parent:IsChanneling() then
     return {
-      [MODIFIER_STATE_HEXED] = false,
-      [MODIFIER_STATE_ROOTED] = false,
-      [MODIFIER_STATE_SILENCED] = false,
-      [MODIFIER_STATE_STUNNED] = false,
-      [MODIFIER_STATE_FROZEN] = false,
-      [MODIFIER_STATE_FEARED] = false,
       [MODIFIER_STATE_DEBUFF_IMMUNE] = true,
     }
   end
   return {}
-end
-
-function modifier_hero_anti_stun_oaa:GetEffectName()
-  return "particles/units/heroes/hero_spirit_breaker/spirit_breaker_haste_owner.vpcf"
-end
-
-function modifier_hero_anti_stun_oaa:GetEffectAttachType()
-  return PATTACH_ABSORIGIN_FOLLOW
 end
 
 function modifier_hero_anti_stun_oaa:GetTexture()
