a9be1cd017e67eb53c7a66f00b8aef6d9b2b41ea
Darko V. <darko1290@gmail.com>
2024-07-24 11:13:23 +0200

    Fixes for core points display bugs
    When selecting enemies or non-hero units the indicator will now always show 0 core points.
    When deselecting allies or enemies the indicator will now properly refresh and show the real amount of your core points.
    When your hero isn't selected and when you gain/use/sell or selected hero gains/uses/sells core points, real-time change of core points on the selected hero will no longer be shown. You need to select your own hero to see the new and correct amount of your core points. If you are want to see the correct amount of core points on the selected ally, just re-select the ally.

--- content/panorama/scripts/custom_game/core_points.js
+++ content/panorama/scripts/custom_game/core_points.js
@@ -1,25 +1,44 @@
-/* global $, GameEvents, Players, HasModifier, GetStackCount, Game */
+/* global $, GameEvents, Players, HasModifier, GetStackCount, Game, Entities */
 
 'use strict';
 
+// this happens for every core points change
 function OnCorePointsChanged (args) {
-  const unit = Players.GetLocalPlayerPortraitUnit();
+  const currentlySelectedUnit = Players.GetLocalPlayerPortraitUnit();
+  // If currently selected unit is not the player's hero then don't continue
+  if (currentlySelectedUnit !== Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer())) return;
+  const cpLabel = $('#CorePointsText');
+  const corePoints = args.cp;
+  if (typeof corePoints === 'number' || typeof corePoints === 'string') {
+    cpLabel.text = corePoints;
+  } else {
+    // Received argument is invalid, do stuff below
+    ShowCorePointsOnSelected();
+  }
+}
+
+// this happens when a player selects any unit
+function ShowCorePointsOnSelected () {
+  const currentlySelectedUnit = Players.GetLocalPlayerPortraitUnit();
+  // If currently selected unit is invalid then don't continue
+  if (!Entities.IsValidEntity(currentlySelectedUnit)) return;
   const modifier = 'modifier_core_points_counter_oaa';
   const cpLabel = $('#CorePointsText');
-  let corePoints = args.cp;
-  if (HasModifier(unit, modifier)) {
+  // Show core points only if currently selected unit has the modifier and if it is on the player's team
+  if (HasModifier(currentlySelectedUnit, modifier) && Entities.GetTeamNumber(currentlySelectedUnit) === Players.GetTeam(Players.GetLocalPlayer())) {
     $.Schedule(0.03, function () {
-      corePoints = GetStackCount(unit, modifier);
+      const corePoints = GetStackCount(currentlySelectedUnit, modifier);
       cpLabel.text = corePoints;
     });
+  } else {
+    cpLabel.text = 0;
   }
-
-  cpLabel.text = corePoints;
 }
 
 (function () {
   GameEvents.Subscribe('core_point_number_changed', OnCorePointsChanged);
-  OnCorePointsChanged({ cp: '-' });
+  GameEvents.Subscribe('dota_player_update_query_unit', ShowCorePointsOnSelected);
+  GameEvents.Subscribe('dota_player_update_selected_unit', ShowCorePointsOnSelected);
 
   if (Game.IsHUDFlipped()) {
     const context = $.GetContextPanel();
