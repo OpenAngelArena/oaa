6f855f786b18f6726910df6e6c0ae23725edd22a
Darko V. <darko1290@gmail.com>
2024-07-24 09:49:38 +0200

    Update core_points.js

--- content/panorama/scripts/custom_game/core_points.js
+++ content/panorama/scripts/custom_game/core_points.js
@@ -1,25 +1,43 @@
-/* global $, GameEvents, Players, HasModifier, GetStackCount, Game */
+/* global $, GameEvents, Players, HasModifier, GetStackCount, Game, Entities */
 
 'use strict';
 
+// this should trigger when a player gains or loses core points and only if currently selected unit is player's hero
 function OnCorePointsChanged (args) {
-  const unit = Players.GetLocalPlayerPortraitUnit();
+  const currentlySelectedUnit = Players.GetLocalPlayerPortraitUnit();
+  if (currentlySelectedUnit !== Players.GetPlayerHeroEntityIndex(Players.GetLocalPlayer())) return;
   const modifier = 'modifier_core_points_counter_oaa';
   const cpLabel = $('#CorePointsText');
-  let corePoints = args.cp;
-  if (HasModifier(unit, modifier)) {
+  const corePoints = args.cp;
+  if (typeof corePoints === 'number' || typeof corePoints === 'string') {
+    cpLabel.text = corePoints;
+  } else {
+    ShowCorePointsOnSelected();
+  }
+}
+
+// this should trigger when a player selects any unit
+function ShowCorePointsOnSelected () {
+  const currentlySelectedUnit = Players.GetLocalPlayerPortraitUnit();
+  // If selected unit is invalid don't continue
+  if (!Entities.IsValidEntity(currentlySelectedUnit)) return;
+  const modifier = 'modifier_core_points_counter_oaa';
+  const cpLabel = $('#CorePointsText');
+  // Show core points only if selected unit is on player's team
+  if (HasModifier(currentlySelectedUnit, modifier) && Entities.GetTeamNumber(currentlySelectedUnit) !== Players.GetTeam(Players.GetLocalPlayer())) {
     $.Schedule(0.03, function () {
-      corePoints = GetStackCount(unit, modifier);
+      const corePoints = GetStackCount(currentlySelectedUnit, modifier);
       cpLabel.text = corePoints;
     });
+  } else {
+    cpLabel.text = 0;
   }
-
-  cpLabel.text = corePoints;
 }
 
 (function () {
   GameEvents.Subscribe('core_point_number_changed', OnCorePointsChanged);
-  OnCorePointsChanged({ cp: '-' });
+  GameEvents.Subscribe('dota_player_update_query_unit', ShowCorePointsOnSelected);
+  GameEvents.Subscribe('dota_player_update_selected_unit', ShowCorePointsOnSelected);
 
   if (Game.IsHUDFlipped()) {
     const context = $.GetContextPanel();
