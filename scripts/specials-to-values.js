eef0c9ae1a60ad0a15dd519dda4f386c7614c8a5
Darko V. <darko1290@gmail.com>
2024-08-02 06:30:43 +0200

    meh it's something

---
+++ scripts/specials-to-values.js
@@ -0,0 +1,88 @@
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+const readline = require('readline');
+
+const tab = '  ';
+const padLenght = 59;
+const padLenghtTooltips = 81;
+
+function stringRepeat (string, num) {
+  let result = '';
+  for (let i = 0; i < num; i++) {
+    result += string;
+  }
+  return result;
+}
+
+const walk = function (directoryName, action) {
+  fs.readdir(directoryName, function (err, files) {
+    if (err) {
+      console.error(chalk.red(err));
+      return;
+    }
+    files.forEach(function (file) {
+      const fullPath = path.join(directoryName, file);
+      fs.stat(fullPath, function (err, f) {
+        if (err) {
+          console.error(chalk.red(err));
+          return;
+        }
+        if (f.isDirectory()) {
+          walk(fullPath, action);
+        } else {
+          action(fullPath);
+        }
+      });
+    });
+  });
+};
+
+function specialsToValues (file) {
+  if (file.indexOf('.md') > -1) {
+    return;
+  }
+
+  // console.log('Doing ' + chalk.green(file));
+
+  const lineReader = readline.createInterface({
+    input: fs.createReadStream(file)
+  });
+
+  let result = '';
+  let before = '';
+  let error = false;
+  let lineNumber = 0;
+  lineReader.on('line', (line) => {
+    before += line + '\r\n';
+    lineNumber++;
+
+    line = line.replace(/.*"var_type".*/, '');
+
+    line = line.replace('AbilitySpecial', 'AbilityValues');
+
+    result += line + '\r\n';
+    result = result.replace(/^(\s*\r\n){2,}/, '')
+  });
+
+  lineReader.on('close', () => {
+    if (!error && !checkEqual(before, result)) {
+      fs.writeFile(file, result, { encoding: 'utf8' }, function (err) {
+        if (err) {
+          return console.error(chalk.red(err));
+        }
+        console.log('Done ' + chalk.green(file));
+      });
+    }
+  });
+}
+
+function checkEqual (s1, s2) {
+  if (s1 === s2) {
+    return true;
+  } else {
+    return s1.replace(/[^a-zA-Z0-9 "{}]/g, '') === s2.replace(/[^a-zA-Z0-9 "{}]/g, '');
+  }
+}
+
+walk('game/scripts/npc', specialsToValues);
