c52be564fa1ee12b245077b2ddf79447e45b007d
Darko V. <darko1290@gmail.com>
2024-08-07 23:19:39 +0200

    Tweaks

--- game/scripts/vscripts/abilities/boss/tormentor_boss/tormentor_reflect.lua
+++ game/scripts/vscripts/abilities/boss/tormentor_boss/tormentor_reflect.lua
@@ -14,7 +14,7 @@ function tormentor_boss_reflect_oaa:Precache(context)
 end
 
 function tormentor_boss_reflect_oaa:GetIntrinsicModifierName()
-	return "modifier_tormentor_reflect_oaa"
+  return "modifier_tormentor_reflect_oaa"
 end
 
 function tormentor_boss_reflect_oaa:IsStealable()
@@ -30,15 +30,15 @@ end
 modifier_tormentor_reflect_oaa = class({})
 
 function modifier_tormentor_reflect_oaa:IsHidden() -- needs tooltip
-	return false
+  return false
 end
 
 function modifier_tormentor_reflect_oaa:IsDebuff()
-	return false
+  return false
 end
 
 function modifier_tormentor_reflect_oaa:IsPurgable()
-	return false
+  return false
 end
 
 function modifier_tormentor_reflect_oaa:DeclareFunctions()
@@ -50,29 +50,29 @@ function modifier_tormentor_reflect_oaa:DeclareFunctions()
 end
 
 function modifier_tormentor_reflect_oaa:OnCreated()
-	if not IsServer() then return end
+  if not IsServer() then return end
 
-	self.parent = self:GetParent()
-	self.ability = self:GetAbility()
+  self.parent = self:GetParent()
+  self.ability = self:GetAbility()
 
-	self.radius = self.ability:GetSpecialValueFor("radius")
-	self.illusion_damage_pct = self.ability:GetSpecialValueFor("illusion_damage_pct")
+  self.radius = self.ability:GetSpecialValueFor("radius")
+  self.illusion_damage_pct = self.ability:GetSpecialValueFor("illusion_damage_pct")
   self.reflection = self.ability:GetSpecialValueFor("passive_reflection_pct")
 
-	self.pfx_name = {}
-	self.pfx_name[DOTA_TEAM_GOODGUYS] = {
-		shield = "particles/neutral_fx/miniboss_shield.vpcf",
-		reflect = "particles/neutral_fx/miniboss_damage_reflect.vpcf",
-		impact = "particles/neutral_fx/miniboss_damage_impact.vpcf",
-		death = "particles/neutral_fx/miniboss_death.vpcf",
-	}
+  self.pfx_name = {}
+  self.pfx_name[DOTA_TEAM_GOODGUYS] = {
+    shield = "particles/neutral_fx/miniboss_shield.vpcf",
+    reflect = "particles/neutral_fx/miniboss_damage_reflect.vpcf",
+    impact = "particles/neutral_fx/miniboss_damage_impact.vpcf",
+    death = "particles/neutral_fx/miniboss_death.vpcf",
+  }
 
-	self.pfx_name[DOTA_TEAM_BADGUYS] = {
-		shield = "particles/neutral_fx/miniboss_shield_dire.vpcf",
-		reflect = "particles/neutral_fx/miniboss_damage_reflect_dire.vpcf",
-		impact = "particles/neutral_fx/miniboss_dire_damage_impact.vpcf",
-		death = "particles/neutral_fx/miniboss_death_dire.vpcf",
-	}
+  self.pfx_name[DOTA_TEAM_BADGUYS] = {
+    shield = "particles/neutral_fx/miniboss_shield_dire.vpcf",
+    reflect = "particles/neutral_fx/miniboss_damage_reflect_dire.vpcf",
+    impact = "particles/neutral_fx/miniboss_dire_damage_impact.vpcf",
+    death = "particles/neutral_fx/miniboss_death_dire.vpcf",
+  }
 
   -- This delay is required because the tormentor team is not set yet when the modifier is created
   local modifier = self
@@ -80,7 +80,7 @@ function modifier_tormentor_reflect_oaa:OnCreated()
     if modifier and not modifier:IsNull() then
       local parent = modifier.parent
       if parent and not parent:IsNull() then
-        modifier.shield_pfx = ParticleManager:CreateParticle(modifier.pfx_name[parent.tormentorTeam].shield, PATTACH_ABSORIGIN_FOLLOW, parent)
+        modifier.shield_pfx = ParticleManager:CreateParticle(modifier.pfx_name[parent.tormentorTeam].shield, PATTACH_CENTER_FOLLOW, parent)
       end
       modifier:SetHasCustomTransmitterData(true)
     end
@@ -88,140 +88,146 @@ function modifier_tormentor_reflect_oaa:OnCreated()
 end
 
 function modifier_tormentor_reflect_oaa:AddCustomTransmitterData()
-	return {
-		reflection = self.reflection,
-	}
+  return {
+    reflection = self.reflection,
+  }
 end
 
 function modifier_tormentor_reflect_oaa:HandleCustomTransmitterData(data)
-	self.reflection = data.reflection
-end
-
-function modifier_tormentor_reflect_oaa:OnTakeDamage(keys)
-	if not IsServer() then return end
-
-	local damage = keys.original_damage
-	local damageType = keys.damage_type
-	local damageFlags = keys.damage_flags
-	local attacker = keys.attacker
-
-	if keys.unit ~= self.parent then return end
-
-	-- Ignore damage that has the no-reflect flag
-	if bit.band(damageFlags, DOTA_DAMAGE_FLAG_REFLECTION) > 0 then
-		return
-	end
-
-	-- Ignore damage that has the no-spell-lifesteal flag
-	if bit.band(damageFlags, DOTA_DAMAGE_FLAG_NO_SPELL_LIFESTEAL) > 0 then
-		return
-	end
-
-	-- Ignore damage that has the no-spell-amplification flag
-	if bit.band(damageFlags, DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION) > 0 then
-		return
-	end
-
-	local enemies = FindUnitsInRadius(
-		self.parent:GetTeamNumber(),
-		self.parent:GetAbsOrigin(),
-		nil,
-		self.radius,
-		DOTA_UNIT_TARGET_TEAM_ENEMY,
-		DOTA_UNIT_TARGET_HERO,
-		DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
-		FIND_ANY_ORDER,
-		false
-	)
-
-	-- Parts of damage table that are always the same
-	local damageTable = {
-		attacker = self.parent,
-		damage_type = damageType,
-		ability = self.ability,
-	}
-
-	if #enemies == 0 then
-		-- Always affect the attacker, doesn't matter where it is even if there are no enemies around
-		damageTable.victim = attacker
-		damageTable.damage = damage * self.reflection / 100
-
-		ApplyDamage(damageTable)
-
-		local pfx = ParticleManager:CreateParticle(self.pfx_name[self.parent.tormentorTeam].reflect, PATTACH_ABSORIGIN_FOLLOW, self.parent)
-		ParticleManager:SetParticleControl(pfx, 0, self.parent:GetAbsOrigin())
-		ParticleManager:SetParticleControlEnt(pfx, 1, attacker, PATTACH_POINT_FOLLOW, "attach_hitloc", attacker:GetAbsOrigin(), true)
-		-- ParticleManager:SetParticleControl(pfx, 1, attacker:GetAbsOrigin())
-		ParticleManager:ReleaseParticleIndex(pfx)
-
-		-- EmitSoundOnClient("Miniboss.Tormenter.Reflect", attacker)
-		attacker:EmitSound("Miniboss.Tormenter.Reflect")
-		return
-	end
-
-	-- Count non-illusion enemies
-	local number_of_valid_enemies = 0
-	for _, enemy in pairs(enemies) do
-		if enemy and not enemy:IsNull() and IsValidEntity(enemy) and enemy:IsAlive() and not enemy:IsIllusion() then
-			number_of_valid_enemies = number_of_valid_enemies + 1
-		end
-	end
-
-	-- When attacker is damaging Tormentor from far away and only ilussions are around
-	-- to prevent division by 0
-	if number_of_valid_enemies == 0 then
-		number_of_valid_enemies = 1
-	end
-
-	-- Distribute the damage among the present units
-	local reflectedDamage = (damage * self.reflection / 100) / number_of_valid_enemies
-	for _, enemy in pairs(enemies) do
-		if enemy and not enemy:IsNull() and IsValidEntity(enemy) and enemy:IsAlive() and enemy ~= attacker then
-			damageTable.victim = enemy
-			damageTable.damage = reflectedDamage
-
-			if enemy:IsIllusion() then
-				damageTable.damage = reflectedDamage * self.illusion_damage_pct / 100
-			end
-
-			ApplyDamage(damageTable)
-
-			local pfx = ParticleManager:CreateParticle(self.pfx_name[self.parent.tormentorTeam].reflect, PATTACH_ABSORIGIN_FOLLOW, self.parent)
-			ParticleManager:SetParticleControl(pfx, 0, self.parent:GetAbsOrigin())
-			ParticleManager:SetParticleControlEnt(pfx, 1, enemy, PATTACH_POINT_FOLLOW, "attach_hitloc", enemy:GetAbsOrigin(), true)
-			-- ParticleManager:SetParticleControl(pfx, 1, enemy:GetAbsOrigin())
-			ParticleManager:ReleaseParticleIndex(pfx)
-
-			-- EmitSoundOnClient("Miniboss.Tormenter.Reflect", enemy)
-			enemy:EmitSound("Miniboss.Tormenter.Reflect")
-		end
-	end
-
-	-- Always affect the attacker, doesn't matter where it is
-	damageTable.victim = attacker
-	damageTable.damage = reflectedDamage
-
-	if attacker:IsIllusion() then
-		damageTable.damage = reflectedDamage * self.illusion_damage_pct / 100
-	end
-
-	ApplyDamage(damageTable)
-
-	local pfx = ParticleManager:CreateParticle(self.pfx_name[self.parent.tormentorTeam].reflect, PATTACH_ABSORIGIN_FOLLOW, self.parent)
-	ParticleManager:SetParticleControl(pfx, 0, self.parent:GetAbsOrigin())
-	ParticleManager:SetParticleControlEnt(pfx, 1, attacker, PATTACH_POINT_FOLLOW, "attach_hitloc", attacker:GetAbsOrigin(), true)
-	-- ParticleManager:SetParticleControl(pfx, 1, attacker:GetAbsOrigin())
-	ParticleManager:ReleaseParticleIndex(pfx)
-
-	-- EmitSoundOnClient("Miniboss.Tormenter.Reflect", attacker)
-	attacker:EmitSound("Miniboss.Tormenter.Reflect")
-end
-
-function modifier_tormentor_reflect_oaa:OnTooltip()
-	return self.reflection
+  self.reflection = data.reflection
 end
 
 if IsServer() then
+  function modifier_tormentor_reflect_oaa:OnTakeDamage(event)
+    local damage = event.original_damage
+    local damageType = event.damage_type
+    local damageFlags = event.damage_flags
+    local attacker = event.attacker
+
+    -- Check if attacker exists
+    if not attacker or attacker:IsNull() then
+      return
+    end
+
+    -- Check if damaged unit has this modifier
+    if event.unit ~= self.parent then return end
+
+    -- Ignore self damage
+    if attacker == self.parent then
+      return
+    end
+
+    -- Ignore damage that has the no-reflect flag
+    if bit.band(damageFlags, DOTA_DAMAGE_FLAG_REFLECTION) > 0 then
+      return
+    end
+
+    -- Ignore damage that has the no-spell-lifesteal flag
+    if bit.band(damageFlags, DOTA_DAMAGE_FLAG_NO_SPELL_LIFESTEAL) > 0 then
+      return
+    end
+
+    -- Ignore damage that has the no-spell-amplification flag
+    if bit.band(damageFlags, DOTA_DAMAGE_FLAG_NO_SPELL_AMPLIFICATION) > 0 then
+      return
+    end
+
+    local enemies = FindUnitsInRadius(
+      self.parent:GetTeamNumber(),
+      self.parent:GetAbsOrigin(),
+      nil,
+      self.radius,
+      DOTA_UNIT_TARGET_TEAM_ENEMY,
+      DOTA_UNIT_TARGET_HERO,
+      DOTA_UNIT_TARGET_FLAG_MAGIC_IMMUNE_ENEMIES,
+      FIND_ANY_ORDER,
+      false
+    )
+
+    -- Parts of damage table that are always the same
+    local damageTable = {
+      attacker = self.parent,
+      damage_type = damageType,
+      ability = self.ability,
+      damage_flags = DOTA_DAMAGE_FLAG_REFLECTION,
+    }
+
+    if #enemies == 0 then
+      -- Always affect the attacker, doesn't matter where it is even if there are no enemies around
+      damageTable.victim = attacker
+      damageTable.damage = damage * self.reflection / 100
+
+      ApplyDamage(damageTable)
+
+      local pfx = ParticleManager:CreateParticle(self.pfx_name[self.parent.tormentorTeam].reflect, PATTACH_ABSORIGIN_FOLLOW, self.parent)
+      ParticleManager:SetParticleControl(pfx, 0, self.parent:GetAbsOrigin())
+      ParticleManager:SetParticleControlEnt(pfx, 1, attacker, PATTACH_POINT_FOLLOW, "attach_hitloc", attacker:GetAbsOrigin(), true)
+      -- ParticleManager:SetParticleControl(pfx, 1, attacker:GetAbsOrigin())
+      ParticleManager:ReleaseParticleIndex(pfx)
+
+      -- EmitSoundOnClient("Miniboss.Tormenter.Reflect", attacker)
+      attacker:EmitSound("Miniboss.Tormenter.Reflect")
+      return
+    end
+
+    -- Count non-illusion enemies
+    local number_of_valid_enemies = 0
+    for _, enemy in pairs(enemies) do
+      if enemy and not enemy:IsNull() and IsValidEntity(enemy) and enemy:IsAlive() and not enemy:IsIllusion() then
+        number_of_valid_enemies = number_of_valid_enemies + 1
+      end
+    end
+
+    -- When attacker is damaging Tormentor from far away and only ilussions are around
+    -- to prevent division by 0
+    if number_of_valid_enemies == 0 then
+      number_of_valid_enemies = 1
+    end
+
+    -- Distribute the damage among the present units
+    local reflectedDamage = (damage * self.reflection / 100) / number_of_valid_enemies
+    for _, enemy in pairs(enemies) do
+      if enemy and not enemy:IsNull() and IsValidEntity(enemy) and enemy:IsAlive() and enemy ~= attacker then
+        damageTable.victim = enemy
+        damageTable.damage = reflectedDamage
+
+        if enemy:IsIllusion() then
+          damageTable.damage = reflectedDamage * self.illusion_damage_pct / 100
+        end
+
+        ApplyDamage(damageTable)
+
+        local pfx = ParticleManager:CreateParticle(self.pfx_name[self.parent.tormentorTeam].reflect, PATTACH_ABSORIGIN_FOLLOW, self.parent)
+        ParticleManager:SetParticleControl(pfx, 0, self.parent:GetAbsOrigin())
+        ParticleManager:SetParticleControlEnt(pfx, 1, enemy, PATTACH_POINT_FOLLOW, "attach_hitloc", enemy:GetAbsOrigin(), true)
+        -- ParticleManager:SetParticleControl(pfx, 1, enemy:GetAbsOrigin())
+        ParticleManager:ReleaseParticleIndex(pfx)
+
+        -- EmitSoundOnClient("Miniboss.Tormenter.Reflect", enemy)
+        enemy:EmitSound("Miniboss.Tormenter.Reflect")
+      end
+    end
+
+    -- Always affect the attacker, doesn't matter where it is
+    damageTable.victim = attacker
+    damageTable.damage = reflectedDamage
+
+    if attacker:IsIllusion() then
+      damageTable.damage = reflectedDamage * self.illusion_damage_pct / 100
+    end
+
+    ApplyDamage(damageTable)
+
+    local pfx = ParticleManager:CreateParticle(self.pfx_name[self.parent.tormentorTeam].reflect, PATTACH_ABSORIGIN_FOLLOW, self.parent)
+    ParticleManager:SetParticleControl(pfx, 0, self.parent:GetAbsOrigin())
+    ParticleManager:SetParticleControlEnt(pfx, 1, attacker, PATTACH_POINT_FOLLOW, "attach_hitloc", attacker:GetAbsOrigin(), true)
+    -- ParticleManager:SetParticleControl(pfx, 1, attacker:GetAbsOrigin())
+    ParticleManager:ReleaseParticleIndex(pfx)
+
+    -- EmitSoundOnClient("Miniboss.Tormenter.Reflect", attacker)
+    attacker:EmitSound("Miniboss.Tormenter.Reflect")
+  end
+
   function modifier_tormentor_reflect_oaa:OnDeath(event)
     local unit = event.unit
 
@@ -236,4 +242,8 @@ if IsServer() then
     ParticleManager:SetParticleControl(pfx, 0, self.parent:GetAbsOrigin())
     ParticleManager:ReleaseParticleIndex(pfx)
   end
+end
+
+function modifier_tormentor_reflect_oaa:OnTooltip()
+  return self.reflection
 end
--- game/scripts/vscripts/abilities/boss/tormentor_boss/tormentor_unyielding_shield.lua
+++ game/scripts/vscripts/abilities/boss/tormentor_boss/tormentor_unyielding_shield.lua
@@ -3,7 +3,7 @@ LinkLuaModifier("modifier_tormentor_unyielding_shield_oaa", "abilities/boss/torm
 tormentor_boss_unyielding_shield_oaa = class(AbilityBaseClass)
 
 function tormentor_boss_unyielding_shield_oaa:GetIntrinsicModifierName()
-	return "modifier_tormentor_unyielding_shield_oaa"
+  return "modifier_tormentor_unyielding_shield_oaa"
 end
 
 function tormentor_boss_unyielding_shield_oaa:IsStealable()
@@ -19,15 +19,15 @@ end
 modifier_tormentor_unyielding_shield_oaa = class(ModifierBaseClass)
 
 function modifier_tormentor_unyielding_shield_oaa:IsHidden() -- needs tooltip
-	return false
+  return false
 end
 
 function modifier_tormentor_unyielding_shield_oaa:IsDebuff()
-	return false
+  return false
 end
 
 function modifier_tormentor_unyielding_shield_oaa:IsPurgable()
-	return false
+  return false
 end
 
 function modifier_tormentor_unyielding_shield_oaa:DeclareFunctions()
@@ -39,101 +39,113 @@ function modifier_tormentor_unyielding_shield_oaa:DeclareFunctions()
 end
 
 function modifier_tormentor_unyielding_shield_oaa:OnCreated()
-	if not IsServer() then return end
+  if not IsServer() then return end
 
-	local parent = self:GetParent()
-	local ability = self:GetAbility()
+  local parent = self:GetParent()
+  local ability = self:GetAbility()
 
-	parent:EmitSound("Miniboss.Tormenter.Spawn")
+  parent:EmitSound("Miniboss.Tormenter.Spawn")
 
-	-- This delay is required because the tormentor team is not set yet when the modifier is created
-	GameRules:GetGameModeEntity():SetContextThink(DoUniqueString("delay"), function()
-		self.maxShield = ability:GetSpecialValueFor("damage_absorb")
-		self.currentShield = self.maxShield
-		self.regenPerSecond = ability:GetSpecialValueFor("regen_per_second")
-		self.regenPerSecondThink = self.regenPerSecond * FrameTime()
+  -- This delay is required because the tormentor team is not set yet when the modifier is created
+  GameRules:GetGameModeEntity():SetContextThink(DoUniqueString("delay"), function()
+    self.maxShield = ability:GetSpecialValueFor("damage_absorb")
+    self.currentShield = self.maxShield
+    self.regenPerSecond = ability:GetSpecialValueFor("regen_per_second")
+    self.regenPerSecondThink = self.regenPerSecond * FrameTime()
 
-		self:SetHasCustomTransmitterData(true)
-		self:StartIntervalThink(FrameTime())
-	end, FrameTime())
+    self:SetHasCustomTransmitterData(true)
+    self:StartIntervalThink(FrameTime())
+  end, FrameTime())
 end
 
 function modifier_tormentor_unyielding_shield_oaa:OnRefresh()
-	self:OnCreated()
+  self:OnCreated()
 
-	-- Tell the client that we need to get the properties again
-	if IsServer() then self:SendBuffRefreshToClients() end
+  -- Tell the client that we need to get the properties again
+  if IsServer() then self:SendBuffRefreshToClients() end
 end
 
 function modifier_tormentor_unyielding_shield_oaa:AddCustomTransmitterData()
-	return {
-		currentShield = self.currentShield,
-		maxShield = self.maxShield,
-		regenPerSecond = self.regenPerSecond, -- sent to client only because of MODIFIER_PROPERTY_TOOLTIP2
-	}
+  return {
+    currentShield = self.currentShield,
+    maxShield = self.maxShield,
+    regenPerSecond = self.regenPerSecond, -- sent to client only because of MODIFIER_PROPERTY_TOOLTIP2
+  }
 end
 
 function modifier_tormentor_unyielding_shield_oaa:HandleCustomTransmitterData(data)
-	self.currentShield = data.currentShield
-	self.maxShield = data.maxShield
-	self.regenPerSecond = data.regenPerSecond
+  self.currentShield = data.currentShield
+  self.maxShield = data.maxShield
+  self.regenPerSecond = data.regenPerSecond
 end
 
 function modifier_tormentor_unyielding_shield_oaa:OnIntervalThink()
-	self.currentShield = math.min(self.currentShield + self.regenPerSecondThink, self.maxShield)
-	self:SendBuffRefreshToClients()
+  self.currentShield = math.min(self.currentShield + self.regenPerSecondThink, self.maxShield)
+  self:SendBuffRefreshToClients()
 end
 
 function modifier_tormentor_unyielding_shield_oaa:GetModifierIncomingDamageConstant(event)
-	-- Return the max health on the client if it's a max report, otherwise return the current health
-	if IsClient() then
-		if event.report_max then
-			return self.maxShield
-		else
-			return self.currentShield
-		end
-	else
-		local damage = event.damage
+  -- Return the max health on the client if it's a max report, otherwise return the current health
+  if IsClient() then
+    if event.report_max then
+      return self.maxShield
+    else
+      return self.currentShield
+    end
+  else
+    local parent = self:GetParent()
+    local damage = event.damage
+    local damage_flags = event.damage_flags
+    local attacker = event.attacker
 
-		-- Don't do anything if damage is 0 or somehow negative
-		if damage <= 0 then
-			return 0
-		end
+    -- Let through self damage (boss degen maybe)
+    if attacker == parent then
+      return
+    end
 
-		-- Don't react to damage with HP removal flag
-		if bit.band(event.damage_flags, DOTA_DAMAGE_FLAG_HPLOSS) == DOTA_DAMAGE_FLAG_HPLOSS then
-			return 0
-		end
+    -- Don't do anything if damage is 0 or somehow negative
+    if damage <= 0 then
+      return 0
+    end
 
-		-- Don't block more than remaining hp
-		local barrier_hp = self.currentShield
-		local block_amount = math.min(damage, barrier_hp)
+    -- Let through the damage with HP removal flag
+    if bit.band(damage_flags, DOTA_DAMAGE_FLAG_HPLOSS) > 0 then
+      return 0
+    end
 
-		-- Reduce barrier hp
-		self.currentShield = self.currentShield - block_amount
+    -- Let through the damage that has the no-reflect flag
+    if bit.band(damage_flags, DOTA_DAMAGE_FLAG_REFLECTION) > 0 then
+      return 0
+    end
 
-		if block_amount > 0 then
-			-- Visual effect
-			local parent = self:GetParent()
-			SendOverheadEventMessage(nil, OVERHEAD_ALERT_BLOCK, parent, block_amount, nil)
-		end
+    -- Don't block more than remaining hp
+    local barrier_hp = self.currentShield
+    local block_amount = math.min(damage, barrier_hp)
 
-		-- Tell the client that we need to update the health property
-		self:SendBuffRefreshToClients()
+    -- Reduce barrier hp
+    self.currentShield = self.currentShield - block_amount
 
-		-- EmitSoundOnClient("Miniboss.Tormenter.Target", event.attacker)
-		event.attacker:EmitSound("Miniboss.Tormenter.Target")
+    if block_amount > 0 then
+      -- Visual effect
+      SendOverheadEventMessage(nil, OVERHEAD_ALERT_BLOCK, parent, block_amount, nil)
+    end
 
-		return -block_amount
-	end
+    -- Tell the client that we need to update the health property
+    self:SendBuffRefreshToClients()
+
+    -- EmitSoundOnClient("Miniboss.Tormenter.Target", event.attacker)
+    attacker:EmitSound("Miniboss.Tormenter.Target")
+
+    return -block_amount
+  end
 end
 
 function modifier_tormentor_unyielding_shield_oaa:OnTooltip()
-	return self.maxShield
+  return self.maxShield
 end
 
 function modifier_tormentor_unyielding_shield_oaa:OnTooltip2()
-	return self.regenPerSecond
+  return self.regenPerSecond
 end
 
 function modifier_tormentor_unyielding_shield_oaa:GetPriority()
