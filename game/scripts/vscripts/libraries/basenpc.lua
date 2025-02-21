69eba631d4860b8c1adc9dd8aab8505d2fb35a7c
Darko V <darko1290@gmail.com>
2025-02-21 01:10:14 +0100

    Gleipnir

--- game/scripts/vscripts/libraries/basenpc.lua
+++ game/scripts/vscripts/libraries/basenpc.lua
@@ -293,11 +293,6 @@ if IsServer() then
     if ability.GetAbilityName then
       local damagingByAccident = {
         item_cloak_of_flames = true,
-        item_gungir = true, -- because of random bounces
-        item_gungir_2 = true,
-        item_gungir_3 = true,
-        item_gungir_4 = true,
-        item_gungir_5 = true,
         item_maelstrom = true, -- because of random bounces
         item_mjollnir = true, -- because of random bounces
         item_mjollnir_2 = true,
