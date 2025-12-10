6dac9cb216468446666c9a2f87270fa4f2b44600
Darko V <darko1290@gmail.com>
2025-12-10 03:56:23 +0100

    Update dagon particles to version 12 + dev dagon

--- game/scripts/vscripts/items/dev_dagon.lua
+++ game/scripts/vscripts/items/dev_dagon.lua
@@ -4,7 +4,7 @@ function item_devDagon:OnSpellStart()
   local target = self:GetCursorTarget()
   local caster = self:GetCaster()
 
-  local particleName = "particles/items_fx/dagon.vpcf"
+  local particleName = "particles/items/dev_dagon.vpcf" --"particles/items_fx/dagon.vpcf"
   local particle = ParticleManager:CreateParticle(particleName, PATTACH_POINT_FOLLOW, caster)
   ParticleManager:SetParticleControlEnt(particle, 0, caster, PATTACH_POINT_FOLLOW, "attach_attack1", caster:GetOrigin(), true)
   ParticleManager:SetParticleControlEnt(particle, 1, target, PATTACH_POINT_FOLLOW, "attach_hitloc", target:GetOrigin(), true)
