d635864a92e6a211bba6e4e1b833e73b3e4ec294
Darko V <darko1290@gmail.com>
2026-04-07 06:05:19 +0200

    Item fixes
    Fixed Greater Guardian Greaves not providing bonus mana.
    Dagon recipe cost reduced from 1150 to 1000.
    Dagon bonus all stats reduced from 7/9/11/13/15/20/25/30/35 to 5/6/7/8/9/14/19/24/29.
    Dagon now provides bonus hp, bonus mana and cast range. (hp and cast range scaling are better than vanilla)
    Hydras Breath recipe cost reduced from 1100 to 600.
    Hydras Breath Miasma max hp dmg reduced from 2.5% to 1.5%
    Dagon no longer instantly kills non-ancient creeps (still kills illusions).
    Dagon no longer has spell lifesteal.
    Dagon no longer heals for 75% of damage dealt.
    Far Sight: Fixed cast range bonus not working if you have Ethereal Blade.

--- game/scripts/vscripts/items/dagon.lua
+++ game/scripts/vscripts/items/dagon.lua
@@ -1,4 +1,5 @@
 LinkLuaModifier("modifier_item_oaa_dagon_passive", "items/dagon.lua", LUA_MODIFIER_MOTION_NONE)
+LinkLuaModifier("modifier_item_oaa_dagon_non_stacking_stats", "items/dagon.lua", LUA_MODIFIER_MOTION_NONE)
 LinkLuaModifier("modifier_item_oaa_dagon_debuff", "items/dagon.lua", LUA_MODIFIER_MOTION_NONE)
 
 item_dagon_oaa_1 = class(ItemBaseClass)
@@ -18,7 +19,7 @@ end
 function item_dagon_oaa_1:GetIntrinsicModifierNames()
   return {
     "modifier_item_oaa_dagon_passive",
-    "modifier_item_spell_lifesteal_oaa",
+    "modifier_item_oaa_dagon_non_stacking_stats",
   }
 end
 
@@ -31,14 +32,14 @@ function item_dagon_oaa_1:OnSpellStart()
   local soundTarget = "DOTA_Item.Dagon5.Target"
 
   local particleName = "particles/items/dagon_oaa.vpcf"
-  local particleThickness = 300 + (100 * level) --Control Point 2 in Dagon's particle effect takes a number between 400 and 2000, depending on its level.
+  local particleThickness = 300 + (100 * level) -- Control Point 2 in Dagon's particle effect takes a number between 400 and 2000, depending on its level.
 
   local damage = self:GetSpecialValueFor("damage") -- Damage should never be a big value because of the spells like Fatal Bonds that share dmg
   local hp_percent = self:GetSpecialValueFor("current_hp_dmg")
+  --local burst_heal_percent = self:GetSpecialValueFor("burst_heal_percent")
+  --local hero_spell_lifesteal = self:GetSpecialValueFor("hero_spell_lifesteal")
+  --local creep_spell_lifesteal = self:GetSpecialValueFor("creep_spell_lifesteal")
   local damage_type = DAMAGE_TYPE_MAGICAL
-  local burst_heal_percent = self:GetSpecialValueFor("burst_heal_percent")
-  local hero_spell_lifesteal = self:GetSpecialValueFor("hero_spell_lifesteal")
-  local creep_spell_lifesteal = self:GetSpecialValueFor("creep_spell_lifesteal")
 
   local particle = ParticleManager:CreateParticle(particleName,  PATTACH_POINT_FOLLOW, caster)
   ParticleManager:SetParticleControlEnt(particle, 0, caster, PATTACH_POINT_FOLLOW, "attach_attack1", caster:GetOrigin(), true)
@@ -57,11 +58,11 @@ function item_dagon_oaa_1:OnSpellStart()
     return
   end
 
-  -- If the target is an illusion, just kill it and don't do damage; same + heal for non-ancient creeps
-  if (target:IsIllusion() and not target:IsNull() and not target:IsStrongIllusionOAA()) or (target:IsCreep() and not target:IsAncient() and not target:IsOAABoss() and not target:IsCreepHero()) then
-    if target:IsCreep() then
-      caster:HealWithParams(target:GetHealth() * burst_heal_percent / 100, self, false, true, caster, true)
-    end
+  -- If the target is an illusion, just kill it and don't do damage;
+  if (target:IsIllusion() and not target:IsNull() and not target:IsStrongIllusionOAA()) then --or (target:IsCreep() and not target:IsAncient() and not target:IsOAABoss() and not target:IsCreepHero()) then
+    --if target:IsCreep() then
+      --caster:HealWithParams(target:GetHealth() * burst_heal_percent / 100, self, false, true, caster, true)
+    --end
     if caster:GetTeamNumber() == DOTA_TEAM_NEUTRALS then
       target:ForceKillOAA(false)
     else
@@ -76,14 +77,15 @@ function item_dagon_oaa_1:OnSpellStart()
     target:AddNewModifier(caster, self, "modifier_item_oaa_dagon_debuff", {duration = debuff_duration})
   end
 
-  local isRealHero = target:IsRealHero() -- do this check before doing dmg to prevent doing check on a killed/deleted target
-  local isStrongIllu = target:IsStrongIllusionOAA()
+  --local isRealHero = target:IsRealHero() -- do this check before doing dmg to prevent doing check on a killed/deleted target
+  --local isStrongIllu = target:IsStrongIllusionOAA()
 
   if target:IsOAABoss() then
     hp_percent = hp_percent * (1 - BOSS_DMG_RED_FOR_PCT_SPELLS/100)
   end
 
-  local damageDone = ApplyDamage({
+  --local damageDone =
+  ApplyDamage({
     victim = target,
     attacker = caster,
     damage = damage + target:GetHealth() * hp_percent * 0.01,
@@ -91,15 +93,15 @@ function item_dagon_oaa_1:OnSpellStart()
     ability = self
   })
 
-  -- healing time!
-  local heal_amount = 0
-  if isRealHero or isStrongIllu then
-    heal_amount = damageDone * (burst_heal_percent - hero_spell_lifesteal) / 100
-  else
+  -- Dagon Burst Spell Lifesteal based on dmg done
+  --local heal_amount = 0
+  --if isRealHero or isStrongIllu then
+    --heal_amount = damageDone * (burst_heal_percent - hero_spell_lifesteal) / 100
+  --else
     -- For ancients and bosses
-    heal_amount = damageDone * (burst_heal_percent - creep_spell_lifesteal) / 100
-  end
-  caster:HealWithParams(heal_amount, self, false, true, caster, true)
+    --heal_amount = damageDone * (burst_heal_percent - creep_spell_lifesteal) / 100
+  --end
+  --caster:HealWithParams(heal_amount, self, false, true, caster, true)
 end
 
 ---------------------------------------------------------------------------------------------------
@@ -126,7 +128,8 @@ function modifier_item_oaa_dagon_passive:OnCreated()
     self.int = ability:GetSpecialValueFor("bonus_int")
     self.str = ability:GetSpecialValueFor("bonus_str")
     self.agi = ability:GetSpecialValueFor("bonus_agi")
-    --self.spell_amp = ability:GetSpecialValueFor("spell_amp")
+    self.hp = ability:GetSpecialValueFor("bonus_health")
+    self.mana = ability:GetSpecialValueFor("bonus_mana")
   end
 end
 
@@ -137,7 +140,8 @@ function modifier_item_oaa_dagon_passive:DeclareFunctions()
     MODIFIER_PROPERTY_STATS_STRENGTH_BONUS,
     MODIFIER_PROPERTY_STATS_AGILITY_BONUS,
     MODIFIER_PROPERTY_STATS_INTELLECT_BONUS,
-    --MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
+    MODIFIER_PROPERTY_HEALTH_BONUS,
+    MODIFIER_PROPERTY_MANA_BONUS,
   }
 end
 
@@ -153,7 +157,59 @@ function modifier_item_oaa_dagon_passive:GetModifierBonusStats_Intellect()
   return self.int or self:GetAbility():GetSpecialValueFor("bonus_int")
 end
 
---function modifier_item_oaa_dagon_passive:GetModifierSpellAmplify_Percentage()
+function modifier_item_oaa_dagon_passive:GetModifierHealthBonus()
+  return self.hp or self:GetAbility():GetSpecialValueFor("bonus_health")
+end
+
+function modifier_item_oaa_dagon_passive:GetModifierManaBonus()
+  return self.mana or self:GetAbility():GetSpecialValueFor("bonus_mana")
+end
+
+---------------------------------------------------------------------------------------------------
+
+modifier_item_oaa_dagon_non_stacking_stats = class(ModifierBaseClass)
+
+function modifier_item_oaa_dagon_non_stacking_stats:IsHidden()
+  return true
+end
+
+function modifier_item_oaa_dagon_non_stacking_stats:IsDebuff()
+  return false
+end
+
+function modifier_item_oaa_dagon_non_stacking_stats:IsPurgable()
+  return false
+end
+
+function modifier_item_oaa_dagon_non_stacking_stats:OnCreated()
+  local ability = self:GetAbility()
+  if ability and not ability:IsNull() then
+    self.cast_range = ability:GetSpecialValueFor("cast_range_bonus")
+    --self.spell_amp = ability:GetSpecialValueFor("spell_amp")
+  end
+end
+
+modifier_item_oaa_dagon_non_stacking_stats.OnRefresh = modifier_item_oaa_dagon_non_stacking_stats.OnCreated
+
+function modifier_item_oaa_dagon_non_stacking_stats:DeclareFunctions()
+  return {
+    MODIFIER_PROPERTY_CAST_RANGE_BONUS_STACKING,
+    --MODIFIER_PROPERTY_SPELL_AMPLIFY_PERCENTAGE,
+  }
+end
+
+function modifier_item_oaa_dagon_non_stacking_stats:GetModifierCastRangeBonusStacking()
+  local parent = self:GetParent()
+
+  -- Prevent stacking with Aether Lens and Far Sight
+  if parent:HasModifier("modifier_item_aether_lens") or parent:HasModifier("modifier_item_far_sight_non_stacking_stats") then
+    return 0
+  end
+
+  return self.cast_range or self:GetAbility():GetSpecialValueFor("cast_range_bonus")
+end
+
+--function modifier_item_oaa_dagon_non_stacking_stats:GetModifierSpellAmplify_Percentage()
   --return self.spell_amp or self:GetAbility():GetSpecialValueFor("spell_amp")
 --end
 
