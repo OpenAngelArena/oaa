1bd775a8f350da17158854f1e866a5553cf81b93
Darko V <darko1290@gmail.com>
2026-05-13 22:01:40 +0200

    sum
    Nightstalker Midnight Feast is now blacklisted for Drunkard, Magus and Multicast modifiers.
    Shadow Fiend Feast of Souls is no longer graylisted for Magus modifier.

--- game/scripts/vscripts/modifiers/funmodifiers/modifier_drunk_oaa.lua
+++ game/scripts/vscripts/modifiers/funmodifiers/modifier_drunk_oaa.lua
@@ -45,7 +45,8 @@ function modifier_drunk_oaa:OnCreated()
     doom_bringer_devour = 1,                             -- instant kill, DOTA_UNIT_TARGET_TEAM_CUSTOM
     morphling_replicate = 1,                             -- bugged, DOTA_UNIT_TARGET_TEAM_CUSTOM
     muerta_parting_shot = 1,                             -- bugged
-    night_stalker_hunter_in_the_night = 1,               -- instant kill
+    --night_stalker_hunter_in_the_night = 1,               -- instant kill
+    night_stalker_midnight_feast = 1,                    -- instant kill
     primal_beast_pulverize = 1,
     --rubick_spell_steal = 1,                              -- stealing boss spells
     snapfire_gobble_up = 1,                              -- instant kill, DOTA_UNIT_TARGET_TEAM_CUSTOM
